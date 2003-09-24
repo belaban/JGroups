f69cf2623fcaf624057a39e34f6951fbc879b3df
belaban <belaban>
2003-09-24 19:06:43 +0000

    *** empty log message ***

--- tests/other/org/jgroups/tests/QueueTest2.java
+++ tests/other/org/jgroups/tests/QueueTest2.java
@@ -5,6 +5,8 @@ import java.util.LinkedList;
 import EDU.oswego.cs.dl.util.concurrent.LinkedQueue;
 import EDU.oswego.cs.dl.util.concurrent.WaitFreeQueue;
 import org.jgroups.util.Queue;
+import org.jgroups.util.QueueClosedException;
+import org.jgroups.util.Queue2;
 
 /**
  *
@@ -13,15 +15,16 @@ import org.jgroups.util.Queue;
  * Time: 2:14:32 PM
  */
 public class QueueTest2 {
-    Queue q=new Queue();
-    // MyQueue q=new MyQueue();
-    // MyLinkedQueue q=new MyLinkedQueue();
-    // MyWaitFreeQueue q=new MyWaitFreeQueue();
+    Queueable q=null;
+
+
 
     long  start, stop;
     long  NUM=1000 * 1000;
 
-    void start() throws Exception {
+    void start(Queueable q, String msg) throws Exception {
+        this.q=q;
+        System.out.println("-- starting test with " + q.getClass() + " (" + msg + ")");
         start=System.currentTimeMillis();
         Adder adder=new Adder();
         Remover remover=new Remover();
@@ -29,15 +32,21 @@ public class QueueTest2 {
         adder.start();
         adder.join();
         remover.join();
-        System.out.println("== total time for " + NUM + " elements: " + (stop-start) + " msecs");
+        System.out.println("-- done with " + q.getClass());
+        System.out.println(" total time for " + NUM + " elements: " + (stop-start) + " msecs\n\n");
     }
 
+
+    public interface Queueable {
+        void addElement(Object o);
+        Object removeElement();
+    }
 
     class Adder extends Thread {
         public void run() {
             for(int i=0; i < NUM; i++) {
                 try {
-                    q.add(new Integer(i));
+                    q.addElement(new Integer(i));
                     //if(i % 1000 == 0)
                       //  System.out.println("-- added " + i);
                 }
@@ -45,7 +54,7 @@ public class QueueTest2 {
                     e.printStackTrace();
                 }
             }
-            System.out.println("-- Adder: done");
+            //System.out.println("-- Adder: done");
         }
     }
 
@@ -54,7 +63,7 @@ public class QueueTest2 {
         public void run() {
             do {
                 try {
-                    q.remove();
+                    q.removeElement();
                     i++;
                     //if(i % 1000 == 0)
                       //  System.out.println("-- removed " + i);
@@ -65,26 +74,72 @@ public class QueueTest2 {
             }
             while(i < NUM);
             stop=System.currentTimeMillis();
-            System.out.println("-- Remover: done");
+            // System.out.println("-- Remover: done");
         }
     }
 
 
-    public static class MyQueue extends LinkedList {
+    public static class JgQueue extends Queue implements Queueable {
+
+        public void addElement(Object o) {
+            try {
+                add(o);
+            }
+            catch(QueueClosedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public Object removeElement() {
+            try {
+                return remove();
+            }
+            catch(QueueClosedException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+    }
+
+
+        public static class JgQueue2 extends Queue2 implements Queueable {
+
+        public void addElement(Object o) {
+            try {
+                add(o);
+            }
+            catch(QueueClosedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public Object removeElement() {
+            try {
+                return remove();
+            }
+            catch(QueueClosedException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+    }
+
+
+
+    public static class MyQueue extends LinkedList implements Queueable {
         Object mutex=new Object();
         boolean waiting=false; // remover waiting on mutex
 
-        public boolean add(Object o) {
+        public void addElement(Object o) {
             synchronized(mutex) {
-                boolean retval=super.add(o);
+                super.add(o);
                 if(waiting)
                     mutex.notifyAll(); // todo: change to notify()
-                return retval;
             }
         }
 
 
-        public Object remove() {
+        public Object removeElement() {
             synchronized(mutex) {
                 if(size() > 0) {
                     return removeFirst();
@@ -108,8 +163,8 @@ public class QueueTest2 {
     }
 
 
-    public static class MyLinkedQueue extends LinkedQueue {
-        public void add(Object o) {
+    public static class MyLinkedQueue extends LinkedQueue implements Queueable {
+        public void addElement(Object o) {
             try {
                 super.put(o);
             }
@@ -118,7 +173,7 @@ public class QueueTest2 {
             }
         }
 
-        public Object remove() {
+        public Object removeElement() {
             try {
                 return super.take();
             }
@@ -130,8 +185,8 @@ public class QueueTest2 {
     }
 
 
-      public static class MyWaitFreeQueue extends WaitFreeQueue {
-        public void add(Object o) {
+      public static class MyWaitFreeQueue extends WaitFreeQueue implements Queueable {
+        public void addElement(Object o) {
             try {
                 super.put(o);
             }
@@ -140,7 +195,7 @@ public class QueueTest2 {
             }
         }
 
-        public Object remove() {
+        public Object removeElement() {
             try {
                 return super.take();
             }
@@ -154,15 +209,22 @@ public class QueueTest2 {
 
     public static void main(String[] args) {
         try {
-            new QueueTest2().start();
+            QueueTest2 qt=new QueueTest2();
 
-//            MyQueue q=new MyQueue();
-//            q.add("Bela");
-//            q.add("Jeannette");
-//            System.out.println(q);
-//            q.remove();
-//            System.out.println(q);
+            Queueable q=new JgQueue();
+            qt.start(q, "based on org.jgroups.util.Queue");
 
+            q=new JgQueue2();
+            qt.start(q, "based on org.jgroups.util.Queue2 (using util.concurrent Mutexes and CondVars)");
+
+            q=new MyQueue();
+            qt.start(q, "based on java.util.LinkedList");
+
+            q=new MyLinkedQueue();
+            qt.start(q, "based on EDU.oswego.cs.dl.util.concurrent.LinkedQueue");
+
+            q=new MyWaitFreeQueue();
+            qt.start(q, "based on EDU.oswego.cs.dl.util.concurrent.WaitFreeQueue");
         }
         catch(Throwable t) {
             t.printStackTrace();
