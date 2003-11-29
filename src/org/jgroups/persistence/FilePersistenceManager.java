865db49a8a234ec38b0bc1e4c1a7fe8eec70850f
Bela Ban <belaban@yahoo.com>
2003-11-29 01:32:27 +0000

    added patches by Dimitri Colbatch

--- src/org/jgroups/persistence/FilePersistenceManager.java
+++ src/org/jgroups/persistence/FilePersistenceManager.java
@@ -9,52 +9,92 @@ package  org.jgroups.persistence;
  */
 
 
-import java.io.Serializable;
-import java.util.Map;
+import java.io.*;
+import java.util.*;
 
 public class FilePersistenceManager implements PersistenceManager
 {
+    private final File file;
 
     /**
      * Default constructor
-     * Currently NOOP
      */
-    public FilePersistenceManager(String filePath)
+    public FilePersistenceManager(String propertiesFilename)
+        throws Exception
     {
+        Properties properties = new Properties();
+        properties.load(new FileInputStream(propertiesFilename));
+        String path = properties.getProperty(PersistenceFactory.persistProp);
+        file = new File(path);
+        file.createNewFile();
     }
 
     /**
      * Save new NV pair as serializable objects or if already exist; store 
      * new state 
-     * @param Serializable;
-     * @param Serializable;
-     * @exception CannotPersistException; 
      */
     public void save(Serializable key, Serializable val) throws CannotPersistException
     {
-	return;
+        try
+        {
+            Map map = retrieveAll();
+            map.put(key, val);
+            saveAll(map);
+        }
+        catch (CannotRetrieveException e)
+        {
+            throw new CannotPersistException(e, "Unable to pre-load existing store.");
+        }
     }
 
     /**
      * Remove existing NV from being persisted
-     * @param Serializable; key value
-     * @return Serializable; gives back the value
-     * @exception CannotRemoveException;
      */
     public Serializable  remove(Serializable key) throws CannotRemoveException
     {
-	return null;
+        Object o;
+        try
+        {
+            Map map = retrieveAll();
+            o = map.remove(key);
+            saveAll(map);
+        }
+        catch (CannotRetrieveException e)
+        {
+            throw new CannotRemoveException(e, "Unable to pre-load existing store.");
+        }
+        catch (CannotPersistException e)
+        {
+            throw new CannotRemoveException(e, "Unable to pre-load existing store.");
+        }
+        return (Serializable) o;
     }
 
 
     /**
      * Use to store a complete map into persistent state
-     * @param Map;
      * @exception CannotPersistException;
      */
     public void saveAll(Map map) throws CannotPersistException
     {
-	return;
+        try
+        {
+            OutputStream fos = new FileOutputStream(file);
+            Properties prop = new Properties();
+            // NB: For some reason Properties.putAll(map) doesn't seem to work - dimc@users.sourceforge.net
+            for (Iterator iterator = map.entrySet().iterator(); iterator.hasNext();)
+            {
+                Map.Entry entry = (Map.Entry) iterator.next();
+                prop.setProperty(entry.getKey().toString(), entry.getValue().toString());
+            }
+            prop.store(fos, null);
+            fos.flush();
+            fos.close();
+        }
+        catch (IOException e)
+        {
+            throw new CannotPersistException(e, "Cannot save to: " + file.getAbsolutePath());
+        }
     }
 
 
@@ -65,7 +105,35 @@ public class FilePersistenceManager implements PersistenceManager
      */
     public Map retrieveAll() throws CannotRetrieveException
     {
-	return null;
+        try
+        {
+            Properties prop = new Properties();
+            FileInputStream fis = new FileInputStream(file);
+            prop.load(fis);
+            fis.close();
+            return filterLoadedValues(prop);
+        }
+        catch (IOException e)
+        {
+            throw new CannotRetrieveException(e, "Unable to load from file: " + file.getAbsolutePath());
+        }
+    }
+
+    /**
+     * Subclasses should override this method to convert the incoming map
+     * of string/string key/value pairs into the types they want.  This
+     * implementation turns the values into Floats to enable
+     * {@link org.jgroups.demos.DistributedHashtableDemo} to work.
+     */
+    protected Map filterLoadedValues(Map in)
+    {
+        Map out = new HashMap();
+        for (Iterator iterator = in.entrySet().iterator(); iterator.hasNext();)
+        {
+            Map.Entry entry = (Map.Entry) iterator.next();
+            out.put(entry.getKey().toString(), Float.valueOf(entry.getValue().toString()));
+        }
+        return out;
     }
 
 
@@ -75,7 +143,14 @@ public class FilePersistenceManager implements PersistenceManager
      x*/
     public void clear() throws CannotRemoveException
     {
-	return;
+        try
+        {
+            saveAll(Collections.EMPTY_MAP);
+        }
+        catch (CannotPersistException e)
+        {
+            throw new CannotRemoveException(e, "Unable to clear map.");
+        }
     }
 
 
