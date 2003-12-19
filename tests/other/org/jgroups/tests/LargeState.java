a97937c20690781e61a4d00ed4714fde25a684a2
Bela Ban <belaban@yahoo.com>
2003-12-19 17:14:08 +0000

    no message

--- tests/other/org/jgroups/tests/LargeState.java
+++ tests/other/org/jgroups/tests/LargeState.java
@@ -1,12 +1,12 @@
-// $Id: LargeState.java,v 1.3 2003/12/12 06:24:33 belaban Exp $
+// $Id: LargeState.java,v 1.4 2003/12/19 17:14:08 belaban Exp $
 
 
 package org.jgroups.tests;
 
 
 import org.jgroups.*;
+import org.jgroups.util.Promise;
 import org.jgroups.log.Trace;
-
 
 
 /**
@@ -21,79 +21,86 @@ import org.jgroups.log.Trace;
  * causing it to drop some packets (default size is 8096 bytes). Therefore the send (and receive) buffers for the unicast
  * socket have been increased (see ucast_send_buf_size and ucast_recv_buf_size below).<p>
  * If we didn't do this, we would have some retransmission, slowing the state transfer down.
+ * 
  * @author Bela Ban Dec 13 2001
  */
 public class LargeState {
-    Channel        channel;
-    byte[]         state=null;
-    Thread         getter=null;
-    boolean        rc=false;
-    String         props;
+    Channel  channel;
+    byte[]   state=null;
+    Thread   getter=null;
+    boolean  rc=false;
+    String   props;
+    long     start, stop;
+    boolean  provider=true;
 
 
-    
     public void start(boolean provider, long size, String props) throws Exception {
-	channel=new JChannel(props);
-	channel.setOpt(Channel.GET_STATE_EVENTS, Boolean.TRUE);
-	channel.connect("TestChannel");
+        this.provider=provider;
+        channel=new JChannel(props);
+        channel.setOpt(Channel.GET_STATE_EVENTS, Boolean.TRUE);
+        channel.connect("TestChannel");
 
-	if(provider) {
-	    System.out.println("Creating state of " + size + " bytes");
-	    state=createLargeState(size);
-	    System.out.println("Done. Waiting for other members to join and fetch large state");
-	}
-	else {
-	    System.out.println("Getting state");
-	    rc=channel.getState(null, 20000);
-	    System.out.println("getState(), rc=" + rc);
-	}
+        if(provider) {
+            System.out.println("Creating state of " + size + " bytes");
+            state=createLargeState(size);
+            System.out.println("Done. Waiting for other members to join and fetch large state");
+        }
+        else {
+            System.out.println("Getting state");
+            start=System.currentTimeMillis();
+            rc=channel.getState(null, 20000);
+            System.out.println("getState(), rc=" + rc);
+        }
 
-	mainLoop();
-	
+        mainLoop();
+        channel.close();
+
     }
 
 
     public void mainLoop() {
-	Object ret;
+        Object ret;
 
-	try {
-	    while(true) {
-		ret=channel.receive(0);
+        try {
+            while(true) {
+                ret=channel.receive(0);
 
-		if(ret instanceof Message) {
-		}
-		else if(ret instanceof GetStateEvent) {
-		    System.out.println("--> returned state: " + ret);
-		    channel.returnState(state);
-		}
-		else if(ret instanceof SetStateEvent) {
-		    byte[] new_state=((SetStateEvent)ret).getArg();
-		    if(new_state != null) {
-			state=new_state;
-			System.out.println("<-- Received state: size is " + state.length);
-		    }
-		}
-	    }
-	}
-	catch(Exception e) {
-	}
+                if(ret instanceof Message) {
+                }
+                else if(ret instanceof GetStateEvent) {
+                    System.out.println("--> returned state: " + ret);
+                    channel.returnState(state);
+                }
+                else if(ret instanceof SetStateEvent) {
+                    stop=System.currentTimeMillis();
+                    byte[] new_state=((SetStateEvent)ret).getArg();
+                    if(new_state != null) {
+                        state=new_state;
+                        System.out.println("<-- Received state, size =" + state.length +
+                                " (took " + (stop-start) + "ms)");
+                    }
+                    if(!provider)
+                        break;
+                }
+            }
+        }
+        catch(Exception e) {
+        }
     }
-    
+
 
     byte[] createLargeState(long size) {
-	StringBuffer ret=new StringBuffer();
-	for(int i=0; i < size; i++)
-	    ret.append(".");
-	return ret.toString().getBytes();
+        StringBuffer ret=new StringBuffer();
+        for(int i=0; i < size; i++)
+            ret.append(".");
+        return ret.toString().getBytes();
     }
-
-
 
 
     public static void main(String[] args) {
-	boolean provider=false;
-	long    size=1024 * 1024;
-        String  props="UDP(mcast_addr=224.0.0.35;mcast_port=45566;ip_ttl=32;" +
+        boolean provider=false;
+        long size=1024 * 1024;
+        String props="UDP(mcast_addr=224.0.0.35;mcast_port=45566;ip_ttl=32;" +
                 "mcast_send_buf_size=150000;mcast_recv_buf_size=80000;" +
                 "ucast_send_buf_size=80000;ucast_recv_buf_size=150000):" +
                 "AUTOCONF:" +
@@ -109,38 +116,38 @@ public class LargeState {
                 "shun=false;print_local_addr=true):" +
                 "pbcast.STATE_TRANSFER";
 
-	Trace.init();
-	
-	for(int i=0; i < args.length; i++) {
-	    if(args[i].equals("-help")) {
-		help();
-		return;
-	    }
-	    if(args[i].equals("-provider")) {
-		provider=true;
-		continue;
-	    }
-	    if(args[i].equals("-size")) {
-		size=new Long(args[++i]).longValue();
-		continue;
-	    }
-	    if(args[i].equals("-props")) {
-		props=args[++i];
-		continue;
-	    }
-	}
+        Trace.init();
+
+        for(int i=0; i < args.length; i++) {
+            if(args[i].equals("-help")) {
+                help();
+                return;
+            }
+            if(args[i].equals("-provider")) {
+                provider=true;
+                continue;
+            }
+            if(args[i].equals("-size")) {
+                size=new Long(args[++i]).longValue();
+                continue;
+            }
+            if(args[i].equals("-props")) {
+                props=args[++i];
+                continue;
+            }
+        }
 
 
-	try {
-	    new LargeState().start(provider, size, props);
-	}
-	catch(Exception e) {
-	    System.err.println(e);
-	}
+        try {
+            new LargeState().start(provider, size, props);
+        }
+        catch(Exception e) {
+            System.err.println(e);
+        }
     }
 
     static void help() {
-	System.out.println("LargeState [-help] [-size <size of state in bytes] [-provider] [-props <properties>]");
+        System.out.println("LargeState [-help] [-size <size of state in bytes] [-provider] [-props <properties>]");
     }
 
 }
