26d70cf0c60c9125680d61dc1530381935090f3e
belaban <belaban>
2004-01-01 00:54:55 +0000

    new unit test

---
+++ tests/junit/org/jgroups/tests/UnicastLoopbackTest.java
@@ -0,0 +1,57 @@
+package org.jgroups.tests;
+
+import junit.framework.TestCase;
+import org.jgroups.*;
+
+/**
+ * Tests unicasts to self (loopback of transport protocol)
+ * @author Bela Ban Dec 31 2003
+ * @version $Id: UnicastLoopbackTest.java,v 1.1 2004/01/01 00:54:55 belaban Exp $
+ */
+public class UnicastLoopbackTest extends TestCase {
+    JChannel channel=null;
+
+
+    public UnicastLoopbackTest(String name) {
+        super(name);
+    }
+
+    protected void setUp() throws Exception {
+        channel=new JChannel();
+        channel.connect("demo-group");
+    }
+
+    protected void tearDown() throws Exception {
+        if(channel != null) {
+            channel.close();
+            channel=null;
+        }
+    }
+
+
+    public void testUnicastMsgs() throws ChannelClosedException, ChannelNotConnectedException, TimeoutException {
+        int NUM=1000;
+        for(int i=1; i <= NUM; i++) {
+            channel.send(new Message(null, null, new Integer(i)));
+            if(i % 100 == 0)
+                System.out.println("-- sent " + i);
+        }
+        int received=0;
+        while(received < NUM) {
+            Object o=channel.receive(0);
+            if(o instanceof Message) {
+                Message m=(Message)o;
+                Integer num=(Integer)m.getObject();
+                received++;
+                if(num.intValue() % 100 == 0)
+                    System.out.println("-- received " + num);
+            }
+        }
+        assertEquals(NUM, received);
+    }
+
+    public static void main(String[] args) {
+        String[] testCaseName={UnicastLoopbackTest.class.getName()};
+        junit.textui.TestRunner.main(testCaseName);
+    }
+}
