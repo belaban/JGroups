6e86dcd1c12033e3c3e6ee85e4403d6761b07225
Bela Ban <belaban@yahoo.com>
2004-02-20 22:50:25 +0000

    no message

--- tests/other/org/jgroups/tests/adaptjms/Request.java
+++ tests/other/org/jgroups/tests/adaptjms/Request.java
@@ -1,11 +1,11 @@
-package org.jgroups.tests.adaptudp;
+package org.jgroups.tests.adaptjms;
 
 import java.io.Serializable;
 
 /**
  * @author Bela Ban Jan 11
  * @author 2004
- * @version $Id: Request.java,v 1.3 2004/02/20 22:50:25 belaban Exp $
+ * @version $Id: Request.java,v 1.1 2004/02/20 22:54:05 belaban Exp $
  */
 public class Request implements Serializable {
     final static int DISCOVERY_REQ=1;
