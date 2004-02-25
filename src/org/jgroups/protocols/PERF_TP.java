b485c8de1cfcbeda0f24d8083e79522b0e20097e
Bela Ban <belaban@yahoo.com>
2004-02-25 02:38:22 +0000

    new test

---
+++ src/org/jgroups/protocols/PERF_TP.java
@@ -0,0 +1,147 @@
+// $Id: PERF_TP.java,v 1.1 2004/02/25 02:38:22 belaban Exp $
+
+package org.jgroups.protocols;
+
+
+import org.jgroups.Address;
+import org.jgroups.Event;
+import org.jgroups.Message;
+import org.jgroups.log.Trace;
+import org.jgroups.stack.Protocol;
+
+import java.util.Properties;
+
+
+/**
+ * Measures the time for a message to travel from the channel to the transport
+ * @author Bela Ban
+ * @version $Id: PERF_TP.java,v 1.1 2004/02/25 02:38:22 belaban Exp $
+ */
+public class PERF_TP extends Protocol {
+    private Address local_addr=null;
+    static  PERF_TP instance=null;
+    long    stop, start;
+    long    num_msgs=0;
+    long    expected_msgs=0;
+    boolean done=false;
+
+
+    public static PERF_TP getInstance() {
+        return instance;
+    }
+
+    public PERF_TP() {
+        if(instance == null)
+            instance=this;
+    }
+
+
+    public String toString() {
+        return "Protocol PERF_TP (local address: " + local_addr + ")";
+    }
+
+
+    public boolean done() {
+        return done;
+    }
+
+    public long getNumMessages() {
+        return num_msgs;
+    }
+
+    public void setExpectedMessages(long m) {
+        expected_msgs=m;
+        num_msgs=0;
+        done=false;
+        start=System.currentTimeMillis();
+    }
+
+    public void reset() {
+        num_msgs=expected_msgs=stop=start=0;
+        done=false;
+    }
+
+    public long getTotalTime() {
+        return stop-start;
+    }
+
+
+    /*------------------------------ Protocol interface ------------------------------ */
+
+    public String getName() {
+        return "PERF_TP";
+    }
+
+
+    /** Setup the Protocol instance acording to the configuration string */
+    public boolean setProperties(Properties props) {
+
+        if(props.size() > 0) {
+            System.err.println("PERF_TP.setProperties(): the following properties are not recognized:");
+            props.list(System.out);
+            return false;
+        }
+        return true;
+    }
+
+    public void init() throws Exception {
+        local_addr=new org.jgroups.stack.IpAddress("localhost", 10000); // fake address
+    }
+
+    public void start() throws Exception {
+        passUp(new Event(Event.SET_LOCAL_ADDRESS, local_addr));
+    }
+
+
+    /**
+     * Caller by the layer above this layer. Usually we just put this Message
+     * into the send queue and let one or more worker threads handle it. A worker thread
+     * then removes the Message from the send queue, performs a conversion and adds the
+     * modified Message to the send queue of the layer below it, by calling Down).
+     */
+    public void down(Event evt) {
+        Message msg;
+        Address dest_addr;
+
+
+        switch(evt.getType()) {
+
+            case Event.MSG:
+                if(done) {
+                    Trace.warn("PERF_TP.down()", "all done (discarding msg)");
+                    break;
+                }
+                msg=(Message)evt.getArg();
+                dest_addr=msg.getDest();
+                if(dest_addr == null)
+                    num_msgs++;
+                if(num_msgs >= expected_msgs) {
+                    stop=System.currentTimeMillis();
+                    synchronized(this) {
+                        done=true;
+                        this.notifyAll();
+                    }
+                    Trace.info("PERF_TP.down()", "all done (num_msgs=" + num_msgs + ", expected_msgs=" + expected_msgs);
+                }
+                break;
+
+            case Event.CONNECT:
+                passUp(new Event(Event.CONNECT_OK));
+                break;
+
+            case Event.DISCONNECT:
+                passUp(new Event(Event.DISCONNECT_OK));
+                break;
+        }
+
+
+    }
+
+
+
+    /*--------------------------- End of Protocol interface -------------------------- */
+
+
+
+
+}
---
+++ tests/other/org/jgroups/tests/PerfTpTest.java
@@ -0,0 +1,85 @@
+package org.jgroups.tests;
+
+import org.jgroups.JChannel;
+import org.jgroups.ChannelException;
+import org.jgroups.Message;
+import org.jgroups.protocols.PERF_TP;
+
+/**
+ * Test of PERF_TP. Requirement: transport needs to be PERF_TP
+ * @author Bela Ban Feb 24, 2004
+ * @version $Id: PerfTpTest.java,v 1.1 2004/02/25 02:38:30 belaban Exp $
+ */
+public class PerfTpTest {
+    JChannel ch=null;
+    PERF_TP  tp=null;
+
+    public static void main(String[] args) {
+        String props=null;
+        int    num_msgs=1000;
+        int    size=1000; // bytes
+
+        for(int i=0; i < args.length; i++) {
+            if(args[i].equals("-props")){
+                props=args[++i];
+                continue;
+            }
+            if(args[i].equals("-num")) {
+                num_msgs=Integer.parseInt(args[++i]);
+                continue;
+            }
+            if(args[i].equals("-size")) {
+                size=Integer.parseInt(args[++i]);
+                continue;
+            }
+            help();
+            return;
+        }
+
+        try {
+            new PerfTpTest().start(props, num_msgs, size);
+        }
+        catch(ChannelException e) {
+            e.printStackTrace();
+        }
+        catch(InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void help() {
+        System.out.println("PerfTpTest [-help] [-props <properties>] [-num <num msgs>] [-size <msg size (in bytes)]");
+    }
+
+    void start(String props, int num_msgs, int size) throws ChannelException, InterruptedException {
+        Message msg;
+        byte[] buf=new byte[size];
+        ch=new JChannel(props);
+        ch.connect("demo");
+        tp=PERF_TP.getInstance();
+        tp.setExpectedMessages(num_msgs);
+        for(int i=0; i < num_msgs; i++) {
+            msg=new Message(null, null, buf);
+            ch.send(msg);
+        }
+        synchronized(tp) {
+            if(tp.done()) {
+                ;
+            }
+            else {
+                tp.wait();
+            }
+        }
+        long total=tp.getTotalTime();
+        double msgs_per_ms=num_msgs / total;
+        double msgs_per_sec=msgs_per_ms * 1000;
+        double time_per_msg=total / (double)num_msgs;
+        double usec_per_msg=time_per_msg * 1000;
+
+        System.out.println("num_msgs = " + num_msgs + ", total_time = " + total + "ms");
+        System.out.println("msgs/millisec = " + msgs_per_ms + ", msgs/sec = " + msgs_per_sec +
+                "\ntime/msg = " + time_per_msg + " ms" +
+                " (" + usec_per_msg + " usec/msg)");
+        ch.close();
+    }
+}
