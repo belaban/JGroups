53a265739b9c1bbe1035333688a9e383d64934d9
Bela Ban <belaban@yahoo.com>
2004-03-29 19:01:19 +0000

    added

---
+++ src/org/jgroups/Global.java
@@ -0,0 +1,9 @@
+package org.jgroups;
+
+/**
+ * @author Bela Ban Mar 29, 2004
+ * @version $Id: Global.java,v 1.1 2004/03/29 19:01:19 belaban Exp $
+ */
+public class Global {
+    public static final boolean debug=false;
+}
