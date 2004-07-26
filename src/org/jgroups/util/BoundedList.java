a965fb970022228bae5a5ae1fd7f728c7dcc2d6b
Bela Ban <belaban@yahoo.com>
2004-07-26 15:23:26 +0000

    no message

--- src/org/jgroups/util/BoundedList.java
+++ src/org/jgroups/util/BoundedList.java
@@ -4,16 +4,21 @@ package org.jgroups.util;
 /**
  * A bounded subclass of List, oldest elements are removed once max capacity is exceeded
  * @author Bela Ban Nov 20, 2003
- * @version $Id: BoundedList.java,v 1.1 2003/11/21 06:54:50 belaban Exp $
+ * @version $Id: BoundedList.java,v 1.2 2004/07/26 15:23:26 belaban Exp $
  */
 public class BoundedList extends List {
     int max_capacity=10;
 
 
+
+    public BoundedList() {
+    }
+
     public BoundedList(int size) {
         super();
         max_capacity=size;
     }
+
 
     /**
      * Adds an element at the tail. Removes an object from the head if capacity is exceeded
