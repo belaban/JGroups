dd4d5c39e66c3ddaa7e303fa5c5a13d68627ffb4
jiwils <jiwils>
2004-07-30 04:43:52 +0000

    Updated to support JChannel's type-specific constructors.

--- src/org/jgroups/conf/ConfiguratorFactory.java
+++ src/org/jgroups/conf/ConfiguratorFactory.java
@@ -1,4 +1,4 @@
-// $Id: ConfiguratorFactory.java,v 1.7 2004/07/16 14:27:34 jiwils Exp $
+// $Id: ConfiguratorFactory.java,v 1.8 2004/07/30 04:43:52 jiwils Exp $
 
 package org.jgroups.conf;
 
@@ -6,12 +6,18 @@ import org.w3c.dom.Element;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import org.jgroups.ChannelException;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import java.net.MalformedURLException;
 import java.net.URL;
 
+import java.util.Properties;
 
 /**
  * The ConfigurationFactory is a factory that returns a protocol stack configurator.
@@ -26,14 +32,181 @@ import java.net.URL;
  * @version 1.0
  */
 public class ConfiguratorFactory {
-    public static final String JAR_MISSING_ERROR=
-            "JAXP Error: XML Parsing libraries are not in your classpath. Make sure you have JAXP compatible " +
-            "libraries in your classpath. JGroups include the Apache Xerces 2.0 parser, the two libraries: " +
-            "xercesxmlapi and xercesimpl can be found in the <JG_ROOT>/lib directory.";
+    public static final String JAXP_MISSING_ERROR_MSG=
+            "JAXP Error: the required XML parsing classes are not available; " +
+            "make sure that JAXP compatible libraries are in the classpath.";
+
+    static final String FORCE_CONFIGURATION="force.properties";
 
     static Log log=LogFactory.getLog(ConfiguratorFactory.class);
 
+    static final String propertiesOverride;
+
+    // Check for the presence of the system property "force.properties", and
+    // act appropriately if it is set.  We only need to do this once since the
+    // system properties are highly unlikely to change.
+    static {
+        Properties properties = System.getProperties();
+        propertiesOverride = properties.getProperty(FORCE_CONFIGURATION);
+
+        if (propertiesOverride != null && log.isInfoEnabled()) {
+            log.info("using properties override: " + propertiesOverride);
+        }
+    }
+
     protected ConfiguratorFactory() {
+    }
+
+    /**
+     * Returns a protocol stack configurator based on the XML configuration
+     * provided at the specified URL.
+     *
+     * @param url a URL pointing to a JGroups XML configuration.
+     *
+     * @return a <code>ProtocolStackConfigurator</code> containing the stack
+     *         configuration.
+     *
+     * @throws ChannelException if problems occur during the configuration of
+     *                          the protocol stack.
+     */
+    public static ProtocolStackConfigurator getStackConfigurator(File file)
+    throws ChannelException {
+        ProtocolStackConfigurator returnValue;
+
+        if (propertiesOverride != null) {
+            returnValue = getStackConfigurator(propertiesOverride);
+        }
+        else {
+            checkForNullConfiguration(file);
+            checkJAXPAvailability();
+
+            try {
+                returnValue=
+                    XmlConfigurator.getInstance(new FileInputStream(file));
+            }
+            catch (IOException ioe) {
+                throw createChannelConfigurationException(ioe);
+            }
+        }
+
+        return returnValue;
+    }
+
+    /**
+     * Returns a protocol stack configurator based on the XML configuration
+     * provided at the specified URL.
+     *
+     * @param url a URL pointing to a JGroups XML configuration.
+     *
+     * @return a <code>ProtocolStackConfigurator</code> containing the stack
+     *         configuration.
+     *
+     * @throws ChannelException if problems occur during the configuration of
+     *                          the protocol stack.
+     */
+    public static ProtocolStackConfigurator getStackConfigurator(URL url)
+    throws ChannelException {
+        ProtocolStackConfigurator returnValue;
+
+        if (propertiesOverride != null) {
+            returnValue = getStackConfigurator(propertiesOverride);
+        }
+        else {
+            checkForNullConfiguration(url);
+            checkJAXPAvailability();
+
+            try {
+                returnValue=XmlConfigurator.getInstance(url);
+            }
+            catch (IOException ioe) {
+                throw createChannelConfigurationException(ioe);
+            }
+        }
+
+        return returnValue;
+    }
+
+    /**
+     * Returns a protocol stack configurator based on the XML configuration
+     * provided by the specified XML element.
+     *
+     * @param element a XML element containing a JGroups XML configuration.
+     *
+     * @return a <code>ProtocolStackConfigurator</code> containing the stack
+     *         configuration.
+     *
+     * @throws ChannelException if problems occur during the configuration of
+     *                          the protocol stack.
+     */
+    public static ProtocolStackConfigurator getStackConfigurator(Element element)
+    throws ChannelException {
+        ProtocolStackConfigurator returnValue;
+
+        if (propertiesOverride != null) {
+            returnValue = getStackConfigurator(propertiesOverride);
+        }
+        else {
+            checkForNullConfiguration(element);
+
+            // Since Element is a part of the JAXP specification and because an
+            // Element instance already exists, there is no need to check for
+            // JAXP availability.
+            //
+            // checkJAXPAvailability();
+
+            try {
+                returnValue=XmlConfigurator.getInstance(element);
+            }
+            catch (IOException ioe) {
+                throw createChannelConfigurationException(ioe);
+            }
+        }
+
+        return returnValue;
+    }
+
+    /**
+     * Returns a protocol stack configurator based on the provided properties
+     * string.
+     *
+     * @param properties an old style property string, a string representing a
+     *                   system resource containing a JGroups XML configuration,
+     *                   a string representing a URL pointing to a JGroups XML
+     *                   XML configuration, or a string representing a file name
+     *                   that contains a JGroups XML configuration.
+     */
+    public static ProtocolStackConfigurator getStackConfigurator(String properties)
+    throws ChannelException {
+        if (propertiesOverride != null && propertiesOverride != properties) {
+            properties = propertiesOverride;
+        }
+
+        checkForNullConfiguration(properties);
+
+        ProtocolStackConfigurator returnValue;
+
+        // Attempt to treat the properties string as a pointer to an XML
+        // configuration.
+        XmlConfigurator configurator = null;
+
+        try {
+            configurator=getXmlConfigurator(properties);
+        }
+        catch (IOException ioe) {
+            throw createChannelConfigurationException(ioe);
+        }
+
+        // Did the properties string point to a JGroups XML configuration?
+        if (configurator != null) {
+            returnValue=configurator;
+        }
+        else {
+            // Attempt to process the properties string as the old style
+            // property string.
+            returnValue=new PlainConfigurator(properties);
+        }
+
+        return returnValue;
     }
 
     /**
@@ -46,9 +219,14 @@ public class ConfiguratorFactory {
      * @return a ProtocolStackConfigurator containing the stack configuration
      * @throws IOException if it fails to parse the XML content
      * @throws IOException if the URL is invalid or a the content can not be reached
+     * @deprecated Used by the JChannel(Object) constructor which has been deprecated.
      */
     public static ProtocolStackConfigurator getStackConfigurator(Object properties) throws IOException {
         InputStream input=null;
+
+        if (propertiesOverride != null) {
+            properties = propertiesOverride;
+        }
 
         if(properties instanceof URL) {
             try {
@@ -92,15 +270,6 @@ public class ConfiguratorFactory {
         }
 
         // try a regular file
-        // if(input == null) {
-        //     try {
-        //         input=new FileInputStream((String)properties);
-        //     }
-        //     catch(Throwable t) {
-        //     }
-        // }
-
-        // try a regular file
         if(input == null && properties instanceof File) {
             try {
                 input=new FileInputStream((File)properties);
@@ -124,14 +293,132 @@ public class ConfiguratorFactory {
     }
 
 
-    static XmlConfigurator getXmlConfigurator(URL url) throws IOException {
+    /**
+     * Returns a JGroups XML configuration InputStream based on the provided
+     * properties string.
+     *
+     * @param properties a string representing a system resource containing a
+     *                   JGroups XML configuration, a string representing a URL
+     *                   pointing to a JGroups ML configuration, or a string
+     *                   representing a file name that contains a JGroups XML
+     *                   configuration.
+     *
+     * @throws IOException  if the provided properties string appears to be a
+     *                      valid URL but is unreachable.
+     */
+    static InputStream getConfigStream(String properties) throws IOException {
+        InputStream configStream = null;
+
+        // Check to see if the properties string is a URL.
         try {
-            //quick check to see if we have the JAXP libraries in the classpath
+            configStream=new URL(properties).openStream();
+        }
+        catch (MalformedURLException mre) {
+            // the properties string is not a URL
+        }
+        // Commented so the caller is notified of this condition, but left in
+        // the code for documentation purposes.
+        //
+        // catch (IOException ioe) {
+            // the specified URL string was not reachable
+        // }
+
+        // Check to see if the properties string is the name of a resource,
+        // e.g. default.xml.
+        if(configStream == null && properties.endsWith("xml")) {
+            ClassLoader classLoader=Thread.currentThread().getContextClassLoader();
+            configStream=classLoader.getResourceAsStream(properties);
+        }
+
+        // Check to see if the properties string is the name of a file.
+        if (configStream == null) {
+            try {
+                configStream=new FileInputStream((String)properties);
+            }
+            catch(FileNotFoundException fnfe) {
+                // the properties string is likely not a file
+            }
+        }
+
+        return configStream;
+    }
+
+    /**
+     * Returns an XmlConfigurator based on the provided properties string (if
+     * possible).
+     *
+     * @param properties a string representing a system resource containing a
+     *                   JGroups XML configuration, a string representing a URL
+     *                   pointing to a JGroups ML configuration, or a string
+     *                   representing a file name that contains a JGroups XML
+     *                   configuration.
+     *
+     * @return an XmlConfigurator instance based on the provided properties
+     *         string; <code>null</code> if the provided properties string does
+     *         not point to an XML configuration.
+     *
+     * @throws IOException  if the provided properties string appears to be a
+     *                      valid URL but is unreachable, or if the JGroups XML
+     *                      configuration pointed to by the URL can not be
+     *                      parsed.
+     */
+    static XmlConfigurator getXmlConfigurator(String properties) throws IOException {
+        XmlConfigurator returnValue=null;
+        InputStream configStream=getConfigStream(properties);
+
+        if (configStream != null) {
+            checkJAXPAvailability();
+
+            returnValue=XmlConfigurator.getInstance(configStream);
+        }
+
+        return returnValue;
+    }
+
+    /**
+     * Creates a <code>ChannelException</code> instance based upon a
+     * configuration problem.
+     *
+     * @param cause the exceptional configuration condition to be used as the
+     *              created <code>ChannelException</code>'s cause.
+     */
+    static ChannelException createChannelConfigurationException(Throwable cause) {
+        return new ChannelException("unable to load the protocol stack", cause);
+    }
+
+    /**
+     * Check to see if the specified configuration properties are
+     * <code>null</null> which is not allowed.
+     *
+     * @param properties the specified protocol stack configuration.
+     *
+     * @throws NullPointerException if the specified configuration properties
+     *                              are <code>null</code>.
+     */
+    static void checkForNullConfiguration(Object properties) {
+        if (properties == null) {
+            final String msg =
+                "the specifed protocol stack configuration was null.";
+
+            throw new NullPointerException(msg);
+        }
+    }
+
+    /**
+     * Checks the availability of the JAXP classes on the classpath.
+     *
+     * @throws NoClassDefFoundError if the required JAXP classes are not
+     *                              availabile on the classpath.
+     */
+    static void checkJAXPAvailability() {
+        try {
+            // TODO: Do some real class checking here instead of forcing the
+            //       load of a JGroups class that happens (by default) to do it
+            //       for us.
             XmlConfigurator.class.getName();
         }
-        catch(java.lang.NoClassDefFoundError x) {
-            throw new java.lang.NoClassDefFoundError(JAR_MISSING_ERROR);
+        catch (NoClassDefFoundError error) {
+            throw new NoClassDefFoundError(JAXP_MISSING_ERROR_MSG);
         }
-        return XmlConfigurator.getInstance(url);
     }
 }
