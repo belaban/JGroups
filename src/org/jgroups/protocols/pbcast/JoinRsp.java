50f72665e86ce684ae8200bc6eb7a6c6482d6d4c
belaban <belaban>
2004-10-07 15:47:05 +0000

    pretty-print

--- src/org/jgroups/protocols/pbcast/JoinRsp.java
+++ src/org/jgroups/protocols/pbcast/JoinRsp.java
@@ -1,4 +1,4 @@
-// $Id: JoinRsp.java,v 1.2 2004/03/30 06:47:18 belaban Exp $
+// $Id: JoinRsp.java,v 1.3 2004/10/07 15:47:05 belaban Exp $
 
 package org.jgroups.protocols.pbcast;
 
@@ -8,31 +8,37 @@ import org.jgroups.View;
 import java.io.Serializable;
 
 
-
-
-
 public class JoinRsp implements Serializable {
-    View    view=null;
-    Digest  digest=null;
+    View view=null;
+    Digest digest=null;
 
     public JoinRsp(View v, Digest d) {
-	view=v;
-	digest=d;
+        view=v;
+        digest=d;
     }
 
 
-    View   getView()   {return view;}
-    Digest getDigest() {return digest;}
+    View getView() {
+        return view;
+    }
+
+    Digest getDigest() {
+        return digest;
+    }
 
 
     public String toString() {
-	StringBuffer sb=new StringBuffer();
-	sb.append("view: ");
-	if(view == null) sb.append("<null>");
-	else sb.append(view);
-	sb.append(", digest: ");
-	if(digest == null) sb.append("<null>");
-	else sb.append(digest);
-	return sb.toString();
+        StringBuffer sb=new StringBuffer();
+        sb.append("view: ");
+        if(view == null)
+            sb.append("<null>");
+        else
+            sb.append(view);
+        sb.append(", digest: ");
+        if(digest == null)
+            sb.append("<null>");
+        else
+            sb.append(digest);
+        return sb.toString();
     }
 }
