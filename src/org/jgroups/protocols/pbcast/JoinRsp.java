b6cd707b2342795304dfa639e9df89fcf6d0f2f0
belaban <belaban>
2004-10-08 12:19:10 +0000

    added Streamable impl

--- src/org/jgroups/protocols/pbcast/JoinRsp.java
+++ src/org/jgroups/protocols/pbcast/JoinRsp.java
@@ -1,14 +1,19 @@
-// $Id: JoinRsp.java,v 1.3 2004/10/07 15:47:05 belaban Exp $
+// $Id: JoinRsp.java,v 1.4 2004/10/08 12:19:10 belaban Exp $
 
 package org.jgroups.protocols.pbcast;
 
 
 import org.jgroups.View;
+import org.jgroups.util.Streamable;
+import org.jgroups.util.Util;
 
 import java.io.Serializable;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.DataInputStream;
 
 
-public class JoinRsp implements Serializable {
+public class JoinRsp implements Serializable, Streamable {
     View view=null;
     Digest digest=null;
 
@@ -26,6 +31,15 @@ public class JoinRsp implements Serializable {
         return digest;
     }
 
+    public void writeTo(DataOutputStream out) throws IOException {
+        Util.writeStreamable(view, out);
+        Util.writeStreamable(digest, out);
+    }
+
+    public void readFrom(DataInputStream in) throws IOException, IllegalAccessException, InstantiationException {
+        view=(View)Util.readStreamable(View.class, in);
+        digest=(Digest)Util.readStreamable(Digest.class, in);
+    }
 
     public String toString() {
         StringBuffer sb=new StringBuffer();
