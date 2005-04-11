3453a9a5f69ee529ae49f6cf10164c02eb942bb0
belaban <belaban>
2005-04-11 12:54:08 +0000

    toString()

--- src/org/jgroups/util/Buffer.java
+++ src/org/jgroups/util/Buffer.java
@@ -3,7 +3,7 @@ package org.jgroups.util;
 /**
  * Buffer with an offset and length. Will be replaced with NIO equivalent once JDK 1.4 becomes baseline
  * @author Bela Ban
- * @version $Id: Buffer.java,v 1.1 2004/09/26 11:10:56 belaban Exp $
+ * @version $Id: Buffer.java,v 1.2 2005/04/11 12:54:08 belaban Exp $
  */
 public class Buffer {
     byte[] buf;
@@ -38,5 +38,13 @@ public class Buffer {
 
     public void setLength(int length) {
         this.length=length;
+    }
+
+    public String toString() {
+        StringBuffer sb=new StringBuffer();
+        sb.append(buf != null? buf.length : '0').append(" bytes");
+        if(offset > 0)
+            sb.append("offset=").append(offset).append(", len=").append(length).append(")");
+        return sb.toString();
     }
 }
