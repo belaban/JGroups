83f03004d7c018d1aef5ad82d297daebbefbc980
Bela Ban <belaban@yahoo.com>
2005-04-13 09:24:03 +0000

    extracted from FRAG/FRAG2

---
+++ src/org/jgroups/protocols/FragHeader.java
@@ -0,0 +1,57 @@
+package org.jgroups.protocols;
+
+import org.jgroups.Header;
+import org.jgroups.util.Streamable;
+
+import java.io.*;
+
+/**
+ * @author Bela Ban
+ * @version $Id: FragHeader.java,v 1.1 2005/04/13 09:24:03 belaban Exp $
+ */
+public class FragHeader extends Header implements Streamable {
+    public long id=0;
+    public int  frag_id=0;
+    public int  num_frags=0;
+
+
+    public FragHeader() {
+    } // used for externalization
+
+    public FragHeader(long id, int frag_id, int num_frags) {
+        this.id=id;
+        this.frag_id=frag_id;
+        this.num_frags=num_frags;
+    }
+
+    public String toString() {
+        return "[id=" + id + ", frag_id=" + frag_id + ", num_frags=" + num_frags + ']';
+    }
+
+    public void writeExternal(ObjectOutput out) throws IOException {
+        out.writeLong(id);
+        out.writeInt(frag_id);
+        out.writeInt(num_frags);
+    }
+
+
+    public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+        id=in.readLong();
+        frag_id=in.readInt();
+        num_frags=in.readInt();
+    }
+
+
+    public void writeTo(DataOutputStream out) throws IOException {
+        out.writeLong(id);
+        out.writeInt(frag_id);
+        out.writeInt(num_frags);
+    }
+
+    public void readFrom(DataInputStream in) throws IOException, IllegalAccessException, InstantiationException {
+        id=in.readLong();
+        frag_id=in.readInt();
+        num_frags=in.readInt();
+    }
+
+}
