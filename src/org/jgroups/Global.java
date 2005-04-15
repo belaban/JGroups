616d662e8dc608fc4cbfd7db0b6c3eab04e44de1
belaban <belaban>
2005-04-15 12:37:19 +0000

    no message

--- src/org/jgroups/Global.java
+++ src/org/jgroups/Global.java
@@ -2,7 +2,7 @@ package org.jgroups;
 
 /**
  * @author Bela Ban Mar 29, 2004
- * @version $Id: Global.java,v 1.2 2004/03/30 06:47:29 belaban Exp $
+ * @version $Id: Global.java,v 1.3 2005/04/15 12:37:19 belaban Exp $
  */
 public class Global {
     /** Allows for conditional compilation, e.g. if(log.isTraceEnabled()) if(log.isInfoEnabled()) log.info(...) would be removed from the code
@@ -17,4 +17,10 @@ public class Global {
      * will be removed entirely
      */
     public static final boolean copy=false;
+    
+
+    public static final int BYTE_SIZE  = 1;
+    public static final int SHORT_SIZE = 2;
+    public static final int INT_SIZE   = 4;
+    public static final int LONG_SIZE  = 8;
 }
