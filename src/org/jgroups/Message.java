68e7fb8b21f04448d4dd56648939ffcea1a10d6c
Bela Ban <belaban@yahoo.com>
2005-04-19 12:34:01 +0000

    fixed bug where additonal_data was not marshalled

--- src/org/jgroups/Message.java
+++ src/org/jgroups/Message.java
@@ -1,4 +1,4 @@
-// $Id: Message.java,v 1.27 2005/04/19 12:25:31 belaban Exp $
+// $Id: Message.java,v 1.28 2005/04/19 12:34:01 belaban Exp $
 
 package org.jgroups;
 
@@ -552,22 +552,23 @@ public class Message implements Externalizable, Streamable {
         // 3. src_addr
         if(src_addr != null) {
             if(src_addr instanceof IpAddress) {
-                IpAddress tmp=(IpAddress)src_addr;
-                if(tmp.getIpAddress() != null)
-                    src_addr.writeTo(out);
-                else {
-                    byte[] additional_data;
-                    out.writeInt(tmp.getPort());
-                    // additional_data
-                    if((additional_data=tmp.getAdditionalData()) != null) {
-                        out.writeByte(1);
-                        out.writeInt(additional_data.length);
-                        out.write(additional_data, 0, additional_data.length);
-                    }
-                    else {
-                        out.writeByte(0); // no additional_data
-                    }
-                }
+                src_addr.writeTo(out);
+//                IpAddress tmp=(IpAddress)src_addr;
+//                if(tmp.getIpAddress() != null)
+//                    src_addr.writeTo(out);
+//                else {
+//                    byte[] additional_data;
+//                    out.writeInt(tmp.getPort());
+//                    // additional_data
+//                    if((additional_data=tmp.getAdditionalData()) != null) {
+//                        out.writeByte(1);
+//                        out.writeInt(additional_data.length);
+//                        out.write(additional_data, 0, additional_data.length);
+//                    }
+//                    else {
+//                        out.writeByte(0); // no additional_data
+//                    }
+//                }
             }
             else {
                 Util.writeAddress(src_addr, out);
@@ -615,20 +616,25 @@ public class Message implements Externalizable, Streamable {
         // 2. src_addr
         if((leading & SRC_SET) == SRC_SET) {
             if((leading & IPADDR_SRC) == IPADDR_SRC) {
-                if((leading & SRC_HOST_NULL) == SRC_HOST_NULL) {
-                    int src_port=in.readInt();
-                    src_addr=new IpAddress(src_port, false); // keep a null host part
-                    if(in.readByte() == 1) {
-                        len=in.readInt();
-                        byte[] additional_data=new byte[len];
-                        in.readFully(additional_data, 0, len);
-                        ((IpAddress)src_addr).setAdditionalData(additional_data);
-                    }
-                }
-                else {
-                    src_addr=new IpAddress();
-                    src_addr.readFrom(in);
-                }
+                src_addr=new IpAddress();
+                src_addr.readFrom(in);
+                
+//                if((leading & SRC_HOST_NULL) == SRC_HOST_NULL) {
+//                    int src_port=in.readInt();
+//                    src_addr=new IpAddress(src_port, false); // keep a null host part
+//                    if(in.readByte() == 1) {
+//                        len=in.readInt();
+//                        byte[] additional_data=new byte[len];
+//                        in.readFully(additional_data, 0, len);
+//                        ((IpAddress)src_addr).setAdditionalData(additional_data);
+//                    }
+//                }
+//                else {
+//                    src_addr=new IpAddress();
+//                    src_addr.readFrom(in);
+//                }
+
+
             }
             else {
                 src_addr=Util.readAddress(in);
