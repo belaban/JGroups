91969807c46bba360a8611aed977e40fa4e35032
belaban <belaban>
2005-04-23 12:43:03 +0000

    no message

--- tests/junit/org/jgroups/tests/NakackTest.java
+++ tests/junit/org/jgroups/tests/NakackTest.java
@@ -1,4 +1,4 @@
-// $Id: NakackTest.java,v 1.4 2005/04/18 15:35:12 belaban Exp $
+// $Id: NakackTest.java,v 1.5 2005/04/23 12:43:03 belaban Exp $
 
 package org.jgroups.tests;
 
@@ -16,7 +16,7 @@ import java.util.Vector;
 
 public class NakackTest extends TestCase {
     final long WAIT_TIME=5000;
-    public final long NUM_MSGS=500;
+    public final long NUM_MSGS=10000;
     long num_msgs_received=0;
     long num_msgs_sent=0;
 
@@ -40,8 +40,7 @@ public class NakackTest extends TestCase {
         Address my_addr=new IpAddress("localhost", 10000);
         ViewId vid=new ViewId(my_addr, 322649);
         Vector mbrs=new Vector();
-        View view=null;
-        IpAddress local_addr=new IpAddress(5555);
+        View view;
 
         mbrs.addElement(my_addr);
         view=new View(vid, mbrs);
@@ -50,9 +49,9 @@ public class NakackTest extends TestCase {
 
         synchronized(mutex) {
             for(long i=0; i < NUM_MSGS; i++) {
-                if(i % 100 == 0 && i > 0)
+                if(i % 1000 == 0 && i > 0)
                     System.out.println("sending msg #" + i);
-                check.down(new Event(Event.MSG, new Message(null, local_addr, new Long(i))));
+                check.down(new Event(Event.MSG, new Message(null, my_addr, new Long(i))));
                 num_msgs_sent++;
             }
             mutex.wait(WAIT_TIME);
@@ -99,6 +98,11 @@ public class NakackTest extends TestCase {
 
             if(evt == null)
                 return;
+
+            if(evt.getType() == Event.SET_LOCAL_ADDRESS) {
+                System.out.println("local address is " + evt.getArg());
+            }
+
             if(evt.getType() != Event.MSG)
                 return;
             msg=(Message)evt.getArg();
@@ -120,7 +124,7 @@ public class NakackTest extends TestCase {
                 received_seqno=s.longValue();
                 if(received_seqno == highest_seqno + 1) {
                     // correct
-                    if(received_seqno % 100 == 0 && received_seqno > 0)
+                    if(received_seqno % 1000 == 0 && received_seqno > 0)
                         System.out.println("PASS: received msg #" + received_seqno);
                     senders.put(sender, new Long(highest_seqno + 1));
                     num_msgs++;
