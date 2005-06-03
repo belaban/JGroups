910708278fc4ea77faf69971b160dfaee2657f95
belaban <belaban>
2005-06-03 08:49:16 +0000

    first version of JMX instrumentation

---
+++ src/org/jgroups/jmx/JChannelMBean.java
@@ -0,0 +1,100 @@
+package org.jgroups.jmx;
+
+import org.jgroups.*;
+import org.w3c.dom.Element;
+
+import java.io.Serializable;
+
+/**
+ * @author Bela Ban
+ * @version $Id: JChannelMBean.java,v 1.1 2005/06/03 08:49:17 belaban Exp $
+ */
+public interface JChannelMBean {
+    void create() throws Exception;
+    void start() throws Exception;
+    void stop();
+    void destroy();
+
+    String getProperties();
+    void setProperties(String props);
+
+    String getVersion();
+
+    String getObjectName();
+    void setObjectName(String name);
+
+    /** To configure via XML file */
+    void setClusterConfig(Element el);
+
+    String getGroupName();
+    void setGroupName(String group_name);
+
+    String getClusterName();
+    void setClusterName(String cluster_name);
+
+    boolean getReceiveViews();
+    void setReceiveViewEvents(boolean flag);
+
+    boolean getReceiveSuspectEvents();
+    void setReceiveSuspectEvents(boolean flag);
+
+    boolean getReceiveBlockEvents();
+    void setReceiveBlockEvents(boolean flag);
+
+    boolean getReceiveStateEvents();
+    void setReceiveStateEvents(boolean flag);
+
+    boolean getReceiveLocalMessages();
+    void setReceiveLocalMessages(boolean flag);
+
+    boolean getAutoReconnect();
+    void setAutoReconnect(boolean flag);
+
+    boolean getAutoGetState();
+    void setAutoGetState(boolean flag);
+
+
+    View getView();
+    String getViewAsString();
+    Address getLocalAddress();
+    String getLocalAddressAsString();
+    void setChannelListener(ChannelListener channel_listener);
+
+    boolean isOpen();
+
+    boolean isConnected();
+
+    int getNumMessages();
+
+    String dumpQueue();
+
+    String printProtocolSpec(boolean include_properties);
+
+    String toString(boolean print_details);
+
+    void connect(String channel_name) throws ChannelException, ChannelClosedException;
+
+    void disconnect();
+
+    void close();
+
+    void send(Message msg) throws ChannelNotConnectedException, ChannelClosedException;
+
+    void send(Address dst, Address src, Serializable obj) throws ChannelNotConnectedException, ChannelClosedException;
+
+    void sendToAll(String msg) throws ChannelNotConnectedException, ChannelClosedException;
+
+    /** @param evt
+     * @deprecated */
+    void down(Event evt);
+
+    Object receive(long timeout) throws ChannelNotConnectedException, ChannelClosedException, TimeoutException;
+
+    Object peek(long timeout) throws ChannelNotConnectedException, ChannelClosedException, TimeoutException;
+
+    void blockOk();
+
+    boolean getState(Address target, long timeout) throws ChannelNotConnectedException, ChannelClosedException;
+
+    void returnState(byte[] state);
+}
---
+++ src/org/jgroups/jmx/JmxConfigurator.java
@@ -0,0 +1,88 @@
+package org.jgroups.jmx;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.jgroups.stack.ProtocolStack;
+import org.jgroups.util.Util;
+
+import javax.management.*;
+import java.util.Vector;
+
+/**
+ * @author Bela Ban
+ * @version $Id: JmxConfigurator.java,v 1.1 2005/06/03 08:49:17 belaban Exp $
+ */
+public class JmxConfigurator {
+    static final Log log=LogFactory.getLog(JmxConfigurator.class);
+
+    /**
+     * Registers an already created channel with the MBeanServer. Creates an org.jgroups.jmx.JChannel which
+     * delegates to the org.jgroups.JChannel and registers it. Optionally, this method will also try to
+     * create one MBean proxy for each protocol in the channel's protocol stack, and register it as well.
+     * @param channel
+     * @param server
+     * @param name Has to be a JMX ObjectName, e.g. DefaultDomain:type=MyChannel
+     * @param register_protocols
+     * @return
+     */
+    public static org.jgroups.jmx.JChannel registerChannel(org.jgroups.JChannel channel,
+                                                           MBeanServer server, String name,
+                                                           boolean register_protocols) throws Exception {
+        JChannel retval=new JChannel(channel);
+        server.registerMBean(retval, new ObjectName(name));
+        if(register_protocols)
+            registerProtocols(server, channel, name);
+        return retval;
+    }
+
+    public static void unregisterChannel(MBeanServer server, ObjectName name) throws Exception {
+        if(server != null)
+            server.unregisterMBean(name);
+    }
+
+    /**
+     * Takes all protocols of an existing stack, creates corresponding MBean proxies and registers them with
+     * the MBean server
+     * @param channel
+     * @param channel_name
+     */
+    public static void registerProtocols(MBeanServer server, org.jgroups.JChannel channel, String channel_name) throws Exception {
+        ProtocolStack stack=channel.getProtocolStack();
+        Vector protocols=stack.getProtocols();
+        org.jgroups.stack.Protocol prot;
+        for(int i=0; i < protocols.size(); i++) {
+            prot=(org.jgroups.stack.Protocol)protocols.get(i);
+            // System.out.println(prot);
+            org.jgroups.jmx.Protocol p=findProtocol(prot);
+            if(p == null)
+                p=new org.jgroups.jmx.Protocol(prot);
+            ObjectName prot_name=new ObjectName(channel_name + ",protocol=" + prot.getName());
+            server.registerMBean(p, prot_name);
+        }
+    }
+
+    public static void unregisterProtocols(MBeanServer server, org.jgroups.JChannel channel, String channel_name) throws Exception {
+        ProtocolStack stack=channel.getProtocolStack();
+        Vector protocols=stack.getProtocols();
+        org.jgroups.stack.Protocol prot;
+        for(int i=0; i < protocols.size(); i++) {
+            prot=(org.jgroups.stack.Protocol)protocols.get(i);
+            ObjectName prot_name=new ObjectName(channel_name + ",protocol=" + prot.getName());
+            server.unregisterMBean(prot_name);
+        }
+    }
+
+
+    protected static Protocol findProtocol(org.jgroups.stack.Protocol prot) throws IllegalAccessException, InstantiationException {
+        Protocol p;
+        String prot_name=prot.getClass().getName();
+        String clname=prot_name.replaceFirst("org.jgroups.", "org.jgroups.jmx.");
+        Class cl=Util.loadClass(clname, JmxConfigurator.class);
+        if(cl != null) {
+            p=(Protocol)cl.newInstance();
+            p.attachProtocol(prot);
+            return p;
+        }
+        return null;
+    }
+}
