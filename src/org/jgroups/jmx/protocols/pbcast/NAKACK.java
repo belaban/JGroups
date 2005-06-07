a12c10f79e7e2399282f59b398dd5c17101d4d0e
belaban <belaban>
2005-06-07 12:28:08 +0000

    no message

---
+++ src/org/jgroups/jmx/protocols/pbcast/NAKACK.java
@@ -0,0 +1,10 @@
+package org.jgroups.jmx.protocols.pbcast;
+
+import org.jgroups.jmx.Protocol;
+
+/**
+ * @author Bela Ban
+ * @version $Id: NAKACK.java,v 1.1 2005/06/07 12:28:08 belaban Exp $
+ */
+public class NAKACK extends Protocol implements NAKACKMBean {
+}
