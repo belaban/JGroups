b82043112d8bbf4dad1a4c4d8c96d7a813f90205
Bela Ban <belaban@yahoo.com>
2005-06-07 12:28:08 +0000

    no message

---
+++ src/org/jgroups/jmx/protocols/pbcast/NAKACKMBean.java
@@ -0,0 +1,10 @@
+package org.jgroups.jmx.protocols.pbcast;
+
+import org.jgroups.jmx.ProtocolMBean;
+
+/**
+ * @author Bela Ban
+ * @version $Id: NAKACKMBean.java,v 1.1 2005/06/07 12:28:08 belaban Exp $
+ */
+public interface NAKACKMBean extends ProtocolMBean {
+}
