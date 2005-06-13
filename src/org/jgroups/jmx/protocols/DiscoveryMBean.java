b4e029bbb57e71921038b2086410997b177496c3
belaban <belaban>
2005-06-13 11:10:49 +0000

    added findInitialMembers() to Discovery protocol

--- src/org/jgroups/jmx/protocols/DiscoveryMBean.java
+++ src/org/jgroups/jmx/protocols/DiscoveryMBean.java
@@ -2,9 +2,11 @@ package org.jgroups.jmx.protocols;
 
 import org.jgroups.jmx.ProtocolMBean;
 
+import java.util.Vector;
+
 /**
  * @author Bela Ban
- * @version $Id: DiscoveryMBean.java,v 1.1 2005/06/08 15:17:30 belaban Exp $
+ * @version $Id: DiscoveryMBean.java,v 1.2 2005/06/13 11:10:49 belaban Exp $
  */
 public interface DiscoveryMBean extends ProtocolMBean {
     long getTimeout();
@@ -13,4 +15,6 @@ public interface DiscoveryMBean extends ProtocolMBean {
     void setNumInitialMembers(int num_initial_members);
     int getNumPingRequests();
     void setNumPingRequests(int num_ping_requests);
+    Vector findInitialMembers();
+    String findInitialMembersAsString();
 }
