062eb5c7b6289520153a09309ea1e3931d57e450
Bela Ban <belaban@yahoo.com>
2005-06-14 09:23:16 +0000

    Added JMX instrumentation for FC

---
+++ src/org/jgroups/jmx/protocols/FC.java
@@ -0,0 +1,80 @@
+package org.jgroups.jmx.protocols;
+
+import org.jgroups.jmx.Protocol;
+
+/**
+ * @author Bela Ban
+ * @version $Id: FC.java,v 1.1 2005/06/14 09:23:16 belaban Exp $
+ */
+public class FC extends Protocol implements FCMBean {
+    org.jgroups.protocols.FC p;
+
+    public FC() {
+    }
+
+    public FC(org.jgroups.stack.Protocol p) {
+        super(p);
+        this.p=(org.jgroups.protocols.FC)p;
+    }
+
+    public void attachProtocol(org.jgroups.stack.Protocol p) {
+        super.attachProtocol(p);
+        this.p=(org.jgroups.protocols.FC)p;
+    }
+
+    public long getMaxCredits() {
+        return p.getMaxCredits();
+    }
+
+    public void setMaxCredits(long max_credits) {
+        p.setMaxCredits(max_credits);
+    }
+
+    public double getMinThreshold() {
+        return p.getMinThreshold();
+    }
+
+    public void setMinThreshold(double min_threshold) {
+        p.setMinThreshold(min_threshold);
+    }
+
+    public long getMinCredits() {
+        return p.getMinCredits();
+    }
+
+    public void setMinCredits(long min_credits) {
+        p.setMinCredits(min_credits);
+    }
+
+    public boolean isBlocked() {
+        return p.isBlocked();
+    }
+
+    public int getNumberOfBlockings() {
+        return p.getNumberOfBlockings();
+    }
+
+    public long getTotalTimeBlocked() {
+        return p.getTotalTimeBlocked();
+    }
+
+    public int getNumberOfReplenishmentsReceived() {
+        return p.getNumberOfReplenishmentsReceived();
+    }
+
+    public String printSenderCredits() {
+        return p.printSenderCredits();
+    }
+
+    public String printReceiverCredits() {
+        return p.printReceiverCredits();
+    }
+
+    public String printCredits() {
+        return p.printCredits();
+    }
+
+    public void unblock() {
+        p.unblock();
+    }
+}
---
+++ src/org/jgroups/jmx/protocols/FCMBean.java
@@ -0,0 +1,26 @@
+package org.jgroups.jmx.protocols;
+
+import org.jgroups.jmx.ProtocolMBean;
+
+/**
+ * @author Bela Ban
+ * @version $Id: FCMBean.java,v 1.1 2005/06/14 09:23:16 belaban Exp $
+ */
+public interface FCMBean extends ProtocolMBean {
+    long getMaxCredits();
+    void setMaxCredits(long max_credits);
+    double getMinThreshold();
+    void setMinThreshold(double min_threshold);
+    long getMinCredits();
+    void setMinCredits(long min_credits);
+    boolean isBlocked();
+    int getNumberOfBlockings();
+    long getTotalTimeBlocked();
+    int getNumberOfReplenishmentsReceived();
+    String printSenderCredits();
+    String printReceiverCredits();
+    String printCredits();
+    void unblock();
+
+
+}
