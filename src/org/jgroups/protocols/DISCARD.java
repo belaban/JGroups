ab10c43831565d27023ac2bcb5165ec2bd27822a
Bela Ban <belaban@yahoo.com>
2005-08-24 13:12:04 +0000

    added stats

--- src/org/jgroups/protocols/DISCARD.java
+++ src/org/jgroups/protocols/DISCARD.java
@@ -1,4 +1,4 @@
-// $Id: DISCARD.java,v 1.9 2005/08/24 12:57:55 belaban Exp $
+// $Id: DISCARD.java,v 1.10 2005/08/24 13:12:04 belaban Exp $
 
 package org.jgroups.protocols;
 
@@ -9,144 +9,130 @@ import org.jgroups.stack.Protocol;
 
 import java.util.Properties;
 import java.util.Vector;
-
+import java.util.Map;
+import java.util.HashMap;
 
 
 /**
- Discards up or down messages based on a percentage; e.g., setting property 'up' to 0.1 causes 10%
- of all up messages to be discarded. Setting 'down' or 'up' to 0 causes no loss, whereas 1 discards
- all messages (not very useful).
+ * Discards up or down messages based on a percentage; e.g., setting property 'up' to 0.1 causes 10%
+ * of all up messages to be discarded. Setting 'down' or 'up' to 0 causes no loss, whereas 1 discards
+ * all messages (not very useful).
  */
 
-public class DISCARD extends Protocol
-{
-   final Vector members = new Vector();
-   double up = 0.0;    // probability of dropping up   msgs
-   double down = 0.0;  // probability of dropping down msgs
-   boolean excludeItself = false;   //if true don't discard messages sent/received in this stack
-   Address localAddress;
-   final int num_sent = 25;  // don't discard the first 5 messages. Temporary, +++remove later !
+public class DISCARD extends Protocol {
+    final Vector members=new Vector();
+    double up=0.0;    // probability of dropping up   msgs
+    double down=0.0;  // probability of dropping down msgs
+    boolean excludeItself=false;   //if true don't discard messages sent/received in this stack
+    Address localAddress;
+    int num_down=0, num_up=0;
 
 
-   /** All protocol names have to be unique ! */
-   public String getName()
-   {
-      return "DISCARD";
-   }
+    /**
+     * All protocol names have to be unique !
+     */
+    public String getName() {
+        return "DISCARD";
+    }
 
 
-   public boolean setProperties(Properties props)
-   {
-      String str;
+    public boolean setProperties(Properties props) {
+        String str;
 
-       super.setProperties(props);
-      str = props.getProperty("up");
-      if (str != null)
-      {
-         up = Double.parseDouble(str);
-         props.remove("up");
-      }
+        super.setProperties(props);
+        str=props.getProperty("up");
+        if(str != null) {
+            up=Double.parseDouble(str);
+            props.remove("up");
+        }
 
-      str = props.getProperty("down");
-      if (str != null)
-      {
-         down = Double.parseDouble(str);
-         props.remove("down");
-      }
+        str=props.getProperty("down");
+        if(str != null) {
+            down=Double.parseDouble(str);
+            props.remove("down");
+        }
 
-      str = props.getProperty("excludeitself");
-      if (str != null)
-      {
-         excludeItself = Boolean.valueOf(str).booleanValue();
-         props.remove("excludeitself");
-      }
+        str=props.getProperty("excludeitself");
+        if(str != null) {
+            excludeItself=Boolean.valueOf(str).booleanValue();
+            props.remove("excludeitself");
+        }
 
 
-      if (props.size() > 0)
-      {
-         log.error("DISCARD.setProperties(): these properties are not recognized: " + props);
+        if(props.size() > 0) {
+            log.error("DISCARD.setProperties(): these properties are not recognized: " + props);
 
-         return false;
-      }
-      return true;
-   }
-
-
-   public void up(Event evt)
-   {
-      Message msg;
-      double r;
-
-      if (evt.getType() == Event.SET_LOCAL_ADDRESS)
-         localAddress = (Address) evt.getArg();
+            return false;
+        }
+        return true;
+    }
 
 
-      if (evt.getType() == Event.MSG)
-      {
-         msg = (Message) evt.getArg();
-         if (up > 0)
-         {
-            /*if(num_sent > 0) {
-                    num_sent--;
-                    passUp(evt);
-                    return;
-                }*/
+    public void up(Event evt) {
+        Message msg;
+        double r;
+
+        if(evt.getType() == Event.SET_LOCAL_ADDRESS)
+            localAddress=(Address)evt.getArg();
 
 
-            r = Math.random();
-            if (r < up)
-            {
-               if (excludeItself && msg.getSrc().equals(localAddress))
-               {
-                   if(log.isTraceEnabled()) log.trace("excluding itself");
-               }
-               else
-               {
-                   if(log.isTraceEnabled()) log.trace("dropping message");
-                  return;
-               }
+        if(evt.getType() == Event.MSG) {
+            msg=(Message)evt.getArg();
+            if(up > 0) {
+                r=Math.random();
+                if(r < up) {
+                    if(excludeItself && msg.getSrc().equals(localAddress)) {
+                        if(log.isTraceEnabled()) log.trace("excluding itself");
+                    }
+                    else {
+                        if(log.isTraceEnabled()) log.trace("dropping message");
+                        num_up++;
+                        return;
+                    }
+                }
             }
-         }
-      }
+        }
 
 
-      passUp(evt);
-   }
+        passUp(evt);
+    }
 
 
-   public void down(Event evt)
-   {
-      Message msg;
-      double r;
+    public void down(Event evt) {
+        Message msg;
+        double r;
 
-      if (evt.getType() == Event.MSG)
-      {
-         msg = (Message) evt.getArg();
+        if(evt.getType() == Event.MSG) {
+            msg=(Message)evt.getArg();
 
-         if (down > 0)
-         {
-            /*if(num_sent > 0) {
-                    num_sent--;
-                    passDown(evt);
-                    return;
-                }*/
-            r = Math.random();
-            if (r < down)
-            {
-
-               if (excludeItself && msg.getSrc().equals(localAddress))
-               {
-                   if(log.isTraceEnabled()) log.trace("excluding itself");
-               }
-               else
-               {
-                   if(log.isTraceEnabled()) log.trace("dropping message");
-                  return;
-               }
+            if(down > 0) {
+                r=Math.random();
+                if(r < down) {
+                    if(excludeItself && msg.getSrc().equals(localAddress)) {
+                        if(log.isTraceEnabled()) log.trace("excluding itself");
+                    }
+                    else {
+                        if(log.isTraceEnabled())
+                            log.trace("dropping message");
+                        num_down++;
+                        return;
+                    }
+                }
             }
-         }
 
-      }
-      passDown(evt);
-   }
+        }
+        passDown(evt);
+    }
+
+    public void resetStats() {
+        super.resetStats();
+        num_down=num_up=0;
+    }
+
+    public Map dumpStats() {
+        Map m=new HashMap(2);
+        m.put("num_dropped_down", new Integer(num_down));
+        m.put("num_dropped_up", new Integer(num_up));
+        return m;
+    }
 }
