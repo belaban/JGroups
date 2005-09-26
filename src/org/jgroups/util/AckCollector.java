e5c5abfc82f12a88c4a636d56e5aedefb9341bbf
belaban <belaban>
2005-09-26 08:39:31 +0000

    no message

---
+++ src/org/jgroups/util/AckCollector.java
@@ -0,0 +1,47 @@
+package org.jgroups.util;
+
+import org.jgroups.TimeoutException;
+
+import java.util.ArrayList;
+
+/**
+ * @author Bela Ban
+ * @version $Id: AckCollector.java,v 1.1 2005/09/26 08:39:31 belaban Exp $
+ */
+public class AckCollector {
+    /** List<Object>: list of members from whom we haven't received an ACK yet */
+    final java.util.List missing_acks;
+    final Promise all_acks_received=new Promise();
+
+    public AckCollector(java.util.List l) {
+        missing_acks=new ArrayList(l);
+    }
+
+    public int size() {
+        return missing_acks.size();
+    }
+
+    public void ack(Object member) {
+        missing_acks.remove(member);
+        if(missing_acks.size() == 0)
+            all_acks_received.setResult(Boolean.TRUE);
+    }
+
+    public boolean waitForAllAcks() {
+        Object result=all_acks_received.getResult();
+        if(result != null && result instanceof Boolean && ((Boolean)result).booleanValue())
+            return true;
+        return false;
+    }
+
+    public boolean waitForAllAcks(long timeout) throws TimeoutException {
+        Object result=all_acks_received.getResultWithTimeout(timeout);
+        if(result != null && result instanceof Boolean && ((Boolean)result).booleanValue())
+            return true;
+        return false;
+    }
+
+    public String toString() {
+        return missing_acks.toString();
+    }
+}
