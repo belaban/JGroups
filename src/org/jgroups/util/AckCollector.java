1b326d95f62d7260652aa66744e5f6b254edf03d
Bela Ban <belaban@yahoo.com>
2005-09-26 11:41:44 +0000

    no message

--- src/org/jgroups/util/AckCollector.java
+++ src/org/jgroups/util/AckCollector.java
@@ -2,23 +2,37 @@ package org.jgroups.util;
 
 import org.jgroups.TimeoutException;
 
-import java.util.ArrayList;
+import java.util.*;
 
 /**
  * @author Bela Ban
- * @version $Id: AckCollector.java,v 1.2 2005/09/26 11:25:20 belaban Exp $
+ * @version $Id: AckCollector.java,v 1.3 2005/09/26 11:41:44 belaban Exp $
  */
 public class AckCollector {
     /** List<Object>: list of members from whom we haven't received an ACK yet */
     final java.util.List missing_acks;
+    final Set received_acks=new HashSet();
     final Promise all_acks_received=new Promise();
+
+    public AckCollector() {
+        missing_acks=new ArrayList();
+    }
 
     public AckCollector(java.util.List l) {
         missing_acks=new ArrayList(l);
     }
 
+    public java.util.List getMissing() {
+        return missing_acks;
+    }
+
+    public Set getReceived() {
+        return received_acks;
+    }
+
     public void reset(java.util.List l) {
         missing_acks.clear();
+        received_acks.clear();
         if(l != null)
             missing_acks.addAll(l);
         all_acks_received.reset();
@@ -30,6 +44,7 @@ public class AckCollector {
 
     public void ack(Object member) {
         missing_acks.remove(member);
+        received_acks.add(member);
         if(missing_acks.size() == 0)
             all_acks_received.setResult(Boolean.TRUE);
     }
@@ -49,6 +64,6 @@ public class AckCollector {
     }
 
     public String toString() {
-        return missing_acks.toString();
+        return "missing=" + missing_acks + ", received=" + received_acks;
     }
 }
