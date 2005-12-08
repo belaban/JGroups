8f2d309738a7ceea5c43feb5d30c7e3d3ff950ad
Bela Ban <belaban@yahoo.com>
2005-12-08 09:51:56 +0000

    added ViewChecker

--- tests/junit/org/jgroups/tests/MergeTest.java
+++ tests/junit/org/jgroups/tests/MergeTest.java
@@ -4,15 +4,15 @@ package org.jgroups.tests;
 
 import junit.framework.TestCase;
 import org.jgroups.JChannel;
+import org.jgroups.ReceiverAdapter;
 import org.jgroups.View;
-import org.jgroups.util.Util;
 import org.jgroups.stack.GossipRouter;
 
 
 /**
  * Tests merging
  * @author Bela Ban
- * @version $Id: MergeTest.java,v 1.4 2005/12/07 12:32:29 belaban Exp $
+ * @version $Id: MergeTest.java,v 1.5 2005/12/08 09:51:56 belaban Exp $
  */
 public class MergeTest extends TestCase {
     JChannel     channel;
@@ -25,8 +25,7 @@ public class MergeTest extends TestCase {
     String props="TUNNEL(router_port=" + router_port + ";router_host=" +bind_addr+ ";loopback=true):" +
             "PING(timeout=1000;num_initial_members=2;gossip_host=" +bind_addr+";gossip_port=" + router_port + "):" +
             "MERGE2(min_interval=5000;max_interval=8000):" +
-            "FD(timeout=2000;max_tries=2;shun=false):" +
-            "VERIFY_SUSPECT(timeout=1500):" +
+            "FD(timeout=1000;max_tries=2;shun=false):" +
             "pbcast.NAKACK(gc_lag=50;retransmit_timeout=600,1200,2400,4800):" +
             "UNICAST(timeout=600,1200,2400):" +
             "pbcast.STABLE(desired_avg_gossip=20000):" +
@@ -50,9 +49,12 @@ public class MergeTest extends TestCase {
     }
 
     public void testPartitionAndSubsequentMerge() throws Exception {
+        ViewChecker checker=new ViewChecker();
         ch1=new JChannel(props);
+        ch1.setReceiver(checker);
         ch1.connect("demo");
         ch2=new JChannel(props);
+        ch2.setReceiver(checker);
         ch2.connect("demo");
         View v=ch2.getView();
         System.out.println("view is " + v);
@@ -62,7 +64,9 @@ public class MergeTest extends TestCase {
         stopRouter();
 
         System.out.println("sleeping for 10 secs");
-        Util.sleep(10000);
+        // Util.sleep(10000);
+        checker.waitForNViews(2, 10000);
+
         v=ch1.getView();
         System.out.println("-- ch1.view: " + v);
 
@@ -74,7 +78,8 @@ public class MergeTest extends TestCase {
         startRouter();
 
         System.out.println("sleeping for 30 secs");
-        Util.sleep(30000);
+        // Util.sleep(30000);
+        checker.waitForNViews(2, 30000);
 
         v=ch1.getView();
         System.out.println("-- ch1.view: " + v);
@@ -86,6 +91,8 @@ public class MergeTest extends TestCase {
     }
 
 
+
+
     private void startRouter() throws Exception {
         router=new GossipRouter(router_port, bind_addr);
         router.start();
@@ -95,6 +102,34 @@ public class MergeTest extends TestCase {
         router.stop();
     }
 
+    private class ViewChecker extends ReceiverAdapter {
+        final Object mutex=new Object();
+        int          count=0;
+
+        public void viewAccepted(View new_view) {
+            synchronized(mutex) {
+                count++;
+                System.out.println("-- view: " + new_view);
+                mutex.notifyAll();
+            }
+        }
+
+
+        public void waitForNViews(int n, long timeout) {
+            long sleep_time=timeout, curr, start;
+            synchronized(mutex) {
+                count=0;
+                start=System.currentTimeMillis();
+                while(count < n) {
+                    try {mutex.wait(sleep_time);} catch(InterruptedException e) {}
+                    curr=System.currentTimeMillis();
+                    sleep_time-=(curr - start);
+                    if(sleep_time <= 0)
+                        break;
+                }
+            }
+        }
+    }
 
 
     public static void main(String[] args) {
