7ae7ca967a2a5b438b1ed7bdea7ffea078c3d174
Bela Ban <belaban@yahoo.com>
2005-12-09 13:12:10 +0000

    changed version to 2.2.9

--- src/org/jgroups/Version.java
+++ src/org/jgroups/Version.java
@@ -8,9 +8,9 @@ package org.jgroups;
  */
 public class Version {
 	
-    public static final String description="2.2.9 rc1";
+    public static final String description="2.2.9";
     public static final short version=229;
-    public static final String cvs="$Id: Version.java,v 1.25 2005/11/14 13:36:01 belaban Exp $";
+    public static final String cvs="$Id: Version.java,v 1.26 2005/12/09 13:12:10 belaban Exp $";
 
     /**
      * Prints the value of the description and cvs fields to System.out.
