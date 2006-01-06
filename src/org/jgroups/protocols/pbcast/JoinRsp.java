45a367c3d7134717cd0f964a85df592262a71285
belaban <belaban>
2006-01-06 12:18:27 +0000

    added fail_string to JoinRsp, plus unit test to SizeTest

--- src/org/jgroups/protocols/pbcast/JoinRsp.java
+++ src/org/jgroups/protocols/pbcast/JoinRsp.java
@@ -1,4 +1,4 @@
-// $Id: JoinRsp.java,v 1.8 2005/12/08 13:13:07 belaban Exp $
+// $Id: JoinRsp.java,v 1.9 2006/01/06 12:18:27 belaban Exp $
 
 package org.jgroups.protocols.pbcast;
 
@@ -17,7 +17,11 @@ import java.io.DataInputStream;
 public class JoinRsp implements Serializable, Streamable {
     View view=null;
     Digest digest=null;
-    private static final long serialVersionUID = 2949193438640587597L;
+    /** only set if JOIN failed, e.g. in AUTH */
+    String fail_reason=null;
+    static final long serialVersionUID = -212620440767943314L;
+
+
 
     public JoinRsp() {
 
@@ -28,6 +32,9 @@ public class JoinRsp implements Serializable, Streamable {
         digest=d;
     }
 
+    public JoinRsp(String fail_reason) {
+        this.fail_reason=fail_reason;
+    }
 
     View getView() {
         return view;
@@ -37,14 +44,25 @@ public class JoinRsp implements Serializable, Streamable {
         return digest;
     }
 
+    public String getFailReason() {
+        return fail_reason;
+    }
+
+    public void setFailReason(String r) {
+        fail_reason=r;
+    }
+
+
     public void writeTo(DataOutputStream out) throws IOException {
         Util.writeStreamable(view, out);
         Util.writeStreamable(digest, out);
+        Util.writeString(fail_reason, out);
     }
 
     public void readFrom(DataInputStream in) throws IOException, IllegalAccessException, InstantiationException {
         view=(View)Util.readStreamable(View.class, in);
         digest=(Digest)Util.readStreamable(Digest.class, in);
+        fail_reason=Util.readString(in);
     }
 
     public int serializedSize() {
@@ -53,6 +71,10 @@ public class JoinRsp implements Serializable, Streamable {
             retval+=view.serializedSize();
         if(digest != null)
             retval+=digest.serializedSize();
+
+        retval+=Global.BYTE_SIZE; // presence byte for fail_reason
+        if(fail_reason != null)
+            retval+=fail_reason.length() +2;
         return retval;
     }
 
@@ -68,6 +90,8 @@ public class JoinRsp implements Serializable, Streamable {
             sb.append("<null>");
         else
             sb.append(digest);
+        if(fail_reason != null)
+            sb.append(", fail reason: ").append(fail_reason);
         return sb.toString();
     }
 }
