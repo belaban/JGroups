4cdb59b1ede80bce0b6e65c0845d7e0d8599c14c
Bela Ban <belaban@yahoo.com>
2006-03-17 09:04:45 +0000

    removed single constructor

--- src/org/jgroups/GetStateEvent.java
+++ src/org/jgroups/GetStateEvent.java
@@ -1,4 +1,4 @@
-// $Id: GetStateEvent.java,v 1.5 2006/03/16 15:55:06 belaban Exp $
+// $Id: GetStateEvent.java,v 1.6 2006/03/17 09:04:55 belaban Exp $
 
 package org.jgroups;
 
@@ -10,7 +10,10 @@ public class GetStateEvent {
     Object requestor=null;
     String state_id=null;
 
-    public GetStateEvent(Object requestor) {this.requestor=requestor;}
+    public GetStateEvent(Object requestor, String state_id) {
+        this.requestor=requestor;
+        this.state_id=state_id;
+    }
 
     public Object getRequestor() {return requestor;}
 
--- src/org/jgroups/SetStateEvent.java
+++ src/org/jgroups/SetStateEvent.java
@@ -1,4 +1,4 @@
-// $Id: SetStateEvent.java,v 1.5 2006/03/16 15:54:49 belaban Exp $
+// $Id: SetStateEvent.java,v 1.6 2006/03/17 09:04:45 belaban Exp $
 
 package org.jgroups;
 
@@ -11,16 +11,11 @@ package org.jgroups;
  * Encapsulates a state returned by <code>Channel.receive()</code>, as requested by
  * <code>Channel.getState(s)</code> previously.
  * @author Bela Ban
- * @version $Id: SetStateEvent.java,v 1.5 2006/03/16 15:54:49 belaban Exp $
+ * @version $Id: SetStateEvent.java,v 1.6 2006/03/17 09:04:45 belaban Exp $
  */
 public class SetStateEvent {
     byte[]     state=null;
     String     state_id=null;
-
-
-    public SetStateEvent(byte[] state) {
-        this.state=state;
-    }
 
     public SetStateEvent(byte[] state, String state_id) {
         this.state=state;
