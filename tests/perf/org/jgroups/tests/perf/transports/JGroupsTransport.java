7753a9f57341459e237d3d87fb81f1b02cae1da1
belaban <belaban>
2006-06-01 09:25:15 +0000

    switched to use Util.getMBeanServer()

--- tests/perf/org/jgroups/tests/perf/transports/JGroupsTransport.java
+++ tests/perf/org/jgroups/tests/perf/transports/JGroupsTransport.java
@@ -1,21 +1,21 @@
 package org.jgroups.tests.perf.transports;
 
-import org.jgroups.Address;
-import org.jgroups.JChannel;
-import org.jgroups.Message;
+import org.jgroups.*;
+import org.jgroups.util.Util;
 import org.jgroups.jmx.JmxConfigurator;
 import org.jgroups.tests.perf.Receiver;
 import org.jgroups.tests.perf.Transport;
-import org.jgroups.util.Util;
 
 import javax.management.MBeanServer;
+import javax.management.MBeanServerFactory;
+import java.util.ArrayList;
 import java.util.Map;
 import java.util.Properties;
 
 /**
  * @author Bela Ban Jan 22
  * @author 2004
- * @version $Id: JGroupsTransport.java,v 1.13 2006/06/01 09:26:26 belaban Exp $
+ * @version $Id: JGroupsTransport.java,v 1.12 2006/06/01 09:25:36 belaban Exp $
  */
 public class JGroupsTransport extends org.jgroups.ReceiverAdapter implements Transport  {
     Properties config=null;
