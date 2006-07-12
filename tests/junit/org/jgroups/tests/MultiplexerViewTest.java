0be4924877847b059b08e481daba39d07211a159
belaban <belaban>
2006-07-12 14:09:58 +0000

    first ci

---
+++ tests/junit/org/jgroups/tests/MultiplexerViewTest.java
@@ -0,0 +1,88 @@
+package org.jgroups.tests;
+
+import junit.framework.TestCase;
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import org.jgroups.*;
+import org.jgroups.util.Util;
+
+import java.util.*;
+
+/**
+ * Test the multiplexer functionality provided by JChannelFactory, especially the service views and cluster views
+ * @author Bela Ban
+ * @version $Id: MultiplexerViewTest.java,v 1.1 2006/07/12 14:09:58 belaban Exp $
+ */
+public class MultiplexerViewTest extends TestCase {
+    private Channel c1, c2;
+    static final String CFG="stacks.xml";
+    static final String STACK_NAME="fc-fast-minimalthreads";
+    JChannelFactory factory, factory2;
+
+    public MultiplexerViewTest(String name) {
+        super(name);
+    }
+
+
+    public void setUp() throws Exception {
+        super.setUp();
+        factory=new JChannelFactory();
+        factory.setMultiplexerConfig(MultiplexerViewTest.CFG);
+
+        factory2=new JChannelFactory();
+        factory2.setMultiplexerConfig(MultiplexerViewTest.CFG);
+    }
+
+    protected void tearDown() throws Exception {
+        super.tearDown();
+        if(c2 != null)
+            c2.close();
+        if(c1 != null)
+            c1.close();
+        factory.destroy();
+        factory2.destroy();
+    }
+
+    public void testBasicLifeCycle() throws Exception {
+        c1=factory.createMultiplexerChannel(STACK_NAME, "service-1");
+        System.out.println("c1: " + c1);
+        assertTrue(c1.isOpen());
+        assertFalse(c1.isConnected());
+        View v=c1.getView();
+        assertNull(v);
+        Address local_addr=c1.getLocalAddress();
+        assertNull(local_addr);
+
+        c1.connect("bla");
+        assertTrue(c1.isConnected());
+        local_addr=c1.getLocalAddress();
+        assertNotNull(local_addr);
+        v=c1.getView();
+        assertNotNull(v);
+        assertEquals(1, v.size());
+
+        c1.disconnect();
+        assertFalse(c1.isConnected());
+        assertTrue(c1.isOpen());
+        local_addr=c1.getLocalAddress();
+        assertNull(local_addr);
+        c1.close();
+        assertFalse(c1.isOpen());
+    }
+
+
+    public void testOneChannelOneService() throws Exception {
+        c1=factory.createMultiplexerChannel(STACK_NAME, "service-1");
+
+    }
+
+
+
+    public static Test suite() {
+        return new TestSuite(MultiplexerViewTest.class);
+    }
+
+    public static void main(String[] args) {
+        junit.textui.TestRunner.run(MultiplexerViewTest.suite());
+    }
+}
