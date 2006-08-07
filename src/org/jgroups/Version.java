4000f5179a7a0dc2eb639620fe8eca5006c2fdf0
Bela Ban <belaban@yahoo.com>
2006-08-07 10:22:21 +0000

    ns

--- src/org/jgroups/Version.java
+++ src/org/jgroups/Version.java
@@ -8,9 +8,9 @@ package org.jgroups;
  */
 public class Version {
 	
-    public static final String description="2.3";
-    public static final short version=23;
-    public static final String cvs="$Id: Version.java,v 1.35 2006/06/11 19:15:23 belaban Exp $";
+    public static final String description="2.4beta";
+    public static final short version=241;
+    public static final String cvs="$Id: Version.java,v 1.36 2006/08/07 10:22:23 belaban Exp $";
 
     /**
      * Prints the value of the description and cvs fields to System.out.
