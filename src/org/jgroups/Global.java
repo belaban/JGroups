4835e24d11c8b8f129f1777cb424176f8f30a117
belaban <belaban>
2006-09-11 13:28:20 +0000

    added support for jgroups.tcpping.initial_hosts system property

--- src/org/jgroups/Global.java
+++ src/org/jgroups/Global.java
@@ -4,7 +4,7 @@ package org.jgroups;
  * Globals used by JGroups packages.
  * 
  * @author Bela Ban Mar 29, 2004
- * @version $Id: Global.java,v 1.6 2006/09/05 08:38:02 belaban Exp $
+ * @version $Id: Global.java,v 1.7 2006/09/11 13:28:20 belaban Exp $
  */
 public class Global {
     /** Allows for conditional compilation; e.g., if(log.isTraceEnabled()) if(log.isInfoEnabled()) log.info(...) would be removed from the code
@@ -32,5 +32,7 @@ public class Global {
     public static final String BIND_ADDR_OLD="bind.address";
     public static final String IGNORE_BIND_ADDRESS_PROPERTY="jgroups.ignore.bind_addr";
     public static final String IGNORE_BIND_ADDRESS_PROPERTY_OLD="ignore.bind.address";
-    public static final String JBOSS_MARSHALLING_COMPAT="jboss.marshalling.compatible";
+    public static final String MARSHALLING_COMPAT="jgroups.marshalling.compatible";
+    public static final String TCPPING_INITIAL_HOSTS="jgroups.tcpping.initial_hosts";
+
 }
