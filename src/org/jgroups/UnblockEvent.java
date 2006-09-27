3cc019bada6e166c8be7a16afa701c3562ab2cc6
Bela Ban <belaban@yahoo.com>
2006-09-27 12:33:06 +0000

    ns

---
+++ src/org/jgroups/UnblockEvent.java
@@ -0,0 +1,10 @@
+package org.jgroups;
+
+/**
+ * Trivial object that represents a block event.
+ * @author Bela Ban
+ * @version $Id: UnblockEvent.java,v 1.1 2006/09/27 12:33:06 belaban Exp $
+ */
+public class UnblockEvent {
+    public String toString() {return "UnblockEvent";}
+}
