ecb4cb7d1000ea522e1d8ad2edc842872d5f6fba
belaban <belaban>
2006-10-02 09:27:58 +0000

    added support for flushing if we transfer multiple states at the same time

--- src/org/jgroups/mux/Multiplexer.java
+++ src/org/jgroups/mux/Multiplexer.java
@@ -15,7 +15,7 @@ import java.util.*;
  * message is removed and the MuxChannel corresponding to the header's service ID is retrieved from the map,
  * and MuxChannel.up() is called with the message.
  * @author Bela Ban
- * @version $Id: Multiplexer.java,v 1.24 2006/09/28 15:13:31 belaban Exp $
+ * @version $Id: Multiplexer.java,v 1.25 2006/10/02 09:27:58 belaban Exp $
  */
 public class Multiplexer implements UpHandler {
     /** Map<String,MuxChannel>. Maintains the mapping between service IDs and their associated MuxChannels */
@@ -752,13 +752,13 @@ public class Multiplexer implements UpHandler {
 
 
     /** Tell the underlying channel to start the flush protocol, this will be handled by FLUSH */
-    void startFlush() {
-
+    private void startFlush() {
+        channel.down(new Event(Event.SUSPEND));
     }
 
     /** Tell the underlying channel to stop the flush, and resume message sending. This will be handled by FLUSH */
-    void stopFlush() {
-
+    private void stopFlush() {
+        channel.down(new Event(Event.RESUME));
     }
 
 
