c913fb3594f80418525272b83bde70fed2c5d80a
belaban <belaban>
2006-10-30 12:28:14 +0000

    removed threads

--- src/org/jgroups/demos/DrawMultiplexer.java
+++ src/org/jgroups/demos/DrawMultiplexer.java
@@ -5,7 +5,7 @@ import org.jgroups.JChannelFactory;
 
 /**
  * @author Bela Ban
- * @version $Id: DrawMultiplexer.java,v 1.4 2006/10/04 13:34:20 belaban Exp $
+ * @version $Id: DrawMultiplexer.java,v 1.5 2006/10/30 12:28:14 belaban Exp $
  */
 public class DrawMultiplexer {
     JChannelFactory factory;
@@ -29,56 +29,24 @@ public class DrawMultiplexer {
         new DrawMultiplexer().start(props, stack_name);
     }
 
+
     private void start(String props, String stack_name) throws Exception {
         factory=new JChannelFactory();
         factory.setMultiplexerConfig(props);
 
         final Channel ch1, ch2, ch3;
         ch1=factory.createMultiplexerChannel(stack_name, "id-1");
+        Draw draw1=new Draw(ch1);
         ch1.connect("bela");
 
         ch2=factory.createMultiplexerChannel(stack_name, "id-2");
+        Draw draw2=new Draw(ch2);
         ch2.connect("ban");
 
-        // ch3=factory.createMultiplexerChannel("tcp", "TCP-based");
-        // ch3.connect("bla");
 
-        Thread t1=new Thread() {
-            public void run() {
-                try {
-                    Draw draw1=new Draw(ch1);
-                    draw1.go();
-                    draw1.stop();
-                }
-                catch(Throwable t) {
-                    t.printStackTrace();
-                }
-            }
-        };
+        draw1.go();
 
 
-
-        Thread t2=new Thread() {
-            public void run() {
-                try {
-                    Draw draw2=new Draw(ch2);
-                    draw2.go();
-                    draw2.stop();
-                }
-                catch(Throwable t) {
-                    t.printStackTrace();
-                }
-            }
-        };
-
-        t1.start();
-        t2.start();
-
-        t1.join();
-        t2.join();
-
-        ch2.close();
-        ch1.close();
-        System.exit(0);
+        draw2.go();
     }
 }
