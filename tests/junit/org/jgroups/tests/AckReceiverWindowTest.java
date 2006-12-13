eb4bc94c130f42c91a5d3d5765e2068f7f94efb8
belaban <belaban>
2006-12-13 11:43:31 +0000

    new test

--- tests/junit/org/jgroups/tests/AckReceiverWindowTest.java
+++ tests/junit/org/jgroups/tests/AckReceiverWindowTest.java
@@ -10,7 +10,7 @@ import org.jgroups.stack.AckReceiverWindow;
 
 /**
  * @author Bela Ban
- * @version $Id: AckReceiverWindowTest.java,v 1.2 2005/08/21 20:37:50 belaban Exp $
+ * @version $Id: AckReceiverWindowTest.java,v 1.3 2006/12/13 11:43:31 belaban Exp $
  */
 public class AckReceiverWindowTest extends TestCase {
     AckReceiverWindow win;
@@ -64,14 +64,36 @@ public class AckReceiverWindowTest extends TestCase {
         assertNull(m);
     }
 
+
+    public void testDuplicates() {
+        boolean rc;
+        win=new AckReceiverWindow(2);
+        assertEquals(0, win.size());
+        rc=win.add(9, msg());
+        assertTrue(rc);
+        rc=win.add(1, msg());
+        assertFalse(rc);
+        rc=win.add(2, msg());
+        assertTrue(rc);
+        rc=win.add(2, msg());
+        assertFalse(rc);
+        rc=win.add(0, msg());
+        assertFalse(rc);
+        rc=win.add(3, msg());
+        assertTrue(rc);
+        rc=win.add(4, msg());
+        assertTrue(rc);
+        rc=win.add(4, msg());
+        assertFalse(rc);
+    }
+
     private Message msg() {
         return new Message();
     }
 
 
     public static Test suite() {
-        TestSuite s=new TestSuite(AckReceiverWindowTest.class);
-        return s;
+        return new TestSuite(AckReceiverWindowTest.class);
     }
 
 
