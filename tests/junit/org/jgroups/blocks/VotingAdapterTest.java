cd8a0089291d8d74a1cad85f503a6e4ecba5accd
belaban <belaban>
2007-01-16 09:31:43 +0000

    removed up_thread/down_thread=false

--- tests/junit/org/jgroups/blocks/VotingAdapterTest.java
+++ tests/junit/org/jgroups/blocks/VotingAdapterTest.java
@@ -7,24 +7,23 @@ import org.jgroups.JChannel;
 
 public class VotingAdapterTest extends TestCase {
 
-    public static final String SERVER_PROTOCOL_STACK = ""
-            + "UDP(mcast_addr=228.3.11.76;mcast_port=12345;ip_ttl=1;"
-            + "mcast_send_buf_size=150000;mcast_recv_buf_size=80000;down_thread=false)"
+    public static final String SERVER_PROTOCOL_STACK =
+            "UDP(mcast_addr=228.3.11.76;mcast_port=12345;ip_ttl=1;"
+                    + "mcast_send_buf_size=150000;mcast_recv_buf_size=80000)"
 //        + "JMS(topicName=topic/testTopic;cf=UILConnectionFactory;"
 //        + "jndiCtx=org.jnp.interfaces.NamingContextFactory;"
 //        + "providerURL=localhost;ttl=10000)"
-            + ":PING(timeout=500;num_initial_members=1;down_thread=false;up_thread=false)"
-            + ":FD(down_thread=false;up_thread=false)"
-            + ":VERIFY_SUSPECT(timeout=1500;down_thread=false;up_thread=false)"
-
-            + ":pbcast.NAKACK(gc_lag=50;retransmit_timeout=300,600,1200,2400,4800;down_thread=false)"
-            + ":FRAG(frag_size=4096;down_thread=false)"
-            + ":UNICAST(timeout=5000)"
-            + ":pbcast.STABLE(desired_avg_gossip=200;down_thread=false;up_thread=false)"
-            + ":pbcast.GMS(join_timeout=5000;join_retry_timeout=1000;"
-            +     "shun=false;print_local_addr=false;down_thread=true;up_thread=true)"
-            //+ ":SPEED_LIMIT(down_queue_limit=10)"
-            + ":pbcast.STATE_TRANSFER(down_thread=false)"
+                    + ":PING(timeout=500;num_initial_members=1)"
+                    + ":FD"
+                    + ":VERIFY_SUSPECT(timeout=1500)"
+                    + ":pbcast.NAKACK(gc_lag=50;retransmit_timeout=300,600,1200,2400,4800)"
+                    + ":FRAG(frag_size=4096)"
+                    + ":UNICAST(timeout=5000)"
+                    + ":pbcast.STABLE(desired_avg_gossip=200)"
+                    + ":pbcast.GMS(join_timeout=5000;join_retry_timeout=1000;"
+                    +     "shun=false;print_local_addr=false)"
+                    //+ ":SPEED_LIMIT(down_queue_limit=10)"
+                    + ":pbcast.STATE_TRANSFER"
             ;
 
 
@@ -48,52 +47,51 @@ public class VotingAdapterTest extends TestCase {
     protected TestVoteChannelListener listener3;
     protected TestVoteChannelListener listener4;
 
-    protected static
-            boolean logConfigured;
+    protected static boolean logConfigured=false;
 
     public void setUp() throws Exception {
-        
+        super.setUp();
+        listener1=new TestVoteChannelListener(true);
+        listener2=new TestVoteChannelListener(true);
+        listener3=new TestVoteChannelListener(false);
+        listener4=new TestVoteChannelListener(false);
 
+        channel1=new JChannel(SERVER_PROTOCOL_STACK);
+        adapter1=new VotingAdapter(channel1);
 
-        listener1 = new TestVoteChannelListener(true);
-        listener2 = new TestVoteChannelListener(true);
-        listener3 = new TestVoteChannelListener(false);
-        listener4 = new TestVoteChannelListener(false);
-
-        channel1 = new JChannel(SERVER_PROTOCOL_STACK);
-        adapter1 = new VotingAdapter(channel1);
-        
         channel1.connect("voting");
 
         // give some time for the channel to become a coordinator
-		try {
-			Thread.sleep(1000);
-		} catch(Exception ex) {
-		}
-        
-        channel2 = new JChannel(SERVER_PROTOCOL_STACK);
-        adapter2 = new VotingAdapter(channel2);
-        
-        channel2.connect("voting");
-        
         try {
             Thread.sleep(1000);
         }
-        catch (InterruptedException ex) {
+        catch(Exception ex) {
+        }
+
+        channel2=new JChannel(SERVER_PROTOCOL_STACK);
+        adapter2=new VotingAdapter(channel2);
+
+        channel2.connect("voting");
+
+        try {
+            Thread.sleep(1000);
+        }
+        catch(InterruptedException ex) {
         }
     }
 
     public void tearDown() throws Exception {
         channel2.close();
-        
+
         try {
             Thread.sleep(1000);
         }
-        catch (InterruptedException ex) {
+        catch(InterruptedException ex) {
         }
-        
-        
+
+
         channel1.close();
+        super.tearDown();
     }
 
     public void testVoteAll() throws Exception {
