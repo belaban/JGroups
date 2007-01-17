451678dcef93236dd92a76778580a06a79b40f5d
Bela Ban <belaban@yahoo.com>
2007-01-17 14:50:59 +0000

    removed unneeded methods

--- src/org/jgroups/stack/Interval.java
+++ src/org/jgroups/stack/Interval.java
@@ -1,36 +1,40 @@
-// $Id: Interval.java,v 1.1 2003/09/09 01:24:12 belaban Exp $
+// $Id: Interval.java,v 1.2 2007/01/17 14:50:59 belaban Exp $
 
 package org.jgroups.stack;
+
+import org.jgroups.annotations.Immutable;
+import org.jgroups.annotations.GuardedBy;
 
 
 /**
  * Manages retransmission timeouts. Always returns the next timeout, until the last timeout in the
- * array is reached. Returns the last timeout from then on, until reset() is called.
+ * array is reached. Returns the last timeout from then on.
  * @author John Giorgiadis
  * @author Bela Ban
  */
+@Immutable
 public class Interval {
-    private int    next=0;
-    private long[] interval=null;
+    private int          next=0;
+    private final long[] interval;
 
     public Interval(long[] interval) {
-	if (interval.length == 0)
-	    throw new IllegalArgumentException("Interval()");
-	this.interval=interval;
+        if (interval.length == 0)
+            throw new IllegalArgumentException("Interval()");
+        this.interval=interval;
     }
 
-    public long first() { return interval[0]; }
-    
+
     /** @return the next interval */
-    public synchronized long next() {
-	if (next >= interval.length)
-	    return(interval[interval.length-1]);
-	else
-	    return(interval[next++]);
+    @GuardedBy("interval")
+    public long next() {
+        synchronized(interval) {
+            if (next >= interval.length)
+                return(interval[interval.length-1]);
+            else
+                return(interval[next++]);
+        }
     }
     
-    public long[] getInterval() { return interval; }
 
-    public synchronized void reset() { next = 0; }
 }
 
