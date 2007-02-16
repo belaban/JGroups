98e1ebfbd925d39b595c47afa520fa40a24b7088
Bela Ban <belaban@yahoo.com>
2007-02-16 09:12:11 +0000

    no need to marshal the exception as the result is returned locally (http://jira.jboss.com/jira/browse/JGRP-354)

--- src/org/jgroups/blocks/RequestCorrelator.java
+++ src/org/jgroups/blocks/RequestCorrelator.java
@@ -1,4 +1,4 @@
-// $Id: RequestCorrelator.java,v 1.30.2.2 2006/12/09 22:55:45 belaban Exp $
+// $Id: RequestCorrelator.java,v 1.30.2.3 2007/02/16 09:12:11 belaban Exp $
 
 package org.jgroups.blocks;
 
@@ -547,12 +547,7 @@ public class RequestCorrelator {
                     }
                     catch(Exception e) {
                         log.error("failed unmarshalling buffer into return value", e);
-                        try {
-                            retval=marshaller != null? marshaller.objectToByteBuffer(e) : Util.objectToByteBuffer(e);
-                        }
-                        catch(Exception e1) {
-                            log.error("failed marshalling exception " + e1 + " into buffer", e1);
-                        }
+                        retval=e;
                     }
                     coll.receiveResponse(retval, sender);
                 }
