8fb7aa5a53292a97a1f83ba377a8c23f3a4174d5
Bela Ban <belaban@yahoo.com>
2007-02-27 14:49:40 +0000

    added copyright notice

--- src/org/jgroups/annotations/Immutable.java
+++ src/org/jgroups/annotations/Immutable.java
@@ -6,10 +6,15 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 /**
+ * Copyright (c) 2005 Brian Goetz and Tim Peierls
+ * Released under the Creative Commons Attribution License
+ * (http://creativecommons.org/licenses/by/2.5)
+ * Official home: http://www.jcip.net
+ * 
  * Adopted from Java Concurrency in Practice. This annotation defines an immutable class, ie. a class whose
  * instances cannot be modified after creation
  * @author Bela Ban
- * @version $Id: Immutable.java,v 1.1 2007/01/17 14:49:12 belaban Exp $
+ * @version $Id: Immutable.java,v 1.2 2007/02/27 14:49:40 belaban Exp $
  */
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.SOURCE)
