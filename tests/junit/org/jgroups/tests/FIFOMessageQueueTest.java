59f2c2b945bfd2f67669f3349b1838c52307a99e
belaban <belaban>
2007-02-27 17:12:30 +0000

    ns

---
+++ tests/junit/org/jgroups/tests/FIFOMessageQueueTest.java
@@ -0,0 +1,155 @@
+package org.jgroups.tests;
+
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import org.jgroups.util.FIFOMessageQueue;
+import org.jgroups.util.Util;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+/**
+ * @author Bela Ban
+ * @version $Id: FIFOMessageQueueTest.java,v 1.1 2007/02/27 17:12:47 belaban Exp $
+ */
+public class FIFOMessageQueueTest extends TestCase {
+    FIFOMessageQueue<String,Integer> queue;
+    String s1="s1", s2="s2", s3="s3";
+
+    public void setUp() throws Exception {
+        super.setUp();
+        queue=new FIFOMessageQueue<String,Integer>();
+    }
+
+    public void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+
+    public void testSimplePutAndTake() throws InterruptedException {
+        queue.put(s1, 1);
+        assertEquals(1, queue.size());
+        int ret=queue.take();
+        assertEquals(1, ret);
+        assertEquals(0, queue.size());
+    }
+
+    public void testMultiplePutsAndTakes() throws InterruptedException {
+        for(int i=1; i <= 5; i++)
+            queue.put(s1, i);
+        System.out.println("queue is " + queue);
+        assertEquals(5, queue.size());
+        for(int i=1; i <= 5; i++) {
+            int ret=queue.take();
+            assertEquals(i, ret);
+            assertEquals(5-i, queue.size());
+            queue.done(s1);
+        }
+        assertEquals(0, queue.size());
+    }
+
+
+    public void testOrdering() throws InterruptedException {
+        for(int i=1; i <= 3; i++)
+            queue.put(s1, i);
+        assertEquals(3, queue.size());
+
+        int ret=queue.take();
+        assertEquals(1, ret);
+        assertEquals(2, queue.size());
+
+        queue.done(s1);
+        queue.put(s1, 4);
+        queue.put(s1, 5);
+        System.out.println("queue: " + queue);
+
+        for(int i=2; i <= 5; i++) {
+            ret=queue.take();
+            assertEquals(i, ret);
+            assertEquals(5-i, queue.size());
+            queue.done(s1);
+        }
+        assertEquals(0, queue.size());
+    }
+
+
+    public void testOrderingMultipleThreads() throws BrokenBarrierException, InterruptedException {
+        CyclicBarrier barrier=new CyclicBarrier(4);
+        int NUM=5;
+        Producer p1=new Producer(queue, "s1",  1, NUM, barrier);
+        Producer p2=new Producer(queue, "s2", 10, NUM, barrier);
+        Producer p3=new Producer(queue, "s3", 20, NUM, barrier);
+
+        p1.start();
+        p2.start();
+        p3.start();
+        Util.sleep(100);
+        barrier.await(); // starts all 3 threads
+
+        p1.join();
+        p2.join();
+        p3.join();
+        System.out.println("queue: " + queue);
+        assertEquals(NUM * 3, queue.size());
+    }
+
+    public void testOrderingMultipleThreadsWithTakes() throws BrokenBarrierException, InterruptedException {
+        testOrderingMultipleThreads();
+        int ret;
+
+        int size=queue.size();
+        for(int i=0; i < size; i++) {
+            ret=queue.take();
+            System.out.print(ret + " ");
+            queue.done("s1");
+            queue.done("s2");
+            queue.done("s3");
+        }
+    }
+
+
+
+    private static class Producer extends Thread {
+        private FIFOMessageQueue<String,Integer> queue;
+        private String key;
+        private int num_msgs;
+        private CyclicBarrier barrier;
+        private int start_num;
+
+        private Producer(FIFOMessageQueue<String,Integer> queue, String key, int start_num, int num_msgs, CyclicBarrier barrier) {
+            this.queue=queue;
+            this.key=key;
+            this.start_num=start_num;
+            this.num_msgs=num_msgs;
+            this.barrier=barrier;
+        }
+
+
+        public void run() {
+            try {
+                barrier.await();
+            }
+            catch(Exception e) {
+                e.printStackTrace();
+            }
+            for(int i=start_num; i <= num_msgs+start_num-1; i++) {
+                try {
+                    Util.sleepRandom(50);
+                    queue.put(key, i);
+                }
+                catch(InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+
+    public static junit.framework.Test suite() {
+        return new TestSuite(FIFOMessageQueueTest.class);
+    }
+
+    public static void main(String[] args) {
+        junit.textui.TestRunner.run(suite());
+    }
+}
