23e6d6272eda901afae22ed77052738780341af0
Bela Ban <belaban@yahoo.com>
2007-03-08 16:49:40 +0000

    switched Version to use major, minor and micro numbers (http://jira.jboss.com/jira/browse/JGRP-353)

--- src/org/jgroups/Version.java
+++ src/org/jgroups/Version.java
@@ -15,7 +15,7 @@ import org.jgroups.annotations.Immutable;
  * Z = 0-63 for micro versions
  *
  * @author Bela Ban
- * @version $Id: Version.java,v 1.48 2007/03/08 16:28:08 belaban Exp $
+ * @version $Id: Version.java,v 1.49 2007/03/08 16:49:40 belaban Exp $
  * Holds version information for JGroups.
  */
 @Immutable
@@ -27,7 +27,7 @@ public class Version {
 
     public static final short version=encode(major, minor, micro);
     public static final String string_version=print(version);
-    public static final String cvs="$Id: Version.java,v 1.48 2007/03/08 16:28:08 belaban Exp $";
+    public static final String cvs="$Id: Version.java,v 1.49 2007/03/08 16:49:40 belaban Exp $";
 
     private static final int MAJOR_SHIFT = 11;
     private static final int MINOR_SHIFT = 6;
