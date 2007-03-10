3a3c8c8607eb79ec01a785158f7b2ed348d89868
Bela Ban <belaban@yahoo.com>
2007-03-10 10:19:29 +0000

    new unit test

---
+++ tests/junit/org/jgroups/tests/MessageDispatcherUnitTest.java
@@ -0,0 +1,180 @@
+package org.jgroups.tests;
+
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import org.jgroups.Channel;
+import org.jgroups.ChannelException;
+import org.jgroups.JChannel;
+import org.jgroups.Message;
+import org.jgroups.blocks.GroupRequest;
+import org.jgroups.blocks.MessageDispatcher;
+import org.jgroups.blocks.RequestHandler;
+import org.jgroups.util.Rsp;
+import org.jgroups.util.RspList;
+
+
+/**
+ * Tests return values from MessageDispatcher.castMessage()
+ * @author Bela Ban
+ * @version $Id: MessageDispatcherUnitTest.java,v 1.1 2007/03/10 10:19:29 belaban Exp $
+ */
+public class MessageDispatcherUnitTest extends TestCase {
+    MessageDispatcher disp, disp2;
+    Channel ch, ch2;
+    static final String props="udp.xml";
+
+
+    public MessageDispatcherUnitTest(String name) {
+        super(name);
+    }
+
+
+    protected void setUp() throws Exception {
+        super.setUp();
+        ch=new JChannel(props);
+        disp=new MessageDispatcher(ch, null, null, null);
+        ch.connect("x");
+    }
+
+
+    protected void tearDown() throws Exception {
+        super.tearDown();
+        disp.stop();
+        ch.close();
+        if(ch2 != null) {
+            disp2.stop();
+            ch2.close();
+        }
+    }
+
+
+    public void testNullMessageToSelf() {
+        MyHandler handler=new MyHandler(null);
+        disp.setRequestHandler(handler);
+        RspList rsps=disp.castMessage(null, new Message(), GroupRequest.GET_ALL, 0);
+        System.out.println("rsps:\n" + rsps);
+        assertNotNull(rsps);
+        assertEquals(1, rsps.size());
+        Object obj=rsps.getFirst();
+        assertNull(obj);
+    }
+
+
+    public void test200ByteMessageToSelf() {
+        sendMessage(200);
+    }
+
+
+    public void test2000ByteMessageToSelf() {
+        sendMessage(2000);
+    }
+
+    public void test20000ByteMessageToSelf() {
+        sendMessage(20000);
+    }
+
+
+    public void testNullMessage() throws ChannelException {
+        disp.setRequestHandler(new MyHandler(null));
+
+        ch2=new JChannel(props);
+        disp2=new MessageDispatcher(ch2, null, null, new MyHandler(null));
+        ch2.connect("x");
+        assertEquals(2, ch2.getView().size());
+
+        RspList rsps=disp.castMessage(null, new Message(), GroupRequest.GET_ALL, 0);
+        System.out.println("rsps:\n" + rsps);
+        assertNotNull(rsps);
+        assertEquals(2, rsps.size());
+        Rsp rsp=(Rsp)rsps.get(ch.getLocalAddress());
+        assertNotNull(rsp);
+        Object ret=rsp.getValue();
+        assertNull(ret);
+
+
+        rsp=(Rsp)rsps.get(ch2.getLocalAddress());
+        assertNotNull(rsp);
+        ret=rsp.getValue();
+        assertNull(ret);
+    }
+
+    public void test200ByteMessageToAll() throws ChannelException {
+        sendMessageToBothChannels(200);
+    }
+
+    public void test2000ByteMessageToAll() throws ChannelException {
+        sendMessageToBothChannels(2000);
+    }
+
+    public void test20000ByteMessageToAll() throws ChannelException {
+        sendMessageToBothChannels(20000);
+    }
+    
+    private void sendMessage(int size) {
+        long start, stop;
+        MyHandler handler=new MyHandler(new byte[size]);
+        disp.setRequestHandler(handler);
+        start=System.currentTimeMillis();
+        RspList rsps=disp.castMessage(null, new Message(), GroupRequest.GET_ALL, 0);
+        stop=System.currentTimeMillis();
+        System.out.println("rsps:\n" + rsps);
+        System.out.println("call took " + (stop-start) + " ms");
+        assertNotNull(rsps);
+        assertEquals(1, rsps.size());
+        byte[] buf=(byte[])rsps.getFirst();
+        assertNotNull(buf);
+        assertEquals(size, buf.length);
+    }
+
+
+
+    private void sendMessageToBothChannels(int size) throws ChannelException {
+        long start, stop;
+        disp.setRequestHandler(new MyHandler(new byte[size]));
+
+        ch2=new JChannel(props);
+        disp2=new MessageDispatcher(ch2, null, null, new MyHandler(new byte[size]));
+        ch2.connect("x");
+        assertEquals(2, ch2.getView().size());
+
+        start=System.currentTimeMillis();
+        RspList rsps=disp.castMessage(null, new Message(), GroupRequest.GET_ALL, 0);
+        stop=System.currentTimeMillis();
+        System.out.println("rsps:\n" + rsps);
+        System.out.println("call took " + (stop-start) + " ms");
+        assertNotNull(rsps);
+        assertEquals(2, rsps.size());
+        Rsp rsp=(Rsp)rsps.get(ch.getLocalAddress());
+        assertNotNull(rsp);
+        byte[] ret=(byte[])rsp.getValue();
+        assertEquals(size, ret.length);
+
+        rsp=(Rsp)rsps.get(ch2.getLocalAddress());
+        assertNotNull(rsp);
+        ret=(byte[])rsp.getValue();
+        assertEquals(size, ret.length);
+    }
+
+
+    public static junit.framework.Test suite() {
+        return new TestSuite(MessageDispatcherUnitTest.class);
+    }
+
+    public static void main(String[] args) {
+        junit.textui.TestRunner.run(suite());
+    }
+
+
+    private static class MyHandler implements RequestHandler {
+        byte[] retval=null;
+
+
+        public MyHandler(byte[] retval) {
+            this.retval=retval;
+        }
+
+        public Object handle(Message msg) {
+            return retval;
+        }
+    }
+}
