7620de8f09ef77b5dfaf0ec4e291571a3a02db77
Bela Ban <belaban@yahoo.com>
2007-03-13 16:26:15 +0000

    added tests by Denis

--- tests/junit/org/jgroups/tests/DistributedHashtableUnitTest.java
+++ tests/junit/org/jgroups/tests/DistributedHashtableUnitTest.java
@@ -4,12 +4,13 @@ import junit.framework.TestCase;
 import org.jgroups.JChannel;
 import org.jgroups.blocks.DistributedHashtable;
 
-import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
- *
- * @author Bela Ban belaban@yahoo.com
- * @version $Revision: 1.1 $
+ * Test methods for DistributedHashtable
+ * @author denis.pasek@senacor.com
+ * @version $Revision: 1.2 $
  **/
 public class DistributedHashtableUnitTest extends TestCase {
 
@@ -47,6 +48,109 @@ public class DistributedHashtableUnitTest extends TestCase {
         super.tearDown();
     }
 
+    public void testSize() {
+        assertEquals(0, this.map1.size());
+        assertEquals(this.map2.size(), this.map1.size());
+
+        this.map1.put("key1", "value1");
+        assertEquals(1, this.map1.size());
+        assertEquals(this.map2.size(), this.map1.size());
+
+        this.map2.put("key2", "value2");
+        assertEquals(2, this.map1.size());
+        assertEquals(this.map2.size(), this.map1.size());
+    }
+
+    public void testIsEmpty() {
+        assertTrue(this.map1.isEmpty());
+        assertTrue(this.map2.isEmpty());
+
+        this.map1.put("key", "value");
+
+        assertFalse(this.map1.isEmpty());
+        assertFalse(this.map2.isEmpty());
+    }
+
+    public void testContainsKey() {
+        assertFalse(this.map1.containsKey("key1"));
+        assertFalse(this.map2.containsKey("key1"));
+        this.map1.put("key1", "value");
+        assertTrue(this.map1.containsKey("key1"));
+        assertTrue(this.map2.containsKey("key1"));
+        this.map2.put("key2", "value");
+        assertTrue(this.map1.containsKey("key2"));
+        assertTrue(this.map2.containsKey("key2"));
+    }
+
+    public void testContainsValue() {
+        assertFalse(this.map1.containsValue("value1"));
+        assertFalse(this.map2.containsValue("value1"));
+        this.map1.put("key1", "value1");
+        assertTrue(this.map1.containsValue("value1"));
+        assertTrue(this.map2.containsValue("value1"));
+        this.map2.put("key2", "value2");
+        assertTrue(this.map1.containsValue("value2"));
+        assertTrue(this.map2.containsValue("value2"));
+    }
+
+    public void testPutAndGet() {
+        assertNull(this.map1.get("key1"));
+        assertNull(this.map2.get("key1"));
+        this.map1.put("key1", "value1");
+        assertNotNull(this.map1.get("key1"));
+        assertNotNull(this.map2.get("key1"));
+        this.map2.put("key2", "value2");
+        assertNotNull(this.map1.get("key2"));
+        assertNotNull(this.map2.get("key2"));
+    }
+
+    public void testRemove() {
+        assertNull(this.map1.get("key1"));
+        assertNull(this.map2.get("key1"));
+        this.map1.put("key1", "value1");
+        this.map2.put("key2", "value2");
+        assertNotNull(this.map1.get("key1"));
+        assertNotNull(this.map2.get("key1"));
+        assertNotNull(this.map1.get("key2"));
+        assertNotNull(this.map2.get("key2"));
+
+        this.map1.remove("key1");
+        assertNull(this.map1.get("key1"));
+        assertNull(this.map2.get("key1"));
+        assertNotNull(this.map1.get("key2"));
+        assertNotNull(this.map2.get("key2"));
+
+        this.map2.remove("key2");
+        assertNull(this.map1.get("key2"));
+        assertNull(this.map2.get("key2"));
+    }
+
+    public void testPutAll() {
+        Map all1 = new HashMap();
+        all1.put("key1", "value1");
+        all1.put("key2", "value2");
+        Map all2 = new HashMap();
+        all2.put("key3", "value3");
+        all2.put("key4", "value4");
+
+        this.map1.putAll(all1);
+        assertEquals(2, this.map1.size());
+        assertEquals(2, this.map2.size());
+        this.map2.putAll(all2);
+        assertEquals(4, this.map1.size());
+        assertEquals(4, this.map2.size());
+
+        assertTrue(this.map1.containsKey("key1"));
+        assertTrue(this.map1.containsKey("key2"));
+        assertTrue(this.map1.containsKey("key3"));
+        assertTrue(this.map1.containsKey("key4"));
+
+        assertTrue(this.map2.containsKey("key1"));
+        assertTrue(this.map2.containsKey("key2"));
+        assertTrue(this.map2.containsKey("key3"));
+        assertTrue(this.map2.containsKey("key4"));
+    }
+
     public void testClear() {
         assertTrue(this.map1.isEmpty());
         assertTrue(this.map2.isEmpty());
@@ -58,16 +162,51 @@ public class DistributedHashtableUnitTest extends TestCase {
         this.map1.clear();
         assertTrue(this.map1.isEmpty());
         assertTrue(this.map2.isEmpty());
-        /*
-           this.map2.put("key", "value");
-           assertFalse(this.map1.isEmpty());
-           assertFalse(this.map2.isEmpty());
+        this.map2.put("key", "value");
+        assertFalse(this.map1.isEmpty());
+        assertFalse(this.map2.isEmpty());
 
-           this.map2.clear();
-           assertTrue(this.map1.isEmpty());
-           assertTrue(this.map2.isEmpty());
-            */
+        this.map2.clear();
+        assertTrue(this.map1.isEmpty());
+        assertTrue(this.map2.isEmpty());
     }
+
+    public void testKeySet() {
+        Map all1 = new HashMap();
+        all1.put("key1", "value1");
+        all1.put("key2", "value2");
+        Map all2 = new HashMap();
+        all2.put("key3", "value3");
+        all2.put("key4", "value4");
+
+        this.map1.putAll(all1);
+        assertEquals(all1.keySet(), this.map1.keySet());
+        assertEquals(all1.keySet(), this.map2.keySet());
+
+        this.map2.putAll(all2);
+        all1.putAll(all2);
+        assertEquals(all1.keySet(), this.map1.keySet());
+        assertEquals(all1.keySet(), this.map2.keySet());
+    }
+
+    public void testValues() {
+        Map all1 = new HashMap();
+        all1.put("key1", "value1");
+        all1.put("key2", "value2");
+        Map all2 = new HashMap();
+        all2.put("key3", "value3");
+        all2.put("key4", "value4");
+
+        this.map1.putAll(all1);
+        assertTrue(this.map1.values().containsAll(all1.values()));
+        assertTrue(this.map2.values().containsAll(all1.values()));
+
+        this.map2.putAll(all2);
+        all1.putAll(all2);
+        assertTrue(this.map1.values().containsAll(all1.values()));
+        assertTrue(this.map2.values().containsAll(all1.values()));
+    }
+    
 
 
 }
