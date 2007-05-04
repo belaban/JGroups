2f58d0dc2a6d58cbf26e983ace49fce2e85fb288
belaban <belaban>
2007-05-04 12:48:52 +0000

    added -local

--- tests/other/org/jgroups/tests/LatencyTest.java
+++ tests/other/org/jgroups/tests/LatencyTest.java
@@ -8,18 +8,36 @@ import java.net.MulticastSocket;
 
 /**
  * @author Bela Ban
- * @version $Id: LatencyTest.java,v 1.1 2007/05/04 11:24:36 belaban Exp $
+ * @version $Id: LatencyTest.java,v 1.2 2007/05/04 12:48:52 belaban Exp $
  */
 public class LatencyTest {
     InetAddress GROUP=null;
     int PORT=7500;
 
-    private void start(boolean sender) throws Exception {
+    private void start(boolean sender, boolean local) throws Exception {
         GROUP=InetAddress.getByName("228.1.2.3");
         long start;
         DatagramPacket send_packet, recv_packet;
         byte[] send_buf;
         byte[] recv_buf=new byte[2100];
+
+        if(local) {
+            MulticastSocket send_sock=new MulticastSocket(PORT);
+            send_sock.setTrafficClass(8);
+            MulticastSocket recv_sock=new MulticastSocket(PORT);
+            recv_sock.joinGroup(GROUP);
+            recv_packet=new DatagramPacket(recv_buf, 0, recv_buf.length);
+            for(int i=0; i < 10; i++) {
+                start=System.currentTimeMillis();
+                send_buf=Util.objectToByteBuffer(start);
+                send_packet=new DatagramPacket(send_buf, 0, send_buf.length, GROUP, PORT);
+                send_sock.send(send_packet);
+                recv_sock.receive(recv_packet);
+                start=((Long)Util.objectFromByteBuffer(recv_buf, recv_packet.getOffset(), recv_packet.getLength())).longValue();
+                System.out.println("took " + (System.currentTimeMillis() - start) + " ms");
+                Util.sleep(1000);
+            }
+        }
 
         if(sender) {
             MulticastSocket send_sock=new MulticastSocket(PORT);
@@ -51,19 +69,24 @@ public class LatencyTest {
 
     public static void main(String[] args) throws Exception {
         boolean sender=false;
+        boolean local=true;
         for(int i=0; i < args.length; i++) {
             if(args[i].equalsIgnoreCase("-sender")) {
                 sender=true;
                 continue;
             }
+            if(args[i].equalsIgnoreCase("-local")) {
+                local=true;
+                continue;
+            }
             help();
             return;
         }
-        new LatencyTest().start(sender);
+        new LatencyTest().start(sender, local);
     }
 
     private static void help() {
-        System.out.println("LatencyTest [-sender]");
+        System.out.println("LatencyTest [-sender] [-local (overrides -sender)]");
     }
 
 
