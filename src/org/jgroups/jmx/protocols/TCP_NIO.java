95c7568afc047397880addfa40c4203f044f01fe
Bela Ban <belaban@yahoo.com>
2007-07-10 13:02:50 +0000

    fixed CCE (http://jira.jboss.com/jira/browse/JGRP-552)

--- src/org/jgroups/jmx/protocols/TCP_NIO.java
+++ src/org/jgroups/jmx/protocols/TCP_NIO.java
@@ -2,50 +2,49 @@ package org.jgroups.jmx.protocols;
 
 /**
  * @author Scott Marlow
- * @version $Id: TCP_NIO.java,v 1.2 2007/01/10 19:38:18 smarlownovell Exp $
+ * @version $Id: TCP_NIO.java,v 1.3 2007/07/10 13:02:50 belaban Exp $
  */
 public class TCP_NIO  extends TCP implements TCP_NIOMBean {
 
-   org.jgroups.protocols.TCP_NIO p;
+   org.jgroups.protocols.TCP_NIO my_p;
    public TCP_NIO() {
    }
 
    public TCP_NIO(org.jgroups.stack.Protocol p) {
        super(p);
-       this.p=(org.jgroups.protocols.TCP_NIO)p;
+       this.my_p=(org.jgroups.protocols.TCP_NIO)p;
    }
 
    public void attachProtocol(org.jgroups.stack.Protocol p) {
-       super.attachProtocol(p);
-       this.p=(org.jgroups.protocols.TCP_NIO)p;
+       this.my_p=(org.jgroups.protocols.TCP_NIO)p;
    }
 
    public int getReaderThreads() {
-      return p.getReaderThreads();
+      return my_p.getReaderThreads();
    }
 
    public int getWriterThreads() {
-      return p.getWriterThreads();
+      return my_p.getWriterThreads();
    }
 
    public int getProcessorThreads() {
-      return p.getProcessorThreads();
+      return my_p.getProcessorThreads();
    }
 
    public int getProcessorMinThreads() {
-      return p.getProcessorMinThreads();
+      return my_p.getProcessorMinThreads();
    }
 
    public int getProcessorMaxThreads() {
-      return p.getProcessorMaxThreads();
+      return my_p.getProcessorMaxThreads();
    }
 
    public int getProcessorQueueSize() {
-      return p.getProcessorQueueSize();
+      return my_p.getProcessorQueueSize();
    }
 
    public long getProcessorKeepAliveTime() {
-      return p.getProcessorKeepAliveTime();
+      return my_p.getProcessorKeepAliveTime();
    }
 
 }
