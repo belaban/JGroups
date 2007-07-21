9b70858a293b541eeccc013a8a18bfcfb4487639
Bela Ban <belaban@yahoo.com>
2007-07-21 05:51:03 +0000

    new test

---
+++ tests/junit/org/jgroups/tests/UnicastUnitTest.java
@@ -0,0 +1,94 @@
+package org.jgroups.tests;
+
+import org.jgroups.*;
+
+import java.util.List;
+import java.util.LinkedList;
+
+/**
+ * Tests unicast functionality
+ * @author Bela Ban
+ * @version $Id: UnicastUnitTest.java,v 1.1 2007/07/21 05:51:03 belaban Exp $
+ */
+public class UnicastUnitTest extends ChannelTestBase {
+    JChannel ch1, ch2=null;
+
+
+
+
+    protected void setUp() throws Exception {
+        super.setUp();
+        ch1=createChannel();
+        ch2=createChannel();
+    }
+
+    protected void tearDown() throws Exception {
+        super.tearDown();
+        if(ch2 != null)
+            ch2.close();
+        if(ch1 != null)
+            ch1.close();
+    }
+
+
+
+    public void testUnicastMessageInCallbackNewMember() throws Exception {
+        ch1.connect("x");
+
+        ch2.setReceiver(new MyReceiver(ch2));
+        ch2.connect("x");
+        ch2.setReceiver(null);
+    }
+
+    public void testUnicastMessageInCallbackExistingMember() throws Exception {
+        MyReceiver receiver;
+        ch1.connect("x");
+        receiver=new MyReceiver(ch1);
+        ch1.setReceiver(receiver);
+        ch2.connect("x");
+        Exception ex=receiver.getEx();
+        if(ex != null)
+            throw ex;
+        ch1.setReceiver(null);
+    }
+
+
+    private static class MyReceiver extends ReceiverAdapter {
+        Channel channel;
+        Exception ex;
+
+        public MyReceiver(Channel channel) {
+            this.channel=channel;
+        }
+
+        public Exception getEx() {
+            return ex;
+        }
+
+        public void viewAccepted(View new_view) {
+            Address local_addr=channel.getLocalAddress();
+            assertNotNull(local_addr);
+            System.out.println("[" + local_addr + "]: " + new_view);
+            List<Address> members=new LinkedList<Address>(new_view.getMembers());
+            assertEquals("members=" + members + ", local_addr=" + local_addr, 2, members.size());
+            members.remove(local_addr);
+            assertEquals(1, members.size());
+            Address dest=members.get(0);
+            Message unicast_msg=new Message(dest, null, null);
+            try {
+                channel.send(unicast_msg);
+            }
+            catch(Exception e) {
+                ex=e;
+                e.printStackTrace();
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        String[] testCaseName={UnicastLoopbackTest.class.getName()};
+        junit.textui.TestRunner.main(testCaseName);
+    }
+}
