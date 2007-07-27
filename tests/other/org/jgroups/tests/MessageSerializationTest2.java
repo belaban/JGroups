68959f915044ce73953b1f91c95e16c49d846b86
belaban <belaban>
2007-07-27 09:26:31 +0000

    ns

--- tests/other/org/jgroups/tests/MessageSerializationTest2.java
+++ tests/other/org/jgroups/tests/MessageSerializationTest2.java
@@ -4,24 +4,23 @@ import org.jgroups.Message;
 import org.jgroups.conf.ClassConfigurator;
 import org.jgroups.protocols.*;
 import org.jgroups.stack.IpAddress;
-import org.jgroups.util.List;
 import org.jgroups.util.Buffer;
 import org.jgroups.util.ExposedByteArrayOutputStream;
 
 import java.io.*;
-import java.util.Enumeration;
 import java.util.LinkedList;
+import java.util.List;
 
 /**
  * @author Bela Ban Feb 12, 2004
- * @version $Id: MessageSerializationTest2.java,v 1.14 2005/07/15 09:35:00 belaban Exp $
+ * @version $Id: MessageSerializationTest2.java,v 1.15 2007/07/27 09:26:31 belaban Exp $
  */
 public class MessageSerializationTest2 {
     Message msg;
     Buffer buf;
     long start, stop, total;
     double msgs_per_sec, time_per_msg;
-    List my_list=new List();
+    List<Message> my_list=new LinkedList<Message>();
     int num=50000;
     ObjectOutputStream out;
     ExposedByteArrayOutputStream output;
@@ -30,7 +29,7 @@ public class MessageSerializationTest2 {
     DataOutputStream dos;
     DataInputStream dis;
     int msgs_read=0;
-    List l2=new List();
+    List<Message> l2=new LinkedList<Message>();
 
 
 
@@ -60,7 +59,7 @@ public class MessageSerializationTest2 {
         System.out.println("\n-- total time for creating " + num +
                 " msgs = " + total + "ms \n(" + msgs_per_sec + " msgs/sec, time_per_msg=" + time_per_msg + " ms)");
 
-        LinkedList l_ser=null, l_stream=null;
+        List<Long> l_ser=null, l_stream=null;
 
         if(use_streamable)
             l_stream=marshalMessages();
@@ -73,7 +72,7 @@ public class MessageSerializationTest2 {
     }
 
 
-     void addHeaders(Message msg) {
+     static void addHeaders(Message msg) {
         msg.putHeader("UDP", new UdpHeader("MyGroup"));
         msg.putHeader("PING", new PingHeader(PingHeader.GET_MBRS_REQ, null));
         msg.putHeader("FD_SOCK", new FD_SOCK.FdHeader());
@@ -85,16 +84,16 @@ public class MessageSerializationTest2 {
         msg.putHeader("GMS", new org.jgroups.protocols.pbcast.GMS.GmsHeader());
     }
 
-    private void printDiffs(LinkedList l_ser, LinkedList l_stream) {
-        int size_ser, size_stream;
+    private static void printDiffs(List<Long> l_ser, List<Long> l_stream) {
+        long size_ser, size_stream;
         long write_ser, write_stream, read_ser, read_stream;
 
-        size_ser=((Integer)l_ser.get(0)).intValue();
-        size_stream=((Integer)l_stream.get(0)).intValue();
-        write_ser=((Long)l_ser.get(1)).longValue();
-        read_ser=((Long)l_ser.get(2)).longValue();
-        write_stream=((Long)l_stream.get(1)).longValue();
-        read_stream=((Long)l_stream.get(2)).longValue();
+        size_ser=(l_ser.get(0));
+        size_stream=l_stream.get(0);
+        write_ser=l_ser.get(1).longValue();
+        read_ser=l_ser.get(2).longValue();
+        write_stream=l_stream.get(1).longValue();
+        read_stream=(l_stream.get(2)).longValue();
         System.out.println("\n\nserialized size=" + size_ser + ", streamable size=" + size_stream +
                            ", streamable is " + (100.0 / size_stream * size_ser -100) + " percent smaller");
         System.out.println("serialized write=" + write_ser + ", streamable write=" + write_stream +
@@ -109,18 +108,18 @@ public class MessageSerializationTest2 {
      * @return LinkedList with 3 elements: size of serialized buffer, write time, read time
      * @throws IOException
      */
-    LinkedList serializeMessage() throws IOException {
-        LinkedList retval=new LinkedList();
+    LinkedList<Long> serializeMessage() throws IOException {
+        LinkedList<Long> retval=new LinkedList<Long>();
         System.out.println("-- starting to serialize " + num + " msgs");
         start=System.currentTimeMillis();
         output=new ExposedByteArrayOutputStream(65000);
         out=new ObjectOutputStream(output);
-        my_list.writeExternal(out);
+        serialize(my_list, out);
         out.close();
         stop=System.currentTimeMillis();
         buf=new Buffer(output.getRawBuffer(), 0, output.size());
         System.out.println("** serialized buffer size=" + buf.getLength() + " bytes");
-        retval.add(new Integer(buf.getLength()));
+        retval.add(new Long(buf.getLength()));
 
         total=stop-start;
         retval.add(new Long(total));
@@ -135,7 +134,7 @@ public class MessageSerializationTest2 {
         ObjectInputStream in2=new ObjectInputStream(input2);
 
         try {
-            l2.readExternal(in2);
+            unserialize(l2, in2);
         }
         catch(ClassNotFoundException e) {
             e.printStackTrace();
@@ -148,19 +147,18 @@ public class MessageSerializationTest2 {
         time_per_msg=total / (double)msgs_read;
         System.out.println("\n-- total time for reading " + msgs_read +
                 " msgs = " + total + "ms \n(" + msgs_per_sec + " msgs/sec, time_per_msg=" + time_per_msg + ')');
-        l2.removeAll();
+        l2.clear();
         return retval;
     }
 
-    LinkedList marshalMessages() throws IOException, IllegalAccessException, InstantiationException {
-        LinkedList retval=new LinkedList();
+    LinkedList<Long> marshalMessages() throws IOException, IllegalAccessException, InstantiationException {
+        LinkedList<Long> retval=new LinkedList<Long>();
         System.out.println("\n\n-- starting to marshal " + num + " msgs (using Streamable)");
         start=System.currentTimeMillis();
         output=new ExposedByteArrayOutputStream(65000);
         dos=new DataOutputStream(output);
         dos.writeInt(my_list.size());
-        for(Enumeration en=my_list.elements(); en.hasMoreElements();) {
-            Message tmp=(Message)en.nextElement();
+        for(Message tmp: my_list) {
             tmp.writeTo(dos);
         }
 
@@ -168,7 +166,7 @@ public class MessageSerializationTest2 {
         stop=System.currentTimeMillis();
         buf=new Buffer(output.getRawBuffer(), 0, output.size());
         System.out.println("** marshalled buffer size=" + buf.getLength() + " bytes");
-        retval.add(new Integer(buf.getLength()));
+        retval.add(new Long(buf.getLength()));
 
         total=stop-start;
         retval.add(new Long(total));
@@ -202,6 +200,22 @@ public class MessageSerializationTest2 {
         return retval;
     }
 
+    private static void serialize(List<Message> msgs, ObjectOutputStream out) throws IOException {
+        out.writeInt(msgs.size());
+        for(Message msg: msgs) {
+            msg.writeExternal(out);
+        }
+    }
+
+    private static void unserialize(List<Message> list, ObjectInputStream in) throws IOException, ClassNotFoundException {
+        int length=in.readInt();
+        for(int i=0; i < length; i++) {
+            Message tmp=new Message();
+            tmp.readExternal(in);
+            list.add(tmp);
+        }
+    }
+
     public static void main(String[] args) {
         int num=50000;
         boolean use_serialization=true, use_streamable=true, use_additional_data=false, add_headers=true;
@@ -212,19 +226,19 @@ public class MessageSerializationTest2 {
                 continue;
             }
             if(args[i].equals("-use_serialization")) {
-                use_serialization=new Boolean(args[++i]).booleanValue();
+                use_serialization=Boolean.parseBoolean(args[++i]);
                 continue;
             }
             if(args[i].equals("-use_streamable")) {
-                use_streamable=new Boolean(args[++i]).booleanValue();
+                use_streamable=Boolean.parseBoolean(args[++i]);
                 continue;
             }
             if(args[i].equals("-use_additional_data")) {
-                use_additional_data=new Boolean(args[++i]).booleanValue();
+                use_additional_data=Boolean.parseBoolean(args[++i]);
                 continue;
             }
             if(args[i].equals("-add_headers")) {
-                add_headers=new Boolean(args[++i]).booleanValue();
+                add_headers=Boolean.parseBoolean(args[++i]);
                 continue;
             }
             help();
