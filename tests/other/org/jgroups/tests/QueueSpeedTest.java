3a716603108849ba2b03ffb8d8cd0922391e0316
Bela Ban <belaban@yahoo.com>
2007-07-27 10:49:30 +0000

    ns

--- tests/other/org/jgroups/tests/QueueSpeedTest.java
+++ tests/other/org/jgroups/tests/QueueSpeedTest.java
@@ -1,20 +1,19 @@
-// $Id: QueueSpeedTest.java,v 1.7 2005/05/30 16:15:12 belaban Exp $
+// $Id: QueueSpeedTest.java,v 1.8 2007/07/27 10:49:30 belaban Exp $
 
 
 package org.jgroups.tests;
 
-import org.jgroups.util.LinkedListQueue;
 import org.jgroups.util.Queue;
 
 
 /**
- * Measures the speed of inserting and removing 1 million elements into/from a Queue and a LinkedQueue.
+ * Measures the speed of inserting and removing 1 million elements into/from a Queue
  *
  * @author Bela Ban
  */
 public class QueueSpeedTest {
     int num_elements=1000000;
-    final int NUM=10;
+    static final int NUM=10;
 
 
     public QueueSpeedTest(int num_elements) {
@@ -23,28 +22,21 @@ public class QueueSpeedTest {
 
 
     public void start() throws Exception {
-        double q1=0, q2=0, diff;
+        double q1=0;
 
 
         System.out.println("warming up cache");
         measureQueue();
-        measureLinkedListQueue();
 
         System.out.println("running insertions " + NUM + " times (will take average)");
         for(int i=0; i < NUM; i++) {
             System.out.println("Round #" + (i + 1));
             q1+=measureQueue();
-            q2+=measureLinkedListQueue();
         }
 
         q1=q1 / NUM;
-        q2=q2 / NUM;
 
         System.out.println("Time to insert and remove " + num_elements + " into Queue:           " + q1 + " ms");
-        System.out.println("Time to insert and remove " + num_elements + " into LinkedListQueue: " + q2 + " ms");
-
-        diff=(long)(q2 - q1);
-        System.out.println("diff is " + Math.abs(diff) + "; " + (q2 < q1 ? "LinkedListQueue" : "Queue") + " is faster");
     }
 
 
@@ -68,24 +60,6 @@ public class QueueSpeedTest {
     }
 
 
-    long measureLinkedListQueue() throws Exception {
-        LinkedListQueue q=new LinkedListQueue();
-        long start, stop;
-
-        start=System.currentTimeMillis();
-        for(int i=0; i < num_elements; i++) {
-            if(i % 2 == 0)
-                q.add(new Integer(i));
-            else
-                q.addAtHead(new Integer(i));
-        }
-
-        while(q.size() > 0)
-            q.remove();
-
-        stop=System.currentTimeMillis();
-        return stop - start;
-    }
 
 
     public static void main(String[] args) {
