057b7106b2ca5ff38c3a425323adcf22e9c74dc2
belaban <belaban>
2007-08-08 08:49:49 +0000

    BoundedList now extends ConcurrentLinkedQueue rather than LinkedList (this is reentrant)

--- src/org/jgroups/util/BoundedList.java
+++ src/org/jgroups/util/BoundedList.java
@@ -1,13 +1,15 @@
 package org.jgroups.util;
 
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 
 /**
  * A bounded subclass of LinkedList, oldest elements are removed once max capacity is exceeded. Note that this
  * class is not synchronized (like LinkedList).
  * @author Bela Ban Nov 20, 2003
- * @version $Id: BoundedList.java,v 1.3 2007/07/27 11:01:00 belaban Exp $
+ * @version $Id: BoundedList.java,v 1.4 2007/08/08 08:49:49 belaban Exp $
  */
-public class BoundedList<T> extends java.util.LinkedList<T> {
+public class BoundedList<T> extends ConcurrentLinkedQueue<T> {
     int max_capacity=10;
 
 
@@ -28,27 +30,15 @@ public class BoundedList<T> extends java.util.LinkedList<T> {
     public boolean add(T obj) {
         if(obj == null) return false;
         while(size() >= max_capacity && size() > 0) {
-            removeFirst();
+            remove();
         }
         return super.add(obj);
     }
 
 
-    /**
-     * Adds an object to the head, removes an element from the tail if capacity has been exceeded
-     * @param obj The object to be added
-     */
-    public void addAtHead(T obj) {
-        if(obj == null) return;
-        while(size() >= max_capacity && size() > 0) {
-            removeFirst();
-        }
-        super.add(0, obj);
-    }
-
 
     public T removeFromHead() {
-        return removeFirst();
+        return remove();
     }
 
 
