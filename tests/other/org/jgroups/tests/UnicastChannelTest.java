adc4b3221771d251f822cc7625bf6598c213d119
belaban <belaban>
2007-08-10 07:22:46 +0000

    ns

--- tests/other/org/jgroups/tests/UnicastChannelTest.java
+++ tests/other/org/jgroups/tests/UnicastChannelTest.java
@@ -1,14 +1,18 @@
-// $Id: UnicastChannelTest.java,v 1.4 2004/07/05 14:15:11 belaban Exp $
+// $Id: UnicastChannelTest.java,v 1.5 2007/08/10 07:22:46 belaban Exp $
 
 
 package org.jgroups.tests;
 
 import org.jgroups.JChannel;
 import org.jgroups.Message;
+import org.jgroups.Event;
+import org.jgroups.protocols.UDP;
+import org.jgroups.protocols.TP;
 import org.jgroups.stack.IpAddress;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Properties;
 
 
 /**
@@ -74,6 +78,7 @@ public class UnicastChannelTest {
         ch.connect(null); // unicast channel
 
         addr=new IpAddress(host, port);
+        ch.down(new Event(Event.ENABLE_UNICASTS_TO, addr));
         reader= new BufferedReader(new InputStreamReader(System.in));
 
         while(true) {
@@ -104,9 +109,22 @@ public class UnicastChannelTest {
         Message msg, rsp;
 
         ch=new JChannel(props);
+        System.setProperty("jgroups.bind_addr", host);
+        if(port > 0) {
+            // ugly hack
+            TP transport=(TP)ch.getProtocolStack().findProtocol("UDP");
+            if(transport == null)
+                transport=(TP)ch.getProtocolStack().findProtocol("TCP");
+            if(transport == null)
+                transport=(TP)ch.getProtocolStack().findProtocol("TCP_NIO");
+            if(transport != null) {
+                Properties tmp=new Properties();
+                tmp.setProperty("bind_port", String.valueOf(port));
+                transport.setProperties(tmp);
+            }
+        }
         ch.connect(null); // this makes it a unicast channel
-        System.out.println("server started at " + new java.util.Date() +
-                           ", listening on " + ch.getLocalAddress());
+        System.out.println("server started at " + new java.util.Date() + ", listening on " + ch.getLocalAddress());
         while(true) {
             obj=ch.receive(0);
             if(!(obj instanceof Message)) {
@@ -121,7 +139,7 @@ public class UnicastChannelTest {
         }
     }
 
-    void help() {
+    static void help() {
         System.out.println("UnicastChannelTest [-help] [-server] [-props <props>]" +
                            "[-host <host>] [-port <port>]");
     }
