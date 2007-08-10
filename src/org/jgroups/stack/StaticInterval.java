03cdaf9fe1796dbee0c1eee6c1ac8206e0831999
Bela Ban <belaban@yahoo.com>
2007-08-10 12:47:38 +0000

    removed synchronized as we won't have concurrent access

--- src/org/jgroups/stack/StaticInterval.java
+++ src/org/jgroups/stack/StaticInterval.java
@@ -10,7 +10,7 @@ import org.jgroups.annotations.GuardedBy;
  * so it shouldn't be shared between instances, as {@link #next()} will modify the state.
  * @author John Giorgiadis
  * @author Bela Ban
- * @version $Id: StaticInterval.java,v 1.2 2007/08/10 12:32:17 belaban Exp $
+ * @version $Id: StaticInterval.java,v 1.3 2007/08/10 12:47:38 belaban Exp $
  */
 public class StaticInterval implements Interval {
     private int          next=0;
@@ -29,14 +29,18 @@ public class StaticInterval implements Interval {
     /** @return the next interval */
     @GuardedBy("interval")
     public long next() {
-        synchronized(values) {
+        // we don't need to synchronize because this method won't be called concurrently; each entry has its own copy
+        // of StaticInterval
+        // synchronized(values) {
             if (next >= values.length)
                 return(values[values.length-1]);
             else
                 return(values[next++]);
-        }
+        // }
     }
     
 
 }
+
+
 
