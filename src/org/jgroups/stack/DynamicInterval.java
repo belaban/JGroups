fc393bb78da91f66c2558d93992b97ce2699b893
Bela Ban <belaban@yahoo.com>
2007-08-13 12:29:45 +0000

    new file

--- src/org/jgroups/stack/DynamicInterval.java
+++ src/org/jgroups/stack/DynamicInterval.java
@@ -2,16 +2,32 @@ package org.jgroups.stack;
 
 /**
  * @author Bela Ban
- * @version $Id: DynamicInterval.java,v 1.1 2007/08/10 13:01:37 belaban Exp $
+ * @version $Id: DynamicInterval.java,v 1.2 2007/08/13 12:29:45 belaban Exp $
  */
 public class DynamicInterval implements Interval {
+    private long value=30;
+    private static final long MAX=5000;
+
+    public DynamicInterval() {
+        
+    }
+
+    public DynamicInterval(long value) {
+        this.value=value;
+    }
 
     public long next() {
-        return 30;
+        long retval=value;
+        value=Math.min(MAX, value * 2);
+        return retval;
     }
 
     /** We don't need to copy as we don't have any state */
     public final Interval copy() {
-        return this;
+        return new DynamicInterval(value);
+    }
+
+    public String toString() {
+        return String.valueOf(value);
     }
 }
