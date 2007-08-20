b5a4f1528fe5392e902a2fb5dcdb75b063fa05ef
belaban <belaban>
2007-08-20 12:38:39 +0000

    rewrote Retransmitter, removed Entry, now every task is a simple timer, which is cancelled when a missing message is received (http://jira.jboss.com/jira/browse/JGRP-487)

--- src/org/jgroups/stack/Retransmitter.java
+++ src/org/jgroups/stack/Retransmitter.java
@@ -1,4 +1,4 @@
-// $Id: Retransmitter.java,v 1.22 2007/08/17 15:10:44 belaban Exp $
+// $Id: Retransmitter.java,v 1.23 2007/08/20 12:38:39 belaban Exp $
 
 package org.jgroups.stack;
 
@@ -8,37 +8,40 @@ import org.jgroups.Address;
 import org.jgroups.util.TimeScheduler;
 import org.jgroups.util.Util;
 
-import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.Future;
 
 
 /**
  * Maintains a pool of sequence numbers of messages that need to be retransmitted. Messages
- * are aged and retransmission requests sent according to age (linear backoff used). If a
+ * are aged and retransmission requests sent according to age (configurable backoff). If a
  * TimeScheduler instance is given to the constructor, it will be used, otherwise Reransmitter
  * will create its own. The retransmit timeouts have to be set first thing after creating an instance.
- * The <code>add()</code> method adds a range of sequence numbers of messages to be retransmitted. The
+ * The <code>add()</code> method adds the sequence numbers of messages to be retransmitted. The
  * <code>remove()</code> method removes a sequence number again, cancelling retransmission requests for it.
  * Whenever a message needs to be retransmitted, the <code>RetransmitCommand.retransmit()</code> method is called.
  * It can be used e.g. by an ack-based scheme (e.g. AckSenderWindow) to retransmit a message to the receiver, or
- * by a nak-based scheme to send a retransmission request to the sender of the missing message.
- *
- * @author John Giorgiadis
+ * by a nak-based scheme to send a retransmission request to the sender of the missing message.<br/>
+ * Changes Aug 2007 (bela): the retransmitter was completely rewritten. Entry was removed, instead all tasks
+ * are directly placed into a hashmap, keyed by seqnos. When a message has been received, we simply remove
+ * the task from the hashmap and cancel it. This simplifies the code and avoids having to iterate through
+ * the (previous) message list linearly on removal. Performance is about the same, or slightly better in
+ * informal tests.
  * @author Bela Ban
- * @version $Revision: 1.22 $
+ * @version $Revision: 1.23 $
  */
 public class Retransmitter {
 
     private static final long SEC=1000;
     /** Default retransmit intervals (ms) - exponential approx. */
-    private static Interval      RETRANSMIT_TIMEOUTS=new StaticInterval(2 * SEC, 3 * SEC, 5 * SEC, 8 * SEC);
-
-    private Address              sender=null;
-    private final List<Entry>    msgs=new LinkedList<Entry>();  // List<Entry> of elements to be retransmitted
-    private RetransmitCommand    cmd=null;
-    private boolean              retransmitter_owned;
-    private TimeScheduler        timer=null;
-    protected static final Log   log=LogFactory.getLog(Retransmitter.class);
+    private Interval                       RETRANSMIT_TIMEOUTS=new StaticInterval(2 * SEC, 3 * SEC, 5 * SEC, 8 * SEC);
+    private Address                        sender=null;
+    private final ConcurrentMap<Long,Task> msgs=new ConcurrentHashMap<Long,Task>(11);
+    private RetransmitCommand              cmd=null;
+    private boolean                        retransmitter_owned;
+    private TimeScheduler                  timer=null;
+    protected static final Log             log=LogFactory.getLog(Retransmitter.class);
 
 
     /** Retransmit command (see Gamma et al.) used to retrieve missing messages */
@@ -98,12 +101,14 @@ public class Retransmitter {
             last_seqno=tmp;
         }
 
-        // each entry needs its own retransmission interval, intervals are stateful *and* mutable, so we *need* to copy !
-        Entry entry=new Entry(first_seqno, last_seqno, RETRANSMIT_TIMEOUTS.copy());
-        synchronized(msgs) {
-            msgs.add(entry);
+        Task task;
+        for(long seqno=first_seqno; seqno <= last_seqno; seqno++) {
+            // each task needs its own retransmission interval, as they are stateful *and* mutable, so we *need* to copy !
+            task=new Task(seqno, RETRANSMIT_TIMEOUTS.copy(), cmd, sender);
+            msgs.putIfAbsent(seqno, task);
+            task.doSchedule(timer); // Entry adds itself to the timer
         }
-        entry.doSchedule(timer); // Entry adds itself to the timer
+
     }
 
     /**
@@ -113,24 +118,12 @@ public class Retransmitter {
      * scheduler and remove it from the pending entries
      */
     public int remove(long seqno) {
-        Entry e;
-        int num_retransmits=-1;
-
-        synchronized(msgs) {
-            for(ListIterator<Entry> it=msgs.listIterator(); it.hasNext();) {
-                e=it.next();
-                if(seqno < e.low || seqno > e.high) continue;
-                e.remove(seqno);
-                if(e.low > e.high) {
-                    e.cancel();
-                    it.remove();
-                }
-                num_retransmits=e.getNumRetransmits();
-                break;
-            }
+        Task task=msgs.remove(seqno);
+        if(task != null) {
+            task.cancel();
+            return task.getNumRetransmits();
         }
-
-        return num_retransmits;
+        return -1;
     }
 
     /**
@@ -138,12 +131,9 @@ public class Retransmitter {
      * respective tasks
      */
     public void reset() {
-        synchronized(msgs) {
-            for(Entry entry: msgs) {
-                entry.cancel();
-            }
-            msgs.clear();
-        }
+        for(Task task: msgs.values())
+            task.cancel();
+        msgs.clear();
     }
 
     /**
@@ -158,44 +148,33 @@ public class Retransmitter {
     public void stop() {
         // i. If retransmitter is owned, stop it else cancel all tasks
         // ii. Clear all pending msgs
-        synchronized(msgs) {
-            if(retransmitter_owned) {
-                try {
-                    timer.stop();
-                }
-                catch(InterruptedException ex) {
-                    if(log.isErrorEnabled()) log.error("failed stopping retransmitter", ex);
-                    Thread.currentThread().interrupt(); // set interrupt flag again
-                }
+        if(retransmitter_owned) {
+            try {
+                timer.stop();
             }
-            else {
-                for(Entry entry: msgs) {
-                    entry.cancel();
-                }
+            catch(InterruptedException ex) {
+                if(log.isErrorEnabled()) log.error("failed stopping retransmitter", ex);
+                Thread.currentThread().interrupt(); // set interrupt flag again
             }
-            msgs.clear();
         }
+        else {
+            for(Task task: msgs.values())
+                task.cancel();
+        }
+        msgs.clear();
     }
 
 
     public String toString() {
-        synchronized(msgs) {
-            int size=size();
-            StringBuilder sb=new StringBuilder();
-            sb.append(size).append(" messages to retransmit: ").append(msgs);
-            return sb.toString();
-        }
+        int size=size();
+        StringBuilder sb=new StringBuilder();
+        sb.append(size).append(" messages to retransmit: ").append(msgs.keySet());
+        return sb.toString();
     }
 
 
     public int size() {
-        int size=0;
-        synchronized(msgs) {
-            for(Entry entry: msgs) {
-                size+=entry.size();
-            }
-        }
-        return size;
+        return msgs.size();
     }
 
 
@@ -227,13 +206,19 @@ public class Retransmitter {
     /**
      * The retransmit task executed by the scheduler in regular intervals
      */
-    private static abstract class Task implements TimeScheduler.Task {
-        private final Interval intervals;
-        private Future         future;
-        protected int          num_retransmits=0;
+    private static class Task implements TimeScheduler.Task {
+        private final Interval    intervals;
+        private long              seqno=-1;
+        private Future            future;
+        private Address           sender=null;
+        protected int             num_retransmits=0;
+        private RetransmitCommand command;
 
-        protected Task(Interval intervals) {
+        protected Task(long seqno, Interval intervals, RetransmitCommand cmd, Address sender) {
+            this.seqno=seqno;
             this.intervals=intervals;
+            this.command=cmd;
+            this.sender=sender;
         }
 
         public int getNumRetransmits() {
@@ -252,146 +237,17 @@ public class Retransmitter {
             if(future != null)
                 future.cancel(false);
         }
-    }
 
-
-    /**
-     * The entry associated with an initial group of missing messages
-     * with contiguous sequence numbers and with all its subgroups.<br>
-     * E.g.
-     * - initial group: [5-34]
-     * - msg 12 is acknowledged, now the groups are: [5-11], [13-34]
-     * <p>
-     * Groups are stored in a list as long[2] arrays of the each group's
-     * bounds. For speed and convenience, the lowest & highest bounds of
-     * all the groups in this entry are also stored separately
-     */
-    private class Entry extends Task {
-        private long low;
-        private long high;
-        /** List<long[2]> of ranges to be retransmitted */
-        final java.util.List<long[]> list=new ArrayList<long[]>();
-
-        public Entry(long low, long high, Interval intervals) {
-            super(intervals);
-            this.low=low;
-            this.high=high;
-            list.add(new long[]{low, high});
-        }
-
-        /**
-         * Remove the given seqno and resize or partition groups as
-         * necessary. The algorithm is as follows:<br>
-         * i. Find the group with low <= seqno <= high
-         * ii. If seqno == low,
-         *	a. if low == high, then remove the group
-         *	Adjust global low. If global low was pointing to the group
-         * deleted in the previous step, set it to point to the next group.
-         * If there is no next group, set global low to be higher than
-         * global high. This way the entry is invalidated and will be removed
-         * all together from the pending msgs and the task scheduler
-         * iii. If seqno == high, adjust high, adjust global high if this is
-         * the group at the tail of the list
-         * iv. Else low < seqno < high, break [low,high] into [low,seqno-1]
-         * and [seqno+1,high]
-         *
-         * @param seqno the sequence number to remove
-         */
-        public void remove(long seqno) {
-            int i;
-            long[] bounds=null, newBounds;
-
-            synchronized(list) {
-                for(i=0; i < list.size(); ++i) {
-                    bounds=list.get(i);
-                    if(seqno < bounds[0] || seqno > bounds[1]) continue;
-                    break;
-                }
-                if(i == list.size()) return;
-
-                if(seqno == bounds[0]) {
-                    if(bounds[0] == bounds[1])
-                        list.remove(i);
-                    else
-                        bounds[0]++;
-                    if(i == 0)
-                        low=list.isEmpty()? high + 1 : list.get(i)[0];
-                }
-                else if(seqno == bounds[1]) {
-                    bounds[1]--;
-                    if(i == list.size() - 1) high=list.get(i)[1];
-                }
-                else {
-                    newBounds=new long[2];
-                    newBounds[0]=seqno + 1;
-                    newBounds[1]=bounds[1];
-                    bounds[1]=seqno - 1;
-                    list.add(i + 1, newBounds);
-                }
-            }
-        }
-
-        /**
-         * Retransmission task:<br>
-         * For each interval, call the retransmission callback command
-         */
         public void run() {
-            long[] bounds;
-            List copy;
-
-            synchronized(list) {
-                copy=new LinkedList<long[]>(list);
-            }
-
-            for(Iterator it=copy.iterator(); it.hasNext();) {
-                bounds=(long[])it.next();
-                try {
-                    cmd.retransmit(bounds[0], bounds[1], sender);
-                    num_retransmits++;
-                }
-                catch(Throwable t) {
-                    log.error("failure asking " + cmd + " for retransmission", t);
-                }
-            }
+            command.retransmit(seqno, seqno, sender);
+            num_retransmits++;
         }
-
-        int size() {
-            int size=0;
-            long diff;
-            long[] tmp;
-            synchronized(list) {
-                for(Iterator it=list.iterator(); it.hasNext();) {
-                    tmp=(long[])it.next();
-                    diff=tmp[1] - tmp[0] +1;
-                    size+=diff;
-                }
-            }
-
-            return size;
-        }
-
 
         public String toString() {
-            StringBuilder sb=new StringBuilder();
-            synchronized(list) {
-                long[] range;
-                boolean first=true;
-                for(Iterator it=list.iterator(); it.hasNext();) {
-                    range=(long[])it.next();
-                    if(first) {
-                        first=false;
-                    }
-                    else {
-                        sb.append(", ");
-                    }
-                    sb.append(range[0]).append('-').append(range[1]);
-                }
-            }
-
-            return sb.toString();
+            return String.valueOf(seqno);
         }
-
     }
+
 
 
     public static void main(String[] args) {
@@ -429,6 +285,7 @@ public class Retransmitter {
             System.out.println("retransmitter: " + xmitter);
             xmitter.remove(5);
             System.out.println("retransmitter: " + xmitter);
+            xmitter.stop();
         }
         catch(Exception e) {
             log.error(e);
