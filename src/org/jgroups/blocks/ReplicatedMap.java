dc50104f1f531475fd9315d15df1726267ddbd50
Bela Ban <belaban@yahoo.com>
2007-08-22 10:06:42 +0000

    extends ConcurrentMap now (http://jira.jboss.com/jira/browse/JGRP-581)

--- src/org/jgroups/blocks/ReplicatedMap.java
+++ src/org/jgroups/blocks/ReplicatedMap.java
@@ -2,12 +2,13 @@ package org.jgroups.blocks;
 
 import java.io.Serializable;
 import java.util.Map;
+import java.util.concurrent.ConcurrentMap;
 
 /**
  * @author Bela Ban
- * @version $Id: ReplicatedMap.java,v 1.1 2007/08/22 08:50:07 belaban Exp $
+ * @version $Id: ReplicatedMap.java,v 1.2 2007/08/22 10:06:42 belaban Exp $
  */
-public interface ReplicatedMap<K extends Serializable, V extends Serializable> extends Map<K, V> {
+public interface ReplicatedMap<K extends Serializable, V extends Serializable> extends ConcurrentMap<K, V> {
     V _put(K key, V value);
 
     void _putAll(Map<? extends K, ? extends V> map);
@@ -15,4 +16,12 @@ public interface ReplicatedMap<K extends Serializable, V extends Serializable> e
     void _clear();
 
     V _remove(Object key);
+
+    V _putIfAbsent(K key, V value);
+
+    boolean _remove(Object key, Object value);
+
+    boolean _replace(K key, V oldValue, V newValue);
+
+    V _replace(K key, V value);
 }
