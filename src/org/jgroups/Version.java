2942b7ed6262f74742b1f5ba1aeaa28da3235bfc
belaban <belaban>
2007-09-20 07:43:56 +0000

    ns

--- src/org/jgroups/Version.java
+++ src/org/jgroups/Version.java
@@ -15,7 +15,7 @@ import org.jgroups.annotations.Immutable;
  * Z = 0-63 for micro versions
  *
  * @author Bela Ban
- * @version $Id: Version.java,v 1.56 2007/09/17 18:20:07 belaban Exp $
+ * @version $Id: Version.java,v 1.57 2007/09/20 07:43:56 belaban Exp $
  * Holds version information for JGroups.
  */
 @Immutable
@@ -27,7 +27,7 @@ public class Version {
 
     public static final short version=encode(major, minor, micro);
     public static final String string_version=print(version);
-    public static final String cvs="$Id: Version.java,v 1.56 2007/09/17 18:20:07 belaban Exp $";
+    public static final String cvs="$Id: Version.java,v 1.57 2007/09/20 07:43:56 belaban Exp $";
 
     private static final int MAJOR_SHIFT = 11;
     private static final int MINOR_SHIFT = 6;
@@ -42,8 +42,8 @@ public class Version {
      * @param args
      */
     public static void main(String[] args) {
-        System.out.println("\nVersion: \t" + description);
-        System.out.println("CVS: \t\t" + cvs + "\n");
+        System.out.println("\nVersion:      " + description);
+        System.out.println("CVS:          " + cvs + "\n");
     }
 
 
