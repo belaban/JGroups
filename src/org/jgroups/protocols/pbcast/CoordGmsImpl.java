91d4812c0cfbbe87aba014cb0ab10a04b579fc3d
vlada <vlada>
2007-09-28 10:34:12 +0000

    pass PREPARE_VIEW both up/down

--- src/org/jgroups/protocols/pbcast/CoordGmsImpl.java
+++ src/org/jgroups/protocols/pbcast/CoordGmsImpl.java
@@ -1,4 +1,4 @@
-// $Id: CoordGmsImpl.java,v 1.77 2007/09/27 16:19:52 vlada Exp $
+// $Id: CoordGmsImpl.java,v 1.78 2007/09/28 10:34:12 vlada Exp $
 
 package org.jgroups.protocols.pbcast;
 
@@ -389,6 +389,7 @@ public class CoordGmsImpl extends GmsImpl {
         JoinRsp join_rsp=null;
         View new_view=gms.getNextView(new_mbrs, leaving_mbrs, suspected_mbrs);
         gms.up(new Event(Event.PREPARE_VIEW,new_view));
+        gms.down(new Event(Event.PREPARE_VIEW,new_view));
         
         if(log.isDebugEnabled())
             log.debug("new=" + new_mbrs + ", suspected=" + suspected_mbrs + ", leaving=" + leaving_mbrs +
