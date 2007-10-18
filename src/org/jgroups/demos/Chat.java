950d92e3cc3eb9472b82a7a13466a0ccd3b84375
Bela Ban <belaban@yahoo.com>
2007-10-18 19:55:49 +0000

    added Dino's new Chat

--- src/org/jgroups/demos/Chat.java
+++ src/org/jgroups/demos/Chat.java
@@ -1,309 +1,152 @@
 package org.jgroups.demos;
 
-
-import org.jgroups.*;
-import org.jgroups.util.Util;
-
-import javax.swing.*;
-import java.awt.*;
+import java.awt.Frame;
+import java.awt.Label;
+import java.awt.Rectangle;
+import java.awt.TextArea;
+import java.awt.TextField;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
-import java.io.*;
-import java.util.Iterator;
-import java.util.LinkedList;
 
+import javax.swing.JButton;
 
-/**
- * Simple chat demo
- * @author Bela Ban
- * @version $Id: Chat.java,v 1.16 2007/07/16 11:44:23 belaban Exp $
- */
-public class Chat  extends ExtendedReceiverAdapter implements MouseListener, WindowListener {
-    Channel channel;
-    Thread mainThread;
-    static final String group_name="ChatGroup";
-    String props=null;
-    Frame mainFrame;
-    TextArea ta;
-    TextField tf;
-    Label csLabel;
-    JButton leaveButton;
-    JButton sendButton;
-    JButton clearButton;
-    String username=null;
-    LinkedList history=new LinkedList();
+public class Chat extends ChatCore implements MouseListener, WindowListener {
 
+	Frame mainFrame;
 
-    public Chat(String props) {
-        this.props=props;
-        try {
-            username=System.getProperty("user.name");
-        }
-        catch(Throwable t) {
-        }
-    }
+	TextArea txtArea;
 
+	TextField txtField;
 
-    public static void main(String[] args) {
-        String props=null;
+	Label csLabel;
 
-        for(int i=0; i < args.length; i++) {
-            if("-props".equals(args[i])) {
-                props=args[++i];
-                continue;
-            }
-            help();
-            return;
-        }
+	JButton leaveButton;
 
-        Chat chat=new Chat(props);
-        chat.start();
-    }
+	JButton sendButton;
 
+	JButton clearButton;
 
-    static void help() {
-        System.out.println("Chat [-help] [-props <properties>]");
-    }
+	public Chat(String props) {
+		super(props);
+	}
 
+	public static void main(String[] args) {
+		String props = null;
 
-    public void start() {
-        mainFrame=new Frame();
-        mainFrame.setLayout(null);
-        mainFrame.setSize(600, 507);
-        mainFrame.addWindowListener(this);
+		for (int i = 0; i < args.length; i++) {
+			if ("-props".equals(args[i])) {
+				props = args[++i];
+				continue;
+			}
+			help();
+			return;
+		}
 
-        ta=new TextArea();
-        ta.setBounds(12, 36, 550, 348);
-        ta.setEditable(false);
-        mainFrame.add(ta);
+		Chat instance = new Chat(props);
+		instance.start();
+	}
 
-        tf=new TextField();
-        tf.setBounds(100, 392, 400, 30);
-        mainFrame.add(tf);
+	void post(String msg) {
+		txtArea.append(msg);
+	}
 
-        csLabel=new Label("Send:");
-        csLabel.setBounds(12, 392, 85, 30);
-        mainFrame.add(csLabel);
+	static void help() {
+		System.out.println("Chat [-help] [-props <properties>]");
+	}
 
-        leaveButton=new JButton("Leave");
-        leaveButton.setBounds(12, 428, 150, 30);
-        leaveButton.addMouseListener(this);
-        mainFrame.add(leaveButton);
+	public void start() {
+		mainFrame = new Frame();
+		mainFrame.setLayout(null);
+		mainFrame.setSize(600, 507);
+		mainFrame.addWindowListener(this);
 
-        sendButton=new JButton("Send");
-        sendButton.setBounds(182, 428, 150, 30);
-        sendButton.addMouseListener(this);
-        mainFrame.add(sendButton);
+		txtArea = new TextArea();
+		txtArea.setBounds(12, 36, 550, 348);
+		txtArea.setEditable(false);
+		mainFrame.add(txtArea);
 
-        clearButton=new JButton("Clear");
-        clearButton.setBounds(340, 428, 150, 30);
-        clearButton.addMouseListener(this);
-        mainFrame.add(clearButton);
+		txtField = new TextField();
+		txtField.setBounds(100, 392, 400, 30);
+		mainFrame.add(txtField);
 
-        try {
-            channel=new JChannel(props);
-            channel.setOpt(Channel.AUTO_RECONNECT, Boolean.TRUE);
-            channel.setOpt(Channel.AUTO_GETSTATE, Boolean.TRUE);
-            channel.setOpt(Channel.BLOCK, Boolean.TRUE);
-            System.out.println("Connecting to " + group_name);
-            channel.connect(group_name);
-            channel.setReceiver(this);
-            channel.getState(null, 5000);
-        }
-        catch(Exception e) {
-            ta.append(e.toString());
-        }
-        mainFrame.pack();
-        mainFrame.setLocation(15, 25);
-        mainFrame.setBounds(new Rectangle(580, 480));
-        mainFrame.setVisible(true);
-        mainFrame.setVisible(true);
-        if(!history.isEmpty()) {
-            for(Iterator it=history.iterator(); it.hasNext();) {
-                String s=(String)it.next();
-                ta.append(s + "\n");
-            }
-        }
-    }
+		csLabel = new Label("Send:");
+		csLabel.setBounds(12, 392, 85, 30);
+		mainFrame.add(csLabel);
 
-    /* -------------------- Interface MessageListener ------------------- */
+		leaveButton = new JButton("Leave");
+		leaveButton.setBounds(12, 428, 150, 30);
+		leaveButton.addMouseListener(this);
+		mainFrame.add(leaveButton);
 
-    public void receive(Message msg) {
-        Object o;
+		sendButton = new JButton("Send");
+		sendButton.setBounds(182, 428, 150, 30);
+		sendButton.addMouseListener(this);
+		mainFrame.add(sendButton);
 
-        try {
-            o=msg.getObject();
-            ta.append(o + " [" + msg.getSrc() + "]\n");
-            history.add(o);
-        }
-        catch(Exception e) {
-            ta.append("Chat.receive(): " + e);
-        }
-    }
+		clearButton = new JButton("Clear");
+		clearButton.setBounds(340, 428, 150, 30);
+		clearButton.addMouseListener(this);
+		mainFrame.add(clearButton);
 
-    public byte[] getState(String state_id) {
-        //partial state transfer not used
-        return null;
-    }
+		link();
 
-    public byte[] getState() {
-        try {
-            return Util.objectToByteBuffer(history);
-        }
-        catch(Exception e) {
-            return null;
-        }
-    }
+		mainFrame.pack();
+		mainFrame.setLocation(15, 25);
+		mainFrame.setBounds(new Rectangle(580, 480));
+		mainFrame.setVisible(true);
+		mainFrame.show();
 
-    public void setState(byte[] state) {
-        try {
-            history=(LinkedList)Util.objectFromByteBuffer(state);
-        }
-        catch(Exception e) {
-            e.printStackTrace();
-        }
-    }
+		dumpHist();
 
-    public void setState(String state_id, byte[] state) {
-        //partial state transfer not used
-    }
+	}
 
-    public void getState(OutputStream os) {
-        ObjectOutputStream oos=null;
-        try {
-            oos=new ObjectOutputStream(os);
-            oos.writeObject(history);
-            oos.flush();
-        }
-        catch(IOException e) {
-        }
-        finally {
-            Util.close(oos);
-        }
-    }
+	public void mouseClicked(MouseEvent e) {
+		Object obj = e.getSource();
 
-    public void setState(InputStream is) {
-        ObjectInputStream ois=null;
-        try {
-            ois=new ObjectInputStream(is);
-            history=(LinkedList)ois.readObject();
-        }
-        catch(Exception e) {
-        }
-        finally {
-            Util.close(ois);
-        }
-    }
+		if (obj == leaveButton) {
+			handleLeave();
+			System.exit(0);
+		} else if (obj == sendButton)
+			handleSend(txtField.getText());
+		else if (obj == clearButton)
+			txtArea.setText("");
+	}
 
-    public void getState(String state_id, OutputStream ostream) {
-        //partial state transfer not used
+	public void mouseEntered(MouseEvent e) {
+	}
 
-    }
+	public void mouseExited(MouseEvent e) {
+	}
 
+	public void mousePressed(MouseEvent e) {
+	}
 
-    public void setState(String state_id, InputStream istream) {
-        //partial state transfer not used
-    }
+	public void mouseReleased(MouseEvent e) {
+	}
 
-    /* ----------------- End of Interface MessageListener --------------- */
+	public void windowActivated(WindowEvent e) {
+	}
 
-    /* ------------------- Interface MembershipListener ----------------- */
+	public void windowClosed(WindowEvent e) {
+	}
 
-    public void viewAccepted(View new_view) {
-        ta.append("Received view " + new_view + '\n');
-    }
+	public void windowClosing(WindowEvent e) {
+		handleLeave();
+		System.exit(0);
+	}
 
+	public void windowDeactivated(WindowEvent e) {
+	}
 
-    public void suspect(Address suspected_mbr) {
-    }
+	public void windowDeiconified(WindowEvent e) {
+	}
 
+	public void windowIconified(WindowEvent e) {
+	}
 
-    public void block() {
-    }
-
-    public void unblock() {
-    }
-
-    /* --------------- End of Interface MembershipListener -------------- */
-
-
-    private synchronized void handleLeave() {
-        try {
-            System.out.print("Stopping PullPushAdapter");
-            System.out.println(" -- done");
-
-            System.out.print("Disconnecting the channel");
-            channel.disconnect();
-            System.out.println(" -- done");
-
-            System.out.print("Closing the channel");
-            channel.close();
-            System.out.println(" -- done");
-            System.exit(0);
-        }
-        catch(Exception e) {
-            e.printStackTrace();
-            ta.append("Failed leaving the group: " + e.toString() + '\n');
-        }
-    }
-
-
-    private void handleSend() {
-        try {
-            Message msg=new Message(null, null, username + ": " + tf.getText());
-            channel.send(msg);
-        }
-        catch(Exception e) {
-            ta.append("Failed sending message: " + e.toString() + '\n');
-        }
-    }
-
-
-    public void mouseClicked(MouseEvent e) {
-        Object obj=e.getSource();
-
-        if(obj == leaveButton)
-            handleLeave();
-        else if(obj == sendButton)
-            handleSend();
-        else if(obj == clearButton)
-            ta.setText("");
-    }
-
-    public void mouseEntered(MouseEvent e) {
-    }
-
-    public void mouseExited(MouseEvent e) {
-    }
-
-    public void mousePressed(MouseEvent e) {
-    }
-
-    public void mouseReleased(MouseEvent e) {
-    }
-
-    public void windowActivated(WindowEvent e) {
-    }
-
-    public void windowClosed(WindowEvent e) {
-    }
-
-    public void windowClosing(WindowEvent e) {
-        System.exit(0);
-    }
-
-    public void windowDeactivated(WindowEvent e) {
-    }
-
-    public void windowDeiconified(WindowEvent e) {
-    }
-
-    public void windowIconified(WindowEvent e) {
-    }
-
-    public void windowOpened(WindowEvent e) {
-    }
+	public void windowOpened(WindowEvent e) {
+	}
 
 }
