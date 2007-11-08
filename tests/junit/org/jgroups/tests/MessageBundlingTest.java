1e1454a8c478f76181e8a2c9dca97d19c639629f
Vladimir Blagojevic <vblagoje@redhat.com>
2007-11-08 18:52:59 +0000

    make latency less than service message ack timeout in Multiplexer

--- tests/junit/org/jgroups/tests/MessageBundlingTest.java
+++ tests/junit/org/jgroups/tests/MessageBundlingTest.java
@@ -13,12 +13,12 @@ import java.util.*;
 /**
  * Measure the latency between messages with message bundling enabled at the transport level
  * @author Bela Ban
- * @version $Id: MessageBundlingTest.java,v 1.7 2007/11/08 17:00:04 belaban Exp $
+ * @version $Id: MessageBundlingTest.java,v 1.8 2007/11/08 18:52:59 vlada Exp $
  */
 public class MessageBundlingTest extends ChannelTestBase {
     private JChannel ch1, ch2;
     private MyReceiver r2;
-    private final static long LATENCY=3000L;
+    private final static long LATENCY=1500L;
     private final static long SLEEP=5000L;
     private static final boolean BUNDLING=true;
     private static final int MAX_BYTES=20000;
