7d86822df191b6e1e04e7c482415ae47d64e4d9a
Bela Ban <belaban@yahoo.com>
2007-11-19 09:28:14 +0000

    added hashCode()

--- src/org/jgroups/protocols/PingRsp.java
+++ src/org/jgroups/protocols/PingRsp.java
@@ -1,4 +1,4 @@
-// $Id: PingRsp.java,v 1.10 2007/05/09 23:50:22 belaban Exp $
+// $Id: PingRsp.java,v 1.11 2007/11/19 09:28:14 belaban Exp $
 
 package org.jgroups.protocols;
 
@@ -34,6 +34,17 @@ public class PingRsp implements Serializable, Streamable {
             return false;
         PingRsp other=(PingRsp)obj;
         return own_addr != null && other.own_addr != null && own_addr.equals(other.own_addr);
+    }
+
+    public int hashCode() {
+        int retval=0;
+        if(own_addr != null)
+            retval+=own_addr.hashCode();
+        if(coord_addr != null)
+            retval+=coord_addr.hashCode();
+        if(retval == 0)
+            retval=super.hashCode();
+        return retval;
     }
 
     public boolean isCoord() {
