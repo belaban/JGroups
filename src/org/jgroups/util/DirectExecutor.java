4a8f4e0e6757a35ca8d530acfa0303fb7dd30a27
Bela Ban <belaban@yahoo.com>
2007-11-21 14:00:12 +0000

    changed ExecutorService in TP back to Executor, reason is that between JDK 5 and 6, the interface ExecutorService has changed

--- src/org/jgroups/util/DirectExecutor.java
+++ src/org/jgroups/util/DirectExecutor.java
@@ -1,63 +1,15 @@
 package org.jgroups.util;
 
-import java.util.concurrent.*;
-import java.util.List;
-import java.util.Collection;
+import java.util.concurrent.Executor;
 
 /**
  * @author Bela Ban
- * @version $Id: DirectExecutor.java,v 1.4 2007/11/21 13:02:00 belaban Exp $
+ * @version $Id: DirectExecutor.java,v 1.5 2007/11/21 14:00:14 belaban Exp $
  */
-public class DirectExecutor implements ExecutorService {
+public class DirectExecutor implements Executor {
     public void execute(Runnable command) {
         command.run();
     }
 
 
-    public void shutdown() {
-    }
-
-    public List<Runnable> shutdownNow() {
-        return null;
-    }
-
-    public boolean isShutdown() {
-        return false;
-    }
-
-    public boolean isTerminated() {
-        return false;
-    }
-
-    public boolean awaitTermination(long timeout, TimeUnit unit) throws InterruptedException {
-        return false;
-    }
-
-    public <T> Future<T> submit(Callable<T> task) {
-        return null;
-    }
-
-    public <T> Future<T> submit(Runnable task, T result) {
-        return null;
-    }
-
-    public Future<?> submit(Runnable task) {
-        return null;
-    }
-
-    public <T> List<Future<T>> invokeAll(Collection<Callable<T>> tasks) throws InterruptedException {
-        return null;
-    }
-
-    public <T> List<Future<T>> invokeAll(Collection<Callable<T>> tasks, long timeout, TimeUnit unit) throws InterruptedException {
-        return null;
-    }
-
-    public <T> T invokeAny(Collection<Callable<T>> tasks) throws InterruptedException, ExecutionException {
-        return null;
-    }
-
-    public <T> T invokeAny(Collection<Callable<T>> tasks, long timeout, TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException {
-        return null;
-    }
 }
