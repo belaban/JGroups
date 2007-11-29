1dc80a4993fbc530139bfa7f53def5e57497630c
Bela Ban <belaban@yahoo.com>
2007-11-29 11:25:27 +0000

    fixed incorrect size( if own_addr or coord_addr were null

--- src/org/jgroups/protocols/PingRsp.java
+++ src/org/jgroups/protocols/PingRsp.java
@@ -1,4 +1,4 @@
-// $Id: PingRsp.java,v 1.10 2007/05/09 23:50:22 belaban Exp $
+// $Id: PingRsp.java,v 1.10.2.1 2007/11/29 11:25:27 belaban Exp $
 
 package org.jgroups.protocols;
 
@@ -36,17 +36,31 @@ public class PingRsp implements Serializable, Streamable {
         return own_addr != null && other.own_addr != null && own_addr.equals(other.own_addr);
     }
 
+    public int hashCode() {
+        int retval=0;
+        if(own_addr != null)
+            retval+=own_addr.hashCode();
+        if(coord_addr != null)
+            retval+=coord_addr.hashCode();
+        if(retval == 0)
+            retval=super.hashCode();
+        return retval;
+    }
+
     public boolean isCoord() {
         return is_server && own_addr != null && coord_addr != null && own_addr.equals(coord_addr);
     }
 
     public int size() {
         int retval=Global.BYTE_SIZE *3; // for is_server, plus 2 presence bytes
-        retval+=2 * Global.BYTE_SIZE;   // 1 boolean for each address: IpAddress or other address ?
-        if(own_addr != null)
+        if(own_addr != null) {
+            retval+=Global.BYTE_SIZE; // 1 boolean for: IpAddress or other address ?
             retval+=own_addr.size();
-        if(coord_addr != null)
+        }
+        if(coord_addr != null) {
+            retval+=Global.BYTE_SIZE; // 1 boolean for: IpAddress or other address ?
             retval+=coord_addr.size();
+        }
         return retval;
     }
 
