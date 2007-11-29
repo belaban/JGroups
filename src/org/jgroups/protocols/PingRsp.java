77677bfad0a133f5c80d72b409601d7e144ccfe3
belaban <belaban>
2007-11-29 11:16:40 +0000

    fixed incorrect size() if own addr or coord_addr are null

--- src/org/jgroups/protocols/PingRsp.java
+++ src/org/jgroups/protocols/PingRsp.java
@@ -1,4 +1,4 @@
-// $Id: PingRsp.java,v 1.11 2007/11/19 09:28:14 belaban Exp $
+// $Id: PingRsp.java,v 1.12 2007/11/29 11:16:40 belaban Exp $
 
 package org.jgroups.protocols;
 
@@ -53,11 +53,14 @@ public class PingRsp implements Serializable, Streamable {
 
     public int size() {
         int retval=Global.BYTE_SIZE *3; // for is_server, plus 2 presence bytes
-        retval+=2 * Global.BYTE_SIZE;   // 1 boolean for each address: IpAddress or other address ?
-        if(own_addr != null)
+        if(own_addr != null) {
+            retval+=Global.BYTE_SIZE; // 1 boolean for: IpAddress or other address ?
             retval+=own_addr.size();
-        if(coord_addr != null)
+        }
+        if(coord_addr != null) {
+            retval+=Global.BYTE_SIZE; // 1 boolean for: IpAddress or other address ?
             retval+=coord_addr.size();
+        }
         return retval;
     }
 
