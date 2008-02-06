e15e040c8891660cdd767b2766328ea2567c282d
Bela Ban <belaban@yahoo.com>
2008-02-06 11:44:19 +0000

    ns

--- bin/gossiprouter.sh
+++ bin/gossiprouter.sh
@@ -6,6 +6,7 @@ for i in ../lib/*.jar
   do CP=$CP:$i
 done
 
+OPTS="-Dlog4j.configuration=file:$HOME/log4j.properties -Djava.net.preferIPv4Stack=true"
 
-java -classpath $CLASSPATH $JAVA_OPTS org.jgroups.stack.GossipRouter -port 12001 $*
+java $OPTS -classpath $CLASSPATH $JAVA_OPTS org.jgroups.stack.GossipRouter -port 12001 $*
 
