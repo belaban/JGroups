8f477104fb4ff53f33e7871cfdbda21a523cba13
vlada <vlada>
2008-02-06 02:11:13 +0000

    [JGRP-682] - TUNNEL: reconnector thread is not started in certain scenarios

--- src/org/jgroups/protocols/TUNNEL.java
+++ src/org/jgroups/protocols/TUNNEL.java
@@ -1,8 +1,6 @@
-// $Id: TUNNEL.java,v 1.46 2008/01/22 10:44:30 belaban Exp $
-
+// $Id: TUNNEL.java,v 1.47 2008/02/06 02:11:14 vlada Exp $
 
 package org.jgroups.protocols;
-
 
 import org.jgroups.Address;
 import org.jgroups.Event;
@@ -22,252 +20,275 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
-
 /**
- * Replacement for UDP. Instead of sending packets via UDP, a TCP connection is opened to a Router
- * (using the RouterStub client-side stub),
- * the IP address/port of which was given using channel properties <code>router_host</code> and
- * <code>router_port</code>. All outgoing traffic is sent via this TCP socket to the Router which
- * distributes it to all connected TUNNELs in this group. Incoming traffic received from Router will
+ * Replacement for UDP. Instead of sending packets via UDP, a TCP connection is
+ * opened to a Router (using the RouterStub client-side stub), the IP
+ * address/port of which was given using channel properties
+ * <code>router_host</code> and <code>router_port</code>. All outgoing
+ * traffic is sent via this TCP socket to the Router which distributes it to all
+ * connected TUNNELs in this group. Incoming traffic received from Router will
  * simply be passed up the stack.
  * 
- * <p>A TUNNEL layer can be used to penetrate a firewall, most firewalls allow creating TCP connections
- * to the outside world, however, they do not permit outside hosts to initiate a TCP connection to a host
- * inside the firewall. Therefore, the connection created by the inside host is reused by Router to
+ * <p>
+ * A TUNNEL layer can be used to penetrate a firewall, most firewalls allow
+ * creating TCP connections to the outside world, however, they do not permit
+ * outside hosts to initiate a TCP connection to a host inside the firewall.
+ * Therefore, the connection created by the inside host is reused by Router to
  * send traffic from an outside host to a host inside the firewall.
+ * 
  * @author Bela Ban
  */
-public class TUNNEL extends TP {           
-    private String router_host=null;
-    private int router_port=0;     
-    private RouterStub stub;    
-    long reconnect_interval=5000; /** time to wait in ms between reconnect attempts */
+public class TUNNEL extends TP {
+    private String router_host = null;
+
+    private int router_port = 0;
+
+    private RouterStub stub;
+
+    long reconnect_interval = 5000;
+    
+    /*
+     * flag indicating if tunnel was destroyed intentionally (disconnect, channel destroy etc)
+     */
+    private volatile boolean intentionallyTornDown = false; 
+
+    /** time to wait in ms between reconnect attempts */
 
     @GuardedBy("reconnectorLock")
-    private Future        reconnectorFuture=null;
-    private final Lock    reconnectorLock=new ReentrantLock();
-    
-    public TUNNEL() {}
+    private Future<?> reconnectorFuture = null;
 
+    private final Lock reconnectorLock = new ReentrantLock();
+
+    public TUNNEL(){}
 
     public String toString() {
         return "Protocol TUNNEL(local_addr=" + local_addr + ')';
     }
+
     /*------------------------------ Protocol interface ------------------------------ */
 
     public String getName() {
         return "TUNNEL";
     }
-    
-    public void init() throws Exception {
+
+    public void init() throws Exception {        
         super.init();
         if(stack != null && stack.timer != null)
-            timer=stack.timer;
+            timer = stack.timer;
         else
             throw new Exception("TUNNEL.init(): timer cannot be retrieved from protocol stack");
     }
 
     public void start() throws Exception {
-    	//loopback turned on is mandatory
-    	loopback = true;
-    	
-    	stub = new RouterStub(router_host,router_port,bind_addr);
-    	stub.setConnectionListener(new StubConnectionListener());
-        local_addr=stub.getLocalAddress();
-         if(additional_data != null && local_addr instanceof IpAddress)
-            ((IpAddress)local_addr).setAdditionalData(additional_data); 
+        // loopback turned on is mandatory
+        loopback = true;
+        intentionallyTornDown = false;
+
+        stub = new RouterStub(router_host, router_port, bind_addr);
+        stub.setConnectionListener(new StubConnectionListener());
+        local_addr = stub.getLocalAddress();
+        if(additional_data != null && local_addr instanceof IpAddress)
+            ((IpAddress) local_addr).setAdditionalData(additional_data);
         super.start();
     }
 
-
-    public void stop() {
-    	super.stop();      
-        teardownTunnel();       
-        local_addr=null;        
+    public void stop() {        
+        teardownTunnel();
+        super.stop();        
+        local_addr = null;
     }
-
-
 
     /** Setup the Protocol instance acording to the configuration string */
     public boolean setProperties(Properties props) {
         String str;
 
         super.setProperties(props);
-        str=props.getProperty("router_host");
-        if(str != null) {
-            router_host=str;
+        str = props.getProperty("router_host");
+        if(str != null){
+            router_host = str;
             props.remove("router_host");
         }
 
-        str=props.getProperty("router_port");
-        if(str != null) {
-            router_port=Integer.parseInt(str);
+        str = props.getProperty("router_port");
+        if(str != null){
+            router_port = Integer.parseInt(str);
             props.remove("router_port");
         }
 
-        if(log.isDebugEnabled()) {
+        if(log.isDebugEnabled()){
             log.debug("router_host=" + router_host + ";router_port=" + router_port);
         }
 
-        if(router_host == null || router_port == 0) {
-            if(log.isErrorEnabled()) {
+        if(router_host == null || router_port == 0){
+            if(log.isErrorEnabled()){
                 log.error("both router_host and router_port have to be set !");
                 return false;
             }
         }
 
-        str=props.getProperty("reconnect_interval");
-        if(str != null) {
-            reconnect_interval=Long.parseLong(str);
+        str = props.getProperty("reconnect_interval");
+        if(str != null){
+            reconnect_interval = Long.parseLong(str);
             props.remove("reconnect_interval");
         }
-        
-        if(!props.isEmpty()) {
-            StringBuilder sb=new StringBuilder();
-            for(Enumeration e=props.propertyNames(); e.hasMoreElements();) {
+
+        if(!props.isEmpty()){
+            StringBuilder sb = new StringBuilder();
+            for(Enumeration e = props.propertyNames();e.hasMoreElements();){
                 sb.append(e.nextElement().toString());
-                if(e.hasMoreElements()) {
+                if(e.hasMoreElements()){
                     sb.append(", ");
                 }
             }
-            if(log.isErrorEnabled()) log.error("The following properties are not recognized: " + sb);
+            if(log.isErrorEnabled())
+                log.error("The following properties are not recognized: " + sb);
             return false;
         }
         return true;
-    }     
+    }
 
     /** Tears the TCP connection to the router down */
     void teardownTunnel() {
-    	stopReconnecting();
+        intentionallyTornDown = true;
+        stopReconnecting();
         stub.disconnect();
     }
 
     public Object handleDownEvent(Event evt) {
-        Object retEvent=super.handleDownEvent(evt);
-        switch(evt.getType()) {
-            case Event.CONNECT:
-            case Event.CONNECT_WITH_STATE_TRANSFER:
-                try {
-                    stub.connect(channel_name);
-                }
-                catch(Exception e) {
-                    if(log.isErrorEnabled())
-                        log.error("failed connecting to GossipRouter at " + router_host + ":" + router_port);
-                }
-                break;
+        Object retEvent = super.handleDownEvent(evt);
+        switch(evt.getType()){
+        case Event.CONNECT:
+        case Event.CONNECT_WITH_STATE_TRANSFER:
+            try{
+                stub.connect(channel_name);
+            }catch(Exception e){
+                if(log.isErrorEnabled())
+                    log.error("failed connecting to GossipRouter at " + router_host
+                              + ":"
+                              + router_port);
+                startReconnecting();
+            }
+            break;
 
-            case Event.DISCONNECT:
-                teardownTunnel();
-                break;
+        case Event.DISCONNECT:
+            teardownTunnel();
+            break;
         }
         return retEvent;
     }
-    
+
     private void startReconnecting() {
         reconnectorLock.lock();
-        try {
-            if(reconnectorFuture == null || reconnectorFuture.isDone()) {             
-            	final Runnable reconnector = new Runnable(){
-					public void run() {						
-						try{
-							stub.connect(channel_name);
-						}catch(Exception ex){
-							if(log.isTraceEnabled())
-								log.trace("failed reconnecting", ex);
-						}						
-					}            	
-            	};
-                reconnectorFuture=timer.scheduleWithFixedDelay(reconnector, 0, reconnect_interval, TimeUnit.MILLISECONDS);                
+        try{
+            if(reconnectorFuture == null || reconnectorFuture.isDone()){
+                final Runnable reconnector = new Runnable() {
+                    public void run() {
+                        try{
+                            if(!intentionallyTornDown){
+                                if(log.isDebugEnabled()){
+                                    log.debug("Reconnecting " + getLocalAddress()
+                                              + " to router at "
+                                              + router_host
+                                              + ":"
+                                              + router_port);
+                                }
+                                stub.connect(channel_name);
+                            }
+                        }catch(Exception ex){
+                            if(log.isTraceEnabled())
+                                log.trace("failed reconnecting", ex);
+                        }
+                    }
+                };
+                reconnectorFuture = timer.scheduleWithFixedDelay(reconnector,
+                                                                 0,
+                                                                 reconnect_interval,
+                                                                 TimeUnit.MILLISECONDS);
             }
-        }
-        finally {
+        }finally{
             reconnectorLock.unlock();
         }
     }
 
     private void stopReconnecting() {
         reconnectorLock.lock();
-        try {
-            if(reconnectorFuture != null) {
+        try{
+            if(reconnectorFuture != null){
                 reconnectorFuture.cancel(true);
-                reconnectorFuture=null;
+                reconnectorFuture = null;
             }
-        }
-        finally {
+        }finally{
             reconnectorLock.unlock();
         }
     }
-    
-    private class StubConnectionListener implements RouterStub.ConnectionListener{
 
-    	private volatile int currentState = RouterStub.STATUS_DISCONNECTED;
-		public void connectionStatusChange(int newState) {
-			if(currentState == RouterStub.STATUS_CONNECTED && newState == RouterStub.STATUS_CONNECTION_LOST){
-				startReconnecting();	
-		    }
-			else if(currentState != RouterStub.STATUS_CONNECTED && newState == RouterStub.STATUS_CONNECTED){
-				stopReconnecting();
-				Thread receiver = new Thread(Util.getGlobalThreadGroup(), new TunnelReceiver(), "TUNNEL receiver");
-				receiver.setDaemon(true);
-				receiver.start();
-			}
-			currentState = newState;
-		}    	
+    private class StubConnectionListener implements RouterStub.ConnectionListener {
+
+        private volatile int currentState = RouterStub.STATUS_DISCONNECTED;
+
+        public void connectionStatusChange(int newState) {            
+            if(newState == RouterStub.STATUS_DISCONNECTED){
+                startReconnecting();
+            }else if(currentState != RouterStub.STATUS_CONNECTED && newState == RouterStub.STATUS_CONNECTED){
+                stopReconnecting();
+                Thread t = getProtocolStack().getThreadFactory().newThread(new TunnelReceiver(), "TUNNEL receiver");
+                t.setDaemon(true);
+                t.start();
+            }
+            currentState = newState;
+        }
     }
-    
+
     private class TunnelReceiver implements Runnable {
-		public void run() {
-			while(stub.isConnected()){
-				Address dest = null;
-				Address src = null; 
-				int len;
-				byte[] data = null;
-				DataInputStream input = null;
-				try{
-					input = stub.getInputStream();
-					dest = Util.readAddress(input);
-					len = input.readInt();
-					if(len > 0){
-						data = new byte[len];
-						input.readFully(data, 0, len);						
-						receive(dest, src, data, 0, len);
-					}
-				}catch(SocketException se){
-					//if(log.isWarnEnabled()) log.warn("failure in TUNNEL receiver thread", se);					
-				}catch(IOException ioe){			
-					 //if(log.isWarnEnabled()) log.warn("failure in TUNNEL receiver thread", ioe);					
-				}catch(Exception e){
-					if(log.isWarnEnabled())
-						log.warn("failure in TUNNEL receiver thread", e);
-				}
-			}
-		}
-	}   
-    
-	public void sendToAllMembers(byte[] data, int offset, int length) throws Exception {
-		stub.sendToAllMembers(data, offset, length);		
-	}
+        public void run() {
+            while(stub.isConnected()){
+                Address dest = null;                
+                int len;
+                byte[] data = null;
+                DataInputStream input = null;
+                try{
+                    input = stub.getInputStream();
+                    dest = Util.readAddress(input);                    
+                    len = input.readInt();
+                    if(len > 0){
+                        data = new byte[len];
+                        input.readFully(data, 0, len);                        
+                        receive(dest, null/*src will be read from data*/, data, 0, len);
+                    }
+                }catch(SocketException se){
+                    // if(log.isWarnEnabled()) log.warn("failure in TUNNEL
+                    // receiver thread", se);
+                }catch(IOException ioe){
+                    // if(log.isWarnEnabled()) log.warn("failure in TUNNEL
+                    // receiver thread", ioe);
+                }catch(Exception e){
+                    if(log.isWarnEnabled())
+                        log.warn("failure in TUNNEL receiver thread", e);
+                }
+            }
+        }
+    }
 
+    public void sendToAllMembers(byte[] data, int offset, int length) throws Exception {
+        stub.sendToAllMembers(data, offset, length);
+    }
 
-	public void sendToSingleMember(Address dest, byte[] data, int offset, int length) throws Exception {
-		stub.sendToSingleMember(dest, data, offset, length);		
-	}
+    public void sendToSingleMember(Address dest, byte[] data, int offset, int length) throws Exception {
+        stub.sendToSingleMember(dest, data, offset, length);
+    }
 
-	
-	public String getInfo() {
-		if(stub!=null)
-			return stub.toString();
-		else
-			return "RouterStub not yet initialized";
-	}
+    public String getInfo() {
+        if(stub != null)
+            return stub.toString();
+        else
+            return "RouterStub not yet initialized";
+    }
 
+    public void postUnmarshalling(Message msg, Address dest, Address src, boolean multicast) {
+        msg.setDest(dest);
+    }
 
-	public void postUnmarshalling(Message msg, Address dest, Address src, boolean multicast) {	
-		msg.setDest(dest);
-	}
-
-	
-	public void postUnmarshallingList(Message msg, Address dest, boolean multicast) {
-		msg.setDest(dest);
-	}
+    public void postUnmarshallingList(Message msg, Address dest, boolean multicast) {
+        msg.setDest(dest);
+    }
 }
