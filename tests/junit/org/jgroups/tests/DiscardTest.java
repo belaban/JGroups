c6f0d137d39e11059c725791f0608e0d12424296
vlada <vlada>
2008-02-14 01:49:35 +0000

    update tests to new test framework

--- tests/junit/org/jgroups/tests/DiscardTest.java
+++ tests/junit/org/jgroups/tests/DiscardTest.java
@@ -1,35 +1,34 @@
-
 package org.jgroups.tests;
 
+import java.util.Properties;
 
 import junit.framework.Test;
-import junit.framework.TestCase;
 import junit.framework.TestSuite;
-import org.jgroups.*;
+
+import org.jgroups.Address;
+import org.jgroups.JChannel;
+import org.jgroups.Message;
+import org.jgroups.ReceiverAdapter;
+import org.jgroups.View;
 import org.jgroups.protocols.DISCARD;
 import org.jgroups.stack.ProtocolStack;
 import org.jgroups.util.Promise;
 import org.jgroups.util.Util;
 
-import java.util.Properties;
-
-
 /**
  * Tests the NAKACK (retransmission) and STABLE (garbage collection) protocols
  * by discarding 10% of all network-bound messages
+ * 
  * @author Bela Ban
- * @version $Id: DiscardTest.java,v 1.14 2007/10/01 07:56:42 belaban Exp $
+ * @version $Id: DiscardTest.java,v 1.14.2.1 2008/02/14 01:49:35 vlada Exp $
  */
-public class DiscardTest extends TestCase {
+public class DiscardTest extends ChannelTestBase {
     JChannel ch1, ch2;
-
-    static final String udp_props="udp.xml"; // located in JGroups/conf, needs to be in the classpath
     static final long NUM_MSGS=10000;
-    static final int  MSG_SIZE=1000;
+    static final int MSG_SIZE=1000;
     private static final String GROUP="DiscardTestGroup";
-    final Promise ch1_all_received=new Promise();
-    final Promise ch2_all_received=new Promise();
-
+    final Promise<Long> ch1_all_received=new Promise<Long>();
+    final Promise<Long> ch2_all_received=new Promise<Long>();
 
     public DiscardTest(String name) {
         super(name);
@@ -40,22 +39,28 @@ public class DiscardTest extends TestCase {
         ch1_all_received.reset();
         ch2_all_received.reset();
     }
+    
+    protected void tearDown() throws Exception{
+        ch2.close();
+        ch1.close();
+        super.tearDown();
+    }
 
     public void testDiscardProperties() throws Exception {
-        _testLosslessReception(udp_props, true);
+        _testLosslessReception(true);
     }
 
     public void testFastProperties() throws Exception {
-        _testLosslessReception(udp_props, false);
+        _testLosslessReception(false);
     }
 
-    public void _testLosslessReception(String props, boolean discard) throws Exception {
+    public void _testLosslessReception(boolean discard) throws Exception {
         Address ch1_addr, ch2_addr;
         long start, stop;
 
-        ch1=new JChannel(props);
+        ch1=createChannel();
         ch1.setReceiver(new MyReceiver(ch1_all_received, NUM_MSGS, "ch1"));
-        ch2=new JChannel(props);
+        ch2=createChannel();
         ch2.setReceiver(new MyReceiver(ch2_all_received, NUM_MSGS, "ch2"));
 
         if(discard) {
@@ -63,13 +68,12 @@ public class DiscardTest extends TestCase {
             Properties properties=new Properties();
             properties.setProperty("down", "0.1");
 
-            ch1.getProtocolStack().insertProtocol(discard_prot, ProtocolStack.BELOW, "PING");
+            ch1.getProtocolStack().insertProtocol(discard_prot, ProtocolStack.BELOW, "MERGE2");
             discard_prot=new DISCARD();
             properties=new Properties();
             properties.setProperty("down", "0.1");
-            ch2.getProtocolStack().insertProtocol(discard_prot, ProtocolStack.BELOW, "PING");
+            ch2.getProtocolStack().insertProtocol(discard_prot, ProtocolStack.BELOW, "MERGE2");
         }
-
 
         ch1.connect(GROUP);
         ch1_addr=ch1.getLocalAddress();
@@ -85,7 +89,7 @@ public class DiscardTest extends TestCase {
 
         System.out.println("sending " + NUM_MSGS + " 1K messages to all members (including myself)");
         start=System.currentTimeMillis();
-        for(int i=0; i < NUM_MSGS; i++) {
+        for(int i=0;i < NUM_MSGS;i++) {
             Message msg=createMessage(MSG_SIZE);
             ch1.send(msg);
             if(i % 1000 == 0)
@@ -101,24 +105,25 @@ public class DiscardTest extends TestCase {
         stop=System.currentTimeMillis();
         System.out.println("-- received " + num_msgs + " messages on ch2");
 
-        long diff=stop-start;
+        long diff=stop - start;
         double msgs_sec=NUM_MSGS / (diff / 1000.0);
-        System.out.println("== Sent and received " + NUM_MSGS + " in " + diff + "ms, " +
-                           msgs_sec + " msgs/sec");
-
-        ch2.close();
-        ch1.close();
+        System.out.println("== Sent and received " + NUM_MSGS
+                           + " in "
+                           + diff
+                           + "ms, "
+                           + msgs_sec
+                           + " msgs/sec");
+        
     }
 
-
     static class MyReceiver extends ReceiverAdapter {
-        final Promise p;
+        final Promise<Long> p;
         final long num_msgs_expected;
         long num_msgs=0;
         String channel_name;
         boolean operational=true;
 
-        public MyReceiver(final Promise p, final long num_msgs_expected, String channel_name) {
+        public MyReceiver(final Promise<Long> p,final long num_msgs_expected,String channel_name) {
             this.p=p;
             this.num_msgs_expected=num_msgs_expected;
             this.channel_name=channel_name;
@@ -133,7 +138,9 @@ public class DiscardTest extends TestCase {
                 System.out.println("-- received " + num_msgs + " on " + channel_name);
 
             if(num_msgs >= num_msgs_expected) {
-                System.out.println("SUCCESS: received all " + num_msgs_expected + " messages on " + channel_name);
+                System.out.println("SUCCESS: received all " + num_msgs_expected
+                                   + " messages on "
+                                   + channel_name);
                 operational=false;
                 p.setResult(new Long(num_msgs));
             }
@@ -144,15 +151,12 @@ public class DiscardTest extends TestCase {
         }
     }
 
-
     private static Message createMessage(int size) {
         byte[] buf=new byte[size];
-        for(int i=0; i < buf.length; i++) buf[i]=(byte)'x';
+        for(int i=0;i < buf.length;i++)
+            buf[i]=(byte)'x';
         return new Message(null, null, buf);
     }
-
-
-
 
     public static Test suite() {
         return new TestSuite(DiscardTest.class);
@@ -162,9 +166,4 @@ public class DiscardTest extends TestCase {
         junit.textui.TestRunner.run(suite());
     }
 
-
-
-
 }
-
-
