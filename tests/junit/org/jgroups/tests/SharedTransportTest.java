cf49ab29ba79ba80f87df595cd2efe5867944268
belaban <belaban>
2008-02-22 16:28:50 +0000

    added test for http://jira.jboss.com/jira/browse/JGRP-689

--- tests/junit/org/jgroups/tests/SharedTransportTest.java
+++ tests/junit/org/jgroups/tests/SharedTransportTest.java
@@ -14,7 +14,7 @@ import java.util.List;
 /**
  * Tests which test the shared transport
  * @author Bela Ban
- * @version $Id: SharedTransportTest.java,v 1.5.2.4 2008/01/25 12:13:21 belaban Exp $
+ * @version $Id: SharedTransportTest.java,v 1.5.2.5 2008/02/22 16:28:50 belaban Exp $
  */
 public class SharedTransportTest extends ChannelTestBase {
     private JChannel a, b, c;
@@ -85,6 +85,84 @@ public class SharedTransportTest extends ChannelTestBase {
         assertEquals(1, view.size());
     }
 
+    /** Tests http://jira.jboss.com/jira/browse/JGRP-689: with TCP or UDP.ip_mcast=false, the transport iterates through
+     * the 'members' instance variable to send a group message. However, the 'members' var is the value of the last
+     * view change received. If we receive multiple view changes, this leads to incorrect membership.
+     *
+     * @throws Exception
+     */
+    public void testView3() throws Exception {
+        a=createSharedChannel(SINGLETON_1);
+        b=createSharedChannel(SINGLETON_1);
+        c=createSharedChannel(SINGLETON_2);
+        r1=new MyReceiver("A::" + SINGLETON_1); r2=new MyReceiver("B::" + SINGLETON_1); r3=new MyReceiver("C::" + SINGLETON_2);
+        a.setReceiver(r1);
+        b.setReceiver(r2);
+        c.setReceiver(r3);
+
+        a.connect("a");
+        c.connect("a");
+
+        View view=a.getView();
+        assertEquals(2, view.size());
+        view=c.getView();
+        assertEquals(2, view.size());
+
+        a.send(new Message(null, null, "msg-1"));
+        c.send(new Message(null, null, "msg-2"));
+
+        Util.sleep(1000); // async sending - wait a little
+        List<Message> list=r1.getList();
+        assertEquals(2, list.size());
+        list=r3.getList();
+        assertEquals(2, list.size());
+
+        r1.clear(); r2.clear(); r3.clear();
+        b.connect("b");
+
+        a.send(new Message(null, null, "msg-3"));
+        b.send(new Message(null, null, "msg-4"));
+        c.send(new Message(null, null, "msg-5"));
+        Util.sleep(1000); // async sending - wait a little
+
+        // printLists(r1, r2, r3);
+        list=r1.getList();
+        assertEquals(2, list.size());
+        list=r2.getList();
+        assertEquals(1, list.size());
+        list=r3.getList();
+        assertEquals(2, list.size());
+    }
+
+    private static void  printLists(MyReceiver... receivers) {
+        StringBuilder sb=new StringBuilder();
+        int cnt=1;
+        for(MyReceiver rec: receivers) {
+            List<Message> list=rec.getList();
+            sb.append("receiver #" + cnt++).append(":\n");
+            for(Message msg: list) {
+                sb.append(msg.getObject()).append("\n");
+            }
+        }
+        System.out.println(sb);
+    }
+
+
+    public void testSharedTransportAndNonsharedTransport() throws Exception {
+        a=createSharedChannel(SINGLETON_1);
+        b=createChannel();
+        a.setReceiver(new MyReceiver("first-channel"));
+        b.setReceiver(new MyReceiver("second-channel"));
+
+        a.connect("x");
+        b.connect("x");
+
+        View view=a.getView();
+        assertEquals(2, view.size());
+        view=b.getView();
+        assertEquals(2, view.size());
+    }
+
     public void testCreationOfDifferentCluster() throws Exception {
         a=createSharedChannel(SINGLETON_1);
         b=createSharedChannel(SINGLETON_2);
@@ -138,9 +216,6 @@ public class SharedTransportTest extends ChannelTestBase {
         assertEquals(1, r1.size());
     }
     
-
-
-
     /**
      * Tests that a second channel with the same group name can be
      * created and connected once the first channel is disconnected.
@@ -155,30 +230,6 @@ public class SharedTransportTest extends ChannelTestBase {
         b=createSharedChannel(SINGLETON_1);
         b.setReceiver(new MyReceiver("A'"));
         b.connect("A");
-    }
-
-
-    public void testReCreationWithSurvivingChannel() throws Exception {
-
-        // Create 2 channels sharing a transport
-        System.out.println("-- creating A");
-        a=createSharedChannel(SINGLETON_1);
-        a.setReceiver(new MyReceiver("A"));
-        a.connect("A");
-
-        System.out.println("-- creating B");
-        b=createSharedChannel(SINGLETON_1);
-        b.setReceiver(new MyReceiver("B"));
-        b.connect("B");
-
-        System.out.println("-- disconnecting A");
-        a.disconnect();
-
-        // a is disconnected so we should be able to create a new channel with group "A"
-        System.out.println("-- creating A'");
-        c=createSharedChannel(SINGLETON_1);
-        c.setReceiver(new MyReceiver("A'"));
-        c.connect("A");
     }
 
     /**
@@ -221,6 +272,29 @@ public class SharedTransportTest extends ChannelTestBase {
 
  
 
+     public void testReCreationWithSurvivingChannel() throws Exception {
+
+        // Create 2 channels sharing a transport
+         System.out.println("-- creating A");
+         a=createSharedChannel(SINGLETON_1);
+         a.setReceiver(new MyReceiver("A"));
+         a.connect("A");
+
+         System.out.println("-- creating B");
+         b=createSharedChannel(SINGLETON_1);
+         b.setReceiver(new MyReceiver("B"));
+         b.connect("B");
+
+         System.out.println("-- disconnecting A");
+         a.disconnect();
+
+         // a is disconnected so we should be able to create a new channel with group "A"
+         System.out.println("-- creating A'");
+         c=createSharedChannel(SINGLETON_1);
+         c.setReceiver(new MyReceiver("A'"));
+         c.connect("A");
+     }
+
 
 
     private static JChannel createSharedChannel(String singleton_name) throws ChannelException {
@@ -258,7 +332,9 @@ public class SharedTransportTest extends ChannelTestBase {
         }
 
         public void viewAccepted(View new_view) {
-            System.out.println("[" + name + "]: view = " + new_view);
+            StringBuilder sb=new StringBuilder();
+            sb.append("[" + name + "]: view = " + new_view);
+            System.out.println(sb);
         }
     }
 
