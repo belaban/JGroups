ac0a5c96adf6d09b50a06ca4fa14b17e3371bb17
vlada <vlada>
2008-03-06 00:50:16 +0000

    comments and formatting

--- src/org/jgroups/annotations/ManagedOperation.java
+++ src/org/jgroups/annotations/ManagedOperation.java
@@ -1,16 +1,19 @@
 package org.jgroups.annotations;
 
 /**
- * User: Chris Mills
- * Date: Aug 14, 2007
- * Time: 2:46:34 PM
+ * Indicates that a method in an MBean class defines an MBean operation. 
+ * This annotation can be applied to a public method of a public class 
+ * that is itself annotated with an @MBean annotation, or inherits such 
+ * an annotation from a superclass.
+ * 
+ * @author Chris Mills
+ * @version $Id: ManagedOperation.java,v 1.2 2008/03/06 00:50:16 vlada Exp $
  */
 
 import java.lang.annotation.*;
 
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.METHOD})
-
+@Target( { ElementType.METHOD })
 public @interface ManagedOperation {
     String description() default "";
 }
--- src/org/jgroups/jmx/ResourceDMBean.java
+++ src/org/jgroups/jmx/ResourceDMBean.java
@@ -14,134 +14,172 @@ import java.lang.reflect.Modifier;
 import java.util.Vector;
 
 /**
- * User: Chris Mills
- * Date: Aug 13, 2007
- * Time: 10:06:31 PM
+ * 
+ * A DynamicMBean wrapping an annotated object instance.  
+ * 
+ * @author Chris Mills
+ * @author Vladimir Blagojevic
+ * @version $Id: ResourceDMBean.java,v 1.2 2008/03/06 00:50:17 vlada Exp $
+ * @see ManagedAttribute
+ * @see ManagedOperation
+ * @see MBean
+ * 
  */
 public class ResourceDMBean implements DynamicMBean {
-    private static final Class[] primitives = {int.class, byte.class, short.class, long.class, float.class, double.class, boolean.class, char.class};
-    private final Log log = LogFactory.getLog(ResourceDMBean.class);
-    private static final String MBEAN_DESCRITION = "Dynamic MBean Description";
+    private static final Class[] primitives= { int.class,
+                                              byte.class,
+                                              short.class,
+                                              long.class,
+                                              float.class,
+                                              double.class,
+                                              boolean.class,
+                                              char.class };
+    
+    private final Log log=LogFactory.getLog(ResourceDMBean.class);
+    private static final String MBEAN_DESCRITION="Dynamic MBean Description";
 
     private final String name;
     private final Object obj;
-    private String description;
+    private String description = "";
 
-    int i = 0;
+    int i=0;
 
-    public ResourceDMBean(Object obj, String name){        
-        this.obj = obj;
-        this.name = name;
+    public ResourceDMBean(Object obj,String name) {
+        this.obj=obj;
+        this.name=name;
     }
 
     public synchronized MBeanInfo getMBeanInfo() {
-        try {           
-            Vector<MBeanAttributeInfo> vctAttributes = new Vector<MBeanAttributeInfo>();
-            Vector<MBeanOperationInfo> vctOperations = new Vector<MBeanOperationInfo>();
+        try {
+            Vector<MBeanAttributeInfo> vctAttributes=new Vector<MBeanAttributeInfo>();
+            Vector<MBeanOperationInfo> vctOperations=new Vector<MBeanOperationInfo>();
 
             //set a field to hold the description of the MBean
-            MBean mbean = obj.getClass().getAnnotation(MBean.class);           
-            if(mbean.description() != null && mbean.description().trim().length() > 0){
-                if (log.isInfoEnabled()) {
-                    log.info("@MBean description set - " + mbean.description());
+            MBean mbean=obj.getClass().getAnnotation(MBean.class);
+            if(mbean.description() != null && mbean.description().trim().length() > 0) {
+                if(log.isDebugEnabled()) {
+                    log.debug("@MBean description set - " + mbean.description());
                 }
-                this.description = mbean.description();
-                vctAttributes.add(new MBeanAttributeInfo(ResourceDMBean.MBEAN_DESCRITION, "java.lang.String", "@MBean description", true, false, false));
-            }else{
-                this.description = "";
-            }            
-            
+                this.description=mbean.description();
+                vctAttributes.add(new MBeanAttributeInfo(ResourceDMBean.MBEAN_DESCRITION,
+                                                         "java.lang.String",
+                                                         "@MBean description",
+                                                         true,
+                                                         false,
+                                                         false));
+            }           
             //walk class hierarchy and find all fields
-            for(Class<?> clazz = obj.getClass();clazz != null;clazz = clazz.getSuperclass()){           
-            	Field[] fields = clazz.getDeclaredFields();
-            	for (Field field : fields) {
-            		ManagedAttribute attr = field.getAnnotation(ManagedAttribute.class);
-                    if (attr != null) {
-                        if (Modifier.isFinal(field.getModifiers())) {
+            for(Class<?> clazz=obj.getClass();clazz != null;clazz=clazz.getSuperclass()) {
+                Field[] fields=clazz.getDeclaredFields();
+                for(Field field:fields) {
+                    ManagedAttribute attr=field.getAnnotation(ManagedAttribute.class);
+                    if(attr != null) {
+                        if(Modifier.isFinal(field.getModifiers())) {
                             //field is declared final - no changes allowed
-                            if(log.isWarnEnabled() && attr.writable()){
+                            if(log.isWarnEnabled() && attr.writable()) {
                                 log.warn(field.getName() + " declared final, so cannot be marked as writable - writable is ignored");
                             }
-                            vctAttributes.add(new MBeanAttributeInfo(field.getName(), field.getType().getCanonicalName(), attr.description(), attr.readable(), false, false));
-                        } else {
-                            vctAttributes.add(new MBeanAttributeInfo(field.getName(), field.getType().getCanonicalName(), attr.description(), attr.readable(), attr.writable(), false));
+                            vctAttributes.add(new MBeanAttributeInfo(field.getName(),
+                                                                     field.getType().getCanonicalName(),
+                                                                     attr.description(),
+                                                                     attr.readable(),
+                                                                     false,
+                                                                     false));
                         }
-                        if (log.isInfoEnabled()) {
+                        else {
+                            vctAttributes.add(new MBeanAttributeInfo(field.getName(),
+                                                                     field.getType().getCanonicalName(),
+                                                                     attr.description(),
+                                                                     attr.readable(),
+                                                                     attr.writable(),
+                                                                     false));
+                        }
+                        if(log.isInfoEnabled()) {
                             log.info("@Attr found for field " + field.getName());
-                        }
-                    }				
-				}                                              
-            }
-            
-            //find all methods
-            Method[] methods = obj.getClass().getMethods();
-            for (Method method : methods) {
-            	ManagedAttribute attr = method.getAnnotation(ManagedAttribute.class);
-                if (attr != null) {
-                    if (method.getParameterTypes().length == 0 && method.getReturnType() != java.lang.Void.TYPE) {
-                        vctAttributes.add(new MBeanAttributeInfo(method.getName() + "()", method.getReturnType().getCanonicalName(), attr.description(), attr.readable(), false, false));
-                        if (log.isInfoEnabled()) {
-                            log.info("@Attr found for method " + method.getName());
-                        }
-                    } else {
-                        if (log.isWarnEnabled()) {
-                            log.warn("Method " + method.getName() + " must have a valid return type and zero parameters");
                         }
                     }
                 }
-                ManagedOperation op = method.getAnnotation(ManagedOperation.class);
-                if (op != null) {
+            }
+
+            //find all methods
+            Method[] methods=obj.getClass().getMethods();
+            for(Method method:methods) {
+                ManagedAttribute attr=method.getAnnotation(ManagedAttribute.class);
+                if(attr != null) {
+                    if(method.getParameterTypes().length == 0 && method.getReturnType() != java.lang.Void.TYPE) {
+                        vctAttributes.add(new MBeanAttributeInfo(method.getName() + "()",
+                                                                 method.getReturnType().getCanonicalName(),
+                                                                 attr.description(),
+                                                                 attr.readable(),
+                                                                 false,
+                                                                 false));
+                        if(log.isInfoEnabled()) {
+                            log.info("@Attr found for method " + method.getName());
+                        }
+                    }
+                    else {
+                        if(log.isWarnEnabled()) {
+                            log.warn("Method " + method.getName()
+                                     + " must have a valid return type and zero parameters");
+                        }
+                    }
+                }
+                ManagedOperation op=method.getAnnotation(ManagedOperation.class);
+                if(op != null) {
                     vctOperations.add(new MBeanOperationInfo(op.description(), method));
-                    if (log.isInfoEnabled()) {
+                    if(log.isInfoEnabled()) {
                         log.info("@Operation found for method " + method.getName());
                     }
                 }
-			}
-            
-            MBeanAttributeInfo[] attrInfo = new MBeanAttributeInfo[vctAttributes.size()];
+            }
+
+            MBeanAttributeInfo[] attrInfo=new MBeanAttributeInfo[vctAttributes.size()];
             vctAttributes.toArray(attrInfo);
-            
-            MBeanOperationInfo[] operations = new MBeanOperationInfo[vctOperations.size()];
+
+            MBeanOperationInfo[] operations=new MBeanOperationInfo[vctOperations.size()];
             vctOperations.toArray(operations);
-            
+
             return new MBeanInfo(this.name, this.description, attrInfo, null, operations, null);
-        } catch (Exception e) {
+        }
+        catch(Exception e) {
             e.printStackTrace();
             return null;
         }
     }
 
     public synchronized Object getAttribute(String name) {
-        if (log.isDebugEnabled()) {
+        if(log.isDebugEnabled()) {
             log.debug("getAttribute called for " + name);
         }
-        Attribute attr = getNamedAttribute(name);
-        if (log.isDebugEnabled()) {
+        Attribute attr=getNamedAttribute(name);
+        if(log.isDebugEnabled()) {
             log.debug("getAttribute value found " + attr.getValue());
         }
         return attr.getValue();
     }
 
     public synchronized void setAttribute(Attribute attribute) {
-        if (log.isDebugEnabled()) {
-            log.debug("setAttribute called for " + attribute.getName() + " value " + attribute.getValue());
+        if(log.isDebugEnabled()) {
+            log.debug("setAttribute called for " + attribute.getName()
+                      + " value "
+                      + attribute.getValue());
         }
         setNamedAttribute(attribute);
     }
 
     public synchronized AttributeList getAttributes(String[] names) {
-        if (log.isDebugEnabled()) {
+        if(log.isDebugEnabled()) {
             log.debug("getAttributes called");
-            for (String name : names) {
+            for(String name:names) {
                 log.debug("Attribute name " + name);
             }
         }
-        AttributeList al = new AttributeList();
+        AttributeList al=new AttributeList();
 
-        for (String name : names) {
-            Attribute attr = getNamedAttribute(name);
-            if (attr != null) {
-                if (log.isDebugEnabled()) {
+        for(String name:names) {
+            Attribute attr=getNamedAttribute(name);
+            if(attr != null) {
+                if(log.isDebugEnabled()) {
                     log.debug("Attribute " + name + " found with value " + attr.getValue());
                 }
                 al.add(attr);
@@ -152,52 +190,58 @@ public class ResourceDMBean implements DynamicMBean {
     }
 
     public synchronized AttributeList setAttributes(AttributeList list) {
-        if (log.isDebugEnabled()) {
+        if(log.isDebugEnabled()) {
             log.debug("setAttributes called");
         }
-        AttributeList results = new AttributeList();
-        for (int i = 0; i < list.size(); i++) {
-            Attribute attr = (Attribute) list.get(i);
+        AttributeList results=new AttributeList();
+        for(int i=0;i < list.size();i++) {
+            Attribute attr=(Attribute)list.get(i);
 
-            if (log.isDebugEnabled()) {
+            if(log.isDebugEnabled()) {
                 log.debug("Attribute name " + attr.getName() + " new value is " + attr.getValue());
             }
 
-            if (setNamedAttribute(attr)) {
+            if(setNamedAttribute(attr)) {
                 results.add(attr);
-            } else {
-                if (log.isWarnEnabled()) {
-                    log.debug("Failed to update attribute name " + attr.getName() + " with value " + attr.getValue());
+            }
+            else {
+                if(log.isWarnEnabled()) {
+                    log.debug("Failed to update attribute name " + attr.getName()
+                              + " with value "
+                              + attr.getValue());
                 }
             }
         }
         return results;
     }
 
-    public Object invoke(String name, Object[] args, String[] sig) throws MBeanException, ReflectionException {
+    public Object invoke(String name, Object[] args, String[] sig) throws MBeanException,
+                                                                  ReflectionException {
         try {
-            if (log.isDebugEnabled()) {
+            if(log.isDebugEnabled()) {
                 log.debug("Invoke method called on " + name);
             }
-            Class[] classes = new Class[sig.length];
-            for (int i = 0; i < classes.length; i++) {
-                classes[i] = getClassForName(sig[i]);
+            Class[] classes=new Class[sig.length];
+            for(int i=0;i < classes.length;i++) {
+                classes[i]=getClassForName(sig[i]);
             }
-            Method method = this.obj.getClass().getMethod(name, classes);
+            Method method=this.obj.getClass().getMethod(name, classes);
             return method.invoke(this.obj, args);
-        } catch (Exception e) {
+        }
+        catch(Exception e) {
             throw new MBeanException(e);
         }
     }
 
     public static Class<?> getClassForName(String name) throws ClassNotFoundException {
         try {
-            Class<?> c = Class.forName(name);
+            Class<?> c=Class.forName(name);
             return c;
-        } catch (ClassNotFoundException cnfe) {
+        }
+        catch(ClassNotFoundException cnfe) {
             //Could be a primative - let's check
-            for (int i = 0; i < primitives.length; i++) {
-                if (name.equals(primitives[i].getName())) {
+            for(int i=0;i < primitives.length;i++) {
+                if(name.equals(primitives[i].getName())) {
                     return primitives[i];
                 }
             }
@@ -207,23 +251,27 @@ public class ResourceDMBean implements DynamicMBean {
 
     private Attribute getNamedAttribute(String name) {
         try {
-            if (name.endsWith("()")) {
-                Method method = this.obj.getClass().getMethod(name.substring(0, name.length() - 2), new Class[]{});
-                return new Attribute(name, method.invoke(this.obj, new Object[]{}));
-            } else {
-                if(name.equals(ResourceDMBean.MBEAN_DESCRITION)){
+            if(name.endsWith("()")) {
+                Method method=this.obj.getClass().getMethod(name.substring(0, name.length() - 2),
+                                                            new Class[] {});
+                return new Attribute(name, method.invoke(this.obj, new Object[] {}));
+            }
+            else {
+                if(name.equals(ResourceDMBean.MBEAN_DESCRITION)) {
                     return new Attribute(ResourceDMBean.MBEAN_DESCRITION, this.description);
-                }else{
-                    Field field = getFieldInHierarchy(obj.getClass(), name);
-                    if(field != null){
-	                    if (!field.isAccessible()) {
-	                        field.setAccessible(true);
-	                    }
-	                    return new Attribute(name, field.get(this.obj));
+                }
+                else {
+                    Field field=getFieldInHierarchy(obj.getClass(), name);
+                    if(field != null) {
+                        if(!field.isAccessible()) {
+                            field.setAccessible(true);
+                        }
+                        return new Attribute(name, field.get(this.obj));
                     }
                 }
             }
-        } catch (Exception e) {
+        }
+        catch(Exception e) {
             e.printStackTrace();
         }
         return null;
@@ -231,33 +279,37 @@ public class ResourceDMBean implements DynamicMBean {
 
     private boolean setNamedAttribute(Attribute attribute) {
         try {
-            Field field = getFieldInHierarchy(obj.getClass(),attribute.getName());
-            if(field != null){
-	            if (!field.isAccessible()) {
-	                field.setAccessible(true);
-	            }
-	            field.set(this.obj, attribute.getValue());
-	            return true;
+            Field field=getFieldInHierarchy(obj.getClass(), attribute.getName());
+            if(field != null) {
+                if(!field.isAccessible()) {
+                    field.setAccessible(true);
+                }
+                field.set(this.obj, attribute.getValue());
+                return true;
             }
             return false;
-        } catch (Exception e) {
+        }
+        catch(Exception e) {
             e.printStackTrace();
             return false;
         }
     }
-    
+
     private Field getFieldInHierarchy(Class<?> clazz, String name) {
-		try {
-			return clazz.getDeclaredField(name);
-		} catch (SecurityException e) {
-			return null;
-		} catch (NoSuchFieldException e) {
-			Class<?> superClazz = clazz.getSuperclass();
-			if (superClazz != null) {
-				return getFieldInHierarchy(superClazz, name);
-			} else {
-				return null;
-			}
-		}
-	}    	   
+        try {
+            return clazz.getDeclaredField(name);
+        }
+        catch(SecurityException e) {
+            return null;
+        }
+        catch(NoSuchFieldException e) {
+            Class<?> superClazz=clazz.getSuperclass();
+            if(superClazz != null) {
+                return getFieldInHierarchy(superClazz, name);
+            }
+            else {
+                return null;
+            }
+        }
+    }
 }
