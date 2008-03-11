1eb637b3c15f7c6c881605fd3982d25f09cf02af
Vladimir Blagojevic <vblagoje@redhat.com>
2008-03-11 02:26:20 +0000

    update javadocs after finalizing semantics

--- src/org/jgroups/annotations/ManagedAttribute.java
+++ src/org/jgroups/annotations/ManagedAttribute.java
@@ -1,13 +1,15 @@
 package org.jgroups.annotations;
 
 /**
- * Indicates that a public method or a field with any visibility in an MBean class 
- * defines an MBean attribute. This annotation can be applied to either a field or 
- * a public method of a public class that is itself annotated with an @MBean 
- * annotation, or inherits such an annotation from a superclass.
+ * Indicates that a public method or a field (any visibility) in 
+ * an MBean class defines an MBean attribute. This annotation can 
+ * be applied to either a field or a public setter and/or getter 
+ * method of a public class that is itself is optionally annotated 
+ * with an @MBean annotation, or inherits such an annotation from 
+ * a superclass.
  * 
  * @author Chris Mills
- * @version $Id: ManagedAttribute.java,v 1.2 2008/03/06 00:50:16 vlada Exp $
+ * @version $Id: ManagedAttribute.java,v 1.3 2008/03/11 02:26:20 vlada Exp $
  */
 import java.lang.annotation.*;
 
--- src/org/jgroups/annotations/ManagedOperation.java
+++ src/org/jgroups/annotations/ManagedOperation.java
@@ -1,13 +1,14 @@
 package org.jgroups.annotations;
 
 /**
- * Indicates that a method in an MBean class defines an MBean operation. 
- * This annotation can be applied to a public method of a public class 
- * that is itself annotated with an @MBean annotation, or inherits such 
- * an annotation from a superclass.
+ * Indicates that a method in an MBean class defines an MBean 
+ * operation. @ManagedOperation annotation can be applied to a 
+ * public method of a public class that is itself optionally 
+ * annotated with an @MBean annotation, or inherits such an 
+ * annotation from a superclass.
  * 
  * @author Chris Mills
- * @version $Id: ManagedOperation.java,v 1.2 2008/03/06 00:50:16 vlada Exp $
+ * @version $Id: ManagedOperation.java,v 1.3 2008/03/11 02:26:20 vlada Exp $
  */
 
 import java.lang.annotation.*;
