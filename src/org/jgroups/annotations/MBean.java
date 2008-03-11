11aa556f3cd2faa92f0846ef1ed7c1b7f95304f8
vlada <vlada>
2008-03-11 02:26:20 +0000

    update javadocs after finalizing semantics

--- src/org/jgroups/annotations/MBean.java
+++ src/org/jgroups/annotations/MBean.java
@@ -1,14 +1,25 @@
 package org.jgroups.annotations;
 
 /**
- * Indicates that the annotated class is a Standard MBean. The class must be public. 
- * Public methods within the class can be annotated with @ManagedOperation to indicate 
- * that they are MBean operations. Class fields and public getter and setter methods 
- * within the class can be annotated with @ManagedAttribute to indicate that they 
- * define MBean attributes.
+ * Optional annotation that exposes all public methods in the class 
+ * hierarchy (excluding Object) as MBean operations.
+ * <p>
+ * 
+ * Furthermore, class fields (any visibility) along with public getter 
+ * and setter methods in a class hierarchy of a class annotated with @MBean 
+ * annotation can be annotated with @ManagedAttribute to indicate that 
+ * they define MBean attributes. If such methods and fields are discovered 
+ * they will be exposed as MBean attributes.
+ * <p> 
+ * 
+ * If a more fine grained MBean attribute and operation exposure is needed 
+ * do not use @MBean annotation but annotate fields and public methods directly 
+ * using @ManagedOPeration and @ManagedAttribute annotations.
+ *  
+ * 
  * 
  * @author Chris Mills
- * @version $Id: MBean.java,v 1.2 2008/03/06 00:50:16 vlada Exp $
+ * @version $Id: MBean.java,v 1.3 2008/03/11 02:26:20 vlada Exp $
  */
 import java.lang.annotation.*;
 
