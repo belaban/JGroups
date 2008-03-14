f904d2f43ca33727770daeb4817fdb17b09c129f
Vladimir Blagojevic <vblagoje@redhat.com>
2008-03-14 03:30:16 +0000

    jdk5

--- src/org/jgroups/util/GetNetworkInterfaces.java
+++ src/org/jgroups/util/GetNetworkInterfaces.java
@@ -9,18 +9,18 @@ import java.util.Enumeration;
  * Lists all network interfaces on a system
  * @author Bela Ban Dec 18
  * @author 2003
- * @version $Id: GetNetworkInterfaces.java,v 1.1 2005/06/23 13:31:09 belaban Exp $
+ * @version $Id: GetNetworkInterfaces.java,v 1.2 2008/03/14 03:30:16 vlada Exp $
  */
 public class GetNetworkInterfaces {
 
     public static void main(String[] args) throws SocketException {
-        Enumeration en=NetworkInterface.getNetworkInterfaces();
+        Enumeration<NetworkInterface> en=NetworkInterface.getNetworkInterfaces();
         while(en.hasMoreElements()) {
-            NetworkInterface i=(NetworkInterface)en.nextElement();
+            NetworkInterface i=en.nextElement();
             System.out.println(i.getName() + ':');
             System.out.println("  \t" + i.getDisplayName());
-            for(Enumeration en2=i.getInetAddresses(); en2.hasMoreElements();) {
-                InetAddress addr=(InetAddress)en2.nextElement();
+            for(Enumeration<InetAddress> en2=i.getInetAddresses(); en2.hasMoreElements();) {
+                InetAddress addr=en2.nextElement();
                 System.out.println("  \t" + addr + " (" + addr.getHostName() + ')');
             }
             System.out.println("---------------------");
