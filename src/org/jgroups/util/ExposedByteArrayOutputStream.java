6ce9a9eb55f14868085efe7e7543ca3480fe6634
belaban <belaban>
2008-03-27 11:16:15 +0000

    added reset() method (http://jira.jboss.com/jira/browse/JGRP-725)

--- src/org/jgroups/util/ExposedByteArrayOutputStream.java
+++ src/org/jgroups/util/ExposedByteArrayOutputStream.java
@@ -6,15 +6,24 @@ import java.io.ByteArrayOutputStream;
  * Extends ByteArrayOutputStream, but exposes the internal buffer. This way we don't need to call
  * toByteArray() which copies the internal buffer
  * @author Bela Ban
- * @version $Id: ExposedByteArrayOutputStream.java,v 1.2 2005/07/25 15:53:36 belaban Exp $
+ * @version $Id: ExposedByteArrayOutputStream.java,v 1.3 2008/03/27 11:16:15 belaban Exp $
  */
 public class ExposedByteArrayOutputStream extends ByteArrayOutputStream {
 
     public ExposedByteArrayOutputStream() {
+        super();
     }
 
     public ExposedByteArrayOutputStream(int size) {
         super(size);
+    }
+
+    /** Resets count and creates a new buf if the current buf is > max_size. This method is not synchronized */
+    public void reset(int max_size) {
+        super.reset();
+        if(buf.length > max_size) {
+            buf=new byte[max_size];
+        }
     }
 
     public byte[] getRawBuffer() {
