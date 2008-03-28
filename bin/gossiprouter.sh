ba20f63389a408d97e1651d1e43beba46f9c8e2c
belaban <belaban>
2008-03-28 06:12:06 +0000

    ns

--- bin/gossiprouter.sh
+++ bin/gossiprouter.sh
@@ -7,6 +7,7 @@ for i in ../lib/*.jar
 done
 
 OPTS="-Dlog4j.configuration=file:$HOME/log4j.properties -Djava.net.preferIPv4Stack=true"
+OPTS="$OPTS -Dcom.sun.management.jmxremote"
 
 java $OPTS -classpath $CLASSPATH $JAVA_OPTS org.jgroups.stack.GossipRouter -port 12001 $*
 
