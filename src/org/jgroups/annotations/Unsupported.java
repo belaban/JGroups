ccba00c94434722193f03d350983d19064f8da45
Bela Ban <belaban@yahoo.com>
2008-04-08 14:36:32 +0000

    added @Unsupported and @Experimental

---
+++ src/org/jgroups/annotations/Unsupported.java
@@ -0,0 +1,16 @@
+package org.jgroups.annotations;
+
+import java.lang.annotation.Target;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * Elements annotated with this annotation are unsupported and may get removed from the distribution at any time
+ * @author Bela Ban
+ * @version $Id: Unsupported.java,v 1.1 2008/04/08 14:36:33 belaban Exp $
+ */
+@Target({ElementType.TYPE, ElementType.METHOD, ElementType.PACKAGE})
+@Retention(RetentionPolicy.SOURCE)
+public @interface Unsupported {
+}
