52bc5acb901575d451b582b67ad16b9784bc6659
Bela Ban <belaban@yahoo.com>
2008-04-14 07:04:10 +0000

    readFrom() is now correct: was reading size instead of len

--- src/org/jgroups/stack/IpAddress.java
+++ src/org/jgroups/stack/IpAddress.java
@@ -1,4 +1,4 @@
-// $Id: IpAddress.java,v 1.45 2008/04/14 06:41:27 belaban Exp $
+// $Id: IpAddress.java,v 1.46 2008/04/14 07:04:10 belaban Exp $
 
 package org.jgroups.stack;
 
@@ -305,9 +305,10 @@ public class IpAddress implements Address {
 
     public void readFrom(DataInputStream in) throws IOException {
         int len=in.readByte();
-        if(len > 0 && (len != Global.IPV4_SIZE && size != Global.IPV6_SIZE))
-            throw new IOException("length has to be " + Global.IPV4_SIZE + " or " + Global.IPV6_SIZE + " bytes");
-            byte[] a = new byte[len]; // 4 bytes (IPv4) or 16 bytes (IPv6)
+        if(len > 0 && (len != Global.IPV4_SIZE && len != Global.IPV6_SIZE))
+            throw new IOException("length has to be " + Global.IPV4_SIZE + " or " + Global.IPV6_SIZE + " bytes (was " +
+                    len + " bytes)");
+        byte[] a = new byte[len]; // 4 bytes (IPv4) or 16 bytes (IPv6)
             in.readFully(a);
             this.ip_addr=InetAddress.getByAddress(a);
 
