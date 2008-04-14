e077f74ebdda99f7bfed391146b5d91a5d9b2032
Bela Ban <belaban@yahoo.com>
2008-04-14 06:44:12 +0000

    readFrom() checking for 0 length (null ip_addr field)

--- src/org/jgroups/stack/IpAddress.java
+++ src/org/jgroups/stack/IpAddress.java
@@ -1,4 +1,4 @@
-// $Id: IpAddress.java,v 1.42.2.2 2008/04/12 12:31:21 belaban Exp $
+// $Id: IpAddress.java,v 1.42.2.3 2008/04/14 06:44:12 belaban Exp $
 
 package org.jgroups.stack;
 
@@ -302,7 +302,7 @@ public class IpAddress implements Address {
 
     public void readFrom(DataInputStream in) throws IOException {
         int len=in.readByte();
-        if(len != Global.IPV4_SIZE && size != Global.IPV6_SIZE)
+        if(len > 0 && (len != Global.IPV4_SIZE && size != Global.IPV6_SIZE))
             throw new IOException("length has to be " + Global.IPV4_SIZE + " or " + Global.IPV6_SIZE + " bytes");
         byte[] a = new byte[len]; // 4 bytes (IPv4) or 16 bytes (IPv6)
         in.readFully(a);
