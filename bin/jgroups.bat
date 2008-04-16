05698e2dfdf9cbf4880d9abb5404831f930cca94
belaban <belaban>
2008-04-16 06:52:24 +0000

    ns

--- bin/jgroups.bat
+++ bin/jgroups.bat
@@ -14,6 +14,6 @@ set VMFLAGS=-Xmx500M -Xms500M -XX:NewRatio=1 -XX:+AggressiveHeap -verbose:gc -XX
 rem LOG="-Dorg.apache.commons.logging.Log=org.apache.commons.logging.impl.Jdk14Logger -Djava.util.logging.config.file=c:\logging.properties"
 set LOG=-Dlog4j.configuration=file:c:\log4j.properties
 
-set FLAGS=-Djava.net.preferIPv4Stack=true -Djgroups.bind_addr=192.168.5.2 -Djgroups.tcpping.initial_hosts=127.0.0.1[7800]
+set FLAGS=-Djava.net.preferIPv4Stack=true -Djgroups.bind_addr=192.168.1.5 -Djgroups.tcpping.initial_hosts=127.0.0.1[7800]
 
 java -Ddisable_canonicalization=false -classpath %CP% %LOG% %VMFLAGS% %FLAGS% -Dcom.sun.management.jmxremote -Dresolve.dns=false org.jgroups.%*
