f008d689c11dd0b7287ebde5e25d050fc34c1788
belaban <belaban>
2008-04-22 07:04:00 +0000

    ns

--- tests/junit/org/jgroups/protocols/FRAG2_Test.java
+++ tests/junit/org/jgroups/protocols/FRAG2_Test.java
@@ -1,25 +1,14 @@
 package org.jgroups.protocols;
 
 
-
-import org.testng.annotations.*;
-import org.jgroups.stack.IpAddress;
 import org.jgroups.stack.Protocol;
-import org.jgroups.View;
-import org.jgroups.Message;
-import org.jgroups.Event;
-import org.jgroups.Global;
-import org.jgroups.debug.Simulator;
-
-import java.util.Vector;
-import java.util.Properties;
-import java.nio.ByteBuffer;
+import org.testng.annotations.Test;
 
 /**
  * Tests the fragmentation (FRAG2) protocol for http://jira.jboss.com/jira/browse/JGRP-216
  * @author Bela Ban
  */
-@Test(groups="temp")
+@Test(groups={"temp", "protocols"})
 public class FRAG2_Test extends FRAG_Test {
 
     protected Protocol createProtocol() {
@@ -27,9 +16,6 @@ public class FRAG2_Test extends FRAG_Test {
     }
 
 
-    public void testFragmentation() throws InterruptedException {
-        super.testFragmentation();
-    }
-
+  
 
 }
