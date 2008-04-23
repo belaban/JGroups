638508a76aa1e4bd8bb98901a18b80ef8dedd306
belaban <belaban>
2008-04-23 09:48:07 +0000

    adjusted code to TestNG

--- tests/junit/org/jgroups/tests/stack/GossipClientTest.java
+++ tests/junit/org/jgroups/tests/stack/GossipClientTest.java
@@ -1,15 +1,12 @@
-// $Id: GossipClientTest.java,v 1.5 2008/04/09 13:06:58 belaban Exp $
 
 package org.jgroups.tests.stack;
 
 import org.jgroups.Address;
+import org.jgroups.Global;
 import org.jgroups.stack.GossipClient;
 import org.jgroups.stack.IpAddress;
 import org.jgroups.util.Util;
-import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 import java.util.List;
 
@@ -19,9 +16,10 @@ import java.util.List;
  *
  * @author Ovidiu Feodorov <ovidiuf@users.sourceforge.net>
  * @author Bela Ban
- * @version $Revision: 1.5 $
+ * @version $Id: GossipClientTest.java,v 1.6 2008/04/23 09:48:07 belaban Exp $
  * @since 2.2.1
  */
+@Test(groups=Global.STACK_INDEPENDENT,sequential=true)
 public class GossipClientTest {
     GossipClient client;
     private int port=-1;
@@ -29,33 +27,31 @@ public class GossipClientTest {
 
 
 
-    @BeforeMethod
-    public void setUp() throws Exception {
+    @BeforeClass
+    void setUp() throws Exception {
         port=Utilities.startGossipRouter(expiryTime, "127.0.0.1");
         client=new GossipClient(new IpAddress("127.0.0.1", port), expiryTime, 1000, null);
         client.setRefresherEnabled(false); // don't refresh the registrations
     }
 
-    @AfterMethod
-    public void tearDown() throws Exception {
+    @AfterClass
+    void tearDown() throws Exception {
         client.stop();
         Utilities.stopGossipRouter();
     }
 
 
-    @Test
     public void testEmptyGET() throws Exception {
         String groupName="nosuchgroup";
         List mbrs=client.getMembers(groupName);
         assert mbrs != null;
-        Assert.assertEquals(0, mbrs.size());
+        assert mbrs.isEmpty();
     }
 
 
     /**
      * Registers an address with a group and then sends a GET request for that group.
      */
-    @Test
     public void test_REGISTER_GET() throws Exception {
         String groupName="TESTGROUP";
         int mbrPort=7777;
@@ -63,43 +59,41 @@ public class GossipClientTest {
         client.register(groupName, mbr, true);
 
         List mbrs=client.getMembers(groupName);
-        Assert.assertEquals(1, mbrs.size());
-        Assert.assertEquals(new IpAddress("127.0.0.1", mbrPort), mbrs.get(0));
+        assert mbrs.size() == 1;
+        assert mbrs.get(0).equals(new IpAddress("127.0.0.1", mbrPort));
     }
 
-    @Test
     public void test_REGISTER_UNREGISTER_GET() throws Exception {
-        String groupName="TESTGROUP";
+        String groupName="TESTGROUP-2";
         int mbrPort=7777;
         Address mbr=new IpAddress("127.0.0.1", mbrPort);
         client.register(groupName, mbr);
 
         List mbrs=client.getMembers(groupName);
-        Assert.assertEquals(1, mbrs.size());
-        Assert.assertEquals(new IpAddress("127.0.0.1", mbrPort), mbrs.get(0));
+        assert mbrs.size() == 1;
+        assert mbrs.get(0).equals(new IpAddress("127.0.0.1", mbrPort));
 
         client.unregister(groupName, mbr);// done asynchronous, on a separate thread
         Util.sleep(500);
         mbrs=client.getMembers(groupName);
         assert mbrs != null;
-        Assert.assertEquals(0, mbrs.size());
+        assert mbrs.isEmpty();
     }
 
 
     /**
      * Test if a member is removed from group after EXPIRY_TIME ms.
      */
-    @Test
     public void testSweep() throws Exception {
-        String groupName="TESTGROUP";
+        String groupName="TESTGROUP-3";
         int mbrPort=7777;
         Address mbr=new IpAddress("127.0.0.1", mbrPort);
 
         client.register(groupName, mbr);
 
         List mbrs=client.getMembers(groupName);
-        Assert.assertEquals(1, mbrs.size());
-        Assert.assertEquals(new IpAddress("127.0.0.1", mbrPort), mbrs.get(0));
+        assert mbrs.size() == 1;
+        assert mbrs.get(0).equals(new IpAddress("127.0.0.1", mbrPort));
 
         // because the sweep is ran at fixed expiryTime intervals, if
         // an entry was added immediately after a sweep run, it actually 
