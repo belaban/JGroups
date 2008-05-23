ca2aa2884b2d8feb71913327136402e49101e805
Bela Ban <belaban@yahoo.com>
2008-05-23 08:20:21 +0000

    ns

--- tests/other/org/jgroups/tests/UnicastChannelTest.java
+++ tests/other/org/jgroups/tests/UnicastChannelTest.java
@@ -1,19 +1,16 @@
-// $Id: UnicastChannelTest.java,v 1.8 2007/08/20 07:21:49 belaban Exp $
+// $Id: UnicastChannelTest.java,v 1.8.2.1 2008/05/23 08:20:21 belaban Exp $
 
 
 package org.jgroups.tests;
 
-import org.jgroups.Address;
-import org.jgroups.Event;
-import org.jgroups.JChannel;
-import org.jgroups.Message;
+import org.jgroups.*;
 import org.jgroups.protocols.TP;
 import org.jgroups.stack.IpAddress;
 import org.jgroups.stack.Protocol;
+import org.jgroups.util.Util;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Properties;
 
 
 /**
@@ -57,12 +54,39 @@ public class UnicastChannelTest {
             return;
         }
 
-
+        ch=new JChannel(props);
 
         if(server) {
+            ch.setReceiver(new ReceiverAdapter() {
+                public void receive(Message msg) {
+                    System.out.println("-- " + msg.getObject());
+                    Address sender=msg.getSrc();
+                    Message rsp=new Message(sender, null, "ack for " + msg.getObject());
+                    ch.down(new Event(Event.ENABLE_UNICASTS_TO, sender));
+                    try {
+                        ch.send(rsp);
+                    }
+                    catch(Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
             runServer();
         }
         else {
+            ch.setReceiver(new ReceiverAdapter() {
+                public void receive(Message msg) {
+                    System.out.println("<-- " + msg.getObject());
+                }
+
+                public void viewAccepted(View new_view) {
+                    System.out.println("new_view = " + new_view);
+                }
+
+                public void suspect(Address suspected_mbr) {
+                    System.out.println("suspected_mbr = " + suspected_mbr);
+                }
+            });
             runClient();
         }
 
@@ -71,13 +95,10 @@ public class UnicastChannelTest {
     void runClient() throws Exception {
         IpAddress       addr;
         Message         msg;
-        Object          obj;
         String          line;
         BufferedReader  reader;
 
-        ch=new JChannel(props);
         ch.connect(null); // unicast channel
-
         addr=new IpAddress(host, port);
         ch.down(new Event(Event.ENABLE_UNICASTS_TO, addr));
         reader=new BufferedReader(new InputStreamReader(System.in));
@@ -91,49 +112,21 @@ public class UnicastChannelTest {
             }
             msg=new Message(addr, null, line);
             ch.send(msg);
-            while((obj=ch.peek(1000)) != null) {
-                obj=ch.receive(1000);
-                if(obj == null)
-                    break;
-                if(!(obj instanceof Message)) {
-                    System.out.println("<-- " + obj);
                 }
-                else {
-                    System.out.println("<-- " + ((Message)obj).getObject());
                 }
-            }
-        }
-    }
 
     void runServer() throws Exception {
-        Object  obj;
-        Message msg, rsp;
-
-        ch=new JChannel(props);
         System.setProperty("jgroups.bind_addr", host);
         if(port > 0) {
             Protocol transport=ch.getProtocolStack().getTransport();
-            if(transport != null) {
-                Properties tmp=new Properties();
-                tmp.setProperty("bind_port", String.valueOf(port));
-                transport.setProperties(tmp);
+            if(transport instanceof TP) {
+                ((TP)transport).setBindPort(port);
             }
         }
         ch.connect(null); // this makes it a unicast channel
         System.out.println("server started at " + new java.util.Date() + ", listening on " + ch.getLocalAddress());
-        while(true) {
-            obj=ch.receive(0);
-            if(!(obj instanceof Message)) {
-                System.out.println(obj);
-            }
-            else {
-                msg=(Message)obj;
-                System.out.println("-- " + msg.getObject());
-                Address sender=msg.getSrc();
-                rsp=new Message(sender, null, "ack for " + msg.getObject());
-                ch.down(new Event(Event.ENABLE_UNICASTS_TO, sender));
-                ch.send(rsp);
-            }
+        while(ch.isOpen()) {
+            Util.sleep(10000);
         }
     }
 
