46eab6a8395a3ace4e81120ca92acdc4f705c2b3
Vladimir Blagojevic <vblagoje@redhat.com>
2008-05-28 12:35:15 +0000

    merge ThreadFactory and ExtendedThreadFactory

--- src/org/jgroups/util/ThreadFactory.java
+++ src/org/jgroups/util/ThreadFactory.java
@@ -1,6 +1,11 @@
 package org.jgroups.util;
 
-public interface ThreadFactory extends java.util.concurrent.ThreadFactory {   
+public interface ThreadFactory extends java.util.concurrent.ThreadFactory{   
     Thread newThread(Runnable r,String name);    
     Thread newThread(ThreadGroup group, Runnable r,String name);
+    void setPattern(String pattern);
+    void setIncludeClusterName(boolean includeClusterName);
+    void setClusterName(String channelName);
+    void setAddress(String address);   
+    void renameThread(String base_name, Thread thread);   
 }
