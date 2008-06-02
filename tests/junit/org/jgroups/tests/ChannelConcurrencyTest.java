422e9f8075ebf35c0561313eba12cb3ba1556f7f
Vladimir Blagojevic <vblagoje@redhat.com>
2008-06-02 13:29:26 +0000

    fix compilation errors

--- tests/junit/org/jgroups/tests/ChannelConcurrencyTest.java
+++ tests/junit/org/jgroups/tests/ChannelConcurrencyTest.java
@@ -13,64 +13,65 @@ import org.jgroups.Channel;
 
 /**
  * Tests concurrent startup
+ * 
  * @author Brian Goose
- * @version $Id: ChannelConcurrencyTest.java,v 1.1.2.1 2008/05/31 05:03:38 belaban Exp $
+ * @version $Id: ChannelConcurrencyTest.java,v 1.1.2.1 2008/05/31 05:03:38
+ *          belaban Exp $
  */
 public class ChannelConcurrencyTest extends TestCase {
 
-	public void test() throws Exception {
-		final int count = 8;
+    public void test() throws Exception {
+        final int count=8;
 
-		final Executor executor = Executors.newFixedThreadPool(count);
-		final CountDownLatch latch = new CountDownLatch(count);
-		final JChannel[] channels = new JChannel[count];
+        final Executor executor=Executors.newFixedThreadPool(count);
+        final CountDownLatch latch=new CountDownLatch(count);
+        final JChannel[] channels=new JChannel[count];
 
-		final long start = System.currentTimeMillis();
-		for (int i = 0; i < count; i++) {
-			channels[i] = new JChannel("flush-udp.xml");
-		}
+        final long start=System.currentTimeMillis();
+        for(int i=0;i < count;i++) {
+            channels[i]=new JChannel("flush-udp.xml");
+        }
 
-		for (int i = 0; i < count; i++) {
-			final int me = i;
-			executor.execute(new Runnable() {
+        for(final Channel c:channels) {
+            executor.execute(new Runnable() {
 
-				@Override
-				public void run() {
-					try {
-						channels[me].connect("test");
-						latch.countDown();
-					} catch (final Exception e) {
-						e.printStackTrace();
-					}
-				}
-			});
-		}
+                public void run() {
+                    try {
+                        c.connect("test");
+                        latch.countDown();
+                    }
+                    catch(final Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
 
-		// Wait for all channels to finish connecting
-		latch.await();
+        // Wait for all channels to finish connecting
+        latch.await();
 
-		// Wait for all channels to have the correct number of members in their
-		// current view
-		for (;;) {
-			boolean done = true;
-			for (final JChannel channel : channels) {
-				if (channel.getView().size() < count) {
-					done = false;
-				}
-			}
-			if (done) {
-				break;
-			} else {
-				SECONDS.sleep(1);
-			}
-		}
+        // Wait for all channels to have the correct number of members in their
+        // current view
+        for(;;) {
+            boolean done=true;
+            for(final JChannel channel:channels) {
+                if(channel.getView().size() < count) {
+                    done=false;
+                }
+            }
+            if(done) {
+                break;
+            }
+            else {
+                SECONDS.sleep(100);
+            }
+        }
 
-        for(final Channel ch: channels) {
+        for(final Channel ch:channels) {
             System.out.println(ch.getView());
         }
 
-        final long duration = System.currentTimeMillis() - start;
-		System.out.println("Converged to a single group after " + duration
-				+ " ms");
-	}
+        final long duration=System.currentTimeMillis() - start;
+        System.out.println("Converged to a single group after " + duration + " ms");
+    }
 }
