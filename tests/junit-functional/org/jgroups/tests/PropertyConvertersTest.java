69549aa930dc3d5478132f3e6aa82bad0d9fbc82
belaban <belaban>
2008-06-02 08:13:09 +0000

    implemented toString() methods

--- tests/junit-functional/org/jgroups/tests/PropertyConvertersTest.java
+++ tests/junit-functional/org/jgroups/tests/PropertyConvertersTest.java
@@ -1,15 +1,19 @@
 package org.jgroups.tests;
 
-import org.testng.annotations.Test;
 import org.jgroups.Global;
 import org.jgroups.conf.PropertyConverter;
 import org.jgroups.conf.PropertyConverters;
+import org.testng.annotations.Test;
 
+import java.net.InetAddress;
+import java.net.NetworkInterface;
 import java.util.Arrays;
+import java.util.Properties;
+import java.util.List;
 
 /**
  * @author Bela Ban
- * @version $Id: PropertyConvertersTest.java,v 1.1 2008/06/02 08:01:48 belaban Exp $
+ * @version $Id: PropertyConvertersTest.java,v 1.2 2008/06/02 08:13:09 belaban Exp $
  */
 @Test(groups=Global.FUNCTIONAL, sequential=false)
 public class PropertyConvertersTest {
@@ -29,13 +33,23 @@ public class PropertyConvertersTest {
     }
 
 
-    public static void testBindAddress() {
+    public static void testBindAddress() throws Exception {
         PropertyConverter conv=new PropertyConverters.BindAddress();
-
+        InetAddress addr=InetAddress.getByName("127.0.0.1");
+        check(InetAddress.class, "127.0.0.1", addr, conv);
     }
 
+    public static void testNetworkList() throws Exception {
+        PropertyConverter conv=new PropertyConverters.NetworkInterfaceList();
+        Object tmp=conv.convert(List.class, new Properties(), "eth0,lo");
+        Object str=conv.toString(tmp);
+        System.out.println("str = " + str);
+        assert str.equals("eth0,lo");
+    }
+
+
     private static void check(Class<?> type, String prop, Object result, PropertyConverter converter) throws Exception {
-        Object tmp=converter.convert(type, null, prop);
+        Object tmp=converter.convert(type, new Properties(), prop);
         assert tmp.equals(result) : " conversion result: " + tmp + " (" + tmp.getClass() + ")" +
                 ", expected result: " + result + " (" + result.getClass() + ")";
 
