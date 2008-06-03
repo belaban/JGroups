cf0bb0631439a0523875439fbfdce44a327cdc5c
Bela Ban <belaban@yahoo.com>
2008-06-03 13:29:01 +0000

    ns

--- tests/junit/org/jgroups/tests/ChannelConcurrencyTest.java
+++ tests/junit/org/jgroups/tests/ChannelConcurrencyTest.java
@@ -18,41 +18,38 @@ import org.jgroups.stack.ProtocolStack;
  * Tests concurrent startup
  * 
  * @author Brian Goose
- * @version $Id: ChannelConcurrencyTest.java,v 1.1.2.5 2008/06/03 10:59:18 belaban Exp $
+ * @version $Id: ChannelConcurrencyTest.java,v 1.1.2.6 2008/06/03 13:29:01 belaban Exp $
  */
 public class ChannelConcurrencyTest extends TestCase {
 
-    public void test() throws Exception {
+    public void test() throws Throwable {
         final int count=8;
 
-        final Executor executor=Executors.newFixedThreadPool(count);
+        final Executor       executor=Executors.newFixedThreadPool(count);
         final CountDownLatch latch=new CountDownLatch(count);
-        final JChannel[] channels=new JChannel[count];
+        final JChannel[]     channels=new JChannel[count];
+        final Task[]         tasks=new Task[count];
 
         final long start=System.currentTimeMillis();
         for(int i=0;i < count;i++) {
             channels[i]=new JChannel("flush-udp.xml");
+            tasks[i]=new Task(latch, channels[i]);
             changeMergeInterval(channels[i]);
             changeViewBundling(channels[i]);
         }
 
-        for(final Channel c:channels) {
-            executor.execute(new Runnable() {
-
-                public void run() {
-                    try {
-                        c.connect("test");
-                        latch.countDown();
-                    }
-                    catch(final Exception e) {
-                        e.printStackTrace();
-                    }
-                }
-            });
+        for(final Task t: tasks) {
+            executor.execute(t);
         }
 
         // Wait for all channels to finish connecting
         latch.await();
+
+        for(Task t: tasks) {
+            Throwable ex=t.getException();
+            if(ex != null)
+                throw ex;
+        }
 
         // Wait for all channels to have the correct number of members in their
         // current view
@@ -94,6 +91,35 @@ public class ChannelConcurrencyTest extends TestCase {
         if(merge != null) {
             merge.setMinInterval(5000);
             merge.setMaxInterval(10000);
+        }
+    }
+
+    private static class Task implements Runnable {
+        private final Channel c;
+        private final CountDownLatch latch;
+        private Throwable exception=null;
+
+
+        public Task(CountDownLatch latch, Channel c) {
+            this.latch=latch;
+            this.c=c;
+        }
+
+        public Throwable getException() {
+            return exception;
+        }
+
+        public void run() {
+            try {
+                c.connect("test");
+            }
+            catch(final Exception e) {
+                exception=e;
+                e.printStackTrace();
+            }
+            finally {
+                latch.countDown();
+            }
         }
     }
 }
