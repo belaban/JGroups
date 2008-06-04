3676fd075d288fd10646d489d2929f549c2e980a
belaban <belaban>
2008-06-04 14:18:47 +0000

    added hasMessageToRemove()

--- src/org/jgroups/stack/AckReceiverWindow.java
+++ src/org/jgroups/stack/AckReceiverWindow.java
@@ -1,5 +1,3 @@
-// $Id: AckReceiverWindow.java,v 1.25 2007/11/02 13:16:00 belaban Exp $
-
 package org.jgroups.stack;
 
 
@@ -23,6 +21,7 @@ import java.util.concurrent.locks.ReentrantLock;
  * a sorted set incurs overhead.
  *
  * @author Bela Ban
+ * @version $Id: AckReceiverWindow.java,v 1.26 2008/06/04 14:18:47 belaban Exp $
  */
 public class AckReceiverWindow {
     long                    next_to_remove=0;
@@ -80,6 +79,13 @@ public class AckReceiverWindow {
             }
         }
         return retval;
+    }
+
+
+    public boolean hasMessagesReadyToRemove() {
+        synchronized(msgs) {
+            return msgs.containsKey(next_to_remove);
+        }
     }
 
 
