05c90f490f9909d179427aedac25a35c37dc4ea4
belaban <belaban>
2008-06-06 08:06:26 +0000

    new protocol

--- src/org/jgroups/protocols/DUPL.java
+++ src/org/jgroups/protocols/DUPL.java
@@ -4,12 +4,17 @@ import org.jgroups.stack.Protocol;
 import org.jgroups.annotations.Property;
 import org.jgroups.annotations.ManagedAttribute;
 import org.jgroups.Event;
+import org.jgroups.Message;
+import org.jgroups.Address;
 
 /** Duplicates outgoing or incoming messages by copying them
  * @author Bela Ban
- * @version $Id: DUPL.java,v 1.1 2008/06/05 18:49:34 belaban Exp $
+ * @version $Id: DUPL.java,v 1.2 2008/06/06 08:06:26 belaban Exp $
  */
 public class DUPL extends Protocol {
+
+    private static enum Direction {UP,DOWN};
+
 
     @Property @ManagedAttribute(description="Number of copies of each incoming message (0=no copies)",writable=true)
     protected short incoming_copies=1;
@@ -29,9 +34,50 @@ public class DUPL extends Protocol {
 
 
     public Object down(Event evt) {
-        boolean copy=(copy_multicast_msgs || copy_unicast_msgs) && (incoming_copies > 0 || outgoing_copies > 0);
+        boolean copy=(copy_multicast_msgs || copy_unicast_msgs) && outgoing_copies > 0;
+        if(!copy)
+            return down_prot.down(evt);
 
+        switch(evt.getType()) {
+            case Event.MSG:
+                Message msg=(Message)evt.getArg();
+                copy(msg, outgoing_copies, Direction.DOWN);
+                break;
+        }
 
         return down_prot.down(evt);
+    }
+
+    public Object up(Event evt) {
+        boolean copy=(copy_multicast_msgs || copy_unicast_msgs) && incoming_copies > 0;
+        if(!copy)
+            return up_prot.up(evt);
+
+        switch(evt.getType()) {
+            case Event.MSG:
+                Message msg=(Message)evt.getArg();
+                copy(msg, incoming_copies, Direction.UP);
+                break;
+        }
+
+        return up_prot.up(evt);
+    }
+
+    private void copy(Message msg, int num_copies, Direction direction) {
+        Address dest=msg.getDest();
+        boolean multicast=dest == null || dest.isMulticastAddress();
+        if((multicast && copy_multicast_msgs) ||  (!multicast && copy_unicast_msgs)) {
+            for(int i=0; i < num_copies; i++) {
+                Message copy=msg.copy(true);
+                switch(direction) {
+                    case UP:
+                        up_prot.up(new Event(Event.MSG, copy));
+                        break;
+                    case DOWN:
+                        down_prot.down(new Event(Event.MSG, copy));
+                        break;
+                }
+            }
+        }
     }
 }
