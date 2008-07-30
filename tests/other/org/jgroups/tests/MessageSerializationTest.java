3c90bff7d4ed5be5eaf15d3382fbf12eb894a96c
Bela Ban <belaban@yahoo.com>
2008-07-30 12:04:47 +0000

    backport from CVS head (https://jira.jboss.org/jira/browse/JGRP-806)

--- tests/other/org/jgroups/tests/MessageSerializationTest.java
+++ tests/other/org/jgroups/tests/MessageSerializationTest.java
@@ -1,4 +1,4 @@
-// $Id: MessageSerializationTest.java,v 1.10 2005/08/10 11:31:03 belaban Exp $
+// $Id: MessageSerializationTest.java,v 1.10.14.1 2008/07/30 12:04:49 belaban Exp $
 
 package org.jgroups.tests;
 
@@ -9,8 +9,6 @@ package org.jgroups.tests;
  */
 
 import org.jgroups.Message;
-import org.jgroups.util.MagicObjectOutputStream;
-import org.jgroups.util.MagicObjectInputStream;
 import org.jgroups.util.ExposedByteArrayOutputStream;
 import org.jgroups.util.Buffer;
 import org.jgroups.conf.ClassConfigurator;
@@ -34,8 +32,6 @@ public class MessageSerializationTest {
         boolean add_headers=false;
         InetAddress addr=InetAddress.getLocalHost();
         int num=10000;
-        boolean use_magic=false;
-        boolean use_streamable=false;
 
         for(int i=0; i < args.length; i++) {
             if("-add_headers".equals(args[i])) {
@@ -44,14 +40,6 @@ public class MessageSerializationTest {
             }
             if("-num".equals(args[i])) {
                 num=Integer.parseInt(args[++i]);
-                continue;
-            }
-            if("-use_magic".equals(args[i])) {
-                use_magic=true;
-                continue;
-            }
-            if("-use_streamable".equals(args[i])) {
-                use_streamable=true;
                 continue;
             }
             help();
@@ -69,37 +57,19 @@ public class MessageSerializationTest {
             ExposedByteArrayOutputStream msg_data=new ExposedByteArrayOutputStream();
             Buffer jgbuf;
 
-            if(use_streamable) {
                 DataOutputStream dos=new DataOutputStream(msg_data);
                 m.writeTo(dos);
                 dos.close();
-            }
-            else {
-                ObjectOutputStream msg_out=use_magic? new MagicObjectOutputStream(msg_data) : new ObjectOutputStream(msg_data);
-                m.writeExternal(msg_out);
-                // msg_out.writeObject(m);
-                msg_out.close();
-            }
 
             jgbuf=new Buffer(msg_data.getRawBuffer(), 0, msg_data.size());
 
             ByteArrayInputStream msg_in_data=new ByteArrayInputStream(jgbuf.getBuf(), jgbuf.getOffset(), jgbuf.getLength());
             Message m2=(Message)Message.class.newInstance();
 
-            if(use_streamable) {
                 DataInputStream dis=new DataInputStream(msg_in_data);
                 m2.readFrom(dis);
                 dis.close();
             }
-            else {
-                ObjectInputStream msg_in=use_magic? new MagicObjectInputStream(msg_in_data) : new ObjectInputStream(msg_in_data);
-
-                m2.readExternal(msg_in);
-                // Message m2=(Message)msg_in.readObject();
-                msg_in.close();
-            }
-
-        }
 
         long stop=System.currentTimeMillis();
         System.out.println("Serializing and deserializing a message " + num + " times took " + (stop - start) + "ms.");
@@ -109,7 +79,7 @@ public class MessageSerializationTest {
      * Adds some dummy headers to the message
      */
     static void addHeaders(Message msg) {
-        msg.putHeader("UDP", new UdpHeader("MyGroup"));
+        msg.putHeader("UDP", new TpHeader("MyGroup"));
         msg.putHeader("PING", new PingHeader(PingHeader.GET_MBRS_REQ, null));
         msg.putHeader("FD_SOCK", new FD_SOCK.FdHeader());
         msg.putHeader("VERIFY_SUSPECT", new VERIFY_SUSPECT.VerifyHeader());
@@ -122,7 +92,6 @@ public class MessageSerializationTest {
 
 
     static void help() {
-        System.out.println("MessageSerializationTest [-help] [-add_headers] [-num <iterations>] " +
-                           "[-use_magic] [-use_streamable]");
+        System.out.println("MessageSerializationTest [-help] [-add_headers] [-num <iterations>]");
     }
 }
