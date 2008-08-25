715aec75a8b3e2c4e1676c9bef6d7c869aafc917
Bela Ban <belaban@yahoo.com>
2008-08-25 12:01:18 +0000

    ns

--- src/org/jgroups/blocks/Cache.java
+++ src/org/jgroups/blocks/Cache.java
@@ -6,23 +6,20 @@ import org.jgroups.annotations.Experimental;
 import org.jgroups.annotations.Unsupported;
 
 import java.util.concurrent.*;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.SortedMap;
-import java.util.TreeMap;
+import java.util.*;
 
 /**
  * Simple cache which maintains keys and value. A reaper can be enabled which periodically evicts expired entries.
  * Also, when the cache is configured to be bounded, entries in excess of the max size will be evicted on put(). 
  * @author Bela Ban
- * @version $Id: Cache.java,v 1.1 2008/08/25 11:31:51 belaban Exp $
+ * @version $Id: Cache.java,v 1.2 2008/08/25 12:01:18 belaban Exp $
  */
 @Experimental
 @Unsupported
 public class Cache<K,V> {
     private static final Log log=LogFactory.getLog(Cache.class);
     private final ConcurrentMap<K,Value<V>> map=new ConcurrentHashMap<K,Value<V>>();
-    private final ScheduledThreadPoolExecutor timer=new ScheduledThreadPoolExecutor(1);
+    private ScheduledThreadPoolExecutor timer=new ScheduledThreadPoolExecutor(1);
     private Future task=null;
 
     /** The maximum number of keys, When this value is exceeded we evict older entries, until we drop below this 
@@ -52,7 +49,25 @@ public class Cache<K,V> {
         }
     }
 
+    public void start() {
+        if(timer == null)
+            timer=new ScheduledThreadPoolExecutor(1);
+    }
 
+    public void stop() {
+        timer.shutdown();
+        timer=null;
+    }
+
+    /**
+     *
+     * @param key
+     * @param val
+     * @param caching_time Number of milliseconds to keep an entry in the cache. -1 means don't cache (if reaping
+     * is enabled, we'll evict an entry with -1 caching time), 0 means never evict. In the latter case, we can still
+     * evict an entry with 0 caching time: when we have a bounded cache, we evict in order of insertion no matter
+     * what the caching time is.
+     */
     public void put(K key, V val, long caching_time) {
         if(log.isTraceEnabled())
             log.trace("put(" + key + ", " + val + ", " + caching_time + ")");
@@ -65,15 +80,19 @@ public class Cache<K,V> {
             }
             if(map.size() > max_num_entries) {
                 // still too many entries: now evict entries based on insertion time: oldest first
-                int diff=max_num_entries - map.size(); // we have to evict diff entries
+                int diff=map.size() - max_num_entries; // we have to evict diff entries
                 SortedMap<Long,K> tmp=new TreeMap<Long,K>();
                 for(Map.Entry<K,Value<V>> entry: map.entrySet()) {
                     tmp.put(entry.getValue().insertion_time, entry.getKey());
                 }
 
-                for(K k: tmp.values()) {
-                    if(diff-- > 0)
-                        map.remove(k);
+                Collection<K> vals=tmp.values();
+                for(K k: vals) {
+                    if(diff-- > 0) {
+                        Value<V> v=map.remove(k);
+                        if(log.isTraceEnabled())
+                            log.trace("evicting " + k + ": " + v.value);
+                    }
                     else
                         break;
                 }
@@ -104,10 +123,12 @@ public class Cache<K,V> {
         for(Iterator<Map.Entry<K,Value<V>>> it=map.entrySet().iterator(); it.hasNext();) {
             Map.Entry<K,Value<V>> entry=it.next();
             Value<V> val=entry.getValue();
-            if(val != null && val.expiration_time > 0 && System.currentTimeMillis() > val.expiration_time) {
-                if(log.isTraceEnabled())
-                    log.trace("evicting " + entry.getKey() + ": " + entry.getValue().value);
-                it.remove();
+            if(val != null) {
+                if(val.expiration_time == -1 || (val.expiration_time > 0 && System.currentTimeMillis() > val.expiration_time)) {
+                    if(log.isTraceEnabled())
+                        log.trace("evicting " + entry.getKey() + ": " + entry.getValue().value);
+                    it.remove();
+                }
             }
         }
     }
--- src/org/jgroups/blocks/PartitionedHashMap.java
+++ src/org/jgroups/blocks/PartitionedHashMap.java
@@ -8,13 +8,13 @@ import org.jgroups.annotations.Unsupported;
 
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
-import java.util.List;
+import java.util.*;
 
 /** Hashmap which distributes its keys and values across the cluster. A PUT/GET/REMOVE computes the cluster node to which
  * or from which to get/set the key/value from a hash of the key and then forwards the request to the remote cluster node.
  * We also maintain a local cache (L1 cache) which is a bounded cache that caches retrieved keys/values.
  * @author Bela Ban
- * @version $Id: PartitionedHashMap.java,v 1.1 2008/08/25 07:23:01 belaban Exp $
+ * @version $Id: PartitionedHashMap.java,v 1.2 2008/08/25 12:01:32 belaban Exp $
  */
 @Experimental @Unsupported
 public class PartitionedHashMap implements MembershipListener {
@@ -26,9 +26,10 @@ public class PartitionedHashMap implements MembershipListener {
     private RpcDispatcher disp=null;
     private String props="udp.xml";
     private String cluster_name="PartitionedHashMap-Cluster";
-    private Address[] nodes=null;
     private long call_timeout=1000L;
+    private long caching_time=30000L; // in milliseconds. -1 means don't cache, 0 means cache forever (or until changed)
     private HashFunction hash_function=null;
+    private Set<MembershipListener> membership_listeners=new HashSet<MembershipListener>();
 
 
     public interface HashFunction {
@@ -37,10 +38,10 @@ public class PartitionedHashMap implements MembershipListener {
          * consistent hashing, so that the same key maps to the same node despite cluster view changes. If a view change
          * causes all keys to hash to different nodes, then PartitionedHashMap will redirect requests to different nodes
          * and this causes unnecessary overhead.
-         * @param nodes
+         * @param key The object to be hashed
          * @return
          */
-        Address hash(List<Address> nodes);
+        Address hash(Object key);
     }
 
 
@@ -65,6 +66,22 @@ public class PartitionedHashMap implements MembershipListener {
         this.cluster_name=cluster_name;
     }
 
+    public long getCallTimeout() {
+        return call_timeout;
+    }
+
+    public void setCallTimeout(long call_timeout) {
+        this.call_timeout=call_timeout;
+    }
+
+    public long getCachingTime() {
+        return caching_time;
+    }
+
+    public void setCachingTime(long caching_time) {
+        this.caching_time=caching_time;
+    }
+
     public HashFunction getHashFunction() {
         return hash_function;
     }
@@ -73,7 +90,17 @@ public class PartitionedHashMap implements MembershipListener {
         this.hash_function=hash_function;
     }
 
+    public void addMembershipListener(MembershipListener l) {
+        membership_listeners.add(l);
+    }
+
+    public void removeMembershipListener(MembershipListener l) {
+        membership_listeners.remove(l);
+    }
+
     public void start() throws ChannelException {
+        hash_function=new ConsistentHashFunction();
+        addMembershipListener((MembershipListener)hash_function);
         ch=new JChannel(props);
         disp=new RpcDispatcher(ch, null, this, this);
         ch.connect(cluster_name);
@@ -88,17 +115,7 @@ public class PartitionedHashMap implements MembershipListener {
 
 
     public void put(Object key, Object val) {
-        Address dest_node=getNode(key);
-        try {
-            disp.callRemoteMethod(dest_node, "_put",
-                                  new Object[]{key, val},
-                                  new Class[]{Object.class, Object.class},
-                                  GroupRequest.GET_NONE, 0);
-        }
-        catch(Throwable t) {
-            if(log.isWarnEnabled())
-                log.warn("_put() failed", t);
-        }
+        put(key, val, this.caching_time);
     }
 
     /**
@@ -108,12 +125,12 @@ public class PartitionedHashMap implements MembershipListener {
      * @param caching_time Time to live. -1 means never cache, 0 means cache forever. All other (positive) values
      * are the number of milliseconds to cache the item
      */
-    public void put(Object key, Object val, int caching_time) {
+    public void put(Object key, Object val, long caching_time) {
         Address dest_node=getNode(key);
         try {
             disp.callRemoteMethod(dest_node, "_put",
                                   new Object[]{key, val, caching_time},
-                                  new Class[]{Object.class, Object.class, int.class},
+                                  new Class[]{Object.class, Object.class, long.class},
                                   GroupRequest.GET_NONE, 0);
         }
         catch(Throwable t) {
@@ -128,7 +145,7 @@ public class PartitionedHashMap implements MembershipListener {
             return disp.callRemoteMethod(dest_node, "_get",
                                                 new Object[]{key},
                                                 new Class[]{Object.class},
-                                                GroupRequest.GET_NONE, 0);
+                                                GroupRequest.GET_FIRST, call_timeout);
         }
         catch(Throwable t) {
             if(log.isWarnEnabled())
@@ -159,7 +176,7 @@ public class PartitionedHashMap implements MembershipListener {
         cache.put(key, val);
     }
 
-    public void _put(Object key, Object val, int caching_time) {
+    public void _put(Object key, Object val, long caching_time) {
         if(log.isTraceEnabled())
             log.trace("_put(" + key + ", " + val + ", " + caching_time + ")");
         cache.put(key, val);
@@ -182,13 +199,10 @@ public class PartitionedHashMap implements MembershipListener {
 
 
     public void viewAccepted(View new_view) {
-
         System.out.println("new_view = " + new_view);
         this.view=new_view;
-        nodes=new Address[new_view.size()];
-        int index=0;
-        for(Address addr: new_view.getMembers()) {
-            nodes[index++]=addr;
+        for(MembershipListener l: membership_listeners) {
+            l.viewAccepted(new_view);
         }
     }
 
@@ -200,11 +214,53 @@ public class PartitionedHashMap implements MembershipListener {
 
 
     private Address getNode(Object key) {
-        if(hash_function != null)
-            return hash_function.hash(view.getMembers());
+        return hash_function.hash(key);
+    }
 
-        int hash=Math.abs(key.hashCode());
-        int index=hash % nodes.length;
-        return nodes[index];
+
+    private static class ConsistentHashFunction implements HashFunction, MembershipListener {
+        private SortedMap<Short,Address> nodes=new  TreeMap<Short,Address>();
+        private final static int HASH_SPACE=2000; // must be > max number of nodes in a cluster
+
+        public Address hash(Object key) {
+            int hash=Math.abs(key.hashCode());
+            int index=hash % HASH_SPACE;
+            System.out.println("hash for " + key + " is " + index);
+            Address retval;
+            for(int i=index; i < index + HASH_SPACE; i++) {
+                short new_index=(short)(i % HASH_SPACE);
+                retval=nodes.get(new_index);
+                if(retval != null) {
+                    System.out.println("hashed " + key + " to " + retval);
+                    return retval;
+                }
+            }
+            return null;
+        }
+
+        public void viewAccepted(View new_view) {
+            System.out.println("nodes:");
+            nodes.clear();
+            for(Address node: new_view.getMembers()) {
+                int hash=Math.abs(node.hashCode()) % HASH_SPACE;
+                for(int i=hash; i < hash + HASH_SPACE; i++) {
+                    short new_index=(short)(i % HASH_SPACE);
+                    if(!nodes.containsKey(new_index)) {
+                        nodes.put(new_index, node);
+                        break;
+                    }
+                }
+            }
+
+            for(Map.Entry<Short,Address> entry: nodes.entrySet()) {
+                System.out.println(entry.getKey() + ": " + entry.getValue());
+            }
+        }
+
+        public void suspect(Address suspected_mbr) {
+        }
+
+        public void block() {
+        }
     }
 }
