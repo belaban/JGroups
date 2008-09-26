380c0cb01c5f5d8c6fa19db8f558fe2837e3cca4
Bela Ban <belaban@yahoo.com>
2008-09-26 10:34:17 +0000

    ns

--- bin/jgroups.sh
+++ bin/jgroups.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
-JG=.
-LIB=$JG
+LIB=$JG/lib
 
-CP=$JG/classes:$JG/conf:$LIB/jgroups-all.jar:$LIB/commons-logging.jar:$LIB/log4j.jar:$JG/keystore
+CP=$JG/classes:$JG/conf
 
+for i in $LIB/*.jar
+do
+    CP=$CP:$i
+done
 
-FLAGS="$FLAGS -Djava.net.preferIPv4Stack=true -Djgroups.bind_addr=192.168.1.5 -Djgroups.tcpping.initial_hosts=192.168.1.5[7800]"
+LOG="-Dlog4j.configuration=file:$HOME/log4j.properties"
+JG_FLAGS="-Dresolve.dns=false -Djgroups.bind_addr=$IP_ADDR -Djboss.tcpping.initial_hosts=$IP_ADDR[7800]"
+FLAGS="-server -Xmx600M -Xms600M"
+FLAGS="$FLAGS -XX:CompileThreshold=10000 -XX:+AggressiveHeap -XX:ThreadStackSize=64 -XX:SurvivorRatio=8"
+FLAGS="$FLAGS -XX:TargetSurvivorRatio=90 -XX:MaxTenuringThreshold=31"
+FLAGS="$FLAGS -Djava.net.preferIPv4Stack=true -Djgroups.timer.num_threads=4"
+FLAGS="$FLAGS -Xshare:off -XX:+UseBiasedLocking"
+JMX="-Dcom.sun.management.jmxremote"
+EXPERIMENTAL="-XX:+UseFastAccessorMethods -XX:+UseTLAB"
 
-java -Ddisable_canonicalization=false -classpath $CP $LOG $FLAGS -Dcom.sun.management.jmxremote -Dresolve.dns=false org.jgroups.$*
+java -classpath $CP $LOG $JG_FLAGS $FLAGS $EXPERIMENTAL $JMX  $*
+
