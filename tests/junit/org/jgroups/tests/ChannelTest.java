924c55b7281b8ca75ec3508efa96567ffe8b1087
vlada <vlada>
2008-10-13 14:20:37 +0000

    convert to isolated test units

--- tests/junit/org/jgroups/tests/ChannelTest.java
+++ tests/junit/org/jgroups/tests/ChannelTest.java
@@ -4,8 +4,6 @@ package org.jgroups.tests;
 
 import org.jgroups.*;
 import org.jgroups.util.Util;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.LinkedList;
@@ -15,40 +13,18 @@ import java.util.List;
 /**
  * Tests various methods in JChannel
  * @author Bela Ban
- * @version $Id: ChannelTest.java,v 1.22 2008/08/08 17:07:12 vlada Exp $
+ * @version $Id: ChannelTest.java,v 1.23 2008/10/13 14:20:37 vlada Exp $
  */
 @Test(groups=Global.STACK_DEPENDENT,sequential=false)
-public class ChannelTest extends ChannelTestBase {
-    private final ThreadLocal<JChannel> ch=new ThreadLocal<JChannel>();
-    private final ThreadLocal<String>  GROUP=new ThreadLocal<String>();
-
-
-    @BeforeMethod
-    void init() throws Exception {
-        String cluster_name=getUniqueClusterName("ChannelTest");
-        GROUP.set(cluster_name);
-        JChannel tmp=createChannel(true, 2);
-        tmp.connect(GROUP.get());
-        ch.set(tmp);
-    }
-
-
-    @AfterMethod
-    void cleanup() {
-        Channel tmp_ch=ch.get();
-        Util.close(tmp_ch);
-        ch.set(null);
-        GROUP.set(null);
-    }
-
+public class ChannelTest extends ChannelTestBase {       
 
     @Test
     public void testBasicOperations() throws Exception {
-        Channel c1 = createChannel(ch.get());
-        Channel c2=null;
+        JChannel c1 = createChannel(true,1);
+        JChannel c2=null;
 
         try {
-            c1.connect(GROUP.get());
+            c1.connect("testBasicOperations");
             assert c1.isOpen();
             assert c1.isConnected();
        
@@ -56,7 +32,7 @@ public class ChannelTest extends ChannelTestBase {
             assert c1.getView() != null;
             assert c1.getView().getMembers().contains(c1.getLocalAddress());
        
-            c1.connect(GROUP.get());
+            c1.connect("testBasicOperations");
             c1.disconnect();
             assert c1.isConnected() == false;
             assert c1.isOpen();
@@ -64,12 +40,12 @@ public class ChannelTest extends ChannelTestBase {
             assert c1.getView() == null;
             assert c1.getClusterName() == null;
        
-            c1.connect(GROUP.get());
+            c1.connect("testBasicOperations");
 
             c1.close();
        
             try {
-                c1.connect(GROUP.get());
+                c1.connect("testBasicOperations");
                 throw new IllegalStateException("Should generated exception, and it has NOT");
             }
             catch (Exception e) {
@@ -83,10 +59,10 @@ public class ChannelTest extends ChannelTestBase {
        
             assert c1.getClusterName() == null;
        
-            c1 = createChannel(ch.get());
-            c1.connect(GROUP.get());
-            c2 = createChannel(ch.get());
-            c2.connect(GROUP.get());
+            c1 = createChannel(true,2);
+            c1.connect("testBasicOperations");
+            c2 = createChannel(c1);
+            c2.connect("testBasicOperations");
        
             Util.sleep(1000);
        
@@ -124,48 +100,43 @@ public class ChannelTest extends ChannelTestBase {
             assert c1.getView().getMembers().contains(c2.getLocalAddress()) == false;
         }
         finally {
-            Util.close(c2);
-            Util.close(c1);
+            Util.close(c1,c2);
         }
-    }
-
-    @Test
-    public void testFirstView() throws Exception {
-        Object obj=ch.get().receive(5000);
-        assert obj instanceof View : "first object returned needs to be a View (was " + obj + ")";
-    }
-
+    }  
 
     @Test
     public void testViewChange() throws Exception {
-        ViewChecker checker=new ViewChecker(ch.get());
-        ch.get().setReceiver(checker);
+        JChannel ch1 = createChannel(true,2);
+        ViewChecker checker=new ViewChecker(ch1);
+        ch1.setReceiver(checker);
+        ch1.connect("testViewChange");
 
-        Channel ch2=createChannel(ch.get());
+        Channel ch2=createChannel(ch1);
         try {
-            ch2.connect(GROUP.get());
+            ch2.connect("testViewChange");
             assertTrue(checker.getReason(), checker.isSuccess());
             ch2.close();
             assertTrue(checker.getReason(), checker.isSuccess());
         }
         finally {
-            Util.close(ch2);
+            Util.close(ch1,ch2);
         }
     }
 
 
     @Test
     public void testIsConnectedOnFirstViewChange() throws Exception {
-        Channel ch2=createChannel(ch.get());
+        JChannel ch1 = createChannel(true,2);        
+        Channel ch2=createChannel(ch1);
         ConnectedChecker tmp=new ConnectedChecker(ch2);
         ch2.setReceiver(tmp);
         try {
-            ch2.connect(GROUP.get());
-            assertFalse(tmp.isConnected());
-            ch2.close();
+            ch1.connect("testIsConnectedOnFirstViewChange");
+            ch2.connect("testIsConnectedOnFirstViewChange");
+            assertFalse(tmp.isConnected());            
         }
         finally {
-            Util.close(ch2);
+            Util.close(ch1,ch2);
         }
     }
 
@@ -173,63 +144,88 @@ public class ChannelTest extends ChannelTestBase {
 
     @Test
     public void testNoViewIsReceivedAferDisconnect() throws Exception {
-        final Channel ch2 = createChannel(ch.get());
+        JChannel ch1 = createChannel(true,2);        
+        Channel ch2=createChannel(ch1);
         MyViewChecker ra = new MyViewChecker(ch2);
         ch2.setReceiver(ra);
 
         try {
-            ch2.connect(GROUP.get());
+            ch1.connect("testNoViewIsReceivedAferDisconnect");
+            ch2.connect("testNoViewIsReceivedAferDisconnect");
             Util.sleep(500);
             ch2.disconnect();
             Util.sleep(1000);
             assert !ra.receivedViewWhenDisconnected : "Received view where not member";
         }
         finally {
-            ch2.close();
+            Util.close(ch1,ch2);
         }
     }
     
     @Test
     public void testNoViewIsReceivedAferClose() throws Exception {
-        final Channel ch2 = createChannel(ch.get());
+        JChannel ch1 = createChannel(true,2);        
+        Channel ch2=createChannel(ch1);
         MyViewChecker ra = new MyViewChecker(ch2);
         ch2.setReceiver(ra);
 
         try {
-            ch2.connect(GROUP.get());
+            ch1.connect("testNoViewIsReceivedAferClose");
+            ch2.connect("testNoViewIsReceivedAferClose");
             Util.sleep(200);
             ch2.close();
             Util.sleep(1000);
             assert !ra.receivedViewWhenDisconnected : "Received view where not member";
         }
         finally {
-            Util.close(ch2);
+            Util.close(ch1,ch2);
         }
     }
 
     @Test(expectedExceptions=TimeoutException.class)
-    public void testReceiveTimeout() throws ChannelException, TimeoutException {
-        ch.get().receive(1000); // this one works, because we're expecting a View
-        ch.get().receive(2000); // .. but this one doesn't (no msg available) - needs to throw a TimeoutException
+    public void testReceiveTimeout() throws Exception, TimeoutException {
+        JChannel ch1 = createChannel(true,2);        
+        try{
+            ch1.connect("testReceiveTimeout");
+            ch1.receive(1000); // this one works, because we're expecting a View
+            ch1.receive(2000);// .. but this one doesn't (no msg available) - needs to throw a TimeoutException
+        }
+        finally{
+            Util.close(ch1);
+        }
     }
 
     @Test(expectedExceptions={NullPointerException.class})
-    public void testNullMessage() throws ChannelClosedException, ChannelNotConnectedException {
-        ch.get().send(null);
+    public void testNullMessage() throws Exception {
+        JChannel ch1 = createChannel(true,2);        
+        try{
+            ch1.connect("testNullMessage");
+            ch1.send(null);
+        }
+        finally{
+            Util.close(ch1);
+        }             
     }
 
     @Test
     public void testOrdering() throws Exception {
         final int NUM=100;
+        JChannel ch=createChannel(true, 2);
         MyReceiver receiver=new MyReceiver(NUM);
-        ch.get().setReceiver(receiver);
-        for(int i=1; i <= NUM; i++) {
-            ch.get().send(new Message(null, null, new Integer(i)));
-            // System.out.println("-- sent " + i);
+        ch.setReceiver(receiver);
+        try {
+            ch.connect("testOrdering");
+            for(int i=1;i <= NUM;i++) {
+                ch.send(new Message(null, null, new Integer(i)));
+                // System.out.println("-- sent " + i);
+            }
+            receiver.waitForCompletion();
+            List<Integer> nums=receiver.getNums();
+            checkMonotonicallyIncreasingNumbers(nums);
         }
-        receiver.waitForCompletion();
-        List<Integer> nums=receiver.getNums();
-        checkMonotonicallyIncreasingNumbers(nums);
+        finally {
+            Util.close(ch);
+        }
     }
 
     private static void checkMonotonicallyIncreasingNumbers(List<Integer> nums) {
@@ -316,8 +312,8 @@ public class ChannelTest extends ChannelTestBase {
         public void viewAccepted(View new_view) {
             View view=channel.getView();
             String str="viewAccepted(): channel's view=" + view + "\nreceived view=" + new_view;
-            // System.out.println(str);
-            if(!view.equals(new_view)) {
+            // System.out.println(str);           
+            if(view!= null && !view.equals(new_view)) {
                 success=false;
                 reason+=str + "\n";
             }
