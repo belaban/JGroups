08be91331c5237e522c4daddb6c8943d69baf522
Vladimir Blagojevic <vblagoje@redhat.com>
2008-10-21 08:59:03 +0000

    change retention policy to RUNTIME

--- src/org/jgroups/annotations/Experimental.java
+++ src/org/jgroups/annotations/Experimental.java
@@ -8,10 +8,10 @@ import java.lang.annotation.RetentionPolicy;
 /**
  * Elements annotated with this annotation are experimental and may get removed from the distribution at any time
  * @author Bela Ban
- * @version $Id: Experimental.java,v 1.2 2008/04/08 14:41:21 belaban Exp $
+ * @version $Id: Experimental.java,v 1.3 2008/10/21 08:59:03 vlada Exp $
  */
 @Target({ElementType.TYPE, ElementType.METHOD, ElementType.PACKAGE})
-@Retention(RetentionPolicy.SOURCE)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Experimental {
     String comment() default "";
 }
