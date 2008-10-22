67f96643fce77419d9ed17f6aa8c79155087e9d3
belaban <belaban>
2008-10-22 10:16:07 +0000

    changed SSH login

--- bin/upload_manual.sh
+++ bin/upload_manual.sh
@@ -2,5 +2,5 @@
 #
 # Uploads the manual to SourceForge
 
-scp -r ../doc/manual/build/en/* belaban@shell.sf.net:/home/groups/j/ja/javagroups/htdocs/javagroupsnew/docs/manual
-scp -r ../doc/tutorial/build/en/* belaban@shell.sf.net:/home/groups/j/ja/javagroups/htdocs/javagroupsnew/docs/tutorial
+scp -r ../doc/manual/build/en/* belaban,javagroups@web.sourceforge.net:/home/groups/j/ja/javagroups/htdocs/javagroupsnew/docs/manual
+scp -r ../doc/tutorial/build/en/* belaban,javagroups@web.sourceforge.net:/home/groups/j/ja/javagroups/htdocs/javagroupsnew/docs/tutorial
