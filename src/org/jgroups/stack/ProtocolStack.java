b186d2e080dc08e91de34dd7ff4555b3e71310a9
Bela Ban <belaban@yahoo.com>
2008-10-29 09:19:32 +0000

    widened scope of monitor on 'singletons' to prevent issues with concurrent connections (https://jira.jboss.org/jira/browse/JGRP-849)

--- src/org/jgroups/stack/ProtocolStack.java
+++ src/org/jgroups/stack/ProtocolStack.java
@@ -30,7 +30,7 @@ import java.util.concurrent.locks.ReentrantLock;
  * stacks, and to destroy them again when not needed anymore
  * 
  * @author Bela Ban
- * @version $Id: ProtocolStack.java,v 1.91 2008/10/22 11:43:27 belaban Exp $
+ * @version $Id: ProtocolStack.java,v 1.92 2008/10/29 09:19:32 belaban Exp $
  */
 public class ProtocolStack extends Protocol implements Transport {
     public static final int ABOVE = 1; // used by insertProtocol()
@@ -687,7 +687,7 @@ public class ProtocolStack extends Protocol implements Transport {
      */
     public void startStack(String cluster_name) throws Exception {
         if(stopped == false) return;
-        
+
         Protocol above_prot=null;        
         for(final Protocol prot: getProtocols()) {
             if(prot instanceof TP) {
@@ -695,30 +695,31 @@ public class ProtocolStack extends Protocol implements Transport {
                 TP transport=(TP)prot;
                 if(transport.isSingleton() && cluster_name != null) {
                     final Map<String, Protocol> up_prots=transport.getUpProtocols();
-                    synchronized(up_prots) {
-                        Set<String> keys=up_prots.keySet();
-                        if(keys.contains(cluster_name))
-                            throw new IllegalStateException("cluster '" + cluster_name + "' is already connected to singleton " +
-                                    "transport: " + keys);
 
-                        for(Iterator<Map.Entry<String,Protocol>> it=up_prots.entrySet().iterator(); it.hasNext();) {
-                            Map.Entry<String,Protocol> entry=it.next();
-                            Protocol tmp=entry.getValue();
-                            if(tmp == above_prot) {
-                                it.remove();
+                    synchronized(singleton_transports) {
+                        synchronized(up_prots) {
+                            Set<String> keys=up_prots.keySet();
+                            if(keys.contains(cluster_name))
+                                throw new IllegalStateException("cluster '" + cluster_name + "' is already connected to singleton " +
+                                        "transport: " + keys);
+
+                            for(Iterator<Map.Entry<String,Protocol>> it=up_prots.entrySet().iterator(); it.hasNext();) {
+                                Map.Entry<String,Protocol> entry=it.next();
+                                Protocol tmp=entry.getValue();
+                                if(tmp == above_prot) {
+                                    it.remove();
+                                }
+                            }
+
+                            if(above_prot != null) {
+                                TP.ProtocolAdapter ad=new TP.ProtocolAdapter(cluster_name, prot.getName(), above_prot, prot,
+                                                                             transport.getThreadNamingPattern(),
+                                                                             transport.getLocalAddress());
+                                ad.setProtocolStack(above_prot.getProtocolStack());
+                                above_prot.setDownProtocol(ad);
+                                up_prots.put(cluster_name, ad);
                             }
                         }
-
-                        if(above_prot != null) {
-                            TP.ProtocolAdapter ad=new TP.ProtocolAdapter(cluster_name, prot.getName(), above_prot, prot, 
-                                                                         transport.getThreadNamingPattern(),
-                                                                         transport.getLocalAddress());
-                            ad.setProtocolStack(above_prot.getProtocolStack());
-                            above_prot.setDownProtocol(ad);
-                            up_prots.put(cluster_name, ad);
-                        }
-                    }
-                    synchronized(singleton_transports) {
                         Tuple<TP, ProtocolStack.RefCounter> val=singleton_transports.get(singleton_name);
                         if(val != null) {
                             ProtocolStack.RefCounter counter=val.getVal2();
@@ -726,6 +727,11 @@ public class ProtocolStack extends Protocol implements Transport {
                             if(num_starts >= 1) {
                                 if(above_prot != null)
                                     above_prot.up(new Event(Event.SET_LOCAL_ADDRESS, transport.getLocalAddress()));
+                                continue;
+                            }
+                            else {
+                                prot.start();
+                                above_prot=prot;
                                 continue;
                             }
                         }
