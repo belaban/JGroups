67ed581636609604adfca94420fac7aa6d4e0e16
Bela Ban <belaban@yahoo.com>
2008-10-30 08:27:18 +0000

    ns

--- bin/jgroups.sh
+++ bin/jgroups.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-JG=.
+JG=..
 LIB=$JG
 
-CP=$JG/classes:$JG/conf:$LIB/jgroups-all.jar:$LIB/commons-logging.jar:$LIB/log4j.jar:$JG/keystore
+CP=$JG/classes:$JG/conf:$LIB/commons-logging.jar:$LIB/log4j.jar:$JG/keystore
 
+echo $CP
 
 FLAGS="$FLAGS -Djava.net.preferIPv4Stack=true -Djgroups.bind_addr=192.168.2.5 -Djgroups.tcpping.initial_hosts=192.168.2.5[7800]"
 
