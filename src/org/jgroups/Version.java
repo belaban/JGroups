5af0f93f115dac83b66fe51042e06c2383c8dc17
belaban <belaban>
2008-11-12 11:22:30 +0000

    upgraded to 2.7.0.CR1

--- src/org/jgroups/Version.java
+++ src/org/jgroups/Version.java
@@ -15,7 +15,7 @@ import org.jgroups.annotations.Immutable;
  * Z = 0-63 for micro versions
  *
  * @author Bela Ban
- * @version $Id: Version.java,v 1.63 2008/09/25 11:57:54 belaban Exp $
+ * @version $Id: Version.java,v 1.64 2008/11/12 11:22:30 belaban Exp $
  * Holds version information for JGroups.
  */
 @Immutable
@@ -23,11 +23,11 @@ public class Version {
     public static final short major = 2;
     public static final short minor = 7;
     public static final short micro = 0;
-    public static final String description="2.7.0.ALPHA1";
+    public static final String description="2.7.0.CR1";
 
     public static final short version=encode(major, minor, micro);
     public static final String string_version=print(version);
-    public static final String cvs="$Id: Version.java,v 1.63 2008/09/25 11:57:54 belaban Exp $";
+    public static final String cvs="$Id: Version.java,v 1.64 2008/11/12 11:22:30 belaban Exp $";
 
     private static final int MAJOR_SHIFT = 11;
     private static final int MINOR_SHIFT = 6;
