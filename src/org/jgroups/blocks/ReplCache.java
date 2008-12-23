c9ab3f218222d935ecc588c9bced1d2e7da77779
belaban <belaban>
2008-12-23 16:52:25 +0000

    ns

--- src/org/jgroups/blocks/ReplCache.java
+++ src/org/jgroups/blocks/ReplCache.java
@@ -1,13 +1,22 @@
 package org.jgroups.blocks;
 
+import org.jgroups.util.Streamable;
+
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.ConcurrentHashMap;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.DataInputStream;
+
 /**
  * Cache which allows for replication factors <em>per data items</em>; the factor determines how many replicas
  * of a key/value we create across the cluster.<br/>
  * See doc/design/ReplCache.txt for details.
  * @author Bela Ban
- * @version $Id: ReplCache.java,v 1.1 2008/12/23 16:30:12 belaban Exp $
+ * @version $Id: ReplCache.java,v 1.2 2008/12/23 16:52:25 belaban Exp $
  */
-public class ReplCache {
+public class ReplCache<K,V> {
+    private final ConcurrentMap<K,V> cache=new ConcurrentHashMap<K,V>();
 
     /**
      * Places a key/value pair into one or several nodes in the cluster.
@@ -26,7 +35,7 @@ public class ReplCache {
      * <li>&gt; 0: number of milliseconds to keep an idle element in the cache. An element is idle when not accessed.
      * </ul>
      */
-    public void put(Object key, Object val, short repl_factor, long timeout) {
+    public void put(K key, V val, short repl_factor, long timeout) {
 
     }
 
@@ -35,7 +44,7 @@ public class ReplCache {
      * @param key The key, has to be serializable
      * @return The value associated with key, or null
      */
-    public Object get(Object key) {
+    public V get(K key) {
         return null;
     }
 
@@ -43,7 +52,85 @@ public class ReplCache {
      * Removes key in all nodes in the cluster, both from their local hashmaps and L1 caches
      * @param key The key, needs to be serializable 
      */
-    public void remove(Object key) {
+    public void remove(K key) {
         
+    }
+
+    private static class Message<K,V> implements Streamable {
+        static enum Type {PUT, GET, REMOVE};
+
+        private Type type;
+        private K key;
+        private V val;
+        private short repl_factor=-1; // default is replicate everywhere
+        private long timeout=30000;
+
+        private Message(Type type, K key, V val, short repl_factor, long timeout) {
+            this.type=type;
+            this.key=key;
+            this.val=val;
+            this.repl_factor=repl_factor;
+            this.timeout=timeout;
+        }
+
+        private Message(Type type, K key) {
+            this.type=type;
+            this.key=key;
+        }
+
+        public static <K,V> Message<K,V> createPutMessage(K key, V val, short repl_factor, long timeout) {
+            return new Message<K,V>(Type.PUT, key, val, repl_factor, timeout);
+        }
+
+        public static <K,V> Message<K,V> createGetMessage(K key) {
+            return new Message<K,V>(Type.GET, key);
+        }
+
+        public static <K,V> Message<K,V> createRemoveMessage(K key) {
+            return new Message<K,V>(Type.REMOVE, key);
+        }
+
+
+        public String toString() {
+            return "type=" + type +
+                    ", key=" + key +
+                    ", val=" + val +
+                    ", repl_factor=" + repl_factor +
+                    ", timeout=" + timeout;
+        }
+
+
+        public void writeTo(DataOutputStream out) throws IOException {
+            switch(type) {
+                case PUT:
+                    out.writeByte(1);
+                    break;
+                case GET:
+                    out.writeByte(2);
+                    break;
+                case REMOVE:
+                    out.writeByte(3);
+                    break;
+                default:
+                    throw new IllegalStateException("type " + type + " is not handled");
+            }
+        }
+
+        public void readFrom(DataInputStream in) throws IOException, IllegalAccessException, InstantiationException {
+            byte tmp=in.readByte();
+            switch(tmp) {
+                case 1:
+                    type=Type.PUT;
+                    break;
+                case 2:
+                    type=Type.GET;
+                    break;
+                case 3:
+                    type=Type.REMOVE;
+                    break;
+                default:
+                    throw new IllegalStateException("type " + tmp + " is not handled");
+            }
+        }
     }
 }
