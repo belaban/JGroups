7dd3ed77690f13890e9780c73b610881fb913e05
belaban <belaban>
2009-01-09 08:11:24 +0000

    ns

--- src/org/jgroups/demos/ReplCacheDemo.java
+++ src/org/jgroups/demos/ReplCacheDemo.java
@@ -2,13 +2,20 @@
 package org.jgroups.demos;
 
 
+import org.jgroups.blocks.Cache;
+import org.jgroups.blocks.ReplCache;
+import org.jgroups.jmx.JmxConfigurator;
+
+import javax.management.MBeanServer;
 import javax.swing.*;
-import javax.swing.event.ListSelectionEvent;
-import javax.swing.event.ListSelectionListener;
 import javax.swing.table.AbstractTableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.management.ManagementFactory;
+import java.net.InetAddress;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -16,10 +23,13 @@ import java.util.concurrent.ConcurrentHashMap;
 /**
  * GUI demo of ReplCache
  * @author Bela Ban
- * @version $Id: ReplCacheDemo.java,v 1.4 2009/01/08 15:44:26 belaban Exp $
+ * @version $Id: ReplCacheDemo.java,v 1.5 2009/01/09 08:11:24 belaban Exp $
  */
-public class ReplCacheDemo extends JPanel
-                                implements ActionListener { 
+public class ReplCacheDemo extends JPanel implements ActionListener {
+    private ReplCache<String,String> cache;
+    private static final String BASENAME="replcache";
+
+    private JFrame frame;
     private JTable table;
     private JTextField key_field=new JTextField(10);
     private JTextField value_field=new JTextField(10);
@@ -31,14 +41,15 @@ public class ReplCacheDemo extends JPanel
 
     public ReplCacheDemo() {
         super();
+
+        frame=new JFrame("ReplCacheDemo");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
         table = new JTable(model);
         table.setPreferredScrollableViewportSize(new Dimension(500, 200));
         table.setFillsViewportHeight(true);
-        table.getSelectionModel().addListSelectionListener(new RowListener());
-        table.getColumnModel().getSelectionModel().
-            addListSelectionListener(new ColumnListener());
         add(new JScrollPane(table));
         
         JPanel key=new JPanel(new FlowLayout(FlowLayout.LEFT));
@@ -66,7 +77,7 @@ public class ReplCacheDemo extends JPanel
         buttons.add(createButton("Remove"));
         buttons.add(createButton("Exit"));
         add(buttons);
-
+        setOpaque(true);
     }
 
     private JButton createButton(String text) {
@@ -79,7 +90,6 @@ public class ReplCacheDemo extends JPanel
 
     public void actionPerformed(ActionEvent event) {
         String command = event.getActionCommand();
-        System.out.println("command = " + command);
 
         if(command.equals("Put")) {
             String key=key_field.getText();
@@ -103,27 +113,15 @@ public class ReplCacheDemo extends JPanel
             model.fireTableDataChanged();
         }
         else if(command.equals("Exit")) {
+            if(cache != null)
+                cache.stop();
             System.exit(1);
         }
     }
 
 
 
-    private class RowListener implements ListSelectionListener {
-        public void valueChanged(ListSelectionEvent event) {
-            if (event.getValueIsAdjusting()) {
-                return;
-            }
-        }
-    }
 
-    private class ColumnListener implements ListSelectionListener {
-        public void valueChanged(ListSelectionEvent event) {
-            if (event.getValueIsAdjusting()) {
-                return;
-            }
-        }
-    }
 
     private class Entry {
         private String value=null;
@@ -153,18 +151,6 @@ public class ReplCacheDemo extends JPanel
             map.put("hobbies", new Entry("Tennis, Running, Swimming", -1, 0));
         }
 
-//        private Object[][] data = {
-//            {"name", "Bela",
-//             -1, 5000},
-//            {"id", 322649,
-//             "3", 5000L},
-//            {"hobbies", "Tennis, Running, Biking",
-//             "-1", 0},
-//            {"country", "Switzerland",
-//             "1", "0"},
-//            {"zip", "8280",
-//             "1", 0},
-//        };
 
         public int getColumnCount() {
             return columnNames.length;
@@ -189,7 +175,7 @@ public class ReplCacheDemo extends JPanel
             for(Iterator<String> it=map.keySet().iterator(); it.hasNext();) {
                 it.next();
                 for(int i=0; i < rows.length; i++) {
-                    if(i == count) {
+                    if(rows[i] == count) {
                         it.remove();
                         break;
                     }
@@ -221,88 +207,217 @@ public class ReplCacheDemo extends JPanel
             }
         }
 
-        /*
-         * JTable uses this method to determine the default renderer/
-         * editor for each cell.  If we didn't implement this method,
-         * then the last column would contain text ("true"/"false"),
-         * rather than a check box.
-         */
-        public Class getColumnClass(int c) {
-            return getValueAt(0, c).getClass();
-        }
-
-        /*
-         * Don't need to implement this method unless your table's
-         * editable.
-         */
-        public boolean isCellEditable(int row, int col) {
-            //Note that the data/cell address is constant,
-            //no matter where the cell appears onscreen.
-//            if (col < 2) {
-//                return false;
-//            } else {
-//                return true;
-//            }
-            return false;
-        }
-
-        /*
-         * Don't need to implement this method unless your table's data can change.
-         */
-        public void setValueAt(Object value, int row, int col) {
-            // data[row][col] = value;
-            fireTableCellUpdated(row, col);
-        }
-
-
-//        public void change() {
-//            data = new String[][]{
-//                    {"name", "Michelle",
-//                            "-1", "5000"},
-//                    {"id2", "322649",
-//                            "3", "5000"},
-//                    {"hobbies", "Tennis, Running, Biking",
-//                            "-1", "0"},
-//
-//                    {"zip", "8280",
-//                            "1", "0"},
-//            };
-//
-//            fireTableDataChanged();
-//        }
-
     }
 
-    /**
-     * Create the GUI and show it.  For thread safety,
-     * this method should be invoked from the
-     * event-dispatching thread.
-     */
-    public static void main(String[] args) {
-        //Disable boldface controls.
-        UIManager.put("swing.boldMetal", Boolean.FALSE); 
 
-        //Create and set up the window.
-        JFrame frame = new JFrame("ReplCacheDemo");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    private void start(String props, InetAddress bind_addr, int port, int min_threads, int max_threads,
+                       long rpc_timeout, long caching_time, boolean migrate_data, boolean use_l1_cache,
+                       int l1_max_entries, long l1_reaping_interval,
+                       int l2_max_entries, long l2_reaping_interval) throws Exception {
+        MBeanServer server=ManagementFactory.getPlatformMBeanServer();
 
-        //Create and set up the content pane.
-        ReplCacheDemo newContentPane = new ReplCacheDemo();
-        newContentPane.setOpaque(true); //content panes must be opaque
-        frame.setContentPane(newContentPane);
+        cache=new ReplCache(props, "replcache-cluster");
+        cache.setCallTimeout(rpc_timeout);
+        cache.setCachingTime(caching_time);
+        cache.setMigrateData(migrate_data);
+        JmxConfigurator.register(cache, server, BASENAME + ":name=cache");
+        JmxConfigurator.register(cache.getL2Cache(), server, BASENAME + ":name=l2-cache");
 
-        //Display the window.
+        if(use_l1_cache) {
+            Cache<String,String> l1_cache=new Cache<String,String>();
+            cache.setL1Cache(l1_cache);
+            if(l1_reaping_interval > 0)
+                l1_cache.enableReaping(l1_reaping_interval);
+            if(l1_max_entries > 0)
+                l1_cache.setMaxNumberOfEntries(l1_max_entries);
+            JmxConfigurator.register(cache.getL1Cache(), server, BASENAME + ":name=l1-cache");
+        }
+
+        if(l2_max_entries > 0 || l2_reaping_interval > 0) {
+            Cache<String, ReplCache.Value<String>> l2_cache=cache.getL2Cache();
+            if(l2_max_entries > 0)
+                l2_cache.setMaxNumberOfEntries(l2_max_entries);
+            if(l2_reaping_interval > 0)
+                l2_cache.enableReaping(l2_reaping_interval);
+        }
+
+
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            public void run() {
+                cache.stop();
+            }
+        });
+
+        cache.start();
+
+        frame.setContentPane(this);
         frame.pack();
         frame.setVisible(true);
     }
 
-//    public static void main(String[] args) {
-//        //Schedule a job for the event-dispatching thread:
-//        //creating and showing this application's GUI.
-//        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-//            public void run() {
-//                createAndShowGUI();
-//            }
-//        });
-//    }
+
+    public static void main(String[] args) throws Exception {
+        InetAddress bind_addr=null;
+        int port=11211;
+        String props="udp.xml";
+        int min_threads=1, max_threads=500;
+        long rpc_timeout=1500L, caching_time=30000L;
+        boolean migrate_data=true, use_l1_cache=true;
+        int l1_max_entries=5000, l2_max_entries=-1;
+        long l1_reaping_interval=-1, l2_reaping_interval=30000L;
+
+        for(int i=0; i < args.length; i++) {
+            if(args[i].equals("-bind_addr")) {
+                bind_addr=InetAddress.getByName(args[++i]);
+                continue;
+            }
+            if(args[i].equals("-port") || args[i].equals("-p")) {
+                port=Integer.parseInt(args[++i]);
+                continue;
+            }
+            if(args[i].equals("-props")) {
+                props=args[++i];
+                continue;
+            }
+            if(args[i].equals("-min_threads")) {
+                min_threads=Integer.parseInt(args[++i]);
+                continue;
+            }
+            if(args[i].equals("-max_threads")) {
+                max_threads=Integer.parseInt(args[++i]);
+                continue;
+            }
+            if(args[i].equals("-rpc_timeout")) {
+                rpc_timeout=Long.parseLong(args[++i]);
+                continue;
+            }
+            if(args[i].equals("-caching_time")) {
+                caching_time=Long.parseLong(args[++i]);
+                continue;
+            }
+            if(args[i].equals("-migrate_data")) {
+                migrate_data=Boolean.parseBoolean(args[++i]);
+                continue;
+            }
+            if(args[i].equals("-use_l1_cache")) {
+                use_l1_cache=Boolean.parseBoolean(args[++i]);
+                continue;
+            }
+            if(args[i].equals("-l1_max_entries")) {
+                l1_max_entries=Integer.parseInt(args[++i]);
+                continue;
+            }
+            if(args[i].equals("-l1_reaping_interval")) {
+                l1_reaping_interval=Long.parseLong(args[++i]);
+                continue;
+            }
+            if(args[i].equals("-l2_max_entries")) {
+                l2_max_entries=Integer.parseInt(args[++i]);
+                continue;
+            }
+            if(args[i].equals("-l2_reaping_interval")) {
+                l2_reaping_interval=Long.parseLong(args[++i]);
+                continue;
+            }
+
+            help();
+            return;
+        }
+
+
+
+        ReplCacheDemo demo = new ReplCacheDemo();
+        demo.start(props, bind_addr, port, min_threads, max_threads, rpc_timeout, caching_time,
+                             migrate_data, use_l1_cache, l1_max_entries, l1_reaping_interval,
+                             l2_max_entries, l2_reaping_interval);
+        demo.mainLoop();
+    }
+
+    private void mainLoop() throws IOException {
+          while(true) {
+              int c;
+              System.in.skip(System.in.available());
+              System.out.println("\n[1] Put [2] Get [3] Remove [4] Dump [5] view [x] Exit");
+              c=System.in.read();
+              switch(c) {
+                  case -1:
+                      break;
+                  case '1':
+                      put();
+                      break;
+                  case '2':
+                      String key=readString("key");
+                      String val=cache.get(key);
+                      System.out.println("val = " + val);
+                      break;
+                  case '3':
+                      key=readString("key");
+                      cache.remove(key);
+                      break;
+                  case '4':
+                      System.out.println(cache.dump());
+                      break;
+                  case '5':
+                      System.out.println("view = " + cache.getView());
+                      break;
+                  case 'x':
+                      cache.stop();
+                      return;
+                  default:
+                      break;
+              }
+          }
+      }
+
+      private void put() throws IOException {
+          String key=readString("key");
+          String val=readString("value");
+          String tmp=readString("replication count");
+          short count=Short.parseShort(tmp);
+          tmp=readString("timeout");
+          long timeout=Long.parseLong(tmp);
+          cache.put(key, val, count, timeout);
+      }
+
+
+
+      private static void skip(InputStream in) throws IOException {
+          System.in.skip(in.available());
+      }
+
+      private static String readString(String s) throws IOException {
+          int c;
+          boolean looping=true;
+          StringBuilder sb=new StringBuilder();
+          System.out.print(s + ": ");
+          System.out.flush();
+          skip(System.in);
+
+          while(looping) {
+              c=System.in.read();
+              switch(c) {
+                  case -1:
+                  case '\n':
+                  case 13:
+                      looping=false;
+                      break;
+                  default:
+                      sb.append((char)c);
+                      break;
+              }
+          }
+
+          return sb.toString();
+      }
+
+
+    private static void help() {
+        System.out.println("ReplCacheServer [-help] [-bind_addr <address>] [-port <port>] [-props <props>] " +
+                "[-min_threads <min>] [-max_threads <max>] [-rpc_timeout <ms>] [-caching_time <ms>] " +
+                "[-migrate_data <true|false>] [-use_l1_cache <true|false>] " +
+                "[-l1_max_entries <num>] [-l1_reaping_interval <ms>] " +
+                "[-l2_max_entries <num>] [-l2_reaping_interval <ms>] ");
+    }
+
+
 }
