13e240d37ad8c0f179e15c4fbb7239ac23d7a0f7
belaban <belaban>
2009-02-05 08:18:26 +0000

    check for null mcast_sock before closing

--- src/org/jgroups/protocols/MPING.java
+++ src/org/jgroups/protocols/MPING.java
@@ -20,7 +20,7 @@ import java.util.*;
  * back via the regular transport (e.g. TCP) to the sender (discovery request contained sender's regular address,
  * e.g. 192.168.0.2:7800).
  * @author Bela Ban
- * @version $Id: MPING.java,v 1.46 2009/01/05 08:24:06 belaban Exp $
+ * @version $Id: MPING.java,v 1.47 2009/02/05 08:18:26 belaban Exp $
  */
 public class MPING extends PING implements Runnable {
     
@@ -279,7 +279,7 @@ public class MPING extends PING implements Runnable {
     }
 
     public void stop() {
-        mcast_sock.close();
+        Util.close(mcast_sock);
         mcast_sock=null;
         receiver=null;
         super.stop();
