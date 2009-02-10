e3412d27fd5fc284afe19416b672209a8f9a3f1e
Bela Ban <belaban@yahoo.com>
2009-02-10 15:22:50 +0000

    new class

---
+++ src/org/jgroups/util/UUID.java
@@ -0,0 +1,205 @@
+package org.jgroups.util;
+
+import org.jgroups.Address;
+import org.jgroups.Global;
+
+import java.io.*;
+import java.security.SecureRandom;
+
+/** Logical address which is unique over space and time.
+ * <br/>
+ * Copied from java.util.UUID, but unneeded fields from the latter have been removed. UUIDs needs to
+ * have a small memory footprint.
+ * @author Bela Ban
+ * @version $Id: UUID.java,v 1.1 2009/02/10 15:22:50 belaban Exp $
+ */
+public final class UUID implements Address, Streamable, Comparable<Address> {
+
+    private long mostSigBits;
+
+    private long leastSigBits;
+
+    /** The random number generator used by this class to create random based UUIDs */
+    private static volatile SecureRandom numberGenerator=null;
+    private static final long serialVersionUID=3972962439975931228L;
+
+
+    public UUID() {
+    }
+
+
+    public UUID(long mostSigBits, long leastSigBits) {
+        this.mostSigBits = mostSigBits;
+        this.leastSigBits = leastSigBits;
+    }
+
+    /** Private constructor which uses a byte array to construct the new UUID */
+    private UUID(byte[] data) {
+        long msb = 0;
+        long lsb = 0;
+        assert data.length == 16;
+        for (int i=0; i<8; i++)
+            msb = (msb << 8) | (data[i] & 0xff);
+        for (int i=8; i<16; i++)
+            lsb = (lsb << 8) | (data[i] & 0xff);
+        this.mostSigBits = msb;
+        this.leastSigBits = lsb;
+    }
+
+
+
+    /**
+     * Static factory to retrieve a type 4 (pseudo randomly generated) UUID.
+     *
+     * The {@code UUID} is generated using a cryptographically strong pseudo
+     * random number generator.
+     *
+     * @return  A randomly generated {@code UUID}
+     */
+    public static UUID randomUUID() {
+        SecureRandom ng = numberGenerator;
+        if (ng == null) {
+            numberGenerator=ng=new SecureRandom();
+        }
+
+        byte[] randomBytes = new byte[16];
+        ng.nextBytes(randomBytes);
+        return new UUID(randomBytes);
+    }
+
+
+    public long getLeastSignificantBits() {
+        return leastSigBits;
+    }
+
+    /**
+     * Returns the most significant 64 bits of this UUID's 128 bit value.
+     *
+     * @return  The most significant 64 bits of this UUID's 128 bit value
+     */
+    public long getMostSignificantBits() {
+        return mostSigBits;
+    }
+
+
+
+    /**
+     * Returns a {@code String} object representing this {@code UUID}.
+     *
+     * <p> The UUID string representation is as described by this BNF:
+     * <blockquote><pre>
+     * {@code
+     * UUID                   = <time_low> "-" <time_mid> "-"
+     *                          <time_high_and_version> "-"
+     *                          <variant_and_sequence> "-"
+     *                          <node>
+     * time_low               = 4*<hexOctet>
+     * time_mid               = 2*<hexOctet>
+     * time_high_and_version  = 2*<hexOctet>
+     * variant_and_sequence   = 2*<hexOctet>
+     * node                   = 6*<hexOctet>
+     * hexOctet               = <hexDigit><hexDigit>
+     * hexDigit               =
+     *       "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9"
+     *       | "a" | "b" | "c" | "d" | "e" | "f"
+     *       | "A" | "B" | "C" | "D" | "E" | "F"
+     * }</pre></blockquote>
+     *
+     * @return  A string representation of this {@code UUID}
+     */
+    public String toString() {
+        return (digits(mostSigBits >> 32, 8) + "-" +
+                digits(mostSigBits >> 16, 4) + "-" +
+                digits(mostSigBits, 4) + "-" +
+                digits(leastSigBits >> 48, 4) + "-" +
+                digits(leastSigBits, 12));
+    }
+
+    /** Returns val represented by the specified number of hex digits. */
+    private static String digits(long val, int digits) {
+        long hi = 1L << (digits * 4);
+        return Long.toHexString(hi | (val & (hi - 1))).substring(1);
+    }
+
+    /**
+     * Returns a hash code for this {@code UUID}.
+     *
+     * @return  A hash code value for this {@code UUID}
+     */
+    public int hashCode() {
+        return (int)((mostSigBits >> 32) ^
+                mostSigBits ^
+                (leastSigBits >> 32) ^
+                leastSigBits);
+    }
+
+    /**
+     * Compares this object to the specified object.  The result is {@code
+     * true} if and only if the argument is not {@code null}, is a {@code UUID}
+     * object, has the same variant, and contains the same value, bit for bit,
+     * as this {@code UUID}.
+     *
+     * @param  obj
+     *         The object to be compared
+     *
+     * @return  {@code true} if the objects are the same; {@code false}
+     *          otherwise
+     */
+    public boolean equals(Object obj) {
+        if (!(obj instanceof UUID))
+            return false;
+        UUID id = (UUID)obj;
+        return (mostSigBits == id.mostSigBits &&
+                leastSigBits == id.leastSigBits);
+    }
+
+
+    /**
+     * Compares this UUID with the specified UUID.
+     * <p> The first of two UUIDs is greater than the second if the most
+     * significant field in which the UUIDs differ is greater for the first UUID.
+     * @param  other {@code UUID} to which this {@code UUID} is to be compared
+     * @return  -1, 0 or 1 as this {@code UUID} is less than, equal to, or greater than {@code val}
+     */
+    public int compareTo(Address other) {
+        UUID val=(UUID)other;
+        return (this.mostSigBits < val.mostSigBits ? -1 :
+                (this.mostSigBits > val.mostSigBits ? 1 :
+                        (this.leastSigBits < val.leastSigBits ? -1 :
+                                (this.leastSigBits > val.leastSigBits ? 1 :
+                                        0))));
+    }
+
+
+
+    public void writeTo(DataOutputStream out) throws IOException {
+        out.writeLong(leastSigBits);
+        out.writeLong(mostSigBits);
+    }
+
+    public void readFrom(DataInputStream in) throws IOException, IllegalAccessException, InstantiationException {
+        leastSigBits=in.readLong();
+        mostSigBits=in.readLong();
+    }
+
+    public boolean isMulticastAddress() {
+        return false;
+    }
+
+    public int size() {
+        return Global.LONG_SIZE * 2;
+    }
+
+    public void writeExternal(ObjectOutput out) throws IOException {
+        out.writeLong(leastSigBits);
+        out.writeLong(mostSigBits);
+    }
+
+
+    public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+        leastSigBits=in.readLong();
+        mostSigBits=in.readLong();
+    }
+
+
+}
