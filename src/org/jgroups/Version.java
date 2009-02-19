ada63f2693e42ca1b25f0741d3778851a369e0fb
Vladimir Blagojevic <vblagoje@redhat.com>
2009-02-19 13:03:09 +0000

    prepare for 2.6.8

--- src/org/jgroups/Version.java
+++ src/org/jgroups/Version.java
@@ -15,7 +15,7 @@ import org.jgroups.annotations.Immutable;
  * Z = 0-63 for micro versions
  *
  * @author Bela Ban
- * @version $Id: Version.java,v 1.59.2.15 2009/02/09 17:43:08 vlada Exp $
+ * @version $Id: Version.java,v 1.59.2.16 2009/02/19 13:03:09 vlada Exp $
  * Holds version information for JGroups.
  */
 @Immutable
@@ -23,11 +23,11 @@ public class Version {
     public static final short major = 2;
     public static final short minor = 6;
     public static final short micro = 8;
-    public static final String description="2.6.8.CR1";
+    public static final String description="2.6.8.GA";
 
     public static final short version=encode(major, minor, micro);
     public static final String string_version=print(version);
-    public static final String cvs="$Id: Version.java,v 1.59.2.15 2009/02/09 17:43:08 vlada Exp $";
+    public static final String cvs="$Id: Version.java,v 1.59.2.16 2009/02/19 13:03:09 vlada Exp $";
 
     private static final int MAJOR_SHIFT = 11;
     private static final int MINOR_SHIFT = 6;
