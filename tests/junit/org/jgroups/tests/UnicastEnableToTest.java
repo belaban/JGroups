33b0308db9d13bc507929104e7644f2dc67d42b1
belaban <belaban>
2009-04-24 14:01:17 +0000

    forward-ported from the 2.6.merge branch

--- tests/junit/org/jgroups/tests/UnicastEnableToTest.java
+++ tests/junit/org/jgroups/tests/UnicastEnableToTest.java
@@ -1,8 +1,10 @@
 package org.jgroups.tests;
 
 import org.jgroups.*;
+import org.jgroups.protocols.UNICAST;
 import org.jgroups.util.UUID;
 import org.jgroups.util.Util;
+import org.jgroups.util.AgeOutCache;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -14,17 +16,22 @@ import java.util.List;
 /**
  * Tests sending of unicasts to members not in the group (http://jira.jboss.com/jira/browse/JGRP-357)
  * @author Bela Ban
- * @version $Id: UnicastEnableToTest.java,v 1.10 2009/04/09 09:11:16 belaban Exp $
+ * @version $Id: UnicastEnableToTest.java,v 1.11 2009/04/24 14:01:17 belaban Exp $
  */
 @Test(groups=Global.STACK_DEPENDENT,sequential=true)
 public class UnicastEnableToTest extends ChannelTestBase {
     private JChannel c1=null, c2=null;
     private static final String GROUP="UnicastEnableToTest";
+    AgeOutCache cache;
 
     @BeforeMethod
     protected void setUp() throws Exception {
         c1=createChannel(true);
         c1.connect(GROUP);
+        UNICAST ucast=(UNICAST)c1.getProtocolStack().findProtocol(UNICAST.class);
+        cache=ucast != null? ucast.getAgeOutCache() : null;
+        if(cache != null)
+            cache.setTimeout(1000);
     }
 
     @AfterMethod
@@ -36,12 +43,14 @@ public class UnicastEnableToTest extends ChannelTestBase {
     public void testUnicastMessageToUnknownMember() throws Exception {
         Address addr=UUID.randomUUID();
         System.out.println("sending message to non-existing destination " + addr);
-        try {
-            c1.send(new Message(addr, null, "Hello world"));
-            assert false : "we should not get here; sending of message to " + addr + " should have failed";
+        c1.send(new Message(addr, null, "Hello world"));
+        if(cache != null) {
+            System.out.println("age out cache:\n" + cache);
+            assert cache.size() == 1;
         }
-        catch(IllegalArgumentException ex) {
-            System.out.println("received exception as expected");
+        Util.sleep(1500);
+        if(cache != null) {
+            assert cache.size() == 0;
         }
     }
 
@@ -54,6 +63,10 @@ public class UnicastEnableToTest extends ChannelTestBase {
         c2.setReceiver(receiver);
         Address dest=c2.getAddress();
         c1.send(new Message(dest, null, "hello"));
+        if(cache != null) {
+            System.out.println("age out cache:\n" + cache);
+            assert cache.size() == 0;
+        }
         Util.sleep(500);
         List<Message> list=receiver.getMsgs();
         System.out.println("channel2 received the following msgs: " + list);
@@ -69,27 +82,17 @@ public class UnicastEnableToTest extends ChannelTestBase {
         Address dest=c2.getAddress();
         c2.close();
         Util.sleep(100);
-        try {
-            c1.send(new Message(dest, null, "hello"));
-            assert false : "we should not come here as message to previous member " + dest + " should throw exception";
-        }
-        catch(IllegalArgumentException ex) {
-            System.out.println("got an exception, as expected");
-        }
-    }
-
-
-    public void testUnicastMessageToLeftMemberWithEnableUnicastToEvent() throws Exception {
-        c2=createChannel(c1);
-        c2.connect(GROUP);
-        assert 2 == c2.getView().size() : "view=" + c2.getView();
-        Address dest=c2.getAddress();
-        c2.close();
-        Util.sleep(100);
-        c1.down(new Event(Event.ENABLE_UNICASTS_TO, dest));
         c1.send(new Message(dest, null, "hello"));
-        c1.down(new Event(Event.DISABLE_UNICASTS_TO, dest));
+        if(cache != null) {
+            System.out.println("age out cache:\n" + cache);
+            assert cache.size() == 1;
+        }
+        Util.sleep(1500);
+        if(cache != null)
+            assert cache.size() == 1;
     }
+
+
 
 
     private static class MyReceiver extends ExtendedReceiverAdapter {
