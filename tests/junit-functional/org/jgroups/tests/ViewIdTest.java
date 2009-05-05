6626976c2f3ea69abcba04d9c4c08c1e2b8f61af
belaban <belaban>
2009-05-05 15:13:12 +0000

    converted to use UUIDs rather than IpAddresses

--- tests/junit-functional/org/jgroups/tests/ViewIdTest.java
+++ tests/junit-functional/org/jgroups/tests/ViewIdTest.java
@@ -1,23 +1,26 @@
-// $Id: ViewIdTest.java,v 1.2 2008/03/10 15:39:22 belaban Exp $
 
 package org.jgroups.tests;
 
-import org.jgroups.ViewId;
 import org.jgroups.Global;
+import org.jgroups.ViewId;
+import org.jgroups.util.Util;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import java.net.InetAddress;
 import java.net.UnknownHostException;
 
+/**
+ * Author: Bela Ban
+ * Version: $Id: ViewIdTest.java,v 1.3 2009/05/05 15:13:14 belaban Exp $
+ */
 @Test(groups=Global.FUNCTIONAL)
 public class ViewIdTest {
     private ViewId v1, v2, v3;
 
     @BeforeClass
     public void setUp() throws UnknownHostException {
-        v1=new ViewId(new org.jgroups.stack.IpAddress(InetAddress.getByName("localhost"), 1000), 22);
-        v2=new ViewId(new org.jgroups.stack.IpAddress(InetAddress.getByName("localhost"), 1000), 21);
+        v1=new ViewId(Util.createRandomAddress(), 22);
+        v2=new ViewId(Util.createRandomAddress(), 21);
         v3=(ViewId)v1.clone();
     }
 
