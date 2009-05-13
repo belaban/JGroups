a9c0effaf4412e8500987560c935a733fb19f3eb
Bela Ban <belaban@yahoo.com>
2009-05-13 13:26:23 +0000

    set version to alpha4

--- src/org/jgroups/Version.java
+++ src/org/jgroups/Version.java
@@ -15,7 +15,7 @@ import org.jgroups.annotations.Immutable;
  * Z = 0-63 for micro versions
  *
  * @author Bela Ban
- * @version $Id: Version.java,v 1.69 2009/05/04 08:14:53 belaban Exp $
+ * @version $Id: Version.java,v 1.70 2009/05/13 13:26:23 belaban Exp $
  * Holds version information for JGroups.
  */
 @Immutable
@@ -23,11 +23,11 @@ public class Version {
     public static final short major = 2;
     public static final short minor = 8;
     public static final short micro = 0;
-    public static final String description="2.8.0.Alpha3";
+    public static final String description="2.8.0.Alpha4";
 
     public static final short version=encode(major, minor, micro);
     public static final String string_version=print(version);
-    public static final String cvs="$Id: Version.java,v 1.69 2009/05/04 08:14:53 belaban Exp $";
+    public static final String cvs="$Id: Version.java,v 1.70 2009/05/13 13:26:23 belaban Exp $";
 
     private static final int MAJOR_SHIFT = 11;
     private static final int MINOR_SHIFT = 6;
