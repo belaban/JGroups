0c1b6ab3ff2193efd12432d7660ce66804de58d3
Bela Ban <belaban@yahoo.com>
2009-05-20 06:53:41 +0000

    ns

--- tests/junit/org/jgroups/protocols/GMS_MergeTest.java
+++ tests/junit/org/jgroups/protocols/GMS_MergeTest.java
@@ -14,34 +14,28 @@ import org.testng.annotations.*;
 
 import java.nio.ByteBuffer;
 import java.util.Vector;
+import java.util.Set;
+import java.util.HashSet;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Tests the GMS protocol for merging functionality
  * @author Bela Ban
- * @version $Id: GMS_MergeTest.java,v 1.2 2009/05/19 15:35:32 belaban Exp $
+ * @version $Id: GMS_MergeTest.java,v 1.3 2009/05/20 06:53:41 belaban Exp $
  */
-@Test(groups={Global.STACK_INDEPENDENT})
+@Test(groups={Global.STACK_INDEPENDENT}, sequential=true)
 public class GMS_MergeTest extends ChannelTestBase {
-    static final String props="SHARED_LOOPBACK:PING:pbcast.NAKACK:UNICAST:pbcast.STABLE:pbcast.GMS:FC:FRAG2";
+    static final String props="SHARED_LOOPBACK:PING(timeout=50):pbcast.NAKACK:UNICAST:pbcast.STABLE:pbcast.GMS:FC:FRAG2";
 
-    private JChannel c1, c2, c3;
 
 
     @BeforeClass
     void setUp() throws Exception {
-        c1=new JChannel(props);
-        c2=new JChannel(props);
-        c3=new JChannel(props);
-        c1.connect("GMS_MergeTest");
-        c2.connect("GMS_MergeTest");
-        c3.connect("GMS_MergeTest");
     }
 
 
     @AfterClass
     void tearDown() throws Exception {
-        Util.close(c3, c2, c1);
     }
 
 
@@ -49,27 +43,102 @@ public class GMS_MergeTest extends ChannelTestBase {
      * Simulates the death of a merge leader after having sent a MERG_REQ. Because there is no MergeView or CANCEL_MERGE
      * message, the MergeCanceller has to null merge_id after a timeout
      */
-    public void testMergeRequestTimeout() throws Exception {
-        Message merge_request=new Message();
-        GMS.GmsHeader hdr=new GMS.GmsHeader(GMS.GmsHeader.MERGE_REQ);
-        MergeId new_merge_id=MergeId.create(c1.getAddress());
-        hdr.setMergeId(new_merge_id);
-        merge_request.putHeader(GMS.name, hdr);
-        GMS gms=(GMS)c1.getProtocolStack().findProtocol(GMS.class);
-        MergeId merge_id=gms.getMergeId();
-        assert merge_id == null;
-        gms.up(new Event(Event.MSG, merge_request));
-        merge_id=gms.getMergeId();
-        assert new_merge_id.equals(merge_id);
+    public static void testMergeRequestTimeout() throws Exception {
+        JChannel c1=new JChannel(props);
+        try {
+            c1.connect("GMS_MergeTest");
+            Message merge_request=new Message();
+            GMS.GmsHeader hdr=new GMS.GmsHeader(GMS.GmsHeader.MERGE_REQ);
+            MergeId new_merge_id=MergeId.create(c1.getAddress());
+            hdr.setMergeId(new_merge_id);
+            merge_request.putHeader(GMS.name, hdr);
+            GMS gms=(GMS)c1.getProtocolStack().findProtocol(GMS.class);
+            MergeId merge_id=gms.getMergeId();
+            assert merge_id == null;
+            System.out.println("starting merge");
+            gms.up(new Event(Event.MSG, merge_request));
+            merge_id=gms.getMergeId();
+            System.out.println("merge_id = " + merge_id);
+            assert new_merge_id.equals(merge_id);
 
-        long timeout=(long)(gms.getMergeTimeout() * 1.5);
-        System.out.println("sleeping for " + timeout + " ms, then fetching merge_id: should be null (cancelled by the MergeCanceller)");
-        Util.sleep(timeout);
-        merge_id=gms.getMergeId();
-        assert merge_id == null : "MergeCanceller didn't kick in";
+            long timeout=(long)(gms.getMergeTimeout() * 1.5);
+            System.out.println("sleeping for " + timeout + " ms, then fetching merge_id: should be null (cancelled by the MergeCanceller)");
+            Util.sleep(timeout);
+            merge_id=gms.getMergeId();
+            System.out.println("merge_id = " + merge_id);
+            assert merge_id == null : "MergeCanceller didn't kick in";
+        }
+        finally {
+            Util.close(c1);
+        }
     }
 
 
+    public static void testSimpleMerge() throws Exception {
+        JChannel[] channels=null;
+        try {
+            channels=create("A", "B", "C", "D");
+            print(channels);
+            View view=channels[channels.length -1].getView();
+            assert view.size() == channels.length : "view is " + view;
+
+            System.out.println("\ncreating partitions: ");
+            String[][] partitions=generate(new String[]{"A", "B"}, new String[]{"C", "D"});
+            createPartitions(channels, partitions);
+            print(channels);
+        }
+        finally {
+            close(channels);
+        }
+    }
+
+
+
+    private static void close(JChannel[] channels) {
+        for(int i=channels.length -1; i <= 0; i--) {
+            JChannel ch=channels[i];
+            Util.close(ch);
+        }
+    }
+
+
+    private static JChannel[] create(String ... names) throws Exception {
+        JChannel[] retval=new JChannel[names.length];
+        for(int i=0; i < retval.length; i++) {
+            JChannel ch=new JChannel(props);
+            ch.setName(names[i]);
+            retval[i]=ch;
+            ch.connect("GMS_MergeTest");
+        }
+        return retval;
+    }
+
+    private static void createPartitions(JChannel[] channels, String[]... partitions) throws Exception {
+        checkUniqueness(partitions);
+
+    }
+
+    private static String[][] generate(String[] ... partitions) {
+        String[][] retval=new String[partitions.length][];
+        System.arraycopy(partitions, 0, retval, 0, partitions.length);
+        return retval;
+    }
+
+    private static void checkUniqueness(String[] ... partitions) throws Exception {
+        Set<String> set=new HashSet<String>();
+        for(String[] partition: partitions) {
+            for(String tmp: partition) {
+                if(!set.add(tmp))
+                    throw new Exception("partitions are overlapping: element " + tmp + " is in multiple partitions");
+            }
+        }
+    }
+
+    private static void print(JChannel[] channels) {
+        for(JChannel ch: channels) {
+            System.out.println(ch.getName() + ": " + ch.getView());
+        }
+    }
 
 
 }
