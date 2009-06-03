c6253d3dfbe493f380b5b86b548785b6a4dccca2
Vladimir Blagojevic <vblagoje@redhat.com>
2009-06-03 13:49:28 +0000

    [JGRP-986] - FLUSH: relax event sequencing for a joining member

--- src/org/jgroups/ExtendedMembershipListener.java
+++ src/org/jgroups/ExtendedMembershipListener.java
@@ -2,13 +2,21 @@ package org.jgroups;
 
 /**
  * @author Bela Ban
- * @version $Id: ExtendedMembershipListener.java,v 1.2 2006/09/27 12:53:22 belaban Exp $
+ * @version $Id: ExtendedMembershipListener.java,v 1.2.6.1 2009/06/03 13:49:28 vlada Exp $
  */
 public interface ExtendedMembershipListener extends MembershipListener {
 
     /**
-     * Called <em>after</em> the FLUSH protocol has unblocked previously blocked senders, and messages can be sent again. This
-     * callback only needs to be implemented if we require a notification of that.
-     */
-    void unblock();
+    * Called <em>after</em> the FLUSH protocol has unblocked previously blocked senders, and
+    * messages can be sent again. This callback only needs to be implemented if we require a
+    * notification of that.
+    * 
+    * <p>
+    * Note that during new view installation we provide guarantee that unblock invocation strictly
+    * follows view installation at some node A belonging to that view . However, some other message
+    * M may squeeze in between view and unblock callbacks.
+    * 
+    * For more details see https://jira.jboss.org/jira/browse/JGRP-986
+    */
+   void unblock();
 }
