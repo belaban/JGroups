ce369e0203c8d77ad432754fc0303e3cc869a33f
jiwils <jiwils>
2009-06-20 00:05:49 +0000

    Added version id change in Version.main.

--- src/org/jgroups/Version.java
+++ src/org/jgroups/Version.java
@@ -13,18 +13,18 @@ package org.jgroups;
  * Z = 0-63 for micro versions
  *
  * @author Bela Ban
- * @version $Id: Version.java,v 1.42.2.13 2009/05/11 22:43:18 rachmatowicz Exp $
+ * @version $Id: Version.java,v 1.42.2.11.4.1 2009/06/20 00:05:49 jiwils Exp $
  * Holds version information for JGroups.
  */
 public class Version {
     public static final short major = 2;
     public static final short minor = 4;
-    public static final short micro = 6;
-    public static final String description="2.4.6.GA";
+    public static final short micro = 5;
+    public static final String description="2.4.5.GA";
 
     public static final short version=encode(major, minor, micro);
     public static final String string_version=print(version);
-    public static final String cvs="$Id: Version.java,v 1.42.2.13 2009/05/11 22:43:18 rachmatowicz Exp $";
+    public static final String cvs="$Id: Version.java,v 1.42.2.11.4.1 2009/06/20 00:05:49 jiwils Exp $";
 
     private static final int MAJOR_SHIFT = 11;
     private static final int MINOR_SHIFT = 6;
@@ -39,7 +39,7 @@ public class Version {
      * @param args
      */
     public static void main(String[] args) {
-        System.out.println("\nVersion: \t" + description);
+        System.out.println("\nVersion: \t" + description + "_JGRP-549");
         System.out.println("CVS: \t\t" + cvs);
         System.out.println("History: \t(see doc/history.txt for details)\n");
     }
