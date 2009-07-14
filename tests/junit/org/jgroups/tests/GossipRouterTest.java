5b12c6dff94cf815835baae752ff2196552b505c
rachmatowicz <rachmatowicz>
2009-07-14 21:37:23 +0000

    Update testsuite for IPv6 testing.

--- tests/junit/org/jgroups/tests/GossipRouterTest.java
+++ tests/junit/org/jgroups/tests/GossipRouterTest.java
@@ -18,22 +18,27 @@ import java.util.concurrent.TimeUnit;
 
 /**
  * @author Bela Ban
- * @version $Id: GossipRouterTest.java,v 1.1.2.5 2008/10/30 11:18:56 belaban Exp $
+ * @version $Id: GossipRouterTest.java,v 1.1.2.6 2009/07/14 21:37:26 rachmatowicz Exp $
  */
 public class GossipRouterTest extends TestCase {
-    final static String PROPS="tunnel.xml";
+    String props = null;
     GossipRouter router;
+    String router_host = null ;
+    int router_port = 0 ;
     JChannel c1, c2;
 
     protected void setUp() throws Exception {
         super.setUp();
-
+        
+    	router_host = System.getProperty("jgroups.tunnel.router_host", "127.0.0.1") ;
+        router_port = Integer.parseInt(System.getProperty("jgroups.tunnel.router_port", "12001")) ;
+        
+        props = getTUNNELProps(router_host, router_port) ;
     }
 
     protected void tearDown() throws Exception {
         if(router != null) {
-            router.stop();
-            router=null;
+        	stopRouter() ;
         }
         if(c2 != null) {
             c2.close();
@@ -44,6 +49,23 @@ public class GossipRouterTest extends TestCase {
         super.tearDown();
     }
 
+    private String getTUNNELProps(String routerHost, int routerPort) {
+        return
+	    "TUNNEL(router_port=" + router_port + ";router_host=" + router_host + ";loopback=true):" +
+            "PING(timeout=2000;num_initial_members=2;gossip_refresh=10000;gossip_host=" + router_host +";gossip_port=" + router_port + ";num_ping_requests=1):" +
+            "MERGE2(min_interval=5000;max_interval=20000):" +
+            "FD(timeout=2000;max_tries=3;shun=true):" +
+            "VERIFY_SUSPECT(timeout=1500):" +
+            "pbcast.NAKACK(use_mcast_xmit=false;gc_lag=0;retransmit_timeout=300,600,1200,2400,4800;discard_delivered_msgs=true):" +
+            "UNICAST(timeout=300,600,1200,2400,3600):" +
+            "pbcast.STABLE(stability_delay=1000;desired_avg_gossip=5000;max_bytes=400000):" +
+            "pbcast.GMS(join_timeout=3000;join_retry_timeout=2000;print_local_addr=true;shun=false;view_bundling=true;view_ack_collection_timeout=5000):" +
+            "FC(max_credits=2000000;min_threshold=0.10):" +
+            "FRAG2(frag_size=60000):" +
+            "pbcast.STATE_TRANSFER:" +
+            "pbcast.FLUSH(timeout=0)" ;
+    }
+    
     /**
      * Tests the following scenario (http://jira.jboss.com/jira/browse/JGRP-682):
      * - First node is started with tunnel.xml, cannot connect
@@ -57,7 +79,7 @@ public class GossipRouterTest extends TestCase {
         AtomicBoolean done=new AtomicBoolean(false);
 
         System.out.println("-- starting first channel");
-        c1=new JChannel(PROPS);
+        c1=new JChannel(props);
         changeMergeInterval(c1);
         setReconnectInterval(c1);
         setRefreshInterval(c1);
@@ -65,7 +87,7 @@ public class GossipRouterTest extends TestCase {
         c1.connect("demo");
 
         System.out.println("-- starting second channel");
-        c2=new JChannel(PROPS);
+        c2=new JChannel(props);
         changeMergeInterval(c2);
         setReconnectInterval(c2);
         setRefreshInterval(c2);
@@ -73,8 +95,7 @@ public class GossipRouterTest extends TestCase {
         c2.connect("demo");
 
         System.out.println("-- starting GossipRouter");
-        router=new GossipRouter(12001, "127.0.0.1");
-        router.start();
+        startRouter(router_host, router_port) ;
 
         System.out.println("-- waiting for merge to happen --");
         long target_time=System.currentTimeMillis() + 40000;
@@ -117,6 +138,16 @@ public class GossipRouterTest extends TestCase {
         if(ping != null) {
             ping.setGossipRefresh(1000);
         }
+    }
+
+    private void startRouter(String router_host, int router_port) throws Exception {
+        router=new GossipRouter(router_port, router_host);
+        router.start();
+    }
+
+    private void stopRouter() {
+        router.stop();
+        router = null ;
     }
 
     private static class MyReceiver extends ReceiverAdapter {
