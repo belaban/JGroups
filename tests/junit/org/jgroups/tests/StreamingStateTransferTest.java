9b89c92f1967219a588c2ffe95f27c048280e5e2
belaban <belaban>
2009-08-20 08:15:52 +0000

    ns

--- tests/junit/org/jgroups/tests/StreamingStateTransferTest.java
+++ tests/junit/org/jgroups/tests/StreamingStateTransferTest.java
@@ -1,21 +1,16 @@
 package org.jgroups.tests;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.OutputStream;
+import org.jgroups.*;
+import org.jgroups.util.Util;
+import org.testng.annotations.Test;
+
+import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
-
-import org.jgroups.*;
-import org.jgroups.util.Util;
-import org.testng.Assert;
-import org.testng.annotations.Test;
 
 /**
  * Tests streaming state transfer.
@@ -28,20 +23,11 @@ import org.testng.annotations.Test;
 @Test(groups= { Global.FLUSH }, sequential=false)
 public class StreamingStateTransferTest extends ChannelTestBase {
 
+
+
     @Test
     public void testTransfer() {
         String channelNames[]= { "A", "B", "C", "D" };
-        transferHelper(channelNames, false);
-    }
-
-    @Test
-    public void testRpcChannelTransfer() {
-        String channelNames[]= { "A", "B", "C", "D" };
-        transferHelper(channelNames, true);
-    }
-
-
-    private void transferHelper(String channelNames[], boolean useDispatcher) {
         int channelCount=channelNames.length;
         ArrayList<StreamingStateTransferApplication> channels=new ArrayList<StreamingStateTransferApplication>(channelCount);
 
@@ -51,28 +37,22 @@ public class StreamingStateTransferTest extends ChannelTestBase {
         try {
 
             semaphore.acquire(channelCount);
-            boolean crashed=false;
             // Create activation threads that will block on the semaphore
             for(int i=0;i < channelCount;i++) {
-                StreamingStateTransferApplication channel=null;
+                StreamingStateTransferApplication channel;
                 if(i == 0)
                     channel=new StreamingStateTransferApplication(channelNames[i],
-                                                                  semaphore,
-                                                                  useDispatcher,
-                                                                  false);
+                                                                  semaphore);
                 else
-                    channel=new StreamingStateTransferApplication((JChannel)channels.get(0)
-                                                                                    .getChannel(),
+                    channel=new StreamingStateTransferApplication((JChannel)channels.get(0).getChannel(),
                                                                   channelNames[i],
-                                                                  semaphore,
-                                                                  useDispatcher,
-                                                                  false);
-
+                                                                  semaphore);
                 // Start threads and let them join the channel
                 channels.add(channel);
                 semaphore.release(1);
                 channel.start();
-                Util.sleep(2000);
+                if(i == 0)
+                    Util.sleep(3000);
             }
 
             Channel[] tmp=new Channel[channels.size()];
@@ -92,10 +72,7 @@ public class StreamingStateTransferTest extends ChannelTestBase {
 
             int getStateInvokedCount=0;
             int setStateInvokedCount=0;
-            int partialGetStateInvokedCount=0;
-            int partialSetStateInvokedCount=0;
 
-            Util.sleep(3000);
             for(int i=0;i < channels.size();i++) {
                 StreamingStateTransferApplication current=channels.get(i);
                 if(current.getStateInvoked) {
@@ -104,42 +81,28 @@ public class StreamingStateTransferTest extends ChannelTestBase {
                 if(current.setStateInvoked) {
                     setStateInvokedCount++;
                 }
-                if(current.partialGetStateInvoked) {
-                    partialGetStateInvokedCount++;
-                }
-                if(current.partialSetStateInvoked) {
-                    partialSetStateInvokedCount++;
-                }
                 Map<Address,List<Integer>> map=current.getMap();
                 for(int j=0;j < channels.size();j++) {
                     StreamingStateTransferApplication app=channels.get(j);
                     List<?> l=map.get(app.getLocalAddress());
                     int size=l != null? l.size() : 0;
-                    Assert.assertEquals(size,
-                                        StreamingStateTransferApplication.COUNT,
-                                        "Correct element count in map ");
+                    assert size == StreamingStateTransferApplication.COUNT :
+                            "expected " + StreamingStateTransferApplication.COUNT + " in map, but only found " + size +
+                                    ", map=" + map;
                 }
             }
 
-            Assert.assertEquals(getStateInvokedCount, 1, "Correct invocation count of getState ");
-            Assert.assertEquals(setStateInvokedCount,
-                                channelCount - 1,
-                                "Correct invocation count of setState ");
-            Assert.assertEquals(partialGetStateInvokedCount,
-                                1,
-                                "Correct invocation count of partial getState ");
-            Assert.assertEquals(partialSetStateInvokedCount,
-                                channelCount - 1,
-                                "Correct invocation count of partial setState ");
-
+            assert getStateInvokedCount == 1 : "expected get state invocation count of 1 but was " + getStateInvokedCount;
+            assert setStateInvokedCount == channelCount - 1 :
+                    "expected set state invocation count of " + (channelCount -1) + " but was " + setStateInvokedCount;
         }
         catch(Exception ex) {
             log.warn(ex.toString());
         }
         finally {
-            for(int i=0;i < channels.size();i++) {
-                StreamingStateTransferApplication app=channels.get(i);
-                Util.sleep(500);
+            for(StreamingStateTransferApplication app: channels)
+                app.getChannel().setReceiver(null); // we don't want logging about view changes
+            for(StreamingStateTransferApplication app: channels) {
                 app.cleanup();
             }
         }
@@ -150,32 +113,19 @@ public class StreamingStateTransferTest extends ChannelTestBase {
 
         public static final int COUNT=25;
 
-        boolean partialSetStateInvoked=false;
-
-        boolean partialGetStateInvoked=false;
-
         boolean setStateInvoked=false;
-
         boolean getStateInvoked=false;
 
-        boolean largeTransfer=false;
 
-        public StreamingStateTransferApplication(String name,
-                                                 Semaphore s,
-                                                 boolean useDispatcher,
-                                                 boolean largeTransfer) throws Exception {
-            super(name, s, useDispatcher);
-            this.largeTransfer=largeTransfer;
+        public StreamingStateTransferApplication(String name, Semaphore s) throws Exception {
+            super(name, s);
             channel.connect("StreamingStateTransferApplication");
         }
 
         public StreamingStateTransferApplication(JChannel ch,
                                                  String name,
-                                                 Semaphore s,
-                                                 boolean useDispatcher,
-                                                 boolean largeTransfer) throws Exception {
-            super(ch, name, s, useDispatcher);
-            this.largeTransfer=largeTransfer;
+                                                 Semaphore s) throws Exception {
+            super(ch, name, s, false);
             channel.connect("StreamingStateTransferApplication");
         }
 
@@ -203,16 +153,13 @@ public class StreamingStateTransferTest extends ChannelTestBase {
         }
 
         public void getState(OutputStream ostream) {
-            if(largeTransfer)
-                Util.sleep(4000);
-
             super.getState(ostream);
             ObjectOutputStream oos=null;
             try {
                 oos=new ObjectOutputStream(ostream);
-                HashMap copy=null;
+                HashMap<Address,List<Integer>> copy=null;
                 synchronized(stateMap) {
-                    copy=new HashMap(stateMap);
+                    copy=new HashMap<Address,List<Integer>>(stateMap);
                 }
                 oos.writeObject(copy);
                 oos.flush();
@@ -227,9 +174,6 @@ public class StreamingStateTransferTest extends ChannelTestBase {
         }
 
         public byte[] getState() {
-            if(largeTransfer)
-                Util.sleep(4000);
-
             byte[] result=null;
             try {
                 synchronized(stateMap) {
@@ -245,13 +189,11 @@ public class StreamingStateTransferTest extends ChannelTestBase {
             return result;
         }
 
+        @SuppressWarnings("unchecked")
         public void setState(byte[] state) {
-            if(largeTransfer)
-                Util.sleep(4000);
-
-            Map result=null;
+            Map<Address,List<Integer>> result=null;
             try {
-                result=(Map)Util.objectFromByteBuffer(state);
+                result=(Map<Address,List<Integer>>)Util.objectFromByteBuffer(state);
             }
             catch(Exception e) {
                 e.printStackTrace();
@@ -265,15 +207,13 @@ public class StreamingStateTransferTest extends ChannelTestBase {
             }
         }
 
+        @SuppressWarnings("unchecked")
         public void setState(InputStream istream) {
-            if(largeTransfer)
-                Util.sleep(4000);
-
             super.setState(istream);
             ObjectInputStream ois=null;
             try {
                 ois=new ObjectInputStream(istream);
-                Map map=(Map)ois.readObject();
+                Map<Address,List<Integer>> map=(Map<Address,List<Integer>>)ois.readObject();
                 synchronized(stateMap) {
                     stateMap.clear();
                     stateMap.putAll(map);
@@ -285,77 +225,6 @@ public class StreamingStateTransferTest extends ChannelTestBase {
             finally {
                 setStateInvoked=true;
                 Util.close(ois);
-            }
-        }
-
-        public void setState(String stateId, byte[] state) {
-            if(largeTransfer)
-                Util.sleep(4000);
-
-            Object nameTransfer=null;
-            try {
-                nameTransfer=Util.objectFromByteBuffer(state);
-            }
-            catch(Exception e) {
-                e.printStackTrace();
-            }
-            finally {
-                partialSetStateInvoked=true;
-            }
-        }
-
-        public byte[] getState(String stateId) {
-            if(largeTransfer)
-                Util.sleep(4000);
-
-            byte[] result=null;
-            try {
-                result=Util.objectToByteBuffer(stateId);
-            }
-            catch(Exception e) {
-                e.printStackTrace();
-            }
-            finally {
-                partialGetStateInvoked=true;
-            }
-            return result;
-        }
-
-        public void setState(String state_id, InputStream istream) {
-            if(largeTransfer)
-                Util.sleep(4000);
-
-            super.setState(state_id, istream);
-            ObjectInputStream ois=null;
-            try {
-                ois=new ObjectInputStream(istream);
-            }
-            catch(Exception e) {
-                e.printStackTrace();
-            }
-            finally {
-                partialSetStateInvoked=true;
-                Util.close(ois);
-            }
-        }
-
-        public void getState(String state_id, OutputStream ostream) {
-            if(largeTransfer)
-                Util.sleep(4000);
-
-            super.getState(state_id, ostream);
-            ObjectOutputStream oos=null;
-            try {
-                oos=new ObjectOutputStream(ostream);
-                oos.writeObject(state_id);
-                oos.flush();
-            }
-            catch(IOException e) {
-                e.printStackTrace();
-            }
-            finally {
-                partialGetStateInvoked=true;
-                Util.close(oos);
             }
         }
     }
