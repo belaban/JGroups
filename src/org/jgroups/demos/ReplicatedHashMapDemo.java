522dbc130d2dd7e733b37040299df16448cbd0ae
belaban <belaban>
2009-08-28 08:23:09 +0000

    ns

--- src/org/jgroups/demos/ReplicatedHashMapDemo.java
+++ src/org/jgroups/demos/ReplicatedHashMapDemo.java
@@ -24,7 +24,7 @@ import java.io.Serializable;
  * read-only methods such as get() use the local copy. A ReplicatedtHashMap is created given the name
  * of a group; all hashmaps with the same name find each other and form a group.
  * @author Bela Ban
- * @version $Id: ReplicatedHashMapDemo.java,v 1.1 2007/07/23 09:27:51 belaban Exp $
+ * @version $Id: ReplicatedHashMapDemo.java,v 1.2 2009/08/28 08:23:09 belaban Exp $
  */
 public class ReplicatedHashMapDemo extends Frame implements WindowListener, ActionListener,
                                           ReplicatedHashMap.Notification {
@@ -224,7 +224,7 @@ public class ReplicatedHashMapDemo extends Frame implements WindowListener, Acti
 
     public static void main(String args[]) {
         ReplicatedHashMapDemo     client=new ReplicatedHashMapDemo();
-        ChannelFactory            factory=new JChannelFactory();
+        ChannelFactory            factory;
         String                    arg;
         boolean                   persist=false;
 
@@ -252,6 +252,7 @@ public class ReplicatedHashMapDemo extends Frame implements WindowListener, Acti
             return;
         }
         try {
+            factory=new JChannelFactory(props);
             client.start(factory, props, persist);
         }
         catch(Throwable t) {
