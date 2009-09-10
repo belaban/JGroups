aa6ce8371693320ce00cbf04452821a2782c97d8
rachmatowicz <rachmatowicz>
2009-09-10 21:28:46 +0000

    Move processing of bind_addr, bind_interface and diagnostics_addr in LOOPBACK to init(). (JGRP-978)

--- src/org/jgroups/protocols/LOOPBACK.java
+++ src/org/jgroups/protocols/LOOPBACK.java
@@ -1,7 +1,12 @@
-// $Id: LOOPBACK.java,v 1.31 2009/09/06 13:51:07 belaban Exp $
+// $Id: LOOPBACK.java,v 1.32 2009/09/10 21:28:46 rachmatowicz Exp $
 
 package org.jgroups.protocols;
 
+
+import java.net.InetAddress;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
 
 import org.jgroups.Address;
 import org.jgroups.Event;
@@ -43,6 +48,28 @@ public class LOOPBACK extends TP {
 
     /*------------------------------ Protocol interface ------------------------------ */
 
+    public void init() throws Exception {
+        super.init();
+        
+        // the bind address determination moved from TP
+        Properties props = new Properties() ;
+        if (bind_addr_str != null)
+        	props.put("bind_addr", bind_addr_str) ;
+        if (bind_interface_str != null)
+        props.put("bind_interface", bind_interface_str) ;
+        bind_addr = Util.getBindAddress(props) ;
+
+        // the diagnostics determination moved from TP
+        diagnostics_addr_str = DEFAULT_IPV4_DIAGNOSTICS_ADDR_STR ;        
+                
+        if(bind_addr != null) {
+            Map<String, Object> m=new HashMap<String, Object>(1);
+            m.put("bind_addr", bind_addr);
+            up(new Event(Event.CONFIG, m));
+        }
+
+    }    
+    
     public void destroy() {
         try {
             timer.stop();
