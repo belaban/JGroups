160d36b14846673bb01242e6513d4bce970de147
belaban <belaban>
2009-09-10 10:53:39 +0000

    ns

--- src/org/jgroups/protocols/BARRIER.java
+++ src/org/jgroups/protocols/BARRIER.java
@@ -25,7 +25,7 @@ import java.util.concurrent.locks.ReentrantLock;
  * When an OPEN_BARRIER event is received, we simply open the barrier again and let all messages pass in the up
  * direction. This is done by releasing the WL.
  * @author Bela Ban
- * @version $Id: BARRIER.java,v 1.6.4.4 2008/05/22 13:23:06 belaban Exp $
+ * @version $Id: BARRIER.java,v 1.6.4.5 2009/09/10 10:57:07 belaban Exp $
  */
 
 public class BARRIER extends Protocol {
@@ -139,8 +139,8 @@ public class BARRIER extends Protocol {
                     lock.lock();
                     try {
                         if(in_flight_threads.remove(current_thread) == NULL &&
-                                in_flight_threads.isEmpty() &&
-                                barrier_closed.get()) {
+                                barrier_closed.get() &&
+                                in_flight_threads.isEmpty()) {
                             no_msgs_pending.signalAll();
                         }
                     }
