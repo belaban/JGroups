b2263417324e406397a8309ed5a68f18dbc82322
belaban <belaban>
2009-09-28 15:49:41 +0000

    added -name

--- bin/concurrent.sh
+++ bin/concurrent.sh
@@ -6,7 +6,7 @@ while [ $count -lt 20 ]
 do
   echo "Starting Draw instance #$count"
   # change the IP address to your system
-  jgroups.sh org.jgroups.demos.Draw -props /home/bela/udp.xml &
+  jgroups.sh org.jgroups.demos.Draw -props /home/bela/udp.xml -name $count  &
   #  sleep 1
   count=$(($count+1))
 done
