acd33a4ea2cbdcf906d742c1a18b760890c96166
Bela Ban <belaban@yahoo.com>
2009-09-29 04:34:44 +0000

    wrapped retransmission task in catch(Throwable) clause (https://jira.jboss.org/jira/browse/JGRP-1062)

--- src/org/jgroups/stack/Retransmitter.java
+++ src/org/jgroups/stack/Retransmitter.java
@@ -1,4 +1,4 @@
-// $Id: Retransmitter.java,v 1.23.2.3 2009/09/14 09:28:54 belaban Exp $
+// $Id: Retransmitter.java,v 1.23.2.4 2009/09/29 04:34:44 belaban Exp $
 
 package org.jgroups.stack;
 
@@ -29,7 +29,7 @@ import java.util.concurrent.TimeUnit;
  * the (previous) message list linearly on removal. Performance is about the same, or slightly better in
  * informal tests.
  * @author Bela Ban
- * @version $Revision: 1.23.2.3 $
+ * @version $Revision: 1.23.2.4 $
  */
 public class Retransmitter {
 
@@ -215,8 +215,14 @@ public class Retransmitter {
             if(cancelled) {
                 return;
             }
-            command.retransmit(seqno, seqno, msg_sender);
-            num_retransmits++;
+            try {
+                command.retransmit(seqno, seqno, msg_sender);
+                num_retransmits++;
+            }
+            catch(Throwable t) {
+                if(log.isErrorEnabled())
+                    log.error("failed retransmission task", t);
+            }
             doSchedule();
         }
 
