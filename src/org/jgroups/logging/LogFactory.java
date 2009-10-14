23d6cbf8bc23c0788c780b8105e1564d7d45fa17
belaban <belaban>
2009-10-14 06:46:53 +0000

    added flag jgroups.use.jdk_logger to force use of JDK logging even if log4j is on the classpath (https://jira.jboss.org/jira/browse/JGRP-1071)

--- src/org/jgroups/logging/LogFactory.java
+++ src/org/jgroups/logging/LogFactory.java
@@ -1,41 +1,48 @@
 package org.jgroups.logging;
 
+import org.jgroups.Global;
+
 
 /**
  * Factory that creates {@link Log} instances.
  *
  * @author Manik Surtani
+ * @author Bela Ban
  * @since 4.0
  */
 public class LogFactory {
+    public static final boolean IS_LOG4J_AVAILABLE;
 
-   public static final boolean IS_LOG4J_AVAILABLE;
+    static {
+        boolean available;
+        try {
+            Class.forName("org.apache.log4j.Logger");
+            available=true;
+        }
+        catch(ClassNotFoundException cnfe) {
+            available=false;
+        }
+        IS_LOG4J_AVAILABLE=available;
+    }
 
-   static {
-      boolean available;
-      try {
-         Class.forName("org.apache.log4j.Logger");
-         available = true;
-      }
-      catch (ClassNotFoundException cnfe) {
-         available = false;
-      }
-      IS_LOG4J_AVAILABLE = available;
-   }
+    public static Log getLog(Class clazz) {
+        // this call is not executed frequently, so we don't need to move the check for USE_JDK_LOGGER to class init time
+        final boolean use_jdk_logger=Boolean.parseBoolean(System.getProperty(Global.USE_JDK_LOGGER));
+        if(IS_LOG4J_AVAILABLE && !use_jdk_logger) {
+            return new Log4JLogImpl(clazz);
+        }
+        else {
+            return new JDKLogImpl(clazz);
+        }
+    }
 
-   public static Log getLog(Class clazz) {
-       if (IS_LOG4J_AVAILABLE) {
-           return new Log4JLogImpl(clazz);
-       } else {
-           return new JDKLogImpl(clazz);
-       }
-   }
-
-   public static Log getLog(String category) {
-      if (IS_LOG4J_AVAILABLE) {
-         return new Log4JLogImpl(category);
-      } else {
-         return new JDKLogImpl(category);
-      }
-   }
+    public static Log getLog(String category) {
+        final boolean use_jdk_logger=Boolean.parseBoolean(System.getProperty(Global.USE_JDK_LOGGER));
+        if(IS_LOG4J_AVAILABLE && !use_jdk_logger) {
+            return new Log4JLogImpl(category);
+        }
+        else {
+            return new JDKLogImpl(category);
+        }
+    }
 }
