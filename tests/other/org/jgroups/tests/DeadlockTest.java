8a4028c59339f5d3f3e5f502978491b949e4fed5
belaban <belaban>
2009-10-21 07:51:09 +0000

    ns

--- tests/other/org/jgroups/tests/DeadlockTest.java
+++ tests/other/org/jgroups/tests/DeadlockTest.java
@@ -1,4 +1,4 @@
-// $Id: DeadlockTest.java,v 1.8 2008/10/10 14:53:30 belaban Exp $
+// $Id: DeadlockTest.java,v 1.9 2009/10/21 07:51:09 belaban Exp $
 
 package org.jgroups.tests;
 
@@ -8,94 +8,71 @@ import org.jgroups.blocks.GroupRequest;
 import org.jgroups.blocks.MethodCall;
 import org.jgroups.blocks.RpcDispatcher;
 
+import java.lang.reflect.Method;
 
 
 /**
- * Class which tests deadlock detection in RequestCorrelator.
+ * Class which tests deadlocks in RequestCorrelator
  * @author John Giorgiadis
+ * @author Bela Ban
  */
 public class DeadlockTest {
-    private static final Class[] TYPES=new Class[]{};
-    private static final Object[] ARGS=new Object[]{};
 
-    public class InRpc {
-        public void rpc_1() { _in_rpc_1(); }
-        public void rpc_2() { _in_rpc_2(); }
+    public void foo() {
+        System.out.println("foo()");
+        cast_call(BAR, true);
+    }
+    public static void bar() {
+        System.out.println("bar()");
     }
 
+    static final Method FOO, BAR;
 
-	private static class Handler implements MessageListener, MembershipListener {
-		public Handler() { super(); }
-		// MessageListener
-		public byte[] getState() { return(null); }
-		public void setState(byte[] state) {}
-		public void receive(Message msg) {}
-		// MembershipListener
-		public void block() {}
-		public void suspect(Address suspect) {}
-		public void viewAccepted(View view) {}
-	}
+    static {
+        try {
+            FOO=DeadlockTest.class.getMethod("foo");
+            BAR=DeadlockTest.class.getMethod("bar");
+        }
+        catch(NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 	// .......................................................................
 
-	private String name = "JG";
-	private String stack = null; // default stack config
-	private JChannel channel;
+	private String        name = "DeadlockTest";
+	private JChannel      channel;
 	private RpcDispatcher disp;
 
 
-	private void _in_rpc_1() {
-		System.out.println("In rpc_1()");
-		cast_call("rpc_2", ARGS, TYPES);
-		System.out.println("Exiting rpc_1()");
-	}
 
-	private void _in_rpc_2() {
-		System.out.println("In rpc_2()");
-		System.out.println("Exiting rpc_2()");
-	}
-
-
-	private void cast_call(String method, Object[] args, Class[] types) {
-		MethodCall call;
-		call = new MethodCall(method, args, types);
-		disp.callRemoteMethods(null, call, GroupRequest.GET_ALL, 0, false, true);
+	private void cast_call(Method method, boolean oob) {
+		MethodCall call=new MethodCall(method);
+        disp.callRemoteMethods(null, call, GroupRequest.GET_ALL, 0, false, oob);
 	}
 
 	// .......................................................................
 
-	public DeadlockTest(boolean use_deadlock_detection) {
-		Handler handler = new Handler();
-		InRpc in_rpc = new InRpc();
 
+    public void start() {
 		try {
-		    channel = new JChannel(stack);
-		    disp = new RpcDispatcher(channel, handler, handler, in_rpc, use_deadlock_detection);
+		    channel = new JChannel();
+		    disp = new RpcDispatcher(channel, null, null, this);
 		    channel.connect(name);
-		} 
+		}
         catch(ChannelClosedException ex) { ex.printStackTrace(); }
         catch(ChannelException ex) { ex.printStackTrace(); }
-		
-		// Call rpc_1 which in turn calls rpc_2
-		System.out.println("Calling rpc_1()");
-		if(!use_deadlock_detection)
-		    System.out.println("** Not using deadlock detection -- recursive call will hang !");
-		else
-		    System.out.println("** Using deadlock detection -- recursive call will succeed");
-		cast_call("rpc_1", ARGS, TYPES);
-		System.out.println("Out of rpc_1()");
-		channel.disconnect();
+
+		// Call foo() which in turn calls bar()
+		cast_call(FOO, false);
 		channel.close();
 		System.out.println("Disconnected");
 	}
 
 	// .......................................................................
 
-	public static void main(String[] args) {
-	    if(args.length != 1) {
-		System.out.println("DeadlockTest <true|false (use_deadlock_detection)>");
-		return;
-	    }
-	    new DeadlockTest(Boolean.valueOf(args[0]).booleanValue());
-	}
+    public static void main(String[] args) {
+        DeadlockTest test=new DeadlockTest();
+        test.start();
+    }
 }
