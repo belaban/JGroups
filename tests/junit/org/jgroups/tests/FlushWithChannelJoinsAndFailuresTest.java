e05e2a2f189eea37e0160f5946b517f856a8a19a
vlada <vlada>
2009-10-27 18:10:19 +0000

    less number of rounds --> avoid timeout

--- tests/junit/org/jgroups/tests/FlushWithChannelJoinsAndFailuresTest.java
+++ tests/junit/org/jgroups/tests/FlushWithChannelJoinsAndFailuresTest.java
@@ -20,46 +20,31 @@ public class FlushWithChannelJoinsAndFailuresTest extends ChannelTestBase {
 
    private static final String cName = "FlushWithChannelFailuresTest";
    
-   @Test
-   public void testAndLoop() throws Exception {
-
-      int numChannels = 10;
-      Channel channels [] = new Channel[numChannels];
-      for(int j = 0; j<numChannels;j++){         
-         if(j==0){
-            channels[j]= createChannel(true,numChannels);
-         }
-         else{
-            channels[j]= createChannel((JChannel)channels[0]);
-         }         
-         channels[j].connect(cName);         
-      }
-      
-      for (int i = 1; i <= 4; i++) {
-         
-         //kill coordinator
-         Util.shutdown(channels[0]);
-
-         //kill another three members
-         Util.shutdown(channels[4]);
-         Util.shutdown(channels[5]);
-         Util.shutdown(channels[8]); 
-         
-         //now create members again 
-         channels[0]= createChannel((JChannel)channels[1]);
-         channels[0].connect(cName);      
-         channels[4]= createChannel((JChannel)channels[1]);
-         channels[4].connect(cName);      
-         channels[5]= createChannel((JChannel)channels[1]);
-         channels[5].connect(cName);  
-         channels[8]= createChannel((JChannel)channels[1]);
-         channels[8].connect(cName);  
-                  
-         System.out.println("***** Round " + i + " done *****");
-      }
-      
-      for(Channel c:channels){
-         assert c.getView().getMembers().size() == 10;
-      }      
-   }
+    @Test
+    public void testAndLoop() throws Exception {
+        int numChannels = 10;
+        Channel channels[] = new Channel[numChannels];
+        for (int j = 0; j < numChannels; j++) {
+            if (j == 0) {
+                channels[j] = createChannel(true, numChannels);
+            } else {
+                channels[j] = createChannel((JChannel) channels[0]);
+            }
+            channels[j].connect(cName);
+        }
+        for (int i = 1; i <= 2; i++) {
+            int killPositions[] = { 0, 3, 5, 8 };
+            for (int index : killPositions) {
+                Util.shutdown(channels[index]);
+            }
+            for (int index : killPositions) {
+                channels[index] = createChannel((JChannel) channels[1]);
+                channels[index].connect(cName);
+            }
+            System.out.println("***** Round " + i + " done *****");
+        }
+        for (Channel c : channels) {
+            assert c.getView().getMembers().size() == 10;
+        }
+    }
 }
