42cc93f95e8748e1cc1950a8334506b0df0b4a15
rachmatowicz <rachmatowicz>
2009-11-03 23:15:21 +0000

    Update version numbers and docs for JGroups 2.4.7.GA release.

--- src/org/jgroups/Version.java
+++ src/org/jgroups/Version.java
@@ -13,18 +13,18 @@ package org.jgroups;
  * Z = 0-63 for micro versions
  *
  * @author Bela Ban
- * @version $Id: Version.java,v 1.42.2.13 2009/05/11 22:43:18 rachmatowicz Exp $
+ * @version $Id: Version.java,v 1.42.2.14 2009/11/03 23:15:25 rachmatowicz Exp $
  * Holds version information for JGroups.
  */
 public class Version {
     public static final short major = 2;
     public static final short minor = 4;
-    public static final short micro = 6;
-    public static final String description="2.4.6.GA";
+    public static final short micro = 7;
+    public static final String description="2.4.7.GA";
 
     public static final short version=encode(major, minor, micro);
     public static final String string_version=print(version);
-    public static final String cvs="$Id: Version.java,v 1.42.2.13 2009/05/11 22:43:18 rachmatowicz Exp $";
+    public static final String cvs="$Id: Version.java,v 1.42.2.14 2009/11/03 23:15:25 rachmatowicz Exp $";
 
     private static final int MAJOR_SHIFT = 11;
     private static final int MINOR_SHIFT = 6;
