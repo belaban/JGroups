ed1f69326bf1e3ba47cf18342c564cd25cffc79b
Bela Ban <belaban@yahoo.com>
2009-11-16 10:24:06 +0000

    ns

---
+++ tests/other/org/jgroups/tests/CheckGaps.java
@@ -0,0 +1,33 @@
+package org.jgroups.tests;
+
+import java.util.SortedSet;
+import java.util.TreeSet;
+import java.util.Set;
+import java.util.HashSet;
+
+/** Reads a sequence of numbers from stdin and verifies that they are in order
+ * @author Bela Ban
+ * @version $Id: CheckGaps.java,v 1.1 2009/11/16 10:24:13 belaban Exp $
+ */
+public class CheckGaps {
+
+
+    public static void main(String[] args) {
+        SortedSet<Integer> set=new TreeSet<Integer>();
+
+        for(int i=0; i < args.length; i++) {
+            set.add(Integer.parseInt(args[i]));
+        }
+
+        int low=set.first(), high=set.last();
+        System.out.println("input has " + set.size() + " numbers, low=" + low + ", high=" + high);
+
+        Set<Integer> correct_set=new HashSet<Integer>();
+        for(int i=low; i < high; i++) {
+            correct_set.add(i);
+        }
+
+        correct_set.removeAll(set);
+        System.out.println("missing seqnos: " + correct_set);
+    }
+}
