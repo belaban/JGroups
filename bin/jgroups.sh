604fb93bd919dcf1cbcffec3ddc4090de10afe94
Bela Ban <belaban@yahoo.com>
2009-12-18 14:45:19 +0000

    ns

--- bin/jgroups.sh
+++ bin/jgroups.sh
@@ -1,6 +1,6 @@
 
 # Author: Bela Ban
-# version: $Id: jgroups.sh,v 1.15 2009/12/18 13:33:48 belaban Exp $
+# version: $Id: jgroups.sh,v 1.16 2009/12/18 14:45:19 belaban Exp $
 
 #!/bin/bash
 
@@ -26,12 +26,13 @@ fi;
 
 JG_FLAGS="-Dresolve.dns=false -Djgroups.bind_addr=$IP_ADDR -Djboss.tcpping.initial_hosts=$IP_ADDR[7800]"
 JG_FLAGS="$JG_FLAGS -Djava.net.preferIPv4Stack=true -Djgroups.timer.num_threads=4"
-FLAGS="-server -Xmx600M -Xms600M -Xmn500M"
+FLAGS="-server -Xmx600M -Xms600M -Xmn500M -Xss128K"
 FLAGS="$FLAGS -XX:CompileThreshold=10000 -XX:+AggressiveHeap -XX:ThreadStackSize=64K -XX:SurvivorRatio=8"
 FLAGS="$FLAGS -XX:TargetSurvivorRatio=90 -XX:MaxTenuringThreshold=31"
 FLAGS="$FLAGS -Xshare:off -XX:+UseBiasedLocking"
 JMX="-Dcom.sun.management.jmxremote"
 EXPERIMENTAL="-XX:+UseFastAccessorMethods -XX:+UseTLAB -XX:+DoEscapeAnalysis -XX:+EliminateLocks -XX:+UseCompressedOops"
+EXPERIMENTAL="$EXPERIMENTAL -XX:+AggressiveOpts"
 #EXPERIMENTAL="$EXPERIMENTAL -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC"
 
 #java -Xrunhprof:cpu=samples,monitor=y,interval=5,lineno=y,thread=y -classpath $CP $LOG $JG_FLAGS $FLAGS $EXPERIMENTAL $JMX  $*
