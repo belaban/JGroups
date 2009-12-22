1d90cedd1f40ad6b06062a2a7874a6b3425a914d
Bela Ban <belaban@yahoo.com>
2009-12-22 16:29:05 +0000

    ns

--- tests/other/org/jgroups/tests/GridFileTest.java
+++ tests/other/org/jgroups/tests/GridFileTest.java
@@ -9,7 +9,7 @@ import java.util.Map;
 
 /**
  * @author Bela Ban
- * @version $Id: GridFileTest.java,v 1.1 2009/12/11 13:22:43 belaban Exp $
+ * @version $Id: GridFileTest.java,v 1.2 2009/12/22 16:29:05 belaban Exp $
  */
 public class GridFileTest {
 
@@ -37,7 +37,7 @@ public class GridFileTest {
 
         File file=new GridFile("/home/bela/grid", cache, chunk_size);
         file.mkdirs();
-        System.out.println("file = " + file);
+        System.out.println("input file = " + file + " \n");
 
         for(String name: new String[]{"a.txt", "b.txt", "one.log"}) {
             File f2=new GridFile(file, name, cache, chunk_size);
@@ -59,9 +59,11 @@ public class GridFileTest {
         }
 
         String[] list=file.list();
-        if(list != null)
+        if(list != null) {
             for(String filename: list)
                 System.out.println("file = " + filename);
+            System.out.println("(" + list.length + " children)");
+        }
 
        /* for(Map.Entry<String,Cache.Value<ReplCache.Value<GridFile.Metadata>>> entry: cache.getL2Cache().entrySet()) {
             String key=entry.getKey();
