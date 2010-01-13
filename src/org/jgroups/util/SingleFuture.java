597d006a31ac5744046431bf56de661787cdf463
belaban <belaban>
2010-01-13 13:18:24 +0000

    changed to use Unicastrequest (https://jira.jboss.org/jira/browse/JGRP-1123)

--- src/org/jgroups/util/SingleFuture.java
+++ src/org/jgroups/util/SingleFuture.java
@@ -1,21 +1,21 @@
 package org.jgroups.util;
 
-import org.jgroups.blocks.GroupRequest;
+import org.jgroups.blocks.UnicastRequest;
 
-import java.util.concurrent.Future;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
 /**
  * Implements a future with a single result (versus a RspList)
  * @author Bela Ban
- * @version $Id: SingleFuture.java,v 1.1 2009/06/16 08:25:18 belaban Exp $
+ * @version $Id: SingleFuture.java,v 1.2 2010/01/13 13:18:24 belaban Exp $
  */
 public class SingleFuture<T> implements Future<T>  {
-    protected final GroupRequest req;
+    protected final UnicastRequest req;
 
-    public SingleFuture(GroupRequest req) {
+    public SingleFuture(UnicastRequest req) {
         if(req == null)
             throw new IllegalArgumentException("argument cannot be null");
         this.req=req;
@@ -34,18 +34,13 @@ public class SingleFuture<T> implements Future<T>  {
     }
 
     public T get() throws InterruptedException, ExecutionException {
-        RspList rsps=req.get();
-        return _get(rsps);
+        Rsp rsp=req.get();
+        return rsp != null? (T)rsp.getValue() : null;
     }
 
     public T get(long timeout, TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException {
-        RspList rsps=req.get(timeout, unit);
-        return _get(rsps);
+        Rsp rsp=req.get(timeout, unit);
+        return rsp != null? (T)rsp.getValue() : null;
     }
 
-    private T _get(RspList rsps) {
-        if(rsps == null || rsps.isEmpty())
-            return null;
-        return (T)rsps.getFirst();
-    }
 }
