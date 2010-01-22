725e8a5648246960b139a64c1727eedafdb0db41
Bela Ban <belaban@yahoo.com>
2010-01-22 12:03:18 +0000

    ns

--- tests/other/org/jgroups/tests/UnicastTestRpcDist.java
+++ tests/other/org/jgroups/tests/UnicastTestRpcDist.java
@@ -150,6 +150,7 @@ public class UnicastTestRpcDist extends ReceiverAdapter {
     public Results startTest() throws Throwable {
         System.out.println("invoking " + num_msgs + " RPCs of " + Util.printBytes(msg_size) + ", sync=" + sync + ", oob=" + oob);
         int total_gets=0, total_puts=0;
+        long total_get_time=0, total_put_time=0;
 
         Invoker[] invokers=new Invoker[num_threads];
         for(int i=0; i < invokers.length; i++)
@@ -163,11 +164,13 @@ public class UnicastTestRpcDist extends ReceiverAdapter {
             invoker.join();
             total_gets+=invoker.numGets();
             total_puts+=invoker.numPuts();
+            total_get_time+=invoker.getTotalGetTime();
+            total_put_time+=invoker.getTotalPutTime();
         }
 
         long total_time=System.currentTimeMillis() - start;
         System.out.println("done (in " + total_time + " ms)");
-        return new Results(total_gets, total_puts, total_time);
+        return new Results(total_gets, total_puts, total_time, total_get_time, total_put_time);
     }
 
 
@@ -406,6 +409,8 @@ public class UnicastTestRpcDist extends ReceiverAdapter {
         private final int            number_of_msgs;
         private int                  num_gets=0;
         private int                  num_puts=0;
+        private long                 total_get_time=0;
+        private long                 total_put_time=0;
 
 
         public Invoker(Collection<Address> dests, int number_of_msgs) {
@@ -417,6 +422,8 @@ public class UnicastTestRpcDist extends ReceiverAdapter {
         
         public int numGets() {return num_gets;}
         public int numPuts() {return num_puts;}
+        public long getTotalGetTime() {return total_get_time;}
+        public long getTotalPutTime() {return total_put_time;}
 
 
         public void run() {
@@ -438,6 +445,7 @@ public class UnicastTestRpcDist extends ReceiverAdapter {
                 boolean get=Util.tossWeightedCoin(read_percentage);
 
                 try {
+                    long start=System.currentTimeMillis();
                     if(get) { // sync GET
                         Address target=pickTarget();
                         get_args[0]=i;
@@ -450,6 +458,11 @@ public class UnicastTestRpcDist extends ReceiverAdapter {
                         disp.callRemoteMethods(targets, put_call, put_options);
                         num_puts++;
                     }
+                    long diff=System.currentTimeMillis() - start;
+                    if(get)
+                        total_get_time+=diff;
+                    else
+                        total_put_time+=diff;
                 }
                 catch(Throwable throwable) {
                     throwable.printStackTrace();
@@ -476,38 +489,52 @@ public class UnicastTestRpcDist extends ReceiverAdapter {
 
 
     public static class Results implements Streamable {
+        long num_gets=0;
+        long num_puts=0;
+        long time=0;
+        long total_get_time=0;
+        long total_put_time=0;
 
         public Results() {
             
         }
 
-        public Results(int num_gets, int num_puts, long time) {
+        public Results(int num_gets, int num_puts, long time, long total_get_time, long total_put_time) {
             this.num_gets=num_gets;
             this.num_puts=num_puts;
             this.time=time;
+            this.total_get_time=total_get_time;
+            this.total_put_time=total_put_time;
         }
 
-        long num_gets=0;
-        long num_puts=0;
-        long time=0;
+
 
 
         public void writeTo(DataOutputStream out) throws IOException {
             out.writeLong(num_gets);
             out.writeLong(num_puts);
             out.writeLong(time);
+            out.writeLong(total_get_time);
+            out.writeLong(total_put_time);
         }
 
         public void readFrom(DataInputStream in) throws IOException, IllegalAccessException, InstantiationException {
             num_gets=in.readLong();
             num_puts=in.readLong();
             time=in.readLong();
+            total_get_time=in.readLong();
+            total_put_time=in.readLong();
         }
 
         public String toString() {
             long total_reqs=num_gets + num_puts;
-            double reqs_per_sec=total_reqs / (time / 1000.0);
-            return num_gets + " GETs and " + num_puts + " PUTs in " + time + " ms: " + f.format(reqs_per_sec) + " requests / sec";
+            double total_reqs_per_sec=total_reqs / (time / 1000.0);
+            double gets_per_sec=num_gets / (total_get_time / 1000.0);
+            double puts_per_sec=num_puts / (total_put_time / 1000.0);
+
+            return f.format(total_reqs_per_sec) + " requests/sec: " +
+                    f.format(gets_per_sec) + " GETs/sec (" + num_gets + " reqs), " +
+                    f.format(puts_per_sec) + " PUTs/sec (" + num_puts + " reqs)";
         }
     }
 
