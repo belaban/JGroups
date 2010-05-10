b20e47dfea1aaf27db041e977ed7eb2e4d5a91ff
belaban <belaban>
2010-05-10 16:09:07 +0000

    ns

--- bin/gossiprouter.sh
+++ bin/gossiprouter.sh
@@ -9,5 +9,8 @@ done
 OPTS="-Dlog4j.configuration=file:$HOME/log4j.properties -Djava.net.preferIPv4Stack=true"
 OPTS="$OPTS -Dcom.sun.management.jmxremote"
 
+## Uncomment for remote JMX access. Also modify JAVA_HOME/jre/lib/management/jmxremote.passwords
+# OPTS="$OPTS -Dcom.sun.management.jmxremote.port=7000 -Dcom.sun.management.jmxremote.ssl=false"
+
 java $OPTS -classpath $CLASSPATH $JAVA_OPTS org.jgroups.stack.GossipRouter -port 12001 $*
 
