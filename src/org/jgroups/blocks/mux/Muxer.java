e4c49d430a063b7a85a60eb1bccb9d27da6fb062
bstansberry <bstansberry>
2010-06-09 03:24:51 +0000

    [JGRP-1209] MuxUpHandler should handle all event types [JGRP-1210] Formalize usage of default UpHandler with MuxUpHandler

--- src/org/jgroups/blocks/mux/Muxer.java
+++ src/org/jgroups/blocks/mux/Muxer.java
@@ -3,7 +3,7 @@ package org.jgroups.blocks.mux;
 /**
  * Allows registration/deregistrator of multiplexed handlers by mux id.
  * @author Paul Ferraro
- * @version $Id: Muxer.java,v 1.1 2010/04/13 17:57:07 ferraro Exp $
+ * @version $Id: Muxer.java,v 1.2 2010/06/09 03:24:51 bstansberry Exp $
  */
 public interface Muxer<T> {
 
@@ -15,8 +15,31 @@ public interface Muxer<T> {
     void add(short id, T handler);
     
     /**
+     * Gets the handler registered under the specified id
+     * @param id a mux id
+     * @return the handler, or <code>null</code> if no handler is registered under
+     *         <code>id</code>
+     */
+    T get(short id);
+    
+    /**
      * Unregisters the handler associated with the specifed mux identifier
      * @param id a mux id
      */
     void remove(short id);
+    
+    /**
+     * Gets the handler for messages that have no mux header.
+     * 
+     * @return the default handler, or <code>null</code> if no default handler
+     *         has been set
+     */
+    T getDefaultHandler();
+    
+    /**
+     * Sets the handler for messages that have no mux header.
+     * 
+     * @param handler a handler for messages that have no mux header
+     */
+    void setDefaultHandler(T handler);
 }
