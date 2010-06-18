b87672bc795c3cfa05fd4ed1b54d4e5fa18f7583
Bela Ban <belaban@yahoo.com>
2010-06-18 04:39:08 +0000

    removed @Unsupported (https://jira.jboss.org/browse/JGRP-1221)

--- src/org/jgroups/protocols/S3_PING.java
+++ src/org/jgroups/protocols/S3_PING.java
@@ -35,9 +35,9 @@ import static java.lang.String.valueOf;
  * Discovery protocol using Amazon's S3 storage. The S3 access code reuses the example shipped by Amazon.
  * This protocol is unsupported and experimental !
  * @author Bela Ban
- * @version $Id: S3_PING.java,v 1.10 2010/06/17 14:57:15 belaban Exp $
+ * @version $Id: S3_PING.java,v 1.11 2010/06/18 04:39:08 belaban Exp $
  */
-@Experimental @Unsupported
+@Experimental
 public class S3_PING extends FILE_PING {
 
     @Property(description="The access key to AWS (S3)")
