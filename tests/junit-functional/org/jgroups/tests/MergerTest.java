c1ba0593621ecf7f3fa778ee6b313c7cd92a7fef
Bela Ban <belaban@yahoo.com>
2010-07-08 11:51:49 +0000

    ns

--- tests/junit-functional/org/jgroups/tests/MergerTest.java
+++ tests/junit-functional/org/jgroups/tests/MergerTest.java
@@ -2,6 +2,7 @@ package org.jgroups.tests;
 
 import org.jgroups.Address;
 import org.jgroups.Global;
+import org.jgroups.View;
 import org.jgroups.protocols.pbcast.Merger;
 import org.jgroups.util.Util;
 import org.testng.annotations.Test;
@@ -10,7 +11,7 @@ import java.util.*;
 
 /**
  * @author Bela Ban
- * @version $Id: MergerTest.java,v 1.2 2010/07/07 09:43:11 belaban Exp $
+ * @version $Id: MergerTest.java,v 1.3 2010/07/08 11:51:49 belaban Exp $
  */
 @Test(groups=Global.FUNCTIONAL,sequential=false)
 public class MergerTest {
@@ -29,16 +30,16 @@ public class MergerTest {
       * D: CD
      */
      public void testSimpleMerge() {
-         Map<Address, Collection<Address>> map=new HashMap<Address,Collection<Address>>();
-         map.put(a, makeList(a,b));
-         map.put(b, makeList(a,b));
-         map.put(c, makeList(c,d));
-         map.put(d, makeList(c,d));
-         System.out.println("map = " + map);
+         Map<Address,View> map=new HashMap<Address,View>();
+         map.put(a, makeView(a, a,b));
+         map.put(b, makeView(a, a,b));
+         map.put(c, makeView(c, c,d));
+         map.put(d, makeView(c, c,d));
+         System.out.println("map:\n" + print(map));
 
          assert map.size() == 4;
-         Merger.sanitize(map);
-         System.out.println("map after sanitization: " + map);
+         Merger.sanitizeViews(map);
+         System.out.println("map after sanitization:\n" + print(map));
 
          assert map.size() == 4;
          assert map.get(a).size() == 2;
@@ -54,15 +55,15 @@ public class MergerTest {
      * C: BC
      */
     public void testOverlappingMerge() {
-        Map<Address, Collection<Address>> map=new HashMap<Address,Collection<Address>>();
-        map.put(a, makeList(a,b,c));
-        map.put(b, makeList(b,c));
-        map.put(c, makeList(b,c));
-        System.out.println("map = " + map);
+        Map<Address,View> map=new HashMap<Address,View>();
+        map.put(a, makeView(a, a,b,c));
+        map.put(b, makeView(b, b,c));
+        map.put(c, makeView(b, b,c));
+        System.out.println("map:\n" + print(map));
 
         assert map.size() == 3;
-        Merger.sanitize(map);
-        System.out.println("map after sanitization: " + map);
+        Merger.sanitizeViews(map);
+        System.out.println("map after sanitization:\n" + print(map));
 
         assert map.size() == 3;
         assert map.get(a).size() == 1;
@@ -76,14 +77,14 @@ public class MergerTest {
      * B: B
      */
     public void testOverlappingMerge2() {
-        Map<Address, Collection<Address>> map=new HashMap<Address,Collection<Address>>();
-        map.put(a, makeList(a,b));
-        map.put(b, makeList(b));
-        System.out.println("map = " + map);
+        Map<Address,View> map=new HashMap<Address,View>();
+        map.put(a, makeView(a, a,b));
+        map.put(b, makeView(b, b));
+        System.out.println("map:\n" + print(map));
 
         assert map.size() == 2;
-        Merger.sanitize(map);
-        System.out.println("map after sanitization: " + map);
+        Merger.sanitizeViews(map);
+        System.out.println("map after sanitization:\n" + print(map));
 
         assert map.size() == 2;
         assert map.get(a).size() == 1;
@@ -98,16 +99,16 @@ public class MergerTest {
       * D: DE
       */
      public void testOverlappingMerge3() {
-         Map<Address, Collection<Address>> map=new HashMap<Address,Collection<Address>>();
-         map.put(a, makeList(a,b));
-         map.put(b, makeList(b,c));
-         map.put(c, makeList(c,d));
-         map.put(d, makeList(d,e));
-         System.out.println("map = " + map);
+         Map<Address,View> map=new HashMap<Address,View>();
+         map.put(a, makeView(a, a,b));
+         map.put(b, makeView(b, b,c));
+         map.put(c, makeView(c, c,d));
+         map.put(d, makeView(d, d,e));
+         System.out.println("map:\n" + print(map));
 
          assert map.size() == 4;
-         Merger.sanitize(map);
-         System.out.println("map after sanitization: " + map);
+         Merger.sanitizeViews(map);
+         System.out.println("map after sanitization:\n" + print(map));
 
          assert map.size() == 4;
          assert map.get(a).size() == 1;
@@ -120,5 +121,18 @@ public class MergerTest {
     private static <T> Collection<T> makeList(T ... elements) {
         return new ArrayList<T>(Arrays.asList(elements));
     }
+
+    private static View makeView(Address coord, Address ... members) {
+        return new View(coord, 1, new Vector<Address>(Arrays.asList(members)));
+    }
+
+    static String print(Map<Address,View> map) {
+        StringBuilder sb=new StringBuilder();
+        for(Map.Entry<Address,View> entry: map.entrySet()) {
+            sb.append(entry.getKey()).append(": ").append(entry.getValue().getMembers()).append("\n");
+        }
+        return sb.toString();
+    }
+
 
 }
