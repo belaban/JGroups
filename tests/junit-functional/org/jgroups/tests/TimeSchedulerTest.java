1f652ce93b5ff325a58d310029d10211a973d6e9
belaban <belaban>
2010-07-29 08:54:30 +0000

    ns

--- tests/junit-functional/org/jgroups/tests/TimeSchedulerTest.java
+++ tests/junit-functional/org/jgroups/tests/TimeSchedulerTest.java
@@ -28,7 +28,7 @@ import java.util.concurrent.TimeUnit;
  * 
  * Test cases for TimeScheduler
  * @author Bela Ban
- * @version $Id: TimeSchedulerTest.java,v 1.18 2010/07/28 14:14:23 belaban Exp $
+ * @version $Id: TimeSchedulerTest.java,v 1.19 2010/07/29 08:54:30 belaban Exp $
  */
 @Test(groups=Global.FUNCTIONAL,dataProvider="createTimer")
 public class TimeSchedulerTest {
@@ -134,6 +134,8 @@ public class TimeSchedulerTest {
             assert num_executions >= 1 : "task should have executed at least 1 time, as it was cancelled after 500ms";
             if(timer instanceof DefaultTimeScheduler)
                 ((DefaultTimeScheduler)timer).purge(); // removes cancelled tasks
+            else
+                Util.sleep(1000);
             assert timer.size() == 0 : " timer size should be 0, but is " + size;
         }
         finally {
