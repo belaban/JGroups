2704c7dd81c819e7c97b4505e46e4aef820ead36
belaban <belaban>
2010-09-20 04:39:35 +0000

    Removed stack trace for not being able to connect to GR (https://jira.jboss.org/browse/JGRP-1237)

--- src/org/jgroups/protocols/TUNNEL.java
+++ src/org/jgroups/protocols/TUNNEL.java
@@ -43,7 +43,7 @@ import org.jgroups.util.Util;
  * 
  * @author Bela Ban
  * @author Vladimir Blagojevic
- * @version $Id: TUNNEL.java,v 1.102 2010/06/15 10:10:40 belaban Exp $
+ * @version $Id: TUNNEL.java,v 1.103 2010/09/20 04:39:35 belaban Exp $
  */
 @Experimental
 public class TUNNEL extends TP {
@@ -418,7 +418,7 @@ public class TUNNEL extends TP {
                }
                catch (Exception e) {
                    if (log.isWarnEnabled())
-                       log.warn("Failed connecting to GossipRouter at " + stub.getGossipRouterAddress(),e);
+                       log.warn("Failed connecting to GossipRouter at " + stub.getGossipRouterAddress());
                    stubManager.startReconnecting(stub);
                }
            }
