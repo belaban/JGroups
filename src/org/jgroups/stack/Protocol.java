c0aae5af0336cbb53abe82b9c49971540726d2d4
belaban <belaban>
2010-09-30 09:47:54 +0000

    added ergonomics flag (https://jira.jboss.org/browse/JGRP-1052)

--- src/org/jgroups/stack/Protocol.java
+++ src/org/jgroups/stack/Protocol.java
@@ -38,7 +38,7 @@ import java.util.*;
  * constructor !</b>
  *
  * @author Bela Ban
- * @version $Id: Protocol.java,v 1.78 2010/09/09 08:20:08 belaban Exp $
+ * @version $Id: Protocol.java,v 1.79 2010/09/30 09:47:54 belaban Exp $
  */
 @DeprecatedProperty(names={"down_thread","down_thread_prio","up_thread","up_thread_prio"})
 public abstract class Protocol {
@@ -47,6 +47,9 @@ public abstract class Protocol {
     
     @Property(description="Determines whether to collect statistics (and expose them via JMX). Default is true",writable=true)
     protected boolean          stats=true;
+
+    @Property(name="Enables ergonomics: dynamically find the best values for properties at runtime")
+    protected boolean ergonomics=false;
 
     /** The name of the protocol. Is by default set to the protocol's classname. This property should rarely need to
      * be set, e.g. only in cases where we want to create more than 1 protocol of the same class in the same stack */
@@ -76,6 +79,14 @@ public abstract class Protocol {
 
     public String getLevel() {
         return log.getLevel();
+    }
+
+    public boolean isErgonomics() {
+        return ergonomics;
+    }
+
+    public void setErgonomics(boolean ergonomics) {
+        this.ergonomics=ergonomics;
     }
 
     /**
