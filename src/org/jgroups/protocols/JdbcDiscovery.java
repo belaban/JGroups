8991cd2310fee26ff4addef4f91a6519e1ece4ff
Bela Ban <belaban@yahoo.com>
2010-10-07 18:30:45 +0000

    new protocol

--- src/org/jgroups/protocols/JdbcDiscovery.java
+++ src/org/jgroups/protocols/JdbcDiscovery.java
@@ -1,8 +1,26 @@
 package org.jgroups.protocols;
 
+import org.jgroups.Address;
+
+import java.util.List;
+
 /**
  * @author Bela Ban
- * @version $Id: JdbcDiscovery.java,v 1.1 2010/10/07 18:30:44 belaban Exp $
+ * @version $Id: JdbcDiscovery.java,v 1.2 2010/10/07 18:33:33 belaban Exp $
  */
 public class JdbcDiscovery extends FILE_PING {
+
+
+    protected void remove(String clustername, Address addr) {
+        super.remove(clustername, addr);
+    }
+
+    @Override
+    protected List<PingData> readAll(String clustername) {
+        return super.readAll(clustername);
+    }
+
+    protected void writeToFile(PingData data, String clustername) {
+        super.writeToFile(data, clustername);
+    }
 }
