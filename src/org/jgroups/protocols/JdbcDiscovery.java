25170bf21daf045ec2e203275d1275d9988e3680
Bela Ban <belaban@yahoo.com>
2010-10-07 18:30:44 +0000

    new protocol

---
+++ src/org/jgroups/protocols/JdbcDiscovery.java
@@ -0,0 +1,8 @@
+package org.jgroups.protocols;
+
+/**
+ * @author Bela Ban
+ * @version $Id: JdbcDiscovery.java,v 1.1 2010/10/07 18:30:44 belaban Exp $
+ */
+public class JdbcDiscovery extends FILE_PING {
+}
