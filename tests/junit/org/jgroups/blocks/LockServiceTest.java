47c257d1b30f988e047b6d5919ae829ddc68238b
Bela Ban <belaban@yahoo.com>
2011-01-20 13:03:54 +0100

    adding tests for LockService

--- tests/junit/org/jgroups/blocks/LockServiceTest.java
+++ tests/junit/org/jgroups/blocks/LockServiceTest.java
@@ -7,11 +7,11 @@ import org.jgroups.protocols.CENTRAL_LOCK;
 import org.jgroups.stack.ProtocolStack;
 import org.jgroups.tests.ChannelTestBase;
 import org.jgroups.util.Util;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Lock;
 
 /** Tests {@link org.jgroups.blocks.locking.LockService}
@@ -20,94 +20,106 @@ import java.util.concurrent.locks.Lock;
 @Test(groups=Global.STACK_DEPENDENT,sequential=true)
 public class LockServiceTest extends ChannelTestBase {
     protected JChannel c1, c2, c3;
+    protected LockService s1, s2, s3;
+    protected Lock lock;
     protected static final String LOCK="sample-lock";
 
 
-    @BeforeTest
+    @BeforeClass
     protected void init() throws Exception {
-        c1=createChannel(true, 4, "A");
+        c1=createChannel(true, 3, "A");
         addLockingProtocol(c1);
+        s1=new LockService(c1);
         c1.connect("LockServiceTest");
 
         c2=createChannel(c1, "B");
+        s2=new LockService(c2);
         c2.connect("LockServiceTest");
 
         c3=createChannel(c1, "C");
+        s3=new LockService(c3);
         c3.connect("LockServiceTest");
+        
+        lock=s1.getLock(LOCK);
     }
 
-    protected void addLockingProtocol(JChannel ch) {
-        ProtocolStack stack=ch.getProtocolStack();
-        stack.insertProtocolAtTop(new CENTRAL_LOCK());
-    }
 
-    @AfterTest
+    @AfterClass
     protected void cleanup() {
+        s3.unlockAll();
+        s2.unlockAll();
+        s1.unlockAll();
         Util.close(c3,c2,c1);
     }
 
-    @DataProvider(name="createLockService")
-    LockService[][] createLockService() {
-        return new LockService[][] {
-          {new LockService(), new LockService(), new LockService()}
-        };
+
+    public void testSimpleLock() {
+        lock(lock, LOCK);
+        unlock(lock, LOCK);
     }
 
-    @Test(dataProvider="createLockService")
-    public void testSimpleLock(LockService s1, LockService s2, LockService s3) {
-        s1.setChannel(c1);
-        s2.setChannel(c2);
-        s3.setChannel(c3);
+    public void testLockingOfAlreadyAcquiredLock() {
+        lock(lock, LOCK);
+        lock(lock, LOCK);
+        unlock(lock, LOCK);
+    }
 
-        
-        Lock lock=s1.getLock(LOCK);
-
-        System.out.print("acquiring lock " + LOCK + ": ");
-        lock.lock();
+    public void testUnsuccessfulTryLock() {
+        Lock lock2=s2.getLock(LOCK);
+        lock(lock2, LOCK);
         try {
-            System.out.println("OK");
-            Util.sleep(1000);
-            System.out.print("releasing lock " + LOCK + ": ");
+            boolean rc=tryLock(lock, LOCK);
+            assert !rc;
+            unlock(lock, LOCK);
         }
         finally {
-            lock.unlock();
-            System.out.println("OK");
+            unlock(lock2, LOCK);
         }
     }
 
-    @Test(dataProvider="createLockService")
-    public void testBlockingLock(LockService s1, LockService s2, LockService s3) throws InterruptedException {
-        s1.setChannel(c1);
-        s2.setChannel(c2);
-        s3.setChannel(c3);
+    public void testUnsuccessfulTryLockTimeout() throws InterruptedException {
+        Lock lock2=s2.getLock(LOCK);
+        lock(lock2, LOCK);
+        try {
+            boolean rc=tryLock(lock, 1000, LOCK);
+            assert !rc;
+            unlock(lock, LOCK);
+        }
+        finally {
+            unlock(lock2, LOCK);
+        }
+    }
 
-        // todo: revisit test
+    public void testSuccessfulTryLockTimeout() throws InterruptedException, BrokenBarrierException {
+        final CyclicBarrier barrier=new CyclicBarrier(2);
 
-//        final LockService lock_service=s1;
-//        Thread other=new Thread() {
-//            public void run() {
-//                Lock l2=lock_service.getLock(LOCK);
-//                lock(l2, LOCK);
-//                Util.sleep(2000);
-//                unlock(l2, LOCK);
-//            }
-//        };
-//        other.start();
-//
-//        Util.sleep(500);
-//
-//        System.out.println("Locks on S1:\n" + s1.printLocks());
-//
-//        final Lock l1=s1.getLock(LOCK);
-//        lock(l1, LOCK);
-//        try {
-//            ;
-//        }
-//        finally {
-//            unlock(l1, LOCK);
-//        }
-//
-//        other.join();
+        Thread locker=new Locker(barrier);
+        locker.start();
+        barrier.await();
+        boolean rc=tryLock(lock, 10000, LOCK);
+        assert rc;
+    }
+
+    
+    protected class Locker extends Thread {
+        protected final CyclicBarrier barrier;
+
+        public Locker(CyclicBarrier barrier) {
+            this.barrier=barrier;
+        }
+
+        public void run() {
+            lock(lock, LOCK);
+            try {
+                barrier.await();
+                Util.sleep(500);
+            }
+            catch(Exception e) {
+            }
+            finally {
+                unlock(lock, LOCK);
+            }
+        }
     }
 
 
@@ -117,9 +129,30 @@ public class LockServiceTest extends ChannelTestBase {
         System.out.println("[" + Thread.currentThread().getId() + "] locked " + name);
     }
 
+    protected static boolean tryLock(Lock lock, String name) {
+        System.out.println("[" + Thread.currentThread().getId() + "] tryLocking " + name);
+        boolean rc=lock.tryLock();
+        System.out.println("[" + Thread.currentThread().getId() + "] locked " + name);
+        return rc;
+    }
+
+    protected static boolean tryLock(Lock lock, long timeout, String name) throws InterruptedException {
+        System.out.println("[" + Thread.currentThread().getId() + "] tryLocking " + name);
+        boolean rc=lock.tryLock(timeout, TimeUnit.MILLISECONDS);
+        System.out.println("[" + Thread.currentThread().getId() + "] locked " + name);
+        return rc;
+    }
+
     protected static void unlock(Lock lock, String name) {
+        if(lock == null)
+            return;
         System.out.println("[" + Thread.currentThread().getId() + "] releasing " + name);
         lock.unlock();
         System.out.println("[" + Thread.currentThread().getId() + "] released " + name);
+    }
+
+    protected void addLockingProtocol(JChannel ch) {
+        ProtocolStack stack=ch.getProtocolStack();
+        stack.insertProtocolAtTop(new CENTRAL_LOCK());
     }
 }
