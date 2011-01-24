0b88a0872d7e20650dee63a235d5689e343cc5ba
Bela Ban <belaban@yahoo.com>
2011-01-24 13:13:38 +0100

    changed warning message

--- src/org/jgroups/protocols/FlowControl.java
+++ src/org/jgroups/protocols/FlowControl.java
@@ -426,7 +426,7 @@ public abstract class FlowControl extends Protocol {
                 if(frag_size > max_credits) {
                     log.warn("The fragmentation size of the fragmentation protocol is " + frag_size +
                             ", which is greater than the max credits. While this is not incorrect, " +
-                            "it may lead to long blockings. Frag size should be less than max_credits " +
+                            "it may cause blockings. Frag size should be less than max_credits " +
                             "(http://jira.jboss.com/jira/browse/JGRP-590)");
                 }
                 frag_size_received=true;
