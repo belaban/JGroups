26b26c955ac2ec6292c88216ad56eb2246711e0e
Bela Ban <belaban@yahoo.com>
2011-04-15 13:34:40 +0200

    Using the correct classpath to start GossipRouter now

--- bin/gossiprouter.sh
+++ bin/gossiprouter.sh
@@ -12,5 +12,5 @@ OPTS="$OPTS -Dcom.sun.management.jmxremote"
 ## Uncomment for remote JMX access. Also modify JAVA_HOME/jre/lib/management/jmxremote.passwords
 # OPTS="$OPTS -Dcom.sun.management.jmxremote.port=7000 -Dcom.sun.management.jmxremote.ssl=false"
 
-java $OPTS -classpath $CLASSPATH $JAVA_OPTS org.jgroups.stack.GossipRouter -port 12001 $*
+java $OPTS -classpath $CP $JAVA_OPTS org.jgroups.stack.GossipRouter -port 12001 $*
 
