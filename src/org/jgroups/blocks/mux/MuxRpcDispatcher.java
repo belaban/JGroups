d2705533d0e8862d66727184a01b25532e09dfee
Bela Ban <belaban@yahoo.com>
2011-09-28 12:55:28 +0200

    - Preventing chaining of RspFilters with NoMuxHandlerRspFilter (https://issues.jboss.org/browse/JGRP-1369) - Added RequestOptions unit test for https://issues.jboss.org/browse/JGRP-1369

--- src/org/jgroups/blocks/mux/MuxRpcDispatcher.java
+++ src/org/jgroups/blocks/mux/MuxRpcDispatcher.java
@@ -97,9 +97,16 @@ public class MuxRpcDispatcher extends RpcDispatcher {
         super.stop();
     }
 
+    /**
     @Override
     protected <T> GroupRequest<T> cast(Collection<Address> dests, Message msg, RequestOptions options, boolean blockForResults) throws Exception {
         RspFilter filter = options.getRspFilter();
         return super.cast(dests, msg, options.setRspFilter((filter != null) ? new NoMuxHandlerRspFilter(filter) : new NoMuxHandlerRspFilter()), blockForResults);
+    }*/
+
+    @Override
+    protected <T> GroupRequest <T> cast(Collection<Address> dests, Message msg, RequestOptions options, boolean blockForResults) throws Exception {
+        RspFilter filter = options.getRspFilter();
+        return super.cast(dests, msg, options.setRspFilter(NoMuxHandlerRspFilter.createInstance(filter)), blockForResults);
     }
 }
