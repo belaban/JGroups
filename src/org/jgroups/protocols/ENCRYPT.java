62065e7265727609c797311bea779b08870df430
Bela Ban <belaban@yahoo.com>
2011-10-18 14:14:05 +0200

    Removed formatArray(), so secret keys cannot be seen by enabling logging  (https://issues.jboss.org/browse/JGRP-1100)

--- src/org/jgroups/protocols/ENCRYPT.java
+++ src/org/jgroups/protocols/ENCRYPT.java
@@ -886,13 +886,6 @@ public class ENCRYPT extends Protocol {
 
         //encrypt current secret key
         byte[] encryptedKey = tmp.doFinal(secret.getEncoded());
-
-        //SW logout encrypted bytes we are sending so we
-        // can match the clients log to see if they match
-        if (log.isDebugEnabled())
-            log.debug(" Generated encoded key which only client can decode:"
-                    + formatArray(encryptedKey));
-
         newMsg = new Message(source, local_addr, encryptedKey);
 
         newMsg.putHeader(EncryptHeader.KEY, new EncryptHeader(
@@ -1131,20 +1124,6 @@ public class ENCRYPT extends Protocol {
         return pubKey;
     }
 
-
-    /*
-      * simple helper method so we can see the format of the byte arrays in a
-      * readable form could be better to use Base64 but will do for now
-      */
-    private String formatArray(byte[] array)
-    {
-        StringBuffer buf = new StringBuffer();
-        for (int i = 0; i < array.length; i++)
-        {
-            buf.append(Integer.toHexString(array[i]));
-        }
-        return buf.toString();
-    }
 
 
     /**
