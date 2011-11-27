35ee58dd71a695c6f60e1b4f48c3099fe03dd581
Bela Ban <belaban@yahoo.com>
2011-11-27 11:32:28 +0100

    https://issues.jboss.org/browse/JGRP-1393

--- src/org/jgroups/protocols/Discovery.java
+++ src/org/jgroups/protocols/Discovery.java
@@ -326,6 +326,12 @@ public abstract class Discovery extends Protocol {
                             if(logical_addr != null && data.getLogicalName() != null)
                                 UUID.add(logical_addr, data.getLogicalName());
                             discoveryRequestReceived(msg.getSrc(), data.getLogicalName(), physical_addrs);
+                            
+                            synchronized(ping_responses) {
+                                for(Responses response: ping_responses) {
+                                    response.addResponse(data, false);
+                                }
+                            }
                         }
 
                         if(max_rank > 0 && rank > 0 && rank > max_rank) // https://jira.jboss.org/browse/JGRP-1181
