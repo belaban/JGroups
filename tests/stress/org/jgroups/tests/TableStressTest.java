3b3332547f60dd861a4ed03bdf053b2f9a0e68c9
Bela Ban <belaban@yahoo.com>
2012-01-20 17:45:27 +0100

    Changed TableStressTest to use numbers rather than messages

--- tests/stress/org/jgroups/tests/TableStressTest.java
+++ tests/stress/org/jgroups/tests/TableStressTest.java
@@ -1,6 +1,5 @@
 package org.jgroups.tests;
 
-import org.jgroups.Message;
 import org.jgroups.util.Table;
 import org.jgroups.util.Util;
 
@@ -8,12 +7,15 @@ import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.atomic.AtomicInteger;
 
-
+/**
+ * Test which uses 1 or more adder threads to add numbers to a table, and 1 remover thread to remove all of them. Test
+ * concurrency: when all threads are done, exactly N numbers have to have been added and the remover needs to
+ * ensure that it gets the numbers in the correct sequence, that there are no gaps and every number has been
+ * received only once.
+ */
 public class TableStressTest {
     static int NUM_THREADS=10;
-    static int NUM_MSGS=1000000;
-
-    static final Message MSG=new Message(false);
+    static int NUM=1000000;
 
 
     static final AtomicInteger added=new AtomicInteger(0);
@@ -22,11 +24,11 @@ public class TableStressTest {
     public static void main(String[] args) throws InterruptedException {
         for(int i=0; i < args.length; i++) {
             if(args[i].startsWith("-h")) {
-                System.out.println("TableStressTest [-num messages] [-adders <number of adder threads>]");
+                System.out.println("TableStressTest [-num numbers] [-adders <number of adder threads>]");
                 return;
             }
             if(args[i].equals("-num")) {
-                NUM_MSGS=Integer.parseInt(args[++i]);
+                NUM=Integer.parseInt(args[++i]);
                 continue;
             }
             if(args[i].equals("-adders")) {
@@ -34,7 +36,7 @@ public class TableStressTest {
             }
         }
 
-        Table<Message> buf=new Table<Message>(10000, 10000, 0);
+        Table<Integer> buf=new Table<Integer>(10000, 10000, 0);
 
         final CountDownLatch latch=new CountDownLatch(1);
 
@@ -47,8 +49,6 @@ public class TableStressTest {
             adders[i].start();
         }
 
-        Util.sleep(1000);
-
         long start=System.currentTimeMillis();
         latch.countDown();
         while(remover.isAlive()) {
@@ -58,16 +58,16 @@ public class TableStressTest {
         long diff=System.currentTimeMillis() - start;
 
         System.out.println("added messages: " + added + ", removed messages: " + removed);
-        System.out.println("took " + diff + " ms to insert and remove " + NUM_MSGS + " messages");
+        System.out.println("took " + diff + " ms to insert and remove " + NUM + " messages");
     }
 
 
     protected static class Adder extends Thread {
-        protected final Table<Message> buf;
+        protected final Table<Integer> buf;
         protected final AtomicInteger num;
         protected final CountDownLatch latch;
 
-        public Adder(Table<Message> buf, CountDownLatch latch, AtomicInteger num) {
+        public Adder(Table<Integer> buf, CountDownLatch latch, AtomicInteger num) {
             this.buf=buf;
             this.num=num;
             this.latch=latch;
@@ -84,20 +84,20 @@ public class TableStressTest {
 
             while(true) {
                 int seqno=num.incrementAndGet();
-                if(seqno > NUM_MSGS) {
+                if(seqno > NUM) {
                     num.decrementAndGet();
                     break;
                 }
-                buf.add(seqno, MSG);
+                buf.add(seqno, seqno);
             }
         }
     }
 
     protected static class Remover extends Thread {
-        protected final Table<Message> buf;
+        protected final Table<Integer> buf;
         protected final CountDownLatch latch;
 
-        public Remover(Table<Message> buf, CountDownLatch latch) {
+        public Remover(Table<Integer> buf, CountDownLatch latch) {
             this.buf=buf;
             this.latch=latch;
             setName("Remover");
@@ -111,20 +111,29 @@ public class TableStressTest {
                 e.printStackTrace();
             }
             int cnt=0;
+            int expected=1, unexpected=0;
+
             for(;;) {
-                List<Message> msgs=buf.removeMany(true, 100);
-                if(msgs != null) {
-                    for(Message msg: msgs) {
+                List<Integer> nums=buf.removeMany(true, 100);
+                if(nums != null) {
+                    for(Integer num: nums) {
                         cnt++;
                         removed.incrementAndGet();
+                        if(num.intValue() != expected) {
+                            unexpected++;
+                            System.err.println("***** expected " + expected + ", but got " + num);
+                        }
+                        expected++;
                     }
                     continue;
                 }
-                if(cnt >= NUM_MSGS)
+                if(cnt >= NUM)
                     break;
                 Util.sleep(500);
             }
-            System.out.println("-- removed " + cnt + " messages");
+            System.out.println("-- removed " + cnt + " numbers");
+            if(unexpected > 0)
+                System.err.println("****** got " + unexpected + " numbers ! *******");
         }
     }
 }
