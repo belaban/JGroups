74c5f042db503f5f035bd6946b2bedd8bbcf02eb
Bela Ban <belaban@yahoo.com>
2012-01-30 16:45:55 +0100

    changed version to 3.1.0.Alpha2

--- src/org/jgroups/Version.java
+++ src/org/jgroups/Version.java
@@ -22,7 +22,7 @@ public class Version {
     public static final short major = 3;
     public static final short minor = 1;
     public static final short micro = 0;
-    public static final String description="3.1.0.Alpha1";
+    public static final String description="3.1.0.Alpha2";
 
     public static final short version=encode(major, minor, micro);
     public static final String string_version=print(version);
