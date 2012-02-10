739cd50c839e429b8cdf75a5d75d86dadd95953f
Bela Ban <belaban@yahoo.com>
2012-02-10 09:14:27 +0100

    ns

--- tests/junit/org/jgroups/blocks/RpcDispatcherAnycastServerObject.java
+++ tests/junit/org/jgroups/blocks/RpcDispatcherAnycastServerObject.java
@@ -29,7 +29,7 @@ public class RpcDispatcherAnycastServerObject extends ReceiverAdapter {
         // vector is unmodifiable
         List<Address> v=new ArrayList<Address>(c.getView().getMembers());
         if(excludeSelf) v.remove(c.getAddress());
-        RspList rsps=d.callRemoteMethods(v, "doSomething", new Object[]{}, new Class[]{}, new RequestOptions(ResponseMode.GET_ALL, 10000, useAnycast));
+        RspList rsps=d.callRemoteMethods(v, "doSomething", new Object[]{}, new Class[]{}, new RequestOptions(ResponseMode.GET_ALL, 20000, useAnycast));
         Map.Entry entry;
         for(Iterator it=rsps.entrySet().iterator(); it.hasNext();) {
             entry=(Map.Entry)it.next();
