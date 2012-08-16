1dd8c09ef936048284bc01e9916d00d5b2df2407
Bela Ban <belaban@yahoo.com>
2012-08-16 15:24:44 +0200

    Added javadoc to Message.copy() to clarify semantics (no deep copy)

--- src/org/jgroups/Message.java
+++ src/org/jgroups/Message.java
@@ -541,7 +541,10 @@ public class Message implements Streamable {
    /**
     * Create a copy of the message. If offset and length are used (to refer to another buffer), the
     * copy will contain only the subset offset and length point to, copying the subset into the new
-    * copy.
+    * copy.<p/>
+    * Note that for headers, only the arrays holding references to the headers are copied, not the headers themselves !
+    * The consequence is that the headers array of the copy hold the *same* references as the original, so do *not*
+    * modify the headers ! If you want to change a header, copy it and call {@link Message#putHeader(short,Header)} again.
     * 
     * @param copy_buffer
     * @param copy_headers
