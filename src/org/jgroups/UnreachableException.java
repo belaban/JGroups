4a4ade8587115e87eea4995b3b1566c3b765906d
Bela Ban <belaban@yahoo.com>
2012-09-27 17:10:40 +0200

    ns

--- src/org/jgroups/UnreachableException.java
+++ src/org/jgroups/UnreachableException.java
@@ -22,6 +22,6 @@ public class UnreachableException extends RuntimeException {
 
 
     public String toString() {
-        return super.toString() + " (member=" + member + ")";
+        return getMessage() + ": member=" + member;
     }
 }
