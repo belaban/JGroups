5ea053074fc28dde2444d9d093ed40ff926d5686
Bela Ban <belaban@yahoo.com>
2012-11-13 10:45:34 +0100

    Changed version to 3.3.0.Alpha1

--- src/org/jgroups/Version.java
+++ src/org/jgroups/Version.java
@@ -20,7 +20,7 @@ import org.jgroups.annotations.Immutable;
 @Immutable
 public class Version {
     public static final short  major = 3;
-    public static final short  minor = 2;
+    public static final short  minor = 3;
     public static final short  micro = 0;
     public static final String description="3.3.0.Alpha1";
 
