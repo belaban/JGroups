f1b801521285f9b7383df1cd513a16a28ea12c24
Bela Ban <belaban@yahoo.com>
2012-11-28 13:12:06 +0100

    Added test for JGRP-1545

---
+++ tests/byteman/org/jgroups/tests/helpers/MessageBeforeConnectedTestHelper.java
@@ -0,0 +1,33 @@
+package org.jgroups.tests.helpers;
+
+import org.jboss.byteman.rule.Rule;
+import org.jboss.byteman.rule.helper.Helper;
+import org.jgroups.Event;
+import org.jgroups.JChannel;
+import org.jgroups.Message;
+import org.jgroups.protocols.UNICAST2;
+
+/**
+ * @author Bela Ban
+ * @since 3.3
+ */
+public class MessageBeforeConnectedTestHelper extends Helper {
+
+    protected MessageBeforeConnectedTestHelper(Rule rule) {
+        super(rule);
+    }
+
+    /**
+     * Sends a unicast message
+     */
+    public void sendUnicast(JChannel ch) throws Exception {
+        Message msg=new Message(ch.getAddress(), ch.getAddress(), "hello-1");
+
+        // Add a UNICAST2 header
+        UNICAST2 unicast=(UNICAST2)ch.getProtocolStack().findProtocol(UNICAST2.class);
+        UNICAST2.Unicast2Header hdr=UNICAST2.Unicast2Header.createDataHeader(1, (short)1, true);
+        msg.putHeader(unicast.getId(), hdr);
+
+        unicast.up(new Event(Event.MSG, msg));
+    }
+}
