46530d3b4f12e841d00e7eecfdf97e8f88f8e90f
Bela Ban <belaban@yahoo.com>
2013-05-28 12:05:57 +0200

    Added support for unicast probe (https://issues.jboss.org/browse/JGRP-1568)

--- tests/other/org/jgroups/tests/Probe.java
+++ tests/other/org/jgroups/tests/Probe.java
@@ -3,9 +3,7 @@ package org.jgroups.tests;
 import org.jgroups.util.StackType;
 import org.jgroups.util.Util;
 
-import java.net.DatagramPacket;
-import java.net.InetAddress;
-import java.net.MulticastSocket;
+import java.net.*;
 import java.util.*;
 
 /**
@@ -26,33 +24,20 @@ public class Probe {
 
     public void start(InetAddress addr, InetAddress bind_addr, int port, int ttl,
                       final long timeout, List query, String match, boolean weed_out_duplicates, String passcode) throws Exception {
-        mcast_sock=new MulticastSocket();
-        mcast_sock.setTimeToLive(ttl);
-        if(bind_addr != null)
-            mcast_sock.setInterface(bind_addr);
 
-        StringBuilder request=new StringBuilder();
-        byte[] authenticationDigest = null;
-        if(passcode != null){
-           long t1 = (new Date()).getTime();
-           double q1 = Math.random();           
-           authenticationDigest = Util.createAuthenticationDigest(passcode, t1, q1);
+        boolean unicast_dest=addr != null && !addr.isMulticastAddress();
+        if(unicast_dest) {
+            Collection<InetAddress> targets=getPhysicalAddresses(addr, bind_addr, port, timeout);
+            if(targets.isEmpty()) {
+                System.err.println("Found no valid hosts - terminating");
+                return;
+            }
+            for(InetAddress target: targets) {
+                sendRequest(target, bind_addr, port, ttl, query, passcode);
+            }
         }
-        for(int i=0; i < query.size(); i++) {
-            request.append(query.get(i)).append(" ");
-        }
-        byte[] queryPayload = request.toString().getBytes();
-        byte[] payload = queryPayload;
-        if (authenticationDigest != null) {
-           payload = new byte[authenticationDigest.length + queryPayload.length];
-           System.arraycopy(authenticationDigest, 0, payload, 0, authenticationDigest.length);
-           System.arraycopy(queryPayload, 0, payload, authenticationDigest.length, queryPayload.length);
-        }
-        
-        DatagramPacket probe=new DatagramPacket(payload, 0, payload.length, addr, port);
-        mcast_sock.send(probe);
-        System.out.println("\n-- send probe on " + addr + ':' + port + '\n');
-
+        else
+            sendRequest(addr, bind_addr, port, ttl, query, passcode);
 
         new Thread() {
             public void run() {
@@ -77,9 +62,8 @@ public class Probe {
 
             byte[] data=rsp.getData();
             response=new String(data, 0, rsp.getLength());
-            if(weed_out_duplicates && checkDuplicateResponse(response)) {
+            if(weed_out_duplicates && checkDuplicateResponse(response))
                 continue;
-            }
 
             count++;
             if(matches(response, match)) {
@@ -90,6 +74,82 @@ public class Probe {
                 not_matched++;
         }
         System.out.println("\n" + count + " responses (" + matched + " matches, " + not_matched + " non matches)");
+    }
+
+    protected static Collection<InetAddress> getPhysicalAddresses(InetAddress addr, InetAddress bind_addr,
+                                                                  int port, final long timeout) throws Exception {
+        final DatagramSocket sock=new DatagramSocket(new InetSocketAddress(bind_addr, 0));
+        byte[] payload="addrs".getBytes();
+        DatagramPacket probe=new DatagramPacket(payload, 0, payload.length, addr, port);
+        sock.send(probe);
+
+        new Thread() {
+            public void run() {
+                Util.sleep(timeout);
+                sock.close();
+            }
+        }.start();
+
+        Collection<InetAddress> retval=new ArrayList<InetAddress>();
+        long end_time=System.currentTimeMillis() + timeout;
+        while(System.currentTimeMillis() < end_time) {
+            byte[] buf=new byte[70000];
+            DatagramPacket rsp=new DatagramPacket(buf, 0, buf.length);
+            try {
+                sock.receive(rsp);
+            }
+            catch(Throwable t) {
+                break;
+            }
+
+            byte[] data=rsp.getData();
+            String response=new String(data, 0, rsp.getLength());
+            if(response != null && response.startsWith("addrs=")) {
+                response=response.substring("addrs=".length()).trim();
+                List<String> rsps=Util.parseStringList(response,",");
+                for(String tmp: rsps) {
+                    int index=tmp.indexOf(':');
+                    if(index != -1)
+                        tmp=tmp.substring(0, index);
+                    retval.add(InetAddress.getByName(tmp));
+                }
+                return retval;
+            }
+        }
+        return retval;
+    }
+
+
+    protected void sendRequest(InetAddress addr, InetAddress bind_addr, int port, int ttl,
+                               List query, String passcode) throws Exception {
+        if(mcast_sock == null) {
+            mcast_sock=new MulticastSocket();
+            mcast_sock.setTimeToLive(ttl);
+            if(bind_addr != null)
+                mcast_sock.setInterface(bind_addr);
+        }
+
+        StringBuilder request=new StringBuilder();
+        byte[] authenticationDigest = null;
+        if(passcode != null){
+            long t1 = (new Date()).getTime();
+            double q1 = Math.random();
+            authenticationDigest = Util.createAuthenticationDigest(passcode, t1, q1);
+        }
+        for(int i=0; i < query.size(); i++) {
+            request.append(query.get(i)).append(" ");
+        }
+        byte[] queryPayload = request.toString().getBytes();
+        byte[] payload = queryPayload;
+        if (authenticationDigest != null) {
+            payload = new byte[authenticationDigest.length + queryPayload.length];
+            System.arraycopy(authenticationDigest, 0, payload, 0, authenticationDigest.length);
+            System.arraycopy(queryPayload, 0, payload, authenticationDigest.length, queryPayload.length);
+        }
+
+        DatagramPacket probe=new DatagramPacket(payload, 0, payload.length, addr, port);
+        mcast_sock.send(probe);
+        System.out.println("\n-- sending probe on " + addr + ':' + port + '\n');
     }
 
     private boolean checkDuplicateResponse(String response) {
@@ -128,7 +188,7 @@ public class Probe {
         List<String> query=new ArrayList<String>();
         String       match=null;
         boolean      weed_out_duplicates=false;
-        String passcode = null;
+        String       passcode=null;
 
         try {
             for(int i=0; i < args.length; i++) {
