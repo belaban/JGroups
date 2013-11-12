a7de67e6d1ee0b3f24657ccc78c00cd8020a87d7
Richard Achmatowicz <rachmatowicz@jboss.com>
2013-11-11 22:00:15 -0500

    [JGRP-1739] Fix test output leaking across test cases.

--- tests/util/org/jgroups/util/JUnitXMLReporter.java
+++ tests/util/org/jgroups/util/JUnitXMLReporter.java
@@ -101,7 +101,7 @@ public class JUnitXMLReporter implements ITestListener, IConfigurationListener2 
     }
 
     public void onConfigurationSuccess(ITestResult tr) {
-
+        closeStreams();
     }
 
     public void onConfigurationFailure(ITestResult tr) {
@@ -110,6 +110,7 @@ public class JUnitXMLReporter implements ITestListener, IConfigurationListener2 
     }
 
     public void onConfigurationSkip(ITestResult tr) {
+        closeStreams();
     }
 
 
