e3c7d66ffce190ba8ed1af12be860536962c5f38
Bela Ban <belaban@yahoo.com>
2014-04-02 18:43:38 +0200

    Updated docu on GOOGLE_PING

--- src/org/jgroups/protocols/GOOGLE_PING.java
+++ src/org/jgroups/protocols/GOOGLE_PING.java
@@ -1,5 +1,8 @@
 package org.jgroups.protocols;
 
+import org.jgroups.annotations.Experimental;
+import org.jgroups.annotations.Property;
+
 /**
  * Discovery protocol for Google Cloud Storage. Very simple first shot at an impl, based on a simple migration of
  * S3_PING, as discussed in [1].<p/>
@@ -8,15 +11,18 @@ package org.jgroups.protocols;
  * @author Bela Ban
  * @since 3.5
  */
+@Experimental
 public class GOOGLE_PING extends S3_PING {
-    protected static final String DEFAULT_HOST="storage.googleapis.com";
+
+    @Property(description="The name of the Google Cloud Storage server")
+    protected String host="storage.googleapis.com";
 
     public void init() throws Exception {
         super.init();
     }
 
     protected AWSAuthConnection createConnection() {
-        return new AWSAuthConnection(access_key, secret_access_key, false, DEFAULT_HOST);
+        return new AWSAuthConnection(access_key, secret_access_key, false, host);
     }
 }
 
