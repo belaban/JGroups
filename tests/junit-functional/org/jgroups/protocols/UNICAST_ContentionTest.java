fa5165721637f5af36658c8d688ba435201ca7c2
Bela Ban <belaban@yahoo.com>
2014-04-22 09:10:52 +0200

    ns

--- tests/junit-functional/org/jgroups/protocols/UNICAST_ContentionTest.java
+++ tests/junit-functional/org/jgroups/protocols/UNICAST_ContentionTest.java
@@ -17,49 +17,44 @@ import java.util.concurrent.atomic.AtomicInteger;
  */
 @Test(groups=Global.FUNCTIONAL, singleThreaded=true)
 public class UNICAST_ContentionTest {
-    JChannel c1, c2;
-    static final String unicast_props="SHARED_LOOPBACK(thread_pool.queue_max_size=5000;" +
-            "thread_pool.rejection_policy=discard;thread_pool.min_threads=20;thread_pool.max_threads=20;" +
-            "oob_thread_pool.rejection_policy=discard)"+
-            ":UNICAST(xmit_interval=500)";
-    static final String unicast2_props=unicast_props.replace("UNICAST", "UNICAST2");
-    static final String unicast3_props=unicast_props.replace("UNICAST", "UNICAST3");
-    static final int NUM_THREADS=100;
-    static final int NUM_MSGS=100;
-    static final int SIZE=1000; // default size of a message in bytes
+    JChannel         a, b;
+    static final int NUM_THREADS =  100;
+    static final int NUM_MSGS    =  100;
+    static final int SIZE        = 1000; // default size of a message in bytes
 
     @AfterMethod
     protected void tearDown() throws Exception {
-        Util.close(c2, c1);
+        Util.close(b,a);
     }
 
     @DataProvider
     static Object[][] provider() {
         return new Object[][] {
-          {unicast_props},
-          {unicast2_props},
-          {unicast3_props}
+          {UNICAST.class},
+          {UNICAST2.class},
+          {UNICAST3.class}
         };
     }
 
 
+
     @Test(dataProvider="provider")
-    public void testSimpleMessageReception(String props) throws Exception {
-        c1=new JChannel(props); c1.setName("A");
-        c2=new JChannel(props); c2.setName("B");
-        MyReceiver r1=new MyReceiver("c1"), r2=new MyReceiver("c2");
-        c1.setReceiver(r1);
-        c2.setReceiver(r2);
-        c1.connect("testSimpleMessageReception");
-        c2.connect("testSimpleMessageReception");
+    public void testSimpleMessageReception(Class<? extends Protocol> unicast_class) throws Exception {
+        a=create(unicast_class, "A");
+        b=create(unicast_class, "B");
+        MyReceiver r1=new MyReceiver("A"), r2=new MyReceiver("B");
+        a.setReceiver(r1);
+        b.setReceiver(r2);
+        a.connect("testSimpleMessageReception");
+        b.connect("testSimpleMessageReception");
 
         int NUM=100;
-        Address c1_addr=c1.getAddress(), c2_addr=c2.getAddress();
+        Address c1_addr=a.getAddress(), c2_addr=b.getAddress();
         for(int i=1; i <= NUM; i++) {
-            c1.send(c1_addr, "bla");
-            c1.send(c2_addr, "bla");
-            c2.send(c2_addr, "bla");
-            c2.send(c1_addr, "bla");
+            a.send(c1_addr,"bla");
+            a.send(c2_addr,"bla");
+            b.send(c2_addr,"bla");
+            b.send(c1_addr,"bla");
         }
 
         for(int i=0; i < 10; i++) {
@@ -68,8 +63,8 @@ public class UNICAST_ContentionTest {
             Util.sleep(500);
         }
 
-        System.out.println("c1 received " + r1.getNum() + " msgs, " + getNumberOfRetransmissions(c1) + " retransmissions");
-        System.out.println("c2 received " + r2.getNum() + " msgs, " + getNumberOfRetransmissions(c2) + " retransmissions");
+        System.out.println("c1 received " + r1.getNum() + " msgs, " + getNumberOfRetransmissions(a) + " retransmissions");
+        System.out.println("c2 received " + r2.getNum() + " msgs, " + getNumberOfRetransmissions(b) + " retransmissions");
 
         assert r1.getNum() == NUM * 2: "expected " + NUM *2 + ", but got " + r1.getNum();
         assert r2.getNum() == NUM * 2: "expected " + NUM *2 + ", but got " + r2.getNum();
@@ -81,25 +76,25 @@ public class UNICAST_ContentionTest {
      * @throws Exception
      */
     @Test(dataProvider="provider")
-    public void testMessageReceptionUnderHighLoad(String props) throws Exception {
+    public void testMessageReceptionUnderHighLoad(Class<? extends Protocol> unicast_class) throws Exception {
         CountDownLatch latch=new CountDownLatch(1);
-        c1=new JChannel(props); c1.setName("A");
-        c2=new JChannel(props); c2.setName("B");
-        MyReceiver r1=new MyReceiver("c1"), r2=new MyReceiver("c2");
-        c1.setReceiver(r1);
-        c2.setReceiver(r2);
-        c1.connect("testSimpleMessageReception");
-        c2.connect("testSimpleMessageReception");
+        a=create(unicast_class, "A");
+        b=create(unicast_class, "B");
+        MyReceiver r1=new MyReceiver("A"), r2=new MyReceiver("B");
+        a.setReceiver(r1);
+        b.setReceiver(r2);
+        a.connect("testSimpleMessageReception");
+        b.connect("testSimpleMessageReception");
 
-        Address c1_addr=c1.getAddress(), c2_addr=c2.getAddress();
+        Address c1_addr=a.getAddress(), c2_addr=b.getAddress();
         MySender[] c1_senders=new MySender[NUM_THREADS];
         for(int i=0; i < c1_senders.length; i++) {
-            c1_senders[i]=new MySender(c1, c2_addr, latch);
+            c1_senders[i]=new MySender(a, c2_addr, latch);
             c1_senders[i].start();
         }
         MySender[] c2_senders=new MySender[NUM_THREADS];
         for(int i=0; i < c2_senders.length; i++) {
-            c2_senders[i]=new MySender(c2, c1_addr, latch);
+            c2_senders[i]=new MySender(b, c1_addr, latch);
             c2_senders[i].start();
         }
 
@@ -117,28 +112,30 @@ public class UNICAST_ContentionTest {
             if(r1.getNum() == NUM_EXPECTED_MSGS && r2.getNum() == NUM_EXPECTED_MSGS)
                 break;
             Util.sleep(2000);
-            UNICAST2 unicast2=(UNICAST2)c1.getProtocolStack().findProtocol(UNICAST2.class);
+            UNICAST2 unicast2=(UNICAST2)a.getProtocolStack().findProtocol(UNICAST2.class);
             if(unicast2 != null)
                 unicast2.sendStableMessages();
-            unicast2=(UNICAST2)c2.getProtocolStack().findProtocol(UNICAST2.class);
+            unicast2=(UNICAST2)b.getProtocolStack().findProtocol(UNICAST2.class);
             if(unicast2 != null)
                 unicast2.sendStableMessages();
         }
 
-        System.out.println("c1 received " + r1.getNum() + " msgs, " + getNumberOfRetransmissions(c1) + " retransmissions");
-        System.out.println("c2 received " + r2.getNum() + " msgs, " + getNumberOfRetransmissions(c2) + " retransmissions");
+        System.out.println("c1 received " + r1.getNum() + " msgs, " + getNumberOfRetransmissions(a) + " retransmissions");
+        System.out.println("c2 received " + r2.getNum() + " msgs, " + getNumberOfRetransmissions(b) + " retransmissions");
 
         assert r1.getNum() == NUM_EXPECTED_MSGS : "expected " + NUM_EXPECTED_MSGS + ", but got " + r1.getNum();
         assert r2.getNum() == NUM_EXPECTED_MSGS : "expected " + NUM_EXPECTED_MSGS + ", but got " + r2.getNum();
     }
 
-
+    protected JChannel create(Class<? extends Protocol> unicast_class, String name) throws Exception {
+        return new JChannel(new SHARED_LOOPBACK(), unicast_class.newInstance().setValue("xmit_interval", 500)).name(name);
+    }
 
     private static long getNumberOfRetransmissions(JChannel ch) {
         Protocol prot=ch.getProtocolStack().findProtocol(Util.getUnicastProtocols());
         if(prot instanceof UNICAST)
             return ((UNICAST)prot).getNumXmits();
-        return -1;
+        return 0;
     }
 
 
