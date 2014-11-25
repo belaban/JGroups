c9dfc9216f55da0fc42953b1cb9aea2781682884
Bela Ban <belaban@yahoo.com>
2014-11-25 17:41:39 +0100

    pp for Probe

--- src/org/jgroups/JChannel.java
+++ src/org/jgroups/JChannel.java
@@ -1181,11 +1181,20 @@ public class JChannel extends Channel {
                     }
                 }
                 tmp_stats=dumpStats(protocol_name, list);
+                for(Map.Entry<String,Object> entry: tmp_stats.entrySet()) {
+                    Map<String,Object> tmp_map=(Map<String,Object>)entry.getValue();
+                    String key=entry.getKey();
+                    map.put(key, tmp_map != null? tmp_map.toString() : null);
+                }
             }
-            else
+            else {
                 tmp_stats=dumpStats();
-
-            map.put("jmx", tmp_stats != null? Util.mapToString(tmp_stats) : "null");
+                for(Map.Entry<String,Object> entry: tmp_stats.entrySet()) {
+                    Map<String,Object> tmp_map=(Map<String,Object>)entry.getValue();
+                    String key=entry.getKey();
+                    map.put(key, tmp_map != null? tmp_map.toString() : null);
+                }
+            }
         }
 
         /**
