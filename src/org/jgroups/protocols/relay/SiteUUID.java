9bc44b9872c5d57250ecc41402abec05cc53ed02
Bela Ban <belaban@yahoo.com>
2015-06-04 09:50:05 +0200

    bla

--- src/org/jgroups/protocols/relay/SiteUUID.java
+++ src/org/jgroups/protocols/relay/SiteUUID.java
@@ -14,8 +14,8 @@ import java.util.Arrays;
  * @since 3.2
  */
 public class SiteUUID extends ExtendedUUID implements SiteAddress {
-    protected static final byte[] NAME      = Util.stringToBytes("name"); // logical name, can be null
-    protected static final byte[] SITE_NAME = Util.stringToBytes("site");
+    protected static final byte[] NAME      = Util.stringToBytes("relay2.name"); // logical name, can be null
+    protected static final byte[] SITE_NAME = Util.stringToBytes("relay2.site");
     private static final long     serialVersionUID=7128439052905502361L;
 
 
