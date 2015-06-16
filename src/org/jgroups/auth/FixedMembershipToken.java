f1533756133dec84ce8218202585ac85904da7c9
Bela Ban <belaban@yahoo.com>
2015-06-16 13:16:49 +0200

    Added IPv6 support to FixedMembershipToken, plus unit test (to AUTHest)

--- src/org/jgroups/auth/FixedMembershipToken.java
+++ src/org/jgroups/auth/FixedMembershipToken.java
@@ -4,19 +4,22 @@ import org.jgroups.Event;
 import org.jgroups.Message;
 import org.jgroups.PhysicalAddress;
 import org.jgroups.annotations.Property;
+import org.jgroups.stack.IpAddress;
 import org.jgroups.util.Bits;
 import org.jgroups.util.Util;
 
 import java.io.DataInput;
 import java.io.DataOutput;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
 
 /**
  * <p>
- * The FixedMemberShipToken object predefines a list of IP addresses and ports that can join the
- * group.
+ * The FixedMemberShipToken object predefines a list of IP addresses and ports that can join the group.
  * </p>
  * <p>
  * Configuration parameters for this example are shown below:
@@ -30,8 +33,8 @@ import java.util.StringTokenizer;
  * @author Chris Mills (millsy@jboss.com)
  */
 public class FixedMembershipToken extends AuthToken {
-    private List<String> memberList = null;
-    private String token = "emptyToken";
+    private final List<InetSocketAddress> memberList = new ArrayList<>();
+    private String                        token = "emptyToken";
 
     @Property
     private String fixed_members_seperator = ",";
@@ -54,80 +57,74 @@ public class FixedMembershipToken extends AuthToken {
 
     public boolean authenticate(AuthToken token, Message msg) {
         if ((token != null) && (token instanceof FixedMembershipToken) && (this.memberList != null)) {
-            PhysicalAddress src = (PhysicalAddress) auth.down(new Event(Event.GET_PHYSICAL_ADDRESS,
-                            msg.getSrc()));
+            PhysicalAddress src = (PhysicalAddress) auth.down(new Event(Event.GET_PHYSICAL_ADDRESS, msg.getSrc()));
             if (src == null) {
-                if (log.isErrorEnabled())
-                    log.error("didn't find physical address for " + msg.getSrc());
+                log.error("didn't find physical address for " + msg.getSrc());
                 return false;
             }
-
-            String sourceAddressWithPort = src.toString();
-            String sourceAddressWithoutPort = sourceAddressWithPort.substring(0,
-                            sourceAddressWithPort.indexOf(":"));
-
-            if (log.isDebugEnabled()) {
-                log.debug("AUTHToken received from " + sourceAddressWithPort);
-            }
-
-            for (String member : memberList) {
-                if (hasPort(member)) {
-                    if (member.equals(sourceAddressWithPort))
-                        return true;
-                } else {
-                    if (member.equals(sourceAddressWithoutPort))
-                        return true;
-                }
-            }
-            return false;
+            return isInMembersList((IpAddress)src);
         }
 
-        if (log.isWarnEnabled()) {
+        if (log.isWarnEnabled())
             log.warn("Invalid AuthToken instance - wrong type or null");
+        return false;
+    }
+
+    public boolean isInMembersList(IpAddress sender) {
+        if(memberList == null || sender == null)
+            return false;
+
+        for(InetSocketAddress addr: memberList) {
+            if(match(sender, addr))
+                return true;
         }
         return false;
     }
+
+    public static boolean match(IpAddress sender, InetSocketAddress addr) {
+        return !(sender == null || addr == null)
+          && addr.getAddress().equals(sender.getIpAddress())
+          && (addr.getPort() == 0 || addr.getPort() == sender.getPort());
+    }
+
 
     private static boolean hasPort(String member) {
         return member.contains(":");
     }
 
     @Property(name = "fixed_members_value")
-    public void setMemberList(String list) {
-        memberList = new ArrayList<>();
+    public void setMemberList(String list) throws UnknownHostException {
+        memberList.clear();
         StringTokenizer memberListTokenizer = new StringTokenizer(list, fixed_members_seperator);
         while (memberListTokenizer.hasMoreTokens()) {
-            memberList.add(memberListTokenizer.nextToken().replace('/', ':'));
+            String tmp=memberListTokenizer.nextToken().trim();
+            int index=tmp.lastIndexOf('/');
+            int port=index != -1? Integer.parseInt(tmp.substring(index+1)) : 0;
+            String addr_str=index != -1? tmp.substring(0, index) : tmp;
+            InetAddress addr=InetAddress.getByName(addr_str);
+            memberList.add(new InetSocketAddress(addr, port));
         }
     }
 
     /**
      * Required to serialize the object to pass across the wire
-     * 
-     *
-     *
      * @param out
      * @throws java.io.IOException
      */
     public void writeTo(DataOutput out) throws Exception {
-        if (log.isDebugEnabled()) {
+        if (log.isDebugEnabled())
             log.debug("SimpleToken writeTo()");
-        }
         Bits.writeString(this.token,out);
     }
 
     /**
      * Required to deserialize the object when read in from the wire
-     * 
-     *
-     *
      * @param in
      * @throws Exception
      */
     public void readFrom(DataInput in) throws Exception {
-        if (log.isDebugEnabled()) {
+        if (log.isDebugEnabled())
             log.debug("SimpleToken readFrom()");
-        }
         this.token = Bits.readString(in);
     }
 
--- tests/junit-functional/org/jgroups/protocols/AUTHTest.java
+++ tests/junit-functional/org/jgroups/protocols/AUTHTest.java
@@ -2,8 +2,10 @@ package org.jgroups.protocols;
 
 
 import org.jgroups.Global;
+import org.jgroups.auth.FixedMembershipToken;
 import org.jgroups.auth.MD5Token;
 import org.jgroups.auth.SimpleToken;
+import org.jgroups.stack.IpAddress;
 import org.testng.annotations.Test;
 
 /**
@@ -145,6 +147,24 @@ public class AUTHTest {
         header.setToken(token1);
 
         assert !(token2 == header.getToken());
+    }
+
+
+    public static void testFixedMembershipTokenIPv4() throws Exception {
+        FixedMembershipToken tok=new FixedMembershipToken();
+        tok.setMemberList("192.168.1.6,10.1.1.1/7500,localhost/7800");
+        assert !tok.isInMembersList(new IpAddress("192.168.1.3", 7500));
+        assert !tok.isInMembersList(new IpAddress("10.1.1.1", 7000));
+        assert tok.isInMembersList(new IpAddress("10.1.1.1", 7500));
+        assert tok.isInMembersList(new IpAddress("192.168.1.6", 7500)); // port is not matched
+        assert tok.isInMembersList(new IpAddress("192.168.1.6", 0));    // port is not matched
+    }
+
+
+    public static void testFixedMembershipTokenIPv6() throws Exception {
+        FixedMembershipToken tok=new FixedMembershipToken();
+        tok.setMemberList("fe80::aa20:66ff:fe11:d346,2a02:120b:2c45:1b70:aa20:66ff:fe11:d346/7500,2a02:120b:2c45:1b70:f474:e6ca:3038:6b5f/7500");
+        assert tok.isInMembersList(new IpAddress("2a02:120b:2c45:1b70:f474:e6ca:3038:6b5f", 7500));
     }
 
 
