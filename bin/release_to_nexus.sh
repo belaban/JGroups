2a4ac68032f03fd6530a8dffad349f512580180b
Bela Ban <belaban@yahoo.com>
2015-06-17 13:16:41 +0200

    Uploading of artifacts to Nexus is now done through maven: mvn deploy -Dmaven.test.skip=true

--- bin/release_to_nexus.sh
+++ bin/release_to_nexus.sh
@@ -1,31 +1,36 @@
 #!/bin/bash
 
 
-# Uploads the artifacts in ./dist (JAR and src JAR) to the Nexus Maven repo at repository.jboss.org/nexus
-# The artifacts will be in the staging repo, go to repository.jboss.org/nexus and promote them to the releases repo in
-# the next step
+##
+## This script releases a new JGroups version to Nexus. It generates the local artifacts (in target/staging), uploads
+## them to Nexus staging, the closes and releases the upload.
+## Note that if there is an existing artifact in Nexus, the script will fail !
+##
 
 # Author: Bela Ban
 
 
-DIST=../dist
+# DIST=../dist
 POM=../pom.xml
 
-JAR=`find $DIST -name "jgroups-*.jar" | grep -v source`
-SRC_JAR=`find $DIST -name "jgroups-*.jar" | grep source`
+# JAR=`find $DIST -name "jgroups-*.jar" | grep -v source`
+# SRC_JAR=`find $DIST -name "jgroups-*.jar" | grep source`
 
 ## Release directly, skipping the staging repo
 ## REPO=https://repository.jboss.org/nexus/content/repositories/releases/
 
-REPO=https://repository.jboss.org/nexus/service/local/staging/deploy/maven2
-FLAGS="-Dpackaging=jar -DrepositoryId=jboss-releases-repository"
+# REPO=https://repository.jboss.org/nexus/service/local/staging/deploy/maven2
+# FLAGS="-Dpackaging=jar -DrepositoryId=jboss-releases-repository"
 
 
-echo "Deploying $JAR to $REPO"
-mvn deploy:deploy-file -Dfile=$JAR -Durl=$REPO -DpomFile=$POM $FLAGS
+# echo "Deploying $JAR to $REPO"
+# mvn deploy:deploy-file -Dfile=$JAR -Durl=$REPO -DpomFile=$POM $FLAGS
 
 
-echo "Deploying $SRC_JAR to $REPO"
-mvn deploy:deploy-file -Dfile=$SRC_JAR -Durl=$REPO -DpomFile=$POM -Dclassifier=sources $FLAGS
+# echo "Deploying $SRC_JAR to $REPO"
+# mvn deploy:deploy-file -Dfile=$SRC_JAR -Durl=$REPO -DpomFile=$POM -Dclassifier=sources $FLAGS
 
+
+
+mvn -DpomFile=$POM -Dmaven.test.skip=true deploy
 
