58a83b8a9b3b10efcdb1228d93515dfa575a3832
Bela Ban <belaban@yahoo.com>
2015-06-22 18:50:16 +0200

    Added NioServer / NioClient to measure perf of direct versus heap based buffers

---
+++ tests/other/org/jgroups/tests/NioClient.java
@@ -0,0 +1,130 @@
+package org.jgroups.tests;
+
+import org.jgroups.util.Util;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * NIO based client for measuring heap-based vs direct byte buffers. Use {@link NioServer} as server
+ * @author Bela Ban
+ * @since  3.6.4
+ */
+public class NioClient {
+    protected volatile boolean    running=true;
+    protected final AtomicLong    total_bytes_sent=new AtomicLong(0);
+    protected final AtomicLong    total_msgs=new AtomicLong(0);
+    protected Sender[]            senders;
+
+
+
+    protected static ByteBuffer create(int size, boolean direct) {
+        return direct? ByteBuffer.allocateDirect(size) : ByteBuffer.allocate(size);
+    }
+
+    protected void start(InetAddress host, boolean direct, int num_threads) throws Exception {
+        boolean looping=true;
+        while(looping) {
+            int c=Util.keyPress("[1] send [x] exit");
+            switch(c) {
+                case '1':
+                    sendMessages(host, direct, num_threads);
+                    break;
+                case 'x':
+                    looping=false;
+                    break;
+            }
+        }
+    }
+
+
+    protected void sendMessages(InetAddress host, boolean direct, int num_threads) throws Exception {
+        total_msgs.set(0);
+        total_bytes_sent.set(0);
+        senders=new Sender[num_threads];
+        final CountDownLatch latch=new CountDownLatch(1);
+        for(int i=0; i < senders.length; i++)
+            senders[i]=new Sender(host, direct, latch);
+        for(Sender sender: senders)
+            sender.start();
+        latch.countDown();
+        for(Sender sender: senders)
+            sender.join();
+    }
+
+
+
+    protected class Sender extends Thread {
+        protected SocketChannel        ch;
+        protected final CountDownLatch latch;
+        protected final InetAddress    host;
+        protected final boolean        direct;
+        protected final ByteBuffer     buf;
+
+        public Sender(InetAddress host, boolean direct, CountDownLatch latch) {
+            this.latch=latch;
+            this.host=host;
+            this.direct=direct;
+            buf=create(NioServer.SIZE, direct);
+        }
+
+        public void run() {
+             try {
+                 ch=SocketChannel.open();
+                 ch.configureBlocking(true); // we want blocking behavior
+                 ch.connect(new InetSocketAddress(host, 7500));
+                 latch.await();
+             }
+             catch(Exception e) {
+                e.printStackTrace();
+            }
+            for(;;) {
+                if(total_bytes_sent.addAndGet(NioServer.SIZE) > NioServer.BYTES_TO_SEND)
+                    break;
+                buf.rewind();
+                try {
+                    ch.write(buf);
+                    total_msgs.incrementAndGet();
+                }
+                catch(IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            Util.close(ch);
+        }
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        String host="localhost";
+        boolean direct=false;
+        int num_threads=5;
+
+        for(int i=0; i < args.length; i++) {
+            if(args[i].equals("-host")) {
+                host=args[++i];
+                continue;
+            }
+            if(args[i].equals("-direct")) {
+                direct=Boolean.parseBoolean(args[++i]);
+                continue;
+            }
+            if(args[i].equals("-num_threads")) {
+                num_threads=Integer.parseInt(args[++i]);
+                continue;
+            }
+            System.out.println("NioClient [-host host] [-direct true|false] [-num_threads num]");
+            return;
+        }
+
+        new NioClient().start(InetAddress.getByName(host), direct, num_threads);
+    }
+
+
+}
---
+++ tests/other/org/jgroups/tests/NioServer.java
@@ -0,0 +1,122 @@
+package org.jgroups.tests;
+
+import org.jgroups.util.Util;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * NIO based server for measuring heap-based vs direct byte buffers. Use {@link NioClient} as client test driver
+ * @author Bela Ban
+ * @since  3.6.4
+ */
+public class NioServer {
+    protected ServerSocketChannel ch;
+    protected Selector            selector;
+    protected volatile boolean    running=true;
+    protected ByteBuffer          buffer;
+    protected final AtomicLong    total_bytes_received=new AtomicLong(0);
+    protected final AtomicLong    total_msgs=new AtomicLong(0);
+    protected final AtomicLong    start=new AtomicLong(0);
+
+    public static final long      BYTES_TO_SEND=10_000_000;
+    public static final int       SIZE=1000;
+
+
+    protected static ByteBuffer create(int size, boolean direct) {
+        return direct? ByteBuffer.allocateDirect(size) : ByteBuffer.allocate(size);
+    }
+
+    protected void start(boolean direct) throws Exception {
+        selector=Selector.open();
+
+        ch=ServerSocketChannel.open();
+        ch.bind(new InetSocketAddress("0.0.0.0", 7500));
+        ch.configureBlocking(false);
+        ch.register(selector, SelectionKey.OP_ACCEPT, null);
+        System.out.println("-- server ready");
+
+        while(running) {
+            selector.select();
+            Set<SelectionKey> keys=selector.selectedKeys();
+            for(Iterator<SelectionKey> it=keys.iterator(); it.hasNext();) {
+                SelectionKey key=it.next();
+                if(!key.isValid()) {
+                    it.remove();
+                    continue;
+                }
+                it.remove();
+                if(key.isAcceptable()) {
+                    SocketChannel client_ch=ch.accept();
+                    System.out.printf("accepted connection from %s\n", client_ch.getRemoteAddress());
+                    client_ch.configureBlocking(false);
+                    client_ch.register(selector, SelectionKey.OP_READ, create(SIZE, direct));
+                }
+                else if(key.isReadable()) {
+                    if(!handle((SocketChannel)key.channel(), (ByteBuffer)key.attachment())) {
+                        key.cancel();
+                        Util.close(key.channel());
+                    }
+                }
+            }
+        }
+
+        Util.close(selector,ch);
+    }
+
+
+    protected boolean handle(SocketChannel ch, ByteBuffer buf) {
+        try {
+            if(start.get() == 0)
+                start.compareAndSet(0, System.currentTimeMillis());
+            int num=ch.read(buf);
+            if(num < 0)
+                return false;
+            total_bytes_received.addAndGet(num);
+            if(!buf.hasRemaining()) {
+                total_msgs.incrementAndGet();
+                buf.rewind();
+            }
+            if(total_bytes_received.get() >= BYTES_TO_SEND) {
+                long time=System.currentTimeMillis() - start.get();
+                double throughput_sec=total_bytes_received.get() / (time / 1000.0), msgs_sec=total_msgs.get() / (time / 1000.0);
+                System.out.printf("\n===========================\nreceived %d messages in %.2f secs: throughput: %s/sec, %.2f msgs/sec\n",
+                                  total_msgs.get(), time/1000.0, Util.printBytes(throughput_sec), msgs_sec);
+                start.set(0);
+                total_bytes_received.set(0);
+                total_msgs.set(0);
+                return false;
+            }
+        }
+        catch(IOException e) {
+            e.printStackTrace();
+        }
+        return true;
+    }
+
+
+
+    public static void main(String[] args) throws Exception {
+        boolean direct=false;
+        for(int i=0; i < args.length; i++) {
+            if(args[i].equals("-direct")) {
+                direct=Boolean.parseBoolean(args[++i]);
+                continue;
+            }
+            System.out.println("NioServer [-direct true|false]");
+            return;
+        }
+
+        new NioServer().start(direct);
+    }
+
+
+}
