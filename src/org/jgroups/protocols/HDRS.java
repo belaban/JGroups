ca426ab3188bf27506e31344bdcae73c12bf6208
Bela Ban <belaban@yahoo.com>
2015-09-07 12:31:46 +0200

    Revamped HDRS

--- src/org/jgroups/protocols/HDRS.java
+++ src/org/jgroups/protocols/HDRS.java
@@ -3,67 +3,45 @@ package org.jgroups.protocols;
 
 import org.jgroups.Event;
 import org.jgroups.Message;
-import org.jgroups.Header;
-import org.jgroups.conf.ClassConfigurator;
-import org.jgroups.annotations.Unsupported;
+import org.jgroups.annotations.MBean;
+import org.jgroups.annotations.Property;
 import org.jgroups.stack.Protocol;
 import org.jgroups.util.MessageBatch;
 
-import java.util.Map;
-
 
 /**
- * Example of a protocol layer. Contains no real functionality, can be used as a template.
+ * Prints the headers of all sent or received messages
  */
-@Unsupported
+@MBean(description="Prints the headers of all sent and/or received messages")
 public class HDRS extends Protocol {
+    @Property(description="Enables printing of down messages")
+    protected volatile boolean print_down=true;
 
-    private static void printMessage(Message msg, String label) {
-        StringBuilder sb=new StringBuilder();
-        sb.append(label).append(":\n");
-        Map<Short, Header> hdrs=msg.getHeaders();
-        sb.append(print(msg, hdrs));
-        System.out.println(sb);
-    }
-
-    private static String print(Message msg, Map<Short, Header> hdrs) {
-        StringBuilder sb=new StringBuilder();
-        int hdrs_size=0;
-        for(Map.Entry<Short,Header> entry: hdrs.entrySet()) {
-            Class clazz=ClassConfigurator.getProtocol(entry.getKey());
-            String name=clazz != null? clazz.getSimpleName() : null;
-            Header hdr=entry.getValue();
-            int size=hdr.size();
-            hdrs_size+=size;
-            sb.append(name).append(": ").append(" ").append(size).append(" bytes\n");
-        }
-        sb.append("headers=").append(hdrs_size).append(", total msg size=").append(msg.size());
-        sb.append(", msg payload=").append(msg.getLength()).append("\n");
-        return sb.toString();
-    }
-
-
+    @Property(description="Enables printing of up (received) messages")
+    protected volatile boolean print_up=true;
 
 
     public Object up(Event evt) {
-        if(evt.getType() == Event.MSG) {
+        if(print_up && evt.getType() == Event.MSG) {
             Message msg=(Message)evt.getArg();
-            printMessage(msg, "up");
+            System.out.printf("-- [s] from %s (%d bytes): %s\n", msg.src(), msg.getLength(), msg.printHeaders());
         }
         return up_prot.up(evt); // Pass up to the layer above us
     }
 
     public void up(MessageBatch batch) {
-        for(Message msg: batch)
-            printMessage(msg, "up");
+        if(print_up) {
+            for(Message msg : batch)
+                System.out.printf("-- [b] from %s (%d bytes): %s\n", msg.src(), msg.getLength(), msg.printHeaders());
+        }
         if(!batch.isEmpty())
             up_prot.up(batch);
     }
 
     public Object down(Event evt) {
-        if(evt.getType() == Event.MSG) {
+        if(print_down && evt.getType() == Event.MSG) {
             Message msg=(Message)evt.getArg();
-            printMessage(msg, "down");
+            System.out.printf("-- to %s (%d bytes): %s\n", msg.dest(), msg.getLength(), msg.printHeaders());
         }
 
         return down_prot.down(evt);  // Pass on to the layer below us
