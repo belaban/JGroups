72c2921c86add32cdd1a2a5d64c76bd0d48e7523
Bela Ban <belaban@yahoo.com>
2016-05-23 13:24:40 +0200

    ns

--- src/org/jgroups/stack/Protocol.java
+++ src/org/jgroups/stack/Protocol.java
@@ -380,19 +380,7 @@ public abstract class Protocol {
         return up_prot.up(evt);
     }
 
-    /**
-     * Called by the default implementation of {@link #up(org.jgroups.util.MessageBatch)} for each message to determine
-     * if the message should be removed from the message batch (and handled by the current protocol) or not.
-     * @param msg The message. Guaranteed to be non-null
-     * @return True if the message should be handled by this protocol (will be removed from the batch), false if the
-     * message should remain in the batch and be passed up.<p/>
-     * The default implementation tries to find a header matching the current protocol's ID and returns true if there
-     * is a match, or false otherwise
-     */
-    protected boolean accept(Message msg) {
-        short tmp_id=getId();
-        return tmp_id > 0 && msg.getHeader(tmp_id) != null;
-    }
+
 
 
     /**
@@ -440,6 +428,18 @@ public abstract class Protocol {
     }
 
 
-
+    /**
+     * Called by the default implementation of {@link #up(org.jgroups.util.MessageBatch)} for each message to determine
+     * if the message should be removed from the message batch (and handled by the current protocol) or not.
+     * @param msg The message. Guaranteed to be non-null
+     * @return True if the message should be handled by this protocol (will be removed from the batch), false if the
+     * message should remain in the batch and be passed up.<p/>
+     * The default implementation tries to find a header matching the current protocol's ID and returns true if there
+     * is a match, or false otherwise
+     */
+    protected boolean accept(Message msg) {
+        short tmp_id=getId();
+        return tmp_id > 0 && msg.getHeader(tmp_id) != null;
+    }
 
 }
