cb21ee1eaec8486f5870d960c76406dc990b6e67
Bela Ban <belaban@yahoo.com>
2016-06-01 16:50:14 +0200

    Changed version to 3.2.16.Final

--- src/org/jgroups/Version.java
+++ src/org/jgroups/Version.java
@@ -21,7 +21,7 @@ import org.jgroups.annotations.Immutable;
 public class Version {
     public static final short  major = 3;
     public static final short  minor = 2;
-    public static final short  micro = 15;
+    public static final short  micro = 16;
     public static final String description=major + "." + minor + "." + micro + ".Final";
 
     public static final short  version=encode(major, minor, micro);
