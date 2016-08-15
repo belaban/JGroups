71a5c55baf28aa42c8dd651dff6a7b8b20603849
Bela Ban <belaban@yahoo.com>
2016-08-15 11:47:22 +0200

    Changed trace format

--- src/org/jgroups/protocols/tom/TOA.java
+++ src/org/jgroups/protocols/tom/TOA.java
@@ -138,7 +138,7 @@ public class TOA extends Protocol implements DeliveryProtocol {
         message.setDest(localAddress);
 
         if (log.isTraceEnabled()) {
-            log.trace("Deliver message %s (%d) in total order", message, message.getHeader(id));
+            log.trace("Deliver message %s (%s) in total order", message, message.getHeader(id));
         }
 
         up_prot.up(message);
