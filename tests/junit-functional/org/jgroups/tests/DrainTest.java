fea7759614c6572fd46dca0462bd4344981f70f8
Bela Ban <belaban@yahoo.com>
2017-01-03 14:01:38 +0100

    Added unit tests for https://issues.jboss.org/browse/JGRP-2150

---
+++ tests/junit-functional/org/jgroups/tests/DrainTest.java
@@ -0,0 +1,105 @@
+package org.jgroups.tests;
+
+import org.jgroups.Global;
+import org.jgroups.Message;
+import org.jgroups.util.AverageMinMax;
+import org.jgroups.util.Util;
+import org.testng.annotations.Test;
+
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.LongAdder;
+
+/**
+ * @author Bela Ban
+ * @since  4.0
+ */
+@Test(groups=Global.FUNCTIONAL,singleThreaded=true)
+public class DrainTest {
+    protected final Queue<Message> queue=new ArrayBlockingQueue<Message>(50000);
+    protected final AtomicInteger  counter=new AtomicInteger(0);
+    protected final LongAdder      added=new LongAdder();
+    protected final LongAdder      removed=new LongAdder();
+    protected final LongAdder      num_removers=new LongAdder();
+    protected final AverageMinMax  avg_removed=new AverageMinMax();
+
+    public void testDraining() throws InterruptedException {
+        MyThread[] threads=new MyThread[50];
+        final CountDownLatch latch=new CountDownLatch(1);
+        for(int i=0; i < threads.length; i++) {
+            threads[i]=new MyThread(latch);
+            threads[i].start();
+        }
+        latch.countDown();
+
+        Util.sleep(5000);
+
+        System.out.printf("\nStopping threads\n");
+        for(MyThread thread: threads)
+            thread.cancel();
+        System.out.printf("done, joining threads\n");
+        for(MyThread thread: threads)
+            thread.join();
+
+        System.out.printf("\ncounter=%d, added=%d, removed=%d, avg_removed=%s (removers=%d)\n",
+                          counter.get(), added.sum(), removed.sum(), avg_removed, num_removers.sum());
+
+        assert added.sum() == removed.sum();
+        assert counter.get() == 0;
+    }
+
+    protected void add(Message msg) {
+        if(queue.offer(msg))
+            added.increment();
+        drain();
+    }
+
+    protected void drain() {
+        if(counter.getAndIncrement() == 0) {
+            num_removers.increment();
+            int cnt=0;
+            do {
+                Message msg=queue.poll();
+                if(msg == null)
+                    continue; // System.err.printf("got empty message");
+                removed.increment();
+                cnt++;
+            } while(counter.decrementAndGet() != 0);
+            avg_removed.add(cnt);
+        }
+    }
+
+    protected class MyThread extends Thread {
+        protected final CountDownLatch latch;
+        protected volatile boolean     running=true;
+
+        public MyThread(CountDownLatch latch) {
+            this.latch=latch;
+        }
+
+        protected void cancel() {running=false;}
+
+        public void run() {
+            try {
+                latch.await();
+            }
+            catch(InterruptedException e) {
+                e.printStackTrace();
+            }
+            while(running) {
+                Message msg=new Message();
+                add(msg);
+            }
+        }
+    }
+
+    protected Message[] create(int max) {
+        int num=(int)Util.random(max);
+        Message[] msgs=new Message[num];
+        for(int i=0; i < msgs.length; i++)
+            msgs[i]=new Message();
+        return msgs;
+    }
+}
---
+++ tests/junit-functional/org/jgroups/tests/MessageBatchDrainTest.java
@@ -0,0 +1,155 @@
+package org.jgroups.tests;
+
+import org.jgroups.Global;
+import org.jgroups.Message;
+import org.jgroups.util.AverageMinMax;
+import org.jgroups.util.MessageBatch;
+import org.jgroups.util.Util;
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.LongAdder;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @author Bela Ban
+ * @since  4.0
+ */
+@Test(groups=Global.FUNCTIONAL,singleThreaded=true)
+public class MessageBatchDrainTest {
+    protected final Lock           lock=new ReentrantLock();
+    protected final MessageBatch   batch=new MessageBatch(16);
+    protected final AtomicInteger  counter=new AtomicInteger(0);
+    protected final LongAdder      added=new LongAdder();
+    protected final LongAdder      removed=new LongAdder();
+    protected final LongAdder      num_removers=new LongAdder();
+    protected final AverageMinMax  avg_removed=new AverageMinMax();
+    protected final AverageMinMax  avg_remove_loops=new AverageMinMax();
+
+    public void testDraining() throws InterruptedException {
+        MyThread[] threads=new MyThread[10];
+        final CountDownLatch latch=new CountDownLatch(1);
+        for(int i=0; i < threads.length; i++) {
+            threads[i]=new MyThread(latch);
+            threads[i].start();
+        }
+        latch.countDown();
+
+        Util.sleep(5000);
+
+        System.out.printf("\nStopping threads\n");
+        for(MyThread thread: threads)
+            thread.cancel();
+        System.out.printf("done, joining threads\n");
+        for(MyThread thread: threads)
+            thread.join();
+
+        System.out.printf("\ncounter=%d, added=%d, removed=%d, avg_removed=%s, avg_remove_loops=%s (removers=%d)\n",
+                          counter.get(), added.sum(), removed.sum(), avg_removed, avg_remove_loops, num_removers.sum());
+
+        assert added.sum() == removed.sum();
+        assert counter.get() == 0;
+    }
+
+    protected void add(Message msg) {
+        _add(msg);
+        added.increment();
+        drain(1);
+    }
+
+    protected void add(MessageBatch b) {
+        int size=b.size();
+        if(size == 0) return;
+        _add(b);
+        added.add(size);
+        drain(size);
+    }
+
+    protected void drain(int num) {
+        if(counter.getAndAdd(num) == 0) {
+            num_removers.increment();
+            int cnt=0, removed_msgs, total_removed=0;
+            do {
+                removed_msgs=_clear();
+                total_removed+=removed_msgs;
+                removed.add(removed_msgs);
+                cnt++;
+                // LockSupport.parkNanos(4_000);
+            } while(counter.addAndGet(-removed_msgs) != 0);
+            avg_remove_loops.add(cnt);
+            avg_removed.add(total_removed);
+        }
+    }
+
+    protected void _add(Message msg) {
+        lock.lock();
+        try {
+            this.batch.add(msg);
+        }
+        finally {
+            lock.unlock();
+        }
+    }
+
+    protected void _add(MessageBatch b) {
+        lock.lock();
+        try {
+            this.batch.add(b);
+        }
+        finally {
+            lock.unlock();
+        }
+    }
+
+    protected int _clear() {
+        lock.lock();
+        try {
+            int size=batch.size();
+            batch.clear();
+            return size;
+        }
+        finally {
+            lock.unlock();
+        }
+    }
+
+    protected class MyThread extends Thread {
+        protected final CountDownLatch latch;
+        protected volatile boolean     running=true;
+
+        public MyThread(CountDownLatch latch) {
+            this.latch=latch;
+        }
+
+        protected void cancel() {running=false;}
+
+        public void run() {
+            try {
+                latch.await();
+            }
+            catch(InterruptedException e) {
+                e.printStackTrace();
+            }
+            while(running) {
+                if(Util.tossWeightedCoin(.3))
+                    add(new Message());
+                else {
+                    Message[] msgs=create(10);
+                    MessageBatch mb=new MessageBatch(Arrays.asList(msgs));
+                    add(mb);
+                }
+            }
+        }
+    }
+
+    protected Message[] create(int max) {
+        int num=(int)Util.random(max);
+        Message[] msgs=new Message[num];
+        for(int i=0; i < msgs.length; i++)
+            msgs[i]=new Message();
+        return msgs;
+    }
+}
