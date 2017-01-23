a491a2a0cce506983cbbbd9bc216f9e7103f7ee9
Bela Ban <belaban@yahoo.com>
2017-01-23 09:51:18 +0100

    pp

--- src/org/jgroups/protocols/UDP.java
+++ src/org/jgroups/protocols/UDP.java
@@ -186,7 +186,7 @@ public class UDP extends TP {
     }
 
     @ManagedAttribute(description="Number of messages dropped when sending because of insufficient buffer space")
-    public int getDroppedMessage() {
+    public int getDroppedMessages() {
         return suppress_log_out_of_buffer_space != null? suppress_log_out_of_buffer_space.getCache().size() : 0;
     }
 
