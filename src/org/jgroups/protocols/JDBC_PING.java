b1c6839c85f8e2eaaca36b3c45732be90a35e45d
Douglas Adams <douglasryanadams@gmail.com>
2017-06-19 14:35:07 -0700

    More aligned with FILE_PING
    FILE_PING does this:
    
    ```java
        public void stop() {
            super.stop();
            stopInfoWriter();
            remove(cluster_name, local_addr);
        }
    
        protected void remove(String clustername, Address addr) {
            if(clustername == null || addr == null)
                return;
    
            File dir=new File(root_dir, clustername);
            if(!dir.exists())
                return;
    
            log.debug("remove %s", clustername);
    
            String filename=addressToFilename(addr);
            File file=new File(dir, filename);
            deleteFile(file);
        }
    ```

--- src/org/jgroups/protocols/JDBC_PING.java
+++ src/org/jgroups/protocols/JDBC_PING.java
@@ -113,7 +113,7 @@ public class JDBC_PING extends FILE_PING {
     @Override
     public void stop() {
         super.stop();
-        if (is_coord) removeAll(cluster_name);
+        removeAll(cluster_name);
     }
 
     protected void write(List<PingData> list, String clustername) {
