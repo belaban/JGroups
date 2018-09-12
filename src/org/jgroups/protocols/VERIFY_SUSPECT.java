0a52837df5e17c45afa743b32c868c4a2c877e29
Paul Ferraro <paul.ferraro@redhat.com>
2018-09-12 11:08:26 -0400

    JGRP-2286 VERIFY_SUSPECT can return false positives if member is killed and immediately restarted

--- src/org/jgroups/protocols/VERIFY_SUSPECT.java
+++ src/org/jgroups/protocols/VERIFY_SUSPECT.java
@@ -87,6 +87,7 @@ public class VERIFY_SUSPECT extends Protocol implements Runnable {
         return timeout;
     }
 
+    @Override
     public Object down(Event evt) {
         switch(evt.getType()) {
             case Event.SET_LOCAL_ADDRESS:
@@ -100,6 +101,7 @@ public class VERIFY_SUSPECT extends Protocol implements Runnable {
         return down_prot.down(evt);
     }
 
+    @Override
     public Object up(Event evt) {
         switch(evt.getType()) {
 
@@ -124,29 +126,29 @@ public class VERIFY_SUSPECT extends Protocol implements Runnable {
         return up_prot.up(evt);
     }
 
+    @Override
     public Object up(Message msg) {
         VerifyHeader hdr=msg.getHeader(this.id);
         if(hdr == null)
             return up_prot.up(msg);
         switch(hdr.type) {
             case VerifyHeader.ARE_YOU_DEAD:
-                if(hdr.from == null) {
-                    log.error(Util.getMessage("AREYOUDEADHdrFromIsNull"));
-                    return null;
-                }
+                short type = hdr.suspected.equals(this.local_addr) ? VerifyHeader.I_AM_NOT_DEAD : VerifyHeader.MBR_IS_DEAD;
                 Address target=use_mcast_rsps? null : hdr.from;
                 for(int i=0; i < num_msgs; i++) {
                     Message rsp=new Message(target).setFlag(Message.Flag.INTERNAL)
-                      .putHeader(this.id, new VerifyHeader(VerifyHeader.I_AM_NOT_DEAD, local_addr));
+                      .putHeader(this.id, new VerifyHeader(type, hdr.suspected, local_addr));
                     down_prot.down(rsp);
                 }
                 return null;
             case VerifyHeader.I_AM_NOT_DEAD:
-                if(hdr.from == null) {
-                    log.error(Util.getMessage("IAMNOTDEADHdrFromIsNull"));
-                    return null;
-                }
-                unsuspect(hdr.from);
+                log.debug("Received %s response from suspected member: %s", hdr, hdr.suspected);
+                unsuspect(hdr.suspected);
+                return null;
+            case VerifyHeader.MBR_IS_DEAD:
+                log.debug("Received %s response from %s on behalf of suspected member: %s", hdr, hdr.from, hdr.suspected);
+                removeSuspect(hdr.suspected);
+                up_prot.up(new Event(Event.SUSPECT, Collections.singleton(hdr.suspected)));
                 return null;
         }
         return null;
@@ -167,6 +169,7 @@ public class VERIFY_SUSPECT extends Protocol implements Runnable {
      * it and removes a suspect when the timeout for it has elapsed. Sends up a SUSPECT event for every removed suspect.
      * When a suspected member is un-suspected, the member is removed from the queue.
      */
+    @Override
     public void run() {
         while(!suspects.isEmpty() && timer != null) {
             try {
@@ -206,7 +209,7 @@ public class VERIFY_SUSPECT extends Protocol implements Runnable {
         for(Address mbr: mbrs) {
             for(int i=0; i < num_msgs; i++) {
                 Message msg=new Message(mbr).setFlag(Message.Flag.INTERNAL)
-                  .putHeader(this.id, new VerifyHeader(VerifyHeader.ARE_YOU_DEAD, local_addr));
+                  .putHeader(this.id, new VerifyHeader(VerifyHeader.ARE_YOU_DEAD, mbr, local_addr));
                 down_prot.down(msg);
             }
         }
@@ -278,6 +281,7 @@ public class VERIFY_SUSPECT extends Protocol implements Runnable {
         }
     }
 
+    @Override
     public void init() throws Exception {
         super.init();
         if(bind_addr != null)
@@ -285,7 +289,7 @@ public class VERIFY_SUSPECT extends Protocol implements Runnable {
     }
 
 
-
+    @Override
     public synchronized void stop() {
         Thread tmp;
         if(timer != null && timer.isAlive()) {
@@ -327,51 +331,58 @@ public class VERIFY_SUSPECT extends Protocol implements Runnable {
     public static class VerifyHeader extends Header {
         static final short ARE_YOU_DEAD=1;  // 'from' is sender of verify msg
         static final short I_AM_NOT_DEAD=2;  // 'from' is suspected member
+        static final short MBR_IS_DEAD=3; // 'from' is sender of msg on behalf of suspected mbr
 
         short type=ARE_YOU_DEAD;
-        Address from;     // member who wants to verify that suspected_mbr is dead
-
+        Address suspected;
+        Address from;     // member who wants to verify that suspected mbr is dead
 
         public VerifyHeader() {
         } // used for externalization
 
-        VerifyHeader(short type) {
+        VerifyHeader(short type, Address suspected, Address from) {
             this.type=type;
-        }
-
-        VerifyHeader(short type, Address from) {
-            this(type);
+            this.suspected = suspected;
             this.from=from;
         }
 
+        @Override
         public short getMagicId() {return 54;}
 
+        @Override
         public Supplier<? extends Header> create() {return VerifyHeader::new;}
 
+        @Override
         public String toString() {
             switch(type) {
                 case ARE_YOU_DEAD:
                     return "[VERIFY_SUSPECT: ARE_YOU_DEAD]";
                 case I_AM_NOT_DEAD:
                     return "[VERIFY_SUSPECT: I_AM_NOT_DEAD]";
+                case MBR_IS_DEAD:
+                    return "[VERIFY_SUSPECT: MBR_IS_DEAD]";
                 default:
                     return "[VERIFY_SUSPECT: unknown type (" + type + ")]";
             }
         }
 
-
+        @Override
         public void writeTo(DataOutput out) throws Exception {
             out.writeShort(type);
+            Util.writeAddress(suspected, out);
             Util.writeAddress(from, out);
         }
 
+        @Override
         public void readFrom(DataInput in) throws Exception {
             type=in.readShort();
-            from=Util.readAddress(in);
+            suspected=Util.readAddress(in);
+            from = Util.readAddress(in);
         }
 
+        @Override
         public int serializedSize() {
-            return Global.SHORT_SIZE + Util.size(from);
+            return Global.SHORT_SIZE + Util.size(suspected) + Util.size(from);
         }
     }
 
