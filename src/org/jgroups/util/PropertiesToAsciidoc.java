95f7d3c2c9ce0c857c099400ea0a3e82030d261b
Bela Ban <belaban@mailbox.org>
2020-02-14 14:27:02 +0100

    Changed parsing and replacement of variables in manual

--- src/org/jgroups/util/PropertiesToAsciidoc.java
+++ src/org/jgroups/util/PropertiesToAsciidoc.java
@@ -41,28 +41,25 @@ public class PropertiesToAsciidoc {
             // first copy protocols.adoc file into protocols.adoc.xml
             File f = new File(temp_file);
             copy(new FileReader(new File(prot_file)), new FileWriter(f));
-            String s = fileToString(f);
 
             Set<Class<Protocol>> classes = Util.findClassesAssignableFrom("org.jgroups.protocols",Protocol.class);
             classes.addAll(Util.findClassesAssignableFrom("org.jgroups.protocols.pbcast",Protocol.class));
+            classes.addAll(Util.findClassesAssignableFrom("org.jgroups.protocols.dns",Protocol.class));
             classes.addAll(Util.findClassesAssignableFrom("org.jgroups.protocols.relay",Protocol.class));
             classes.addAll(Util.findClassesAssignableFrom("org.jgroups.protocols.rules",Protocol.class));
             classes.addAll(Util.findClassesAssignableFrom("org.jgroups.protocols.tom",Protocol.class));
             Properties props = new Properties();
-            for (Class<Protocol> clazz : classes)
+            for(Class<Protocol> clazz: classes)
                 convertProtocolToAsciidocTable(props,clazz);
-            String result = Util.substituteVariable(s, props);
-            FileWriter fw = new FileWriter(f, false);
-            fw.write(result);
-            fw.flush();
-            fw.close();
-            
-            
-            
+
+            try(InputStream in=new FileInputStream(prot_file); OutputStream out=new FileOutputStream(temp_file)) {
+                replaceVariables(in, out, props);
+            }
+
             // copy installation.adoc file into installation.adoc.tmp
             f=new File(temp_file2);
             copy(new FileReader(new File(inst_file)), new FileWriter(f));
-            s=fileToString(f);
+            String s=fileToString(f);
 
             props=new Properties();
             List<Class<?>> unsupportedClasses = Util.findClassesAnnotatedWith("org.jgroups",Unsupported.class);
@@ -70,8 +67,8 @@ public class PropertiesToAsciidoc {
             List<Class<?>> experimentalClasses = Util.findClassesAnnotatedWith("org.jgroups",Experimental.class);
             convertUnsupportedToAsciidocTable(props,experimentalClasses,"Experimental");
 
-            result=Util.substituteVariable(s,props);
-            fw=new FileWriter(f,false);
+            String result=Util.substituteVariable(s, props);
+            FileWriter fw=new FileWriter(f, false);
             fw.write(result);
             fw.flush();
             fw.close();
@@ -135,11 +132,9 @@ public class PropertiesToAsciidoc {
             }
         }
 
-        // iterate methods
         Method[] methods = clazz.getDeclaredMethods();
         for (Method method : methods) {
             if (method.isAnnotationPresent(Property.class)) {
-
                 Property annotation = method.getAnnotation(Property.class);
                 String desc = annotation.description();
 
@@ -154,7 +149,6 @@ public class PropertiesToAsciidoc {
             }
         }
 
-
         // do we have more than one property (superclass Protocol has only one property (stats))
         if (nameToDescription.isEmpty())
             return;
@@ -168,6 +162,93 @@ public class PropertiesToAsciidoc {
         props.put(clazz.getSimpleName(), tmp);
     }
 
+    /**
+     * Reads from the input stream and replaces occurrences of ${PROT} with p.get("PROT") and writes this to the
+     * output stream. If no value is found, then the ${PROT} will simple be omitted from the output.
+     * Escaped values of the form \${PROT} are not looked up and the value without the backslash will be written
+     * to the output stream.
+     */
+    protected static void replaceVariables(InputStream in, OutputStream out, Properties p) {
+        boolean looping=true;
+        while(looping) {
+            try {
+                int ch=in.read(), n1, n2;
+                if(ch == -1)
+                    break;
+                switch(ch) {
+                    case '\\':
+                        n1=in.read();
+                        n2=in.read();
+                        if(n1 == -1 || n2 == -1) {
+                            looping=false;
+                            if(n1 != -1)
+                                out.write(n1);
+                            break;
+                        }
+
+                        if(n1 == '$' && n2 == '{') {
+                            String s=readUntilBracket(in);
+                            out.write(n1);
+                            out.write(n2);
+                            out.write(s.getBytes());
+                            out.write('}');
+                        }
+                        else {
+                            out.write(ch);
+                            out.write(n1);
+                            out.write(n2);
+                        }
+                        break;
+                    case '$':
+                        n1=in.read();
+                        if(n1 == -1) {
+                            out.write(ch);
+                            looping=false;
+                        }
+                        else {
+                            if(n1 == '{') {
+                                String s=readUntilBracket(in);
+                                writeVarToStream(s, p, out);
+                            }
+                            else {
+                                out.write(ch);
+                                out.write(n1);
+                            }
+                        }
+                        break;
+                    default:
+                        out.write(ch);
+                }
+            }
+            catch(IOException e) {
+                break;
+            }
+        }
+        Util.close(in, out);
+    }
+
+
+    protected static void writeVarToStream(String var, Properties p, OutputStream out) throws IOException {
+        String val=(String)p.get(var);
+        if(val != null)
+            out.write(val.getBytes());
+    }
+
+    /** Reads until the next bracket '}' and returns the string excluding the bracket, or throws an exception if
+     * no bracket has been found */
+    protected static String readUntilBracket(InputStream in) throws IOException {
+        StringBuilder sb=new StringBuilder();
+        while(true) {
+            int ch=in.read();
+            switch(ch) {
+                case -1: throw new EOFException("no matching } found");
+                case '}':
+                    return sb.toString();
+                default:
+                    sb.append((char)ch);
+            }
+        }
+    }
 
     private static String fileToString(File f) throws Exception {
         StringWriter output = new StringWriter();
