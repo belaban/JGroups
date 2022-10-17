10483e6be0acf08465feb791f7c86b11f161ea17
Bela Ban <belaban@mailbox.org>
2022-10-17 12:51:45 +0200

    Changed number of iterations: 10_000 takes 29s... too high!

--- tests/junit-functional/org/jgroups/protocols/FD_SOCK2_Test.java
+++ tests/junit-functional/org/jgroups/protocols/FD_SOCK2_Test.java
@@ -1,12 +1,12 @@
 package org.jgroups.protocols;
 
-import java.util.logging.Logger;
-
 import org.jgroups.Global;
 import org.jgroups.JChannel;
 import org.jgroups.util.Util;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
+
+import java.util.logging.Logger;
 
 /**
  * Test for {@link FD_SOCK2} not closing its sockets within the {@link FD_SOCK2#stop()} call and subsequently failing
@@ -26,7 +26,7 @@ public class FD_SOCK2_Test {
                 new FD_SOCK2().setPortRange(1).setOffset(1))
         ).name("A");
 
-        for (int i = 0; i < 10_000; i++) {
+        for (int i = 0; i < 1_000; i++) {
             try {
                 a.connect(FD_SOCK2_Test.class.getSimpleName());
             } catch (Exception e) {
