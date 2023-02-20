b2a00a1f034483423d307003d68f7b31f9e0276c
Bela Ban <belaban@mailbox.org>
2023-02-20 11:53:13 +0100

    ns

--- src/org/jgroups/protocols/PerDestinationBundler.java
+++ src/org/jgroups/protocols/PerDestinationBundler.java
@@ -9,12 +9,16 @@ import org.jgroups.annotations.Property;
 import org.jgroups.conf.AttributeType;
 import org.jgroups.logging.Log;
 import org.jgroups.util.ByteArrayDataOutputStream;
+import org.jgroups.util.FastArray;
+import org.jgroups.util.Profiler;
 import org.jgroups.util.Util;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.atomic.LongAdder;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
@@ -24,7 +28,8 @@ import static org.jgroups.protocols.TP.MSG_OVERHEAD;
 
 /**
  * Queues messages per destination ('null' is a special destination), sending when the last sender thread to the same
- * destination returns or max_size has been reached.
+ * destination returns or max_size has been reached. This uses 1 thread per destination, so it won't scale to many
+ * cluster members (unless virtual threads are used).
  * <br/>
  * See https://issues.redhat.com/browse/JGRP-2639 for details.
  * @author Bela Ban
@@ -41,10 +46,6 @@ public class PerDestinationBundler implements Bundler {
       description="Maximum number of bytes for messages to be queued (per destination) until they are sent")
     protected int                           max_size=64000;
 
-    @Property(description="The maximum number of queued messages per destination. When the queue is full, a new " +
-      "batch will be sent")
-    protected int                           max_queue_size=128;
-
     @ManagedAttribute(description="Total number of messages sent (single and batches)",type=AttributeType.SCALAR)
     protected final LongAdder               total_msgs_sent=new LongAdder();
 
@@ -55,15 +56,11 @@ public class PerDestinationBundler implements Bundler {
     @ManagedAttribute(description="Number of batches sent",type=AttributeType.SCALAR)
     protected final LongAdder               num_batches_sent=new LongAdder();
 
+    @ManagedAttribute(description="Number of batches sent because no more messages were available",type=AttributeType.SCALAR)
+    protected final LongAdder               num_send_due_to_no_msgs=new LongAdder();
+
     @ManagedAttribute(description="Number of batches sent because the queue was full",type=AttributeType.SCALAR)
     protected final LongAdder               num_sends_due_to_max_size=new LongAdder();
-
-    @ManagedAttribute(description="Number of batches sent because the max number of messages has been " +
-      "reached (max_queue_size)", type=AttributeType.SCALAR)
-    protected final LongAdder               num_sends_due_to_full_queue=new LongAdder();
-
-    @ManagedAttribute(description="Number of batches sent because the last sender thread returned",type=AttributeType.SCALAR)
-    protected final LongAdder               num_sends_due_to_last_thread=new LongAdder();
 
     protected TP                            transport;
     protected Log                           log;
@@ -79,8 +76,6 @@ public class PerDestinationBundler implements Bundler {
     public int     getQueueSize()         {return -1;}
     public int     getMaxSize()           {return max_size;}
     public Bundler setMaxSize(int s)      {this.max_size=s; return this;}
-    public int     getMaxQueueSize()      {return max_queue_size;}
-    public Bundler setMaxQueueSize(int s) {this.max_queue_size=s; return this;}
 
     @ManagedAttribute(description="Average number of messages in an BatchMessage")
     public double avgBatchSize() {
@@ -91,8 +86,11 @@ public class PerDestinationBundler implements Bundler {
     }
 
     @Override public void resetStats() {
-        Stream.of(total_msgs_sent, num_batches_sent, num_sends_due_to_max_size,
-                  num_sends_due_to_full_queue, num_sends_due_to_last_thread).forEach(LongAdder::reset);
+        Stream.of(total_msgs_sent, num_batches_sent, num_single_msgs_sent, num_sends_due_to_max_size)
+          .forEach(LongAdder::reset);
+        p_send_msg_list.reset();
+        p_send_single_msg.reset();
+        p_send_msgs.reset();
     }
 
     public void init(TP transport) {
@@ -102,18 +100,19 @@ public class PerDestinationBundler implements Bundler {
 
     public void start() {
         local_addr=Objects.requireNonNull(transport.getAddress());
+        dests.values().forEach(SendBuffer::start);
     }
 
     public void stop() {
-
+        dests.values().forEach(SendBuffer::stop);
     }
 
     public void send(Message msg) throws Exception {
         if(msg.getSrc() == null)
             msg.setSrc(local_addr);
         Address dest=msg.dest() == null ? NULL : msg.dest();
-        SendBuffer buf=dests.computeIfAbsent(dest, k -> new SendBuffer());
-        buf.addMessage(dest, msg);
+        SendBuffer buf=dests.computeIfAbsent(dest, k -> new SendBuffer(dest).start());
+        buf.send(msg);
     }
 
     public void viewChange(View view) {
@@ -121,7 +120,7 @@ public class PerDestinationBundler implements Bundler {
         if(mbrs == null) return;
 
         mbrs.stream().filter(dest -> !dests.containsKey(dest))
-          .forEach(dest -> dests.putIfAbsent(dest, new SendBuffer()));
+          .forEach(dest -> dests.putIfAbsent(dest, new SendBuffer(dest).start()));
 
         // remove left members
         dests.keySet().stream().filter(dest -> !mbrs.contains(dest) && !(dest == NULL))
@@ -129,91 +128,159 @@ public class PerDestinationBundler implements Bundler {
     }
 
 
-    protected class SendBuffer {
-        private final Message[]                 msgs;
-        private int                             index;
-        private long                            total_bytes;
-        private final AtomicInteger             thread_count=new AtomicInteger();
-        private final ByteArrayDataOutputStream output;
+    @ManagedAttribute final Profiler p_send_msg_list=new Profiler(),
+      p_send_single_msg=new Profiler(),
+      p_send_msgs=new Profiler();
+
+
+    protected class SendBuffer implements Runnable {
+        private final Address                   dest;
+        protected final FastArray<Message>      msgs=new FastArray<Message>(16).increment(10);
         private final Lock                      lock=new ReentrantLock(false);
+        private final BlockingQueue<Message>    queue=new ArrayBlockingQueue<>(8192);
+        private final List<Message>             remove_queue=new ArrayList<>(1024);
+        private final ByteArrayDataOutputStream output=new ByteArrayDataOutputStream(max_size + MSG_OVERHEAD);
+        private volatile Thread                 bundler_thread;
+        private volatile boolean                running=true;
+        private long                            count;
 
 
-        protected SendBuffer() {
-            this.msgs=new Message[max_queue_size];
-            this.index=0;
-            output=new ByteArrayDataOutputStream(max_size + MSG_OVERHEAD);
+        protected SendBuffer(Address dest) {
+            this.dest=dest;
         }
 
-        protected void addMessage(Address dest, Message msg) {
-            int msg_bytes=msg.getLength();
-            thread_count.incrementAndGet();
 
-            lock.lock();
-            try {
-                if(total_bytes + msg_bytes >= max_size) {
-                    num_sends_due_to_max_size.increment();
-                    sendBatch(dest); // will not throw an exception, just log a warning
+        public void run() {
+            while(running) {
+                Message msg=null;
+                try {
+                    if((msg=queue.take()) == null)
+                        continue;
+                    addAndSendIfSizeExceeded(msg);
+                    while(true) {
+                        remove_queue.clear();
+                        int num_msgs=queue.drainTo(remove_queue);
+                        if(num_msgs <= 0)
+                            break;
+                        for(int i=0; i < remove_queue.size(); i++) {
+                            msg=remove_queue.get(i);
+                            addAndSendIfSizeExceeded(msg);
+                        }
+                    }
+                    if(count > 0) {
+                        sendBundledMessages();
+                        num_send_due_to_no_msgs.increment();
+                    }
                 }
-
-                msgs[index++]=msg;
-                total_bytes+=msg_bytes;
-                if(index == msgs.length) {
-                    num_sends_due_to_full_queue.increment();
-                    sendBatch(dest);
+                catch(Throwable t) {
                 }
-
-                if(thread_count.decrementAndGet() == 0) {
-                    num_sends_due_to_last_thread.increment();
-                    sendBatch(dest); // will not throw an exception, just log a warning
-                }
-            }
-            finally {
-                lock.unlock();
             }
         }
 
-        protected void sendBatch(Address destination) {
-            if(index == 0)
+        protected void addAndSendIfSizeExceeded(Message msg) {
+            int size=msg.size();
+            if(count + size >= max_size) {
+                sendBundledMessages();
+                num_sends_due_to_max_size.increment();
+            }
+            addMessage(msg, size);
+        }
+
+        protected void addMessage(Message msg, int size) {
+            msgs.add(msg);
+            count+=size;
+        }
+
+        protected void sendBundledMessages() {
+            sendBatch(dest, msgs);
+            msgs.clear(false);
+            count=0;
+        }
+
+        public SendBuffer start() {
+            if(running)
+                stop();
+            bundler_thread=transport.getThreadFactory().newThread(this, "pd-bundler"); // new Thread(this, "pd-bundler");
+            running=true;
+            bundler_thread.start();
+            return this;
+        }
+
+        public void stop() {
+            running=false;
+            Thread tmp=bundler_thread;
+            if(tmp != null)
+                tmp.interrupt();
+        }
+
+
+        protected void send(Message msg) throws Exception {
+            queue.put(msg);
+        }
+
+
+        protected void sendBatch(Address destination, FastArray<Message> list) {
+            if(list.isEmpty()) // should never happen!
                 return;
-
-            Address dest=destination == NULL? null : destination;
-            if(index == 1) { // send a single message
-                sendSingleMessage(dest, msgs[0]);
-                num_single_msgs_sent.increment();
-            }
-            else {
-                sendMessageList(dest, local_addr, msgs, index);
-                num_batches_sent.increment();
-            }
-            total_msgs_sent.add(index);
-            total_bytes=0;
-            index=0;
+            Address dst=destination == NULL? null : destination;
+            sendMessages(dst, local_addr, list);
         }
 
         protected void sendSingleMessage(final Address dest, final Message msg) {
+            p_send_single_msg.start();
             try {
                 output.position(0);
                 Util.writeMessage(msg, output, dest == null);
                 transport.doSend(output.buffer(), 0, output.position(), dest);
                 if(transport.statsEnabled())
                     transport.getMessageStats().incrNumSingleMsgsSent(1);
+                num_single_msgs_sent.increment();
             }
             catch(Throwable e) {
                 log.error("%s: failed sending message to %s: %s", local_addr, dest, e);
             }
+            finally {
+                p_send_single_msg.stop();
+            }
         }
 
-        protected void sendMessageList(final Address dest, final Address src, final Message[] list, int length) {
+
+        protected void sendMessageList(final Address dest, final Address src, final FastArray<Message> list) {
+            p_send_msg_list.start();
+            output.position(0);
             try {
-                output.position(0);
-                Util.writeMessageList(dest, src, transport.cluster_name.chars(), list, 0,
-                                      length, output, dest == null, transport.getId());
+                Util.writeMessageList(dest, src, transport.cluster_name.chars(), list,
+                                      output, dest == null, transport.getId());
                 transport.doSend(output.buffer(), 0, output.position(), dest);
                 if(transport.statsEnabled())
                     transport.getMessageStats().incrNumBatchesSent(1);
+                num_batches_sent.increment();
             }
             catch(Throwable e) {
                 log.trace(Util.getMessage("FailureSendingMsgBundle"), transport.getAddress(), e);
+            }
+            finally {
+                p_send_msg_list.stop();
+            }
+        }
+
+        protected void sendMessages(final Address dest, final Address src, final FastArray<Message> list) {
+            p_send_msgs.start();
+            try {
+                int size=list.size();
+                if(size == 0)
+                    return;
+                if(size == 1)
+                    sendSingleMessage(dest, list.get(0));
+                else
+                    sendMessageList(dest, src, list);
+                total_msgs_sent.add(size);
+            }
+            catch(Throwable e) {
+                log.trace(Util.getMessage("FailureSendingMsgBundle"), transport.getAddress(), e);
+            }
+            finally {
+                p_send_msgs.stop();
             }
         }
 
@@ -224,7 +291,7 @@ public class PerDestinationBundler implements Bundler {
         protected int size() {
             lock.lock();
             try {
-                return index;
+                return msgs.size();
             }
             finally {
                 lock.unlock();
