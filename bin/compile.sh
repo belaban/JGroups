74dd150d0dd4ab80a950c439539e1ba155f8412a
Bela Ban <belaban@mailbox.org>
2023-11-21 09:45:51 +0100

    ns

--- bin/compile.sh
+++ bin/compile.sh
@@ -7,28 +7,13 @@ CLASSES=`dirname $0`/../classes
 CONF=`dirname $0`/../conf
 CLASSPATH="$CLASSES:$LIB/*"
 
-OPTIONS="-H:+JNI --no-server -H:+ReportExceptionStackTraces --features=org.graalvm.home.HomeFinderFeature"
-
-OPTIONS="$OPTIONS -H:+AllowVMInspection -H:TraceClassInitialization=true --no-fallback --allow-incomplete-classpath"
-
-OPTIONS="$OPTIONS -H:ReflectionConfigurationFiles=$CONF/reflection.json"
-
-# OPTIONS="$OPTIONS -H:+PrintAnalysisCallTree"
-
-
+OPTIONS="$OPTIONS --no-fallback"
 OPTIONS="$OPTIONS -Dgraal.CompilationFailureAction=Diagnose"
-
-# OPTIONS="$OPTIONS -H:IncludeResources=/home/bela/logging.properties -Dfoo=bar -Dcom.sun.management.jmxremote"
 
 #OPTIONS="$OPTIONS --debug-attach=*:8000"
 
-#OPTIONS="$OPTIONS -J-server -J-XX:+UseG1GC -J-XX:+UseAdaptiveSizePolicy -J-XX:MinHeapFreeRatio=20 -J-XX:MaxHeapFreeRatio=20"
-
 OPTIONS="$OPTIONS --initialize-at-build-time="
-
 OPTIONS="$OPTIONS -Dlog4j2.disable.jmx=true" ## Prevents log4j2 from creating an MBeanServer
-
-#OPTIONS="$OPTIONS -H:GenerateDebugInfo=1"
 
 #OPTIONS="$OPTIONS --initialize-at-run-time=com.sun.jmx.mbeanserver.JmxMBeanServer"
 
