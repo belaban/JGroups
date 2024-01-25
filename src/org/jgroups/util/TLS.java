172adba336968ec016d319ef770ecb91a58039c2
Bela Ban <belaban@mailbox.org>
2024-01-25 11:51:04 +0100

    Throwing exception if TLS.keystore_path doesn't exist

--- src/org/jgroups/util/TLS.java
+++ src/org/jgroups/util/TLS.java
@@ -8,6 +8,8 @@ import javax.net.ssl.SNIMatcher;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLParameters;
 import javax.net.ssl.SSLServerSocket;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -110,6 +112,11 @@ public class TLS implements Lifecycle {
             truststore_path=keystore_path;
             truststore_type=keystore_type;
             truststore_password=keystore_password;
+        }
+        if(keystore_path != null) {
+            File tmp=new File(keystore_path);
+            if(!tmp.exists())
+                throw new FileNotFoundException(keystore_path);
         }
     }
 
