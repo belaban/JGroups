6372dde8fd5f8e782c1c1f8879088eafb808d551
Bela Ban <belaban@mailbox.org>
2024-03-21 13:28:03 +0100

    ns

--- tests/other/org/jgroups/tests/CheckGaps.java
+++ tests/other/org/jgroups/tests/CheckGaps.java
@@ -7,6 +7,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.*;
 import java.util.concurrent.ConcurrentSkipListSet;
+import java.util.concurrent.atomic.AtomicLong;
 
 /** Reads a sequence of numbers from stdin and verifies that they are in order
  * @author Bela Ban
@@ -17,9 +18,10 @@ public class CheckGaps {
     public static void main(String[] args) throws IOException {
         List<Long> list=new LinkedList<>();
         Set<Long>  set=new ConcurrentSkipListSet<>();
+        final AtomicLong high=new AtomicLong();
         if(args.length > 1 && args[0].equals("-f")) {
             Files.lines(Path.of(args[1])).forEach(line -> {
-                line=line.replace(",", "");
+                line=line.replace(",", "").replace("[", "").replace("]", "");
                 String[] numbers=line.split("\\s+");
                 for(String s: numbers) {
                     s=s.trim();
@@ -27,6 +29,7 @@ public class CheckGaps {
                         long number=Long.parseLong(s);
                         list.add(number);
                         set.add(number);
+                        high.set(Math.max(high.get(), number));
                     }
                 }
             });
@@ -36,18 +39,18 @@ public class CheckGaps {
                 long num=Long.parseLong(args[i]);
                 list.add(num);
                 set.add(num);
+                high.set(Math.max(high.get(), num));
             }
         }
 
         int size=list.size(), set_size=set.size();
-        long low=list.get(0), high=list.get(size-1), duplicates=size - set_size;
-        // list.removeAll(set);
+        long low=set.iterator().next(), duplicates=size - set_size;
         for(long l: set)
             list.remove(l);
 
 
         System.out.printf("read %d numbers (%d duplicates), low=%d, high=%d\n",
-                          size, duplicates, low, high);
+                          size, duplicates, low, high.get());
         if(!list.isEmpty()) {
             SortedSet<Long> tmp=new ConcurrentSkipListSet<>(list);
             long first=tmp.iterator().next();
@@ -57,7 +60,7 @@ public class CheckGaps {
         }
 
         Set<Long> correct_set=new HashSet<>();
-        for(long i=low; i < high; i++)
+        for(long i=low; i < high.get(); i++)
             correct_set.add(i);
 
         set.forEach(correct_set::remove);
