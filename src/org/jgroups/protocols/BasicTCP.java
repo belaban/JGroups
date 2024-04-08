244cfd1f86b1456bc6a400f4aaa6948037519521
Bela Ban <belaban@mailbox.org>
2024-04-08 19:37:51 +0200

    Set `tcp_nodelay` to true by default (https://issues.redhat.com/browse/JGRP-2781)

--- src/org/jgroups/protocols/BasicTCP.java
+++ src/org/jgroups/protocols/BasicTCP.java
@@ -46,7 +46,7 @@ public abstract class BasicTCP extends TP implements Receiver {
     protected int         max_length;
     
     @Property(description="Should TCP no delay flag be turned on")
-    protected boolean     tcp_nodelay;
+    protected boolean     tcp_nodelay=true; // https://issues.redhat.com/browse/JGRP-2781
 
     @Property(description="SO_LINGER in seconds. Default of -1 disables it")
     protected int         linger=-1; // SO_LINGER (number of seconds, -1 disables it)
