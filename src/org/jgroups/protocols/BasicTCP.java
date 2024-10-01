1bf3b6350bc7c0a336739dd4e843c283bca31020
Bela Ban <belaban@mailbox.org>
2024-10-01 12:54:40 +0200

    Changed tcp_nodelay back to false (https://issues.redhat.com/browse/JGRP-2781)

--- src/org/jgroups/protocols/BasicTCP.java
+++ src/org/jgroups/protocols/BasicTCP.java
@@ -45,8 +45,8 @@ public abstract class BasicTCP extends TP implements Receiver {
       "thrown. 0 disables this", type=AttributeType.BYTES)
     protected int         max_length;
     
-    @Property(description="Should TCP no delay flag be turned on")
-    protected boolean     tcp_nodelay=true; // https://issues.redhat.com/browse/JGRP-2781
+    @Property(description="Should TCP no delay flag be turned on. True: nagling is OFF, false: nagling is ON")
+    protected boolean     tcp_nodelay=false; // https://issues.redhat.com/browse/JGRP-2781
 
     @Property(description="SO_LINGER in seconds. Default of -1 disables it")
     protected int         linger=-1; // SO_LINGER (number of seconds, -1 disables it)
