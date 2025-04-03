0146271e9811724814ebe0d5ff570c9878fc02db
Bela Ban <belaban@mailbox.org>
2025-04-03 11:25:51 +0200

    ns

--- tests/junit-functional/org/jgroups/tests/JDBC_PING2_Test.java
+++ tests/junit-functional/org/jgroups/tests/JDBC_PING2_Test.java
@@ -2,6 +2,7 @@ package org.jgroups.tests;
 
 import org.jgroups.Global;
 import org.jgroups.JChannel;
+import org.jgroups.protocols.pbcast.GMS;
 import org.jgroups.util.Util;
 import org.testng.annotations.Test;
 
@@ -24,7 +25,7 @@ public class JDBC_PING2_Test {
                 long start=System.nanoTime();
                 try(var b=createChannel("jdbc-pg.xml", "B")) {
                     b.connect(CLUSTER);
-                    Util.waitUntilAllChannelsHaveSameView(10000, 500, a,b);
+                    Util.waitUntilAllChannelsHaveSameView(10000, 10, a,b);
                     long time=System.nanoTime()-start;
                     System.out.printf("-- join #%d took %s\n", i, Util.printTime(time, TimeUnit.NANOSECONDS));
                 }
@@ -32,7 +33,13 @@ public class JDBC_PING2_Test {
         }
     }
 
+    protected static JChannel modify(JChannel ch) {
+        GMS gms=ch.stack().findProtocol(GMS.class);
+        gms.setJoinTimeout(3000).setMaxJoinAttempts(5);
+        return ch;
+    }
+
     protected static JChannel createChannel(String cfg, String name) throws Exception {
-        return new JChannel(cfg).name(name);
+        return modify(new JChannel(cfg).name(name));
     }
 }
