77fe925347d501310dc28425cacd8fe525503e04
Bela Ban <belaban@mailbox.org>
2025-08-01 08:51:56 +0200

    Runner now doesn't run into the ABA problem anymore (https://issues.redhat.com/browse/JGRP-2919)

--- src/org/jgroups/util/Runner.java
+++ src/org/jgroups/util/Runner.java
@@ -1,5 +1,7 @@
 package org.jgroups.util;
 
+import org.jgroups.logging.Log;
+
 import java.io.Closeable;
 import java.io.IOException;
 
@@ -13,11 +15,13 @@ public class Runner implements Runnable, Closeable {
     protected       String        thread_name;
     protected final Runnable      function;
     protected final Runnable      stop_function;
-    protected volatile boolean    running;
+    protected volatile State      state=State.stopped;
     protected Thread              thread;
     protected boolean             daemon;
-    protected long                join_timeout=100;
+    protected long                join_timeout=100; // ms
+    protected Log                 log;
 
+    public enum State {stopped, stopping, running}
 
     public Runner(String name, Runnable function, Runnable stop_function) {
         this(new DefaultThreadFactory(name, true, true), name, function, stop_function);
@@ -31,7 +35,9 @@ public class Runner implements Runnable, Closeable {
     }
 
     public Thread  getThread()            {return thread;}
-    public boolean isRunning()            {return running;}
+    public boolean isRunning()            {return state == State.running;}
+    public boolean running()              {return state == State.running;}
+    public State   state()                {return state;}
     public boolean daemon()               {return daemon;}
     public Runner  daemon(boolean d)      {daemon=d; return this;}
     public String  threadName()           {return thread_name;}
@@ -39,34 +45,33 @@ public class Runner implements Runnable, Closeable {
     public long    getJoinTimeout()       {return join_timeout;}
     public Runner  setJoinTimeout(long t) {join_timeout=t; return this;}
     public Runner  joinTimeout(long t)    {return setJoinTimeout(t);}
+    public Log     log()                  {return log;}
+    public Runner  log(Log l)             {log=l; return this;}
 
 
     public synchronized Runner start() {
-        if(running)
+        boolean rc=state(State.running);
+        if(!rc)
             return this;
-        if(thread == null || !thread.isAlive()) {
-            String name=thread_name != null? thread_name : "runner";
-            thread=factory != null? factory.newThread(this, name) : new Thread(this, name);
-            if(thread.getClass() == Thread.class)
-                thread.setDaemon(daemon);
-            running=true;
-            thread.start();
-        }
+        String name=thread_name != null? thread_name : "runner";
+        thread=factory != null? factory.newThread(this, name) : new Thread(this, name);
+        if(thread.getClass() == Thread.class)
+            thread.setDaemon(daemon);
+        thread.start();
         return this;
     }
 
     public synchronized Runner stop() {
-        running=false;
+        boolean rc=state(State.stopping);
+        if(!rc)
+            return this;
         Thread tmp=thread;
-        thread=null;
-        if(tmp != null && tmp.isAlive()) {
+        if(tmp != null) {
             tmp.interrupt();
-            if(join_timeout > 0 && tmp.isAlive()) {
+            if(join_timeout > 0) {
                 try {tmp.join(join_timeout);} catch(InterruptedException e) {}
             }
         }
-        if(stop_function != null)
-            stop_function.run();
         return this;
     }
 
@@ -74,16 +79,80 @@ public class Runner implements Runnable, Closeable {
         stop();
     }
 
-
     public void run() {
-        while(running) {
-            try {
-                function.run();
+        for(;;) {
+            while(state == State.running) {
+                try {
+                    function.run();
+                }
+                catch(Throwable t) {
+                }
             }
-            catch(Throwable t) {
+            synchronized(this) {
+                switch(state) {
+                    case stopped:
+                    case stopping:
+                        if(state == State.stopping) {
+                            runStopFuntion();
+                            state(State.stopped);
+                        }
+                        return;
+                }
             }
         }
+
     }
 
+    @Override
+    public String toString() {
+        return String.format("%s[%s]", thread != null? thread.getName() : "", state);
+    }
+
+    // changes the state, needs to be synchronized
+    protected boolean state(State new_state) {
+        switch(this.state) {
+            case stopped:
+                switch(new_state) {
+                    case stopped:  return false;
+                    case stopping: return false; // not a valid transition
+                    case running:
+                        state=new_state; // called by start() - starts the worker thread
+                        return true;
+                }
+            case stopping:
+                switch(new_state) {
+                    case stopped:
+                        state=new_state; // called by run(), when the worker thread terminates
+                        return false;
+                    case stopping:
+                        return false; // spurious stop(); is ignored
+                    case running:
+                        state=new_state; // stop() - start() sequence; the run() loop will continue
+                        return false; // don't start a new worker thread!
+                }
+            case running:
+                switch(new_state) {
+                    case stopped:
+                        return false; // invalid transition; we can't go directly from running -> stopped
+                    case stopping:
+                        state=new_state; // called by stop()
+                        return true;
+                    case running:
+                        return false;
+                }
+        }
+        throw new IllegalStateException(String.format("illegal transition from %s -> %s", state, new_state));
+    }
+
+    protected void runStopFuntion() {
+        try {
+            if(stop_function != null)
+                stop_function.run();
+        }
+        catch(Throwable t) {
+            if(log != null)
+                log.error("%s: failed running stop_function: %s", thread_name, t.getMessage());
+        }
+    }
 
 }
--- tests/junit-functional/org/jgroups/tests/RunnerTest.java
+++ tests/junit-functional/org/jgroups/tests/RunnerTest.java
@@ -2,13 +2,19 @@ package org.jgroups.tests;
 
 import org.jgroups.Global;
 import org.jgroups.util.DefaultThreadFactory;
+import org.jgroups.util.FastArray;
 import org.jgroups.util.Runner;
 import org.jgroups.util.Util;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.jgroups.util.Runner.State.*;
 
 /**
  * @author Bela Ban
@@ -16,39 +22,119 @@ import java.util.concurrent.atomic.AtomicInteger;
  */
 @Test(groups=Global.FUNCTIONAL,singleThreaded=true)
 public class RunnerTest {
-    protected Runner runner;
+    protected Runner              runner;
     protected final AtomicInteger count=new AtomicInteger();
+    protected final List<Thread>  threads=new FastArray<>();
 
-    @BeforeMethod protected void init() {count.set(0);}
+    @BeforeMethod protected void init() {count.set(0); threads.clear();}
 
     @AfterMethod protected void destroy() {
-        if(runner != null)
-            runner.stop();
-        count.set(0);
+        Util.close(runner); // calls runner.stop() if non-null
     }
 
     protected void increment() {
-        count.incrementAndGet();
-        Util.sleep(10);
+        int old_val=count.incrementAndGet();
+        System.out.printf("[%s] incremented count from %d\n", Thread.currentThread(), old_val);
+        Util.sleep(60_000);
     }
 
-    public void testStartTwice() {
+    protected void delay(final List<String> msgs) {
+        Thread curr=Thread.currentThread();
+        synchronized(threads) {
+            boolean contains=threads.stream().anyMatch(t -> t == curr);
+            if(!contains)
+                threads.add(curr);
+        }
+        sleep(60_000, msgs);
+        sleep(2000, msgs);
+    }
+
+    protected static void sleep(long timeout, List<String> msgs) {
+        long start=System.nanoTime();
+        try {
+            Thread.sleep(timeout);
+            long time=System.nanoTime()-start;
+            msgs.add(String.format("slept(%s), actual(%s)", Util.printTime(timeout, TimeUnit.MILLISECONDS), Util.printTime(time)));
+        }
+        catch(InterruptedException e) {
+            long time=System.nanoTime()-start;
+            msgs.add(String.format("sleep(%s), actual(%s)",
+                                   Util.printTime(timeout, TimeUnit.MILLISECONDS),
+                                   Util.printTime(time)));
+        }
+    }
+
+    public void testStartTwice() throws TimeoutException {
         runner=createRunner(this::increment, null);
         runner.start();
         runner.start(); // should not start a second runner
-        for(int i=0; i < 10; i++) {
-            if(count.get() >= 5)
-                break;
-            Util.sleep(100);
-        }
+        Util.waitUntil(3000, 100, () -> count.get() == 1);
         assert runner.isRunning();
         runner.stop();
-        assert !runner.isRunning();
+        Util.waitUntil(5000, 100, () -> !runner.isRunning());
     }
 
+    //@Test(invocationCount=5)
+    public void testTransitions() throws TimeoutException {
+        final List<String> msgs=new FastArray<>(20);
+        runner=createRunner(() -> delay(msgs), null);
+        assertState(stopped);
+        runner.stop();
+        assertState(stopped);
+
+        runner.start();
+        assertState(running);
+        runner.start();
+        assertState(running);
+        Util.waitUntilTrue(5000, 100, () -> !threads.isEmpty());
+
+        runner.stop();
+        assertState(stopping);
+        runner.start();
+        assertState(running);
+        Thread worker=runner.getThread();
+        assert worker != null && worker.isAlive();
+
+        msgs.clear();
+        runner.stop();
+        Util.waitUntilTrue(5000, 100, () -> runner.state() == stopped);
+        assertState(stopped);
+        System.out.printf("-- msgs: %s\n", msgs);
+    }
+
+    public void testStop() throws TimeoutException {
+        final List<String> msgs=new FastArray<>(20);
+        runner=createRunner(() -> delay(msgs), null)
+          .start();
+        assert runner.state() == running;
+        Util.waitUntilTrue(5000, 100, () -> !threads.isEmpty());
+        runner.stop();
+        Util.waitUntil(5000, 100, () -> runner.state() == stopped);
+    }
+
+    /**
+     * Tests the ABA problem (https://issues.redhat.com/browse/JGRP-2919)
+     */
+    public void testStartStopStartProblem() throws TimeoutException {
+        final List<String> msgs=new FastArray<>(20);
+        runner=createRunner(() -> delay(msgs), null)
+          .joinTimeout(0); // doesn't wait until the thread terminates
+        runner.start();
+        Util.waitUntil(3000, 100, () -> threads.size() == 1);
+        assert runner.isRunning();
+        runner.stop();
+        runner.start();
+        assert runner.isRunning();
+        Util.waitUntilTrue(3000, 100, () -> threads.size() > 1);
+        assert threads.size() == 1;
+    }
 
     protected static Runner createRunner(Runnable func, Runnable stop_func) {
         return new Runner(new DefaultThreadFactory("RunnerTest", false, true),
                           "runner", func, stop_func);
+    }
+
+    protected void assertState(Runner.State expected) {
+        assert expected == runner.state() : String.format("expected %s but got %s", expected, runner.state());
     }
 }
