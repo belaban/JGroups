57e84f7d254d8aa2260f8af04bed506efeccaa1e
Bela Ban <belaban@mailbox.org>
2025-09-10 17:11:56 +0200

    Fixed release.sh

--- bin/release.sh
+++ bin/release.sh
@@ -7,28 +7,32 @@ BASE_DIR="$CURRENT_DIR/../"
 POM=$BASE_DIR/pom.xml
 POM2=$BASE_DIR/pom2.xml
 CURRENT_VERSION=`grep version $POM | head -1 | sed "s/version//g" |  sed "s/[[<>/]*//g" | tr -d " "`
+RELEASE_VERSION=`echo $CURRENT_VERSION | sed "s/-SNAPSHOT//g"`
 NEW_VERSION=`$BASE_DIR/bin/jgroups.sh org.jgroups.Version -incr $CURRENT_VERSION`
 TARGET="$BASE_DIR/target/"
 REPO=`grep nexus.server.url pom.xml | head -1 | sed "s/<nexus.server.url>//g" | sed "s/<\/nexus.server.url>//g" | tr -d " "`
-NEW_TAG="jgroups-$NEW_VERSION"
+TAG=`echo "jgroups-$CURRENT_VERSION" | sed "s/-SNAPSHOT//g"`
 
 echo ""
-echo "current version $CURRENT_VERSION"
-read -p "new version:    $NEW_VERSION: (enter to accept) " answer
+echo "release version: $RELEASE_VERSION"
+
+read -p "tag:             $TAG: (enter to accept) " answer
+if [ ! -z $answer ]; then
+   TAG=$answer
+   echo "set new tag to $answer"
+fi
+
+read -p "new version:     $NEW_VERSION: (enter to accept) " answer
 if [ ! -z $answer ]; then
    NEW_VERSION=$answer
    echo "set new version to $answer"
 fi
 
-read -p "new tag:        $NEW_TAG: (enter to accept) " answer
-if [ ! -z $answer ]; then
-   NEW_TAG=$answer
-   echo "set new tag to $answer"
-fi
+
 
 echo ""
-echo "changing pom.xml to version $NEW_VERSION:"
-cat $POM | sed "s/$CURRENT_VERSION/$NEW_VERSION/g" > $POM2
+echo "changing version in pom.xml from $CURRENT_VERSION to $RELEASE_VERSION:"
+cat $POM | sed "s/$CURRENT_VERSION/$RELEASE_VERSION/g" > $POM2
 mv $POM2 $POM
 
 echo ""
@@ -50,14 +54,20 @@ case $answer in
         [Nn]* ) exit;;
         * ) echo "";;
 esac
+
+## uncomment
 echo "execute: mvn -DskipTests deploy -Prelease"
 echo "Please commit and push your changes"
-echo "git commit -m 'Changed version to $NEW_VERSION' ; git push"
+
+## uncomment
+echo "git commit -m 'Changed version from $CURRENT_VERSION to $RELEASE_VERSION' . ; git push"
 
 echo ""
 echo "================================================================"
-echo "Tagging the repo with $NEW_TAG"
-echo "git tag $NEW_TAG"
+echo "Tagging the repo with $TAG"
+## uncomment
+echo "git tag $TAG"
+## uncomment
 echo "git push --tags"
 
 echo ""
@@ -65,8 +75,9 @@ echo ""
 
 NEXT_VERSION="$NEW_VERSION-SNAPSHOT"
 echo "changing pom.xml to version $NEXT_VERSION:"
-cat $POM | sed "s/$NEW_VERSION/$NEXT_VERSION/g" > $POM2
+cat $POM | sed "s/$RELEASE_VERSION/$NEXT_VERSION/g" > $POM2
 mv $POM2 $POM
 
-echo "Please commit and push the change: git commit -m 'Changed version to $NEXT_VERSION' ; git push"
+## uncomment
+echo "git commit -m 'Changed version from $RELEASE_VERSION to $NEXT_VERSION' . ; git push"
 echo ""
