efed8c682fc2210145c71490c0c9393dd9b109df
Bela Ban <belaban@mailbox.org>
2026-03-23 12:45:32 +0100

    ns

--- src/org/jgroups/util/GetNetworkInterfaces.java
+++ src/org/jgroups/util/GetNetworkInterfaces.java
@@ -16,8 +16,7 @@ public class GetNetworkInterfaces {
         Enumeration<NetworkInterface> en=NetworkInterface.getNetworkInterfaces();
         while(en.hasMoreElements()) {
             NetworkInterface i=en.nextElement();
-            System.out.println(i.getName() + ':');
-            System.out.println("  \t" + i.getDisplayName());
+            System.out.printf("%s (%s)\n", i.getName(), Util.isUp(i)? "up" : "down");
             for(Enumeration<InetAddress> en2=i.getInetAddresses(); en2.hasMoreElements();) {
                 InetAddress addr=en2.nextElement();
                 System.out.println("  \t" + addr);
