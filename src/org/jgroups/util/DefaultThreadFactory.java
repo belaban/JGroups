e9deef6cce4b9d0916508dc39dcac7326749a1b2
Francesco Nigro <nigro.fra@gmail.com>
2026-06-26 13:09:34 +0200

    Refactor thread naming logic for improved efficiency with concat indy

--- src/org/jgroups/util/DefaultThreadFactory.java
+++ src/org/jgroups/util/DefaultThreadFactory.java
@@ -109,21 +109,19 @@ public class DefaultThreadFactory implements ThreadFactory {
             addr=this.address;
 
         String name=base_name != null? base_name : "thread";
-        int estimated_size=name.length() + 10; // additional space for numbering
-        if(cluster_name != null)
-            estimated_size+=cluster_name.length();
-        if(addr != null)
-            estimated_size+=addr.length();
 
-        StringBuilder sb=new StringBuilder(estimated_size).append(name);
         if(use_numbering) {
             int id=counter.incrementAndGet();
-            sb.append("-").append(id);
+            if(includeClusterName)
+                return includeLocalAddress? name + '-' + id + ',' + cluster_name + ',' + addr
+                  : name + '-' + id + ',' + cluster_name;
+            return includeLocalAddress? name + '-' + id + ',' + addr
+              : name + '-' + id;
         }
+
         if(includeClusterName)
-            sb.append(',').append(cluster_name);
-        if(includeLocalAddress)
-            sb.append(',').append(addr);
-        return sb.toString();
+            return includeLocalAddress? name + ',' + cluster_name + ',' + addr
+              : name + ',' + cluster_name;
+        return includeLocalAddress? name + ',' + addr : name;
     }
 }
