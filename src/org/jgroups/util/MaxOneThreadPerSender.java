d5868f72bd3abb6ec889b6c8ec5b6d1673f6f8ef
Bela Ban <belaban@mailbox.org>
2026-08-13 16:35:59 +0200

    - Added ThreadPoolTest and MaxOneThreadPerSenderTest to test https://redhat.atlassian.net/browse/JGRP-3032 - MaxOneThreadPerSender: gurading log.error(0 against exceptions - ShutdownRejectedExecutionHandler: throwing exception when pool is full rather than swallowing it (discard) - ThreadPool: checking for shut-down pool (returning false), guarding catch(RejectedExcecutionException) block against exceptions and use of  ShutdownRejectedExecutionHandler

--- src/org/jgroups/util/MaxOneThreadPerSender.java
+++ src/org/jgroups/util/MaxOneThreadPerSender.java
@@ -273,7 +273,14 @@ public class MaxOneThreadPerSender extends SubmitToThreadPool {
                         tp.passBatchUp(mb, !loopback, !loopback);
                 }
                 catch(Throwable t) {
-                    log.error("failed processing batch", t);
+                    try {
+                        log.error("failed processing batch", t);
+                    }
+                    catch(Throwable tt) {
+                        // e.g. an OOME raised by log.error() above: don't terminate with running still set to true, or else
+                        // no further messages from that sender would ever be delivered: https://redhat.atlassian.net/browse/JGRP-3032
+                        ;
+                    }
                 }
             }
         }
