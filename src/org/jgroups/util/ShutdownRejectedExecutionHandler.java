ea1787041b0f7ff1912d2fc5ef48eabedb4680e6
Bela Ban <belaban@mailbox.org>
2026-08-13 16:40:24 +0200

    - Added ThreadPoolTest and MaxOneThreadPerSenderTest to test https://redhat.atlassian.net/browse/JGRP-3032 - MaxOneThreadPerSender: gurading log.error(0 against exceptions - ShutdownRejectedExecutionHandler: throwing exception when pool is full rather than swallowing it (discard) - ThreadPool: checking for shut-down pool (returning false), guarding catch(RejectedExcecutionException) block against exceptions and use of  ShutdownRejectedExecutionHandler
    # Conflicts:
    #	src/org/jgroups/util/ThreadPool.java

--- src/org/jgroups/util/ShutdownRejectedExecutionHandler.java
+++ src/org/jgroups/util/ShutdownRejectedExecutionHandler.java
@@ -1,6 +1,7 @@
 package org.jgroups.util;
 
 import java.util.Objects;
+import java.util.concurrent.RejectedExecutionException;
 import java.util.concurrent.RejectedExecutionHandler;
 import java.util.concurrent.ThreadPoolExecutor;
 
@@ -11,18 +12,23 @@ import java.util.concurrent.ThreadPoolExecutor;
  * RuntimeException when a task is submitted to ThreadPoolExecutor that has been
  * shutdown. ShutdownRejectedExecutionHandler instead logs only a warning
  * message.
+ * <p>
+ * A {@link ThreadPoolExecutor.DiscardPolicy} drops a rejected task <em>silently</em>, ie. without raising a
+ * RejectedExecutionException. As callers such as {@link ThreadPool#execute(Runnable)} would otherwise treat a
+ * discarded task as accepted, a RejectedExecutionException is raised on the policy's behalf.
  * 
  * @author Vladimir Blagojevic
  * @see ThreadPoolExecutor
  * @see RejectedExecutionHandler
  */
 public class ShutdownRejectedExecutionHandler implements RejectedExecutionHandler {
-
     RejectedExecutionHandler handler;
+    protected final boolean  discards; // true if the decorated handler drops tasks without raising an exception
 
     public ShutdownRejectedExecutionHandler(RejectedExecutionHandler handler) {
         super();
         this.handler=Objects.requireNonNull(handler);
+        this.discards=handler instanceof ThreadPoolExecutor.DiscardPolicy;
     }
 
     public RejectedExecutionHandler handler() {return handler;}
@@ -30,6 +36,8 @@ public class ShutdownRejectedExecutionHandler implements RejectedExecutionHandle
     public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {
         if(!executor.isShutdown()) {
             handler.rejectedExecution(r, executor);
+            if(discards) // the task was dropped silently: tell the caller that it will never be run
+                throw new RejectedExecutionException(String.format("task %s was discarded", r));
         }
     }
 }
---
+++ tests/junit-functional/org/jgroups/util/MaxOneThreadPerSenderTest.java
@@ -0,0 +1,63 @@
+package org.jgroups.util;
+
+import org.jgroups.EmptyMessage;
+import org.jgroups.Global;
+import org.jgroups.Message;
+import org.jgroups.logging.Log;
+import org.jgroups.protocols.UDP;
+import org.testng.annotations.Test;
+
+import java.lang.reflect.Proxy;
+
+/**
+ * Tests {@link MaxOneThreadPerSender}
+ * @author Radoslav Husar
+ */
+@Test(groups=Global.FUNCTIONAL)
+public class MaxOneThreadPerSenderTest {
+
+    /**
+     * A BatchHandlerLoop which terminates abnormally (here: because logging the failure fails, too) must not leave
+     * the entry with running=true, or else messages from that sender are queued forever and never delivered
+     */
+    public void testRunningIsResetOnAbnormalTermination() throws Exception {
+        UDP tp=new UDP() {
+            @Override public void passBatchUp(MessageBatch b, boolean cluster_name_matching, boolean discard_own_mcast) {
+                throw new IllegalStateException("failed passing batch up");
+            }
+        };
+        MaxOneThreadPerSender policy=new MaxOneThreadPerSender();
+        tp.getThreadPool().init();
+        try {
+            policy.init(tp);
+            policy.log=throwingLog();
+            Message msg=new EmptyMessage(null).setSrc(Util.createRandomAddress("A"));
+            assert policy.process(msg, false);
+            MaxOneThreadPerSender.Entry entry=policy.mcasts.map.values().iterator().next();
+            Util.waitUntil(5000, 100, () -> !running(entry), () -> "entry is still marked as running");
+        }
+        finally {
+            tp.getThreadPool().destroy();
+        }
+    }
+
+    protected static boolean running(MaxOneThreadPerSender.Entry entry) {
+        entry.lock.lock();
+        try {
+            return entry.adders.get() > 0;
+        }
+        finally {
+            entry.lock.unlock();
+        }
+    }
+
+    /** Returns a Log which fails when an error is logged, e.g. as an OOME would */
+    protected static Log throwingLog() {
+        return (Log)Proxy.newProxyInstance(MaxOneThreadPerSenderTest.class.getClassLoader(), new Class<?>[]{Log.class},
+                                           (proxy, method, args) -> {
+                                               if(method.getName().equals("error"))
+                                                   throw new IllegalStateException("logging failed");
+                                               return method.getReturnType() == boolean.class? Boolean.FALSE : null;
+                                           });
+    }
+}
