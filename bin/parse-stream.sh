54615b5280633bbf834e948cc54e1d8e46fef248
Bela Ban <belaban@mailbox.org>
2026-08-18 14:45:56 +0200

    Added parse-streams.sh

---
+++ bin/parse-stream.sh
@@ -0,0 +1,19 @@
+
+#!/bin/bash
+
+# Extracts a given stream from a capture and prints its JGroups messages with ParseMessages'
+# To find out the streams: tshark -r <PCAPNG file> -Tfields -etcp.stream|sort|uniq
+
+if [ $# -lt 2 ];
+    then echo "parse-streams.sh <PCAP file> <stream>";
+         exit 1
+fi
+
+
+file=$1
+stream=$2
+
+
+tshark -r $file -qz "follow,tcp,raw,$stream" 2>/dev/null \
+    | grep -v "^===\|^Follow\|^Filter\|^Node\|^$" | sed 's/^\t//' \
+    | java org.jgroups.tests.ParseMessages -tcp -parse-discovery-responses true
